a5d1c21e48074933f4e75d351bf748d297231888
ifof <ifof@users.sourceforge.net>
2003-02-19 15:31:17 +0000

    Type mapping MUST be verified
    SVN: trunk@16

---
+++ src/NHibernate/Dialect/OracleDialect.cs
@@ -0,0 +1,22 @@
+using System;
+
+using NHibernate.Sql;
+
+namespace NHibernate.Dialect
+{
+	/// <summary>
+	/// An SQL dialect for Oracle, compatible with Oracle 8.
+	/// </summary>
+	public class OracleDialect : Oracle9Dialect
+	{
+		public OracleDialect() : base() {
+		}
+		
+		public OuterJoinFragment CreateOuterJoinFragment() {
+			return new OracleOuterJoinFragment();
+		}
+		public CaseFragment CreateCaseFragment() {
+			return new DecodeCaseFragment();
+		}
+	}
+}
