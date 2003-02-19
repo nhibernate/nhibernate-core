42e539757fc8176b0aae4c3680852a57d47b6a27
ifof <ifof@users.sourceforge.net>
2003-02-19 00:31:28 +0000

    Types mapping in constructor MUST be verified. We need GetDefaultProperties method in Dialect class to complete constructor
    SVN: trunk@14

---
+++ src/NHibernate/Dialect/MySQLDialect.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Text;
+using System.Data;
+
+using NHibernate.Util;
+
+namespace NHibernate.Dialect
+{
+	/// <summary>
+	/// An SQL dialect for MySQL
+	/// </summary>
+	public class MySQLDialect : Dialect
+	{
+		public MySQLDialect()
+		{
+			base();
+
+			/*  Type mapping in Java was:
+			 
+				Types.BIT, "BIT"
+				Types.BIGINT, "BIGINT"
+				Types.SMALLINT, "SMALLINT"
+				Types.TINYINT, "TINYINT"
+				Types.INTEGER, "INTEGER"
+				Types.CHAR, "CHAR(1)"
+				Types.VARCHAR, "LONGTEXT"
+				Types.VARCHAR, 16777215, "MEDIUMTEXT"
+				Types.VARCHAR, 65535, "TEXT"
+				Types.VARCHAR, 255, "VARCHAR($l)"
+				Types.FLOAT, "FLOAT"
+				Types.DOUBLE, "DOUBLE PRECISION"
+				Types.DATE, "DATE"
+				Types.TIME, "TIME"
+				Types.TIMESTAMP, "DATETIME"
+				Types.VARBINARY, "LONGBLOB"
+				Types.VARBINARY, 16777215, "MEDIUMBLOB"
+				Types.VARBINARY, 65535, "BLOB"
+				Types.VARBINARY, 255, "VARCHAR($l) BINARY"
+				Types.NUMERIC, "NUMERIC(19, $l)"
+				Types.BLOB, "LONGBLOB"
+				Types.BLOB, 16777215, "MEDIUMBLOB"
+				Types.BLOB, 65535, "BLOB"
+				Types.CLOB, "LONGTEXT"
+				Types.CLOB, 16777215, "MEDIUMTEXT"
+				Types.CLOB, 65535, "TEXT"
+			*/
+
+
+			Register( SqlDbType.Bit, "BIT" );
+			Register( SqlDbType.BigInt,	"BIGINT" );
+			Register( SqlDbType.SmallInt, "SMALLINT" );
+			Register( SqlDbType.TinyInt, "TINYINT" );
+			Register( SqlDbType.Int, "INTEGER" );
+			Register( SqlDbType.Char, "CHAR(1)" );
+			Register( SqlDbType.VarChar, "LONGTEXT" );
+			Register( SqlDbType.VarChar, 1677215, "MEDIUMTEXT" );
+			Register( SqlDbType.VarChar, 65535, "TEXT" );
+			Register( SqlDbType.VarChar, 255, "VARCHAR($1)" );
+			Register( SqlDbType.Float, "FLOAT" );
+			Register( SqlDbType.Real, "DOUBLE PRECISION" );
+			/*
+			And these?
+			register( Types.DATE, "DATE" );
+			register( Types.TIME, "TIME" );
+			*/
+			Register( SqlDbType.Timestamp, "DATETIME" );
+			Register( SqlDbType.VarBinary, "LONGBLOB" );
+			Register( SqlDbType.VarBinary, 16777215, "MEDIUMBLOB" );
+			Register( SqlDbType.VarBinary, 65535, "BLOB" );
+			Register( SqlDbType.VarBinary, 255, "VARCHAR($l) BINARY" );
+			Register( SqlDbType.Decimal, "NUMERIC(19, $l)" );
+			Register( SqlDbType.Image, "LONGBLOB" );
+			Register( SqlDbType.Image, 16777215, "MEDIUMBLOB" );
+			Register( SqlDbType.Image, 65535, "BLOB" );
+			Register( SqlDbType.Text, "LONGTEXT" );
+			Register( SqlDbType.Text, 16777215, "MEDIUMTEXT" );
+			Register( SqlDbType.Text, 65535, "TEXT" );
+			
+			/* TODO:
+			getDefaultProperties().setProperty(Environment.OUTER_JOIN, "true");
+			getDefaultProperties().setProperty(Environment.STATEMENT_BATCH_SIZE, DEFAULT_BATCH_SIZE);
+			*/
+		}
+
+		public string GetAddColumnString() 
+		{
+			return "add column";
+		}
+		public bool DropConstraints() 
+		{
+			return false;
+		}
+		public bool QualifyIndexName() 
+		{
+			return false;
+		}
+	
+		public bool SupportsIdentityColumns() 
+		{
+			return true;
+		}
+		public string GetIdentitySelectString() 
+		{
+			return "SELECT LAST_INSERT_ID()";
+		}
+	
+		public string GetIdentityColumnString() 
+		{
+			return "NOT NULL AUTO_INCREMENT";
+		}
+	
+		public string GetAddForeignKeyConstraintString(string constraintName, string[] foreignKey, string referencedTable, string[] primaryKey) 
+		{
+			string cols = StringHelper.Join(StringHelper.CommaSpace, foreignKey);
+			return new StringBuilder(30)
+				.Append(" add index (")
+				.Append(cols)
+				.Append("), add constraint ")
+				.Append(constraintName)
+				.Append(" foreign key (")
+				.Append(cols)
+				.Append(") references ")
+				.Append(referencedTable)
+				.Append(" (")
+				.Append( StringHelper.Join(StringHelper.CommaSpace, primaryKey) )
+				.Append(')')
+				.ToString();
+		}
+	}
+}
---
+++ src/NHibernate/Dialect/Oracle9Dialect.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Text;
+using System.Data;
+
+using NHibernate.Util;
+
+namespace NHibernate.Dialect
+{
+	/// <summary>
+	/// An SQL dialect for Oracle 9 (uses ANSI-style syntax where possible).
+	/// </summary>
+	public class Oracle9Dialect : Dialect
+	{
+		public Oracle9Dialect() 
+		{
+			base();
+
+			/* Type mapping in Java was:
+			  
+			   Types.BIT, "NUMBER(1,0)"
+			   Types.BIGINT, "NUMBER(19,0)"
+			   Types.SMALLINT, "NUMBER(5,0)"
+			   Types.TINYINT, "NUMBER(3,0)"
+			   Types.INTEGER, "NUMBER(10,0)"
+			   Types.CHAR, "CHAR(1)"
+			   Types.VARCHAR, "VARCHAR2($l)"
+			   Types.FLOAT, "FLOAT"
+			   Types.DOUBLE, "DOUBLE PRECISION"
+			   Types.DATE, "DATE"
+			   Types.TIME, "DATE"
+			   Types.TIMESTAMP, "DATE"
+			   Types.VARBINARY, "RAW($l)"
+			   Types.NUMERIC, "NUMBER(19, $l)" 
+			   Types.BLOB, "BLOB"
+			   Types.CLOB, "CLOB"
+			*/
+			
+			Register( SqlDbType.Bit, "NUMBER(1,0)" );
+			Register( SqlDbType.BigInt, "NUMBER(19,0)" );
+			Register( SqlDbType.SmallInt, "NUMBER(5,0)" );
+			Register( SqlDbType.TinyInt, "NUMBER(3,0)" );
+			Register( SqlDbType.Int, "NUMBER(10,0)" );
+			Register( SqlDbType.Char, "CHAR(1)" );
+			Register( SqlDbType.VarChar, "VARCHAR2($l)" );
+			Register( SqlDbType.Float, "FLOAT" );
+			Register( SqlDbType.Real, "DOUBLE PRECISION" );
+			Register( SqlDbType.DateTime, "DATE" );
+			Register( SqlDbType.SmallDateTime, "DATE" );
+			Register( SqlDbType.Timestamp, "DATE" );
+			Register( SqlDbType.VarBinary, "RAW($l)" );
+			Register( SqlDbType.Decimal, "NUMBER(19, $l)" );
+			Register( SqlDbType.Image, "BLOB" );
+			Register( SqlDbType.Text, "CLOB" );
+
+			/* TODO:
+			getDefaultProperties().setProperty(Environment.USE_STREAMS_FOR_BINARY, "true");
+			getDefaultProperties().setProperty(Environment.STATEMENT_BATCH_SIZE, DEFAULT_BATCH_SIZE);
+			getDefaultProperties().setProperty(Environment.OUTER_JOIN, "true");
+			*/
+		}
+	
+		public string GetAddColumnString() 
+		{
+			return "add";
+		}
+	
+		public string GetSequenceNextValString(string sequenceName) 
+		{
+			return  "select " + sequenceName + ".nextval from dual";
+		}
+		public string GetCreateSequenceString(string sequenceName) 
+		{
+			return "create sequence " + sequenceName;
+		}
+		public string GetDropSequenceString(string sequenceName) 
+		{
+			return "drop sequence " + sequenceName;
+		}
+	
+		public string GetCascadeConstraintsString() 
+		{
+			return " cascade constraints";
+		}
+	
+		public bool SupportsForUpdateNowait() 
+		{
+			return true;
+		}
+	
+		public bool SupportsSequences() 
+		{
+			return true;
+		}
+	}
+}
