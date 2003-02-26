130edf17926cbc2b0eee73a5cf1c6101a9a57467
ifof <ifof@users.sourceforge.net>
2003-02-26 10:31:51 +0000

    *** empty log message ***
    SVN: trunk@47

---
+++ src/NHibernate/NHibernate.cs
@@ -0,0 +1,265 @@
+using System;
+
+using NHibernate.Type;
+using NHibernate.Proxy;
+using NHibernate.Collection;
+
+namespace NHibernate {
+
+	/// <summary>
+	/// Provides access to the full range of NHibernate built-in types.
+	/// IType instances may be used to bind values to query parameters.
+	/// Also a factory for new Blobs and Clobs.
+	/// </summary>
+	public class NHibernate	{
+
+		/// <summary>
+		/// NHibernate long type
+		/// </summary>
+		//public static readonly NullableType Long = new LongType();
+		
+		/// <summary>
+		/// NHibernate short type
+		/// </summary>
+		//public static readonly NullableType Short = new ShortType();
+		
+		/// <summary>
+		/// NHibernate integer type
+		/// </summary>
+		public static readonly NullableType Integer = new IntegerType();
+		
+		/// <summary>
+		/// NHibernate byte type
+		/// </summary>
+		public static readonly NullableType Byte = new ByteType();
+		
+		/// <summary>
+		/// NHibernate float type
+		/// </summary>
+		//public static readonly NullableType Float = new FloatType();
+		
+		/// <summary>
+		/// NHibernate double type
+		/// </summary>
+		//public static readonly NullableType Double = new DoubleType();
+		
+		/// <summary>
+		/// NHibernate character type
+		/// </summary>
+		public static readonly NullableType Character = new CharacterType();
+		
+		/// <summary>
+		/// NHibernate string type
+		/// </summary>
+		//public static readonly NullableType String = new StringType();
+		
+		/// <summary>
+		/// NHibernate time type
+		/// </summary>
+		//public static readonly NullableType Time = new TimeType();
+		
+		/// <summary>
+		/// NHibernate date type
+		/// </summary>
+		//public static readonly NullableType Date = new DateType();
+		
+		/// <summary>
+		/// NHibernate timestamp type
+		/// </summary>
+		//public static readonly NullableType Timestamp = new TimestampType();
+		
+		/// <summary>
+		/// NHibernate boolean type
+		/// </summary>
+		public static readonly NullableType Boolean = new BooleanType();
+		
+		/// <summary>
+		/// NHibernate true_false type
+		/// </summary>
+		public static readonly NullableType TrueFalse = new TrueFalseType();
+		
+		/// <summary>
+		/// NHibernate yes_no type
+		/// </summary>
+		public static readonly NullableType YesNo = new YesNoType();
+		
+		/// <summary>
+		/// NHibernate big_decimal type
+		/// </summary>
+		//public static readonly NullableType BigDecimal = new BigDecimalType();
+	
+		/// <summary>
+		/// NHibernate binary type
+		/// </summary>
+		public static readonly NullableType Binary = new BinaryType();
+		
+		/// <summary>
+		/// NHibernate blob type
+		/// </summary>
+		//public static readonly NullableType Blob = new BlobType();
+		
+		/// <summary>
+		/// NHibernate clob type
+		/// </summary>
+		//public static readonly NullableType Clob = new ClobType();
+		
+		/// <summary>
+		/// NHibernate calendar type
+		/// </summary>
+		//public static readonly NullableType Calendar = new CalendarType();
+		
+		/// <summary>
+		/// NHibernate calendar_date type
+		/// </summary>
+		//public static readonly NullableType CalendarDate = new CalendarDateType();
+		
+		/// <summary>
+		/// NHibernate locale type
+		/// </summary>
+		//public static readonly NullableType Locale = new LocaleType();
+		
+		/// <summary>
+		/// NHibernate currency type
+		/// </summary>
+		//public static readonly NullableType Currency = new CurrencyType();
+		
+		/// <summary>
+		/// NHibernate timezone type
+		/// </summary>
+		//public static readonly NullableType Timezone = new TimeZoneType();
+		
+		/// <summary>
+		/// NHibernate class type
+		/// </summary>
+		//public static readonly NullableType Class = new ClassType();
+		
+		/// <summary>
+		/// NHibernate object type
+		/// </summary>
+		//public static readonly IType Object = new ObjectType();
+		
+		/*
+		/// <summary>
+		/// NHibernate serializable type
+		/// </summary>
+		public static readonly NullableType Serializable = new SerializableType(Serializable.Name);
+		*/
+		
+		/// <summary>
+		/// Cannot be instantiated.
+		/// </summary>
+		private NHibernate() {																	  throw new NotSupportedException();											}
+		
+		/*
+		/// <summary>
+		/// A NHibernate persistent enum type
+		/// </summary>
+		/// <param name="enumClass"></param>
+		/// <returns></returns>
+		public static IType Enum(System.Type enumClass) {
+			return new PersistentEnumType(enumClass);
+		}
+		*/
+		/*
+		/// <summary>
+		/// A NHibernate serializable type
+		/// </summary>
+		/// <param name="serializableClass"></param>
+		/// <returns></returns>
+		public static IType Serializable(System.Type serializableClass) {
+			return new SerializableType(serializableClass);
+		}
+		*/
+
+		/// <summary>
+		/// A NHibernate persistent object (entity) type
+		/// </summary>
+		/// <param name="persistentClass">a mapped entity class</param>
+		/// <returns></returns>
+		public static IType Association(System.Type persistentClass) {
+			// not really a many-to-one association *necessarily*
+			return new ManyToOneType(persistentClass);
+		}
+		
+		/*
+		/// <summary>
+		/// A NHibernate custom type
+		/// </summary>
+		/// <param name="userTypeClass">a class that implements UserType</param>
+		/// <returns></returns>
+		public static IType Custom(System.Type userTypeClass) {
+			return new CustomType(userTypeClass);
+		}
+		*/
+		
+		/* Needs HybernateProxyHelper and PersistentCollection implementation
+		 * 
+		/// <summary>
+		/// Force initialization of a proxy or persistent collection.
+		/// </summary>
+		/// <param name="proxy">a persistable object, proxy, persistent collection or null</param>
+		/// <exception cref="HibernateException">if we can't initialize the proxy at this time, eg. the Session was closed</exception>
+		public static void Initialize(object proxy) {
+			if (proxy==null) {
+				return;
+			}
+			else if ( proxy is IHibernateProxy ) {
+				HibernateProxyHelper.GetLazyInitializer( (IHibernateProxy) proxy ).Initialize();
+			}
+			else if ( proxy is PersistentCollection ) {
+				( (PersistentCollection) proxy ).ForceLoad();
+			}
+		}
+		*/
+	
+		/*
+		/// <summary>
+		/// Create a new Blob. The returned object will be initially immutable.
+		/// </summary>
+		/// <param name="bytes">a byte array</param>
+		/// <returns></returns>
+		public static Blob CreateBlob(byte[] bytes) {
+			return new BlobImpl(bytes);
+		}
+	
+		/// <summary>
+		/// Create a new Blob. The returned object will be initially immutable.
+		/// </summary>
+		/// <param name="stream">a binary stream</param>
+		/// <param name="length">the number of bytes in the stream</param>
+		/// <returns></returns>
+		public static Blob CreateBlob(StreamReader stream, int length) {
+			return new BlobImpl(stream, length);
+		}
+		
+		/// <summary>
+		/// Create a new Blob. The returned object will be initially immutable.
+		/// </summary>
+		/// <param name="stream">a binary stream</param>
+		/// <returns></returns>
+		public static Blob CreateBlob(StreamReader stream) {
+			return new BlobImpl( stream, stream.available() );
+		}
+		
+		/// <summary>
+		/// Create a new Clob. The returned object will be
+		/// initially immutable.
+		/// </summary>
+		/// <param name="str">a String</param>
+		/// <returns></returns>
+		public static Clob CreateClob(String str) {
+			return new ClobImpl(str);
+		}
+
+		/// <summary>
+		/// Create a new Clob. The returned object will be initially immutable.
+		/// </summary>
+		/// <param name="reader">a character stream</param>
+		/// <param name="length">the number of characters in the stream</param>
+		/// <returns></returns>
+		public static Clob CreateClob(StreamReader reader, int length) {
+			return new ClobImpl(reader, length);
+		}
+		*/
+	}
+}
--- src/NHibernate/Type/TypeFactory.cs
+++ src/NHibernate/Type/TypeFactory.cs
@@ -1,17 +1,185 @@
 using System;
+using System.Collections;
 
-namespace NHibernate.Type
-{
+using NHibernate.Type;
+using NHibernate.Collection;
+using NHibernate.Engine;
+
+namespace NHibernate.Type {
+
 	/// <summary>
-	/// Summary description for TypeFactory.
+	/// Used internally to obtain instances of IType.
+	/// Applications should use static methods and constants on NHibernate.NHibernate.
 	/// </summary>
-	public class TypeFactory
-	{
-		public TypeFactory()
-		{
-			//
-			// TODO: Add constructor logic here
-			//
+	public class TypeFactory {
+
+		private static readonly Hashtable basicTypes;
+
+		private TypeFactory() { throw new NotSupportedException(); }
+		
+		/// <summary>
+		/// A one-to-one association type for the given class and cascade style.
+		/// </summary>
+		/// <param name="persistentClass"></param>
+		/// <param name="foreignKeyType"></param>
+		/// <returns></returns>
+		public static IType OneToOne(System.Type persistentClass, ForeignKeyType foreignKeyType) {
+			return new OneToOneType(persistentClass, foreignKeyType);
 		}
+
+		/// <summary>
+		/// A many-to-one association type for the given class and cascade style.
+		/// </summary>
+		/// <param name="persistentClass"></param>
+		/// <returns></returns>
+		public static IType ManyToOne(System.Type persistentClass) {
+			return new ManyToOneType(persistentClass);
+		}
+		
+		/// <summary>
+		/// Given the name of a Hibernate basic type, return an instance
+		/// of NHibernate.Type.IType
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		public static IType Basic(string name) {
+			return (IType) basicTypes[name];
+		}
+
+		public static IType HueristicType(string typeName) {
+			IType type = TypeFactory.Basic(typeName);
+			if (type==null) {
+				System.Type typeClass;
+				try {
+					typeClass = null; //HACK: need ReflectHelper implementation
+					//TODO: typeClass = ReflectHelper.classForName(typeName);
+				}
+				catch (TypeLoadException tle) {
+					typeClass = null;
+				}
+				if (typeClass!=null) {
+					if ( typeof(IType).IsAssignableFrom(typeClass) ) {
+						try {
+							type = (IType) Activator.CreateInstance(typeClass);
+						}
+						catch (Exception e) {
+							throw new MappingException("Could not instantiate IType " + typeClass.Name + ": " + e);
+						}
+					}
+					/*
+					else if ( typeof(UserType).IsAssignableFrom(typeClass) ) {
+						type = new CustomType(typeClass);
+					}
+					*/
+					else if ( typeof(ILifecycle).IsAssignableFrom(typeClass) ) {
+						type = NHibernate.Association(typeClass);
+					}
+
+					/* Do we use standar enum instead of this?
+					 * 
+					else if ( typeof(PersistentEnum).IsAssignableFrom(typeClass) ) {
+						type = NHibernate.Enum(typeClass);
+					}
+					*/
+					
+					//TODO: Check Serializable Attribute????
+					/* 
+					else if  ( Serializable.class.isAssignableFrom(typeClass) ) {
+						type = NHibernate.Serializable(typeClass);
+					}
+					*/
+				}
+			}
+			return type;
+		}
+
+		// Collection Types:
+	
+		/*
+		public static PersistentCollectionType Array(string role, Class elementClass) {
+			return new ArrayType(role, elementClass);
+		}
+		public static PersistentCollectionType List(string role) {
+			return new ListType(role);
+		}
+		public static PersistentCollectionType Bag(string role) {
+			return new BagType(role);
+		}
+		public static PersistentCollectionType Map(string role) {
+			return new MapType(role);
+		}
+		public static PersistentCollectionType Set(string role) {
+			return new SetType(role);
+		}
+		public static PersistentCollectionType SortedMap(string role, Comparator comparator) {
+			return new SortedMapType(role, comparator);
+		}
+		public static PersistentCollectionType SortedSet(string role, Comparator comparator) {
+			return new SortedSetType(role, comparator);
+		}
+		*/
+
+		/// <summary>
+		/// Deep copy values in the first array into the second
+		/// </summary>
+		/// <param name="values"></param>
+		/// <param name="types"></param>
+		/// <param name="copy"></param>
+		/// <param name="target"></param>
+		public static void DeepCopy(object[] values, IType[] types, bool[] copy, object[] target) {
+			for ( int i=0; i<types.Length; i++ ) {
+				if ( copy[i] ) target[i] = types[i].DeepCopy( values[i] );
+			}
+		}
+	
+		/// <summary>
+		/// Determine if any of the given field values are dirty,
+		/// returning an array containing indexes of
+		/// the dirty fields or null if no fields are dirty.
+		/// </summary>
+		/// <param name="types"></param>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="check"></param>
+		/// <param name="session"></param>
+		/// <returns></returns>
+		public static int[] FindDirty(IType[] types, object[] x, object[] y, bool[] check, ISessionImplementor session) {
+			int[] results = null;
+			int count = 0;
+			for (int i=0; i<types.Length; i++) {
+				if ( check[i] && types[i].IsDirty( x[i], y[i], session ) ) {
+					if (results==null) results = new int[ types.Length ];
+					results[count++]=i;
+				}
+			}
+			if (count==0) {
+				return null;
+			}
+			else {
+				int[] trimmed = new int[count];
+				Array.Copy(results, 0, trimmed, 0, count);
+				return trimmed;
+			}
+		}
+	
+		/*
+		/// <summary>
+		/// Return <tt>-1</tt> if non-dirty, or the index of the first dirty value otherwise
+		/// </summary>
+		/// <param name="types"></param>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="owner"></param>
+		/// <param name="factory"></param>
+		/// <returns></returns>
+		public static int FindDirty(IType[] types, object[] x, object[] y, object owner, ISessionFactoryImplementor factory) {
+			for (int i=0; i<types.Length; i++) {
+				if ( types[i].IsDirty( x[i], y[i], owner, factory ) ) {
+					return i;
+				}
+			}
+			return -1;
+		}
+		*/
 	}
 }
