7b8506bb6918a130ef1ba5fc1994d7a6bb9bc197
ifof <ifof@users.sourceforge.net>
2003-03-01 11:09:23 +0000

    Adapted to new new types
    SVN: trunk@60

--- src/NHibernate/Type/TypeFactory.cs
+++ src/NHibernate/Type/TypeFactory.cs
@@ -18,29 +18,33 @@ namespace NHibernate.Type {
 		private static readonly Hashtable basicTypes;
 
 		static TypeFactory() {
-			basicTypes = new Hashtable(41);
+			basicTypes = new Hashtable(41);  //TODO: Fixup to prime number size when decommented all add.
 
 			basicTypes.Add(typeof(bool).Name, NHibernate.Boolean);
-			//basicTypes.Add(typeof(long).Name, NHibernate.Long);
-			//basicTypes.Add(typeof(short).Name, NHibernate.Short);
+			basicTypes.Add(typeof(long).Name, NHibernate.Long);
+			basicTypes.Add(typeof(short).Name, NHibernate.Short);
 			basicTypes.Add(typeof(int).Name, NHibernate.Integer);
 			basicTypes.Add(typeof(byte).Name, NHibernate.Byte);
-			//basicTypes.Add(typeof(float).Name, NHibernate.Float);
-			//basicTypes.Add(typeof(double).Name, NHibernate.Double);
+			basicTypes.Add(typeof(float).Name, NHibernate.Float);
+			basicTypes.Add(typeof(double).Name, NHibernate.Double);
 			basicTypes.Add(typeof(char).Name, NHibernate.Character);
-			//basicTypes.Add(typeof(string).Name, NHibernate.String);
+			basicTypes.Add(typeof(string).Name, NHibernate.String);
+			basicTypes.Add(typeof(decimal).Name, NHibernate.Decimal);
 			basicTypes.Add(NHibernate.Character.Name, NHibernate.Character);
 			basicTypes.Add(NHibernate.Integer.Name, NHibernate.Integer);
-			//basicTypes.Add(NHibernate.String.Name, NHibernate.String);
-			//basicTypes.Add(NHibernate.Date.Name, NHibernate.Date);
+			basicTypes.Add(NHibernate.Decimal.Name, NHibernate.Decimal);
+			basicTypes.Add(NHibernate.Long.Name, NHibernate.Long);
+			basicTypes.Add(NHibernate.Short.Name, NHibernate.Short);
+			basicTypes.Add(NHibernate.String.Name, NHibernate.String);
+			basicTypes.Add(NHibernate.Date.Name, NHibernate.Date);
 			//basicTypes.Add(NHibernate.Time.Name, NHibernate.Time);
-			//basicTypes.Add(NHibernate.Timestamp.Name, NHibernate.Timestamp);
-			//basicTypes.Add(NHibernate.Locale.Name, NHibernate.Locale);
+			basicTypes.Add(typeof(CultureInfo).Name, NHibernate.CultureInfo);
+			basicTypes.Add(NHibernate.CultureInfo.Name, NHibernate.CultureInfo);
+			basicTypes.Add(NHibernate.Timestamp.Name, NHibernate.Timestamp);
 			//basicTypes.Add(NHibernate.Calendar.Name, NHibernate.Calendar);
 			//basicTypes.Add(NHibernate.CalendarDate.Name, NHibernate.CalendarDate);
 			//basicTypes.Add(NHibernate.Currency.Name, NHibernate.Currency);
 			//basicTypes.Add(NHibernate.Timezone.Name, NHibernate.Timezone);
-			//basicTypes.Add(NHibernate.Class.Name, NHibernate.Class);
 			basicTypes.Add(NHibernate.TrueFalse.Name, NHibernate.TrueFalse);
 			basicTypes.Add(NHibernate.YesNo.Name, NHibernate.YesNo);
 			basicTypes.Add(NHibernate.Binary.Name, NHibernate.Binary);
@@ -48,15 +52,15 @@ namespace NHibernate.Type {
 			//basicTypes.Add(NHibernate.Clob.Name, NHibernate.Clob);
 			//basicTypes.Add(NHibernate.BigDecimal.Name, NHibernate.BigDecimal);
 			//basicTypes.Add(NHibernate.Serializable.Name, NHibernate.Serializable);
-			//basicTypes.Add(NHibernate.Object.Name, NHibernate.Object);
+			basicTypes.Add(NHibernate.Object.Name, NHibernate.Object);
 			basicTypes.Add(NHibernate.Boolean.Name, NHibernate.Boolean);
-			//basicTypes.Add(typeof(DateTime).Name, NHibernate.Timestamp);  //Is it correct?
-			//basicTypes.Add(typeof(CultureInfo).Name, NHibernate.Locale);
-			//basicTypes.Add(typeof(Calendar).Name, NHibernate.Calendar); //Is it correct?
+			basicTypes.Add(typeof(DateTime).Name, NHibernate.Timestamp);  
+			//basicTypes.Add(typeof(Calendar).Name, NHibernate.Calendar); 
 			// if ( CurrencyType.CURRENCY_CLASS!=null) basicTypes.put( CurrencyType.CURRENCY_CLASS.getName(), Hibernate.CURRENCY);
 			// basicTypes.put( TimeZone.class.getName(), Hibernate.TIMEZONE);
-			//basicTypes.Add(typeof(object).Name, NHibernate.Object);
-			//basicTypes.Add(typeof(System.Type).Name, NHibernate.Class);
+			basicTypes.Add(typeof(object).Name, NHibernate.Object);
+			basicTypes.Add(typeof(System.Type).Name, NHibernate.Class);
+			basicTypes.Add(NHibernate.Class.Name, NHibernate.Class);
 			basicTypes.Add(typeof(byte[]).Name, NHibernate.Binary);
 			//basicTypes.put( Blob.class.getName(), Hibernate.BLOB);
 			//basicTypes.put( Clob.class.getName(), Hibernate.CLOB);
