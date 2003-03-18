14a1c07438a987cbbf8bdf54832a9cdc0ae2f313
Tom Barrett <tjb300@users.sourceforge.net>
2003-03-18 20:27:45 +0000

    *** empty log message ***
    SVN: trunk@107

---
+++ src/NHibernate/Eg/Edge.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace NHibernate.Eg {
+	
+	public class Edge {
+		private float length;
+		private string name;
+		private float capacity;
+		private Vertex source;
+		private Vertex sink;
+		private long key;
+		private DateTime creationDate = DateTime.Now;
+
+		public float Capacity {
+			get { return capacity; }
+			set { capacity = value; }
+		}
+
+		public float Length {
+			get { return length; }
+			set { length = value; }
+		}
+
+		public string Name {
+			get { return name; }
+			set { name = value; }
+		}
+
+		public Vertex Source {
+			get { return source; }
+			set { source = value; }
+		}
+
+		public Vertex Sink {
+			get { return sink; }
+			set { sink = value; }
+		}
+
+		public long Key {
+			get { return key; }
+			set { key = value; }
+		}
+
+		public DateTime CreationDate {
+			get { return creationDate; }
+			set { creationDate = value; }
+		}
+	}
+}
---
+++ src/NHibernate/Eg/Source.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace NHibernate.Eg {
+	
+	public class Source : Vertex {
+		private float strength;
+
+		public float SourceStrength {
+			get { return strength; }
+			set { strength = value; }
+		}
+
+		public override float ExcessCapacity {
+			get { return base.ExcessCapacity - strength; }
+		}
+	}
+}
