340387e08aa7889a83310b334db13b3aca075711
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-02-17 03:30:07 +0000

    Modified so it was accurate down to the second.
    SVN: trunk@238

--- src/NHibernate/Type/DateTimeType.cs
+++ src/NHibernate/Type/DateTimeType.cs
@@ -3,58 +3,59 @@ using System.Data;
 
 using NHibernate.SqlTypes;
 
-namespace NHibernate.Type {
+namespace NHibernate.Type 
+{
 	
 	/// <summary>
-	/// Maps a System.DateTime Property to a column that stores date & time.
+	/// Maps a System.DateTime Property to a column that stores date & time down to 
+	/// the accuracy of a second.
 	/// </summary>
 	/// <remarks>
-	/// <p>
-	/// The value stored in the database depends on what your Data Provider is capable
-	/// of storing.  So there is a possibility that the DateTime you save will not be
-	/// the same DateTime you get back when you check DateTime.Equals(DateTime) because
-	/// they will have their milliseconds off.
-	/// </p>  
-	/// <p>
-	/// For example - MsSql Server 2000 is only accurate to 3.33 milliseconds.  So if 
-	/// NHibernate writes a value of '01/01/98 23:59:59.995' to the Prepared Command, MsSql
-	/// will store it as '1998-01-01 23:59:59.997'.
-	/// </p>
-	/// <p>
-	/// Please review the documentation of your Database server.
-	/// </p>
-	/// 
+	/// This only stores down to a second, so if you are looking for the most accurate
+	/// date and time storage your provider can give you use the <see cref="TimestampType" />. 
+	/// or the <see cref="TicksType"/>
 	/// </remarks>
 	public class DateTimeType : MutableType, IIdentifierType, ILiteralType 
 	{
 		
-		public DateTimeType(DateTimeSqlType sqlType) : base(sqlType) {
+		public DateTimeType(DateTimeSqlType sqlType) : base(sqlType) 
+		{
 		}
 
-		public override object Get(IDataReader rs, int index) {
-			return rs.GetDateTime(index);
+		public override object Get(IDataReader rs, int index) 
+		{
+			DateTime dbValue = rs.GetDateTime(index);
+			return new DateTime(dbValue.Year, dbValue.Month, dbValue.Day, dbValue.Hour, dbValue.Minute, dbValue.Second);
 		}
 
-		public override object Get(IDataReader rs, string name) {
+		public override object Get(IDataReader rs, string name) 
+		{
 			return Get(rs,rs.GetOrdinal(name));// rs.[name];
 		}
 
-		public override System.Type ReturnedClass {
+		public override System.Type ReturnedClass 
+		{
 			get { return typeof(DateTime); }
 		}
-		public override void Set(IDbCommand st, object value, int index) {
+
+		public override void Set(IDbCommand st, object value, int index) 
+		{
 			IDataParameter parm = st.Parameters[index] as IDataParameter;
 			parm.DbType = DbType.DateTime;
 			//TODO: figure out if this is a good solution for NULL DATES
-			if(value.Equals(System.DateTime.MinValue)) {
+			if(value.Equals(System.DateTime.MinValue)) 
+			{
 				parm.Value = DBNull.Value;
 			}
-			else {
-				parm.Value = value;
+			else 
+			{
+				DateTime dateValue = (DateTime)value;
+				parm.Value = new DateTime(dateValue.Year, dateValue.Month, dateValue.Day, dateValue.Hour, dateValue.Minute, dateValue.Second);
 			}
 		}
 
-		public override bool Equals(object x, object y) {
+		public override bool Equals(object x, object y) 
+		{
 			if (x==y) return true;
 			// DateTime can't be null because it is a struct - so comparing 
 			// them this way is useless - instead use the magic number...
@@ -63,25 +64,43 @@ namespace NHibernate.Type {
 			DateTime date1 = (x==null)? DateTime.MinValue : (DateTime) x;
 			DateTime date2 = (y==null)? DateTime.MinValue : (DateTime) y;
 
-			return date1.Equals(date2);
+			//return date1.Equals(date2);
+			return (date1.Year == date2.Year &&
+				date1.Month == date2.Month &&
+				date1.Day == date2.Day &&
+				date1.Hour == date2.Hour &&
+				date1.Minute == date2.Minute &&
+				date1.Second == date2.Second);
 		}
-		public override string Name {
+
+		public override string Name 
+		{
 			get { return "DateTime"; }
 		}
-		public override string ToXML(object val) {
+
+		public override string ToXML(object val) 
+		{
 			return ((DateTime)val).ToShortDateString();
 		}
-		public override object DeepCopyNotNull(object value) {
+
+		public override object DeepCopyNotNull(object value) 
+		{
 			DateTime old = (DateTime) value;
-			return new DateTime(old.Year, old.Month, old.Day, old.Hour, old.Minute, old.Second, old.Millisecond);
+			return new DateTime(old.Year, old.Month, old.Day, old.Hour, old.Minute, old.Second);
 		}
-		public override bool HasNiceEquals {
+
+		public override bool HasNiceEquals 
+		{
 			get { return true; }
 		}
-		public object StringToObject(string xml) {
+
+		public object StringToObject(string xml) 
+		{
 			return DateTime.Parse(xml);
 		}
-		public string ObjectToSQLString(object value) {
+
+		public string ObjectToSQLString(object value) 
+		{
 			return "'" + value.ToString() + "'";
 		}
 	}
