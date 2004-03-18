5a89da564746d3e8d78eb0f7c6646559027e7622
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-03-18 15:00:48 +0000

    Initial coding of ForUpdateFragment.
    SVN: trunk@272

---
+++ src/NHibernate/SqlCommand/ForUpdateFragment.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Text;
+
+using NHibernate.Sql;
+using NHibernate.Util;
+
+namespace NHibernate.SqlCommand 
+{
+
+	/// <summary>
+	/// Represents an SQL <c>for update of ... nowait</c> statement
+	/// </summary>
+	public class ForUpdateFragment 
+	{
+
+		private StringBuilder aliases = new StringBuilder();
+		private bool nowait;
+
+		public ForUpdateFragment() 
+		{
+		}
+
+		public ForUpdateFragment AddTableAlias(string alias) 
+		{
+			if(aliases.Length > 0 ) aliases.Append(StringHelper.CommaSpace);
+			aliases.Append(alias);
+			return this;
+		}
+
+		public SqlString ToSqlStringFragment() 
+		{
+			return aliases.Length==0 ?
+				new SqlString("") :
+				new SqlString(" for update of " + aliases + ( nowait ? " nowait" : "")); 
+		}
+
+	}
+}
+
+
