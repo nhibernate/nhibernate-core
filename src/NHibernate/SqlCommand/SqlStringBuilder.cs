093dea547cf45f1d7fe37200face94e260c41909
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-03-20 22:28:22 +0000

    renamed a parameter in a method to make it clearer that you are passing a string not a SqlString.
    SVN: trunk@288

--- src/NHibernate/SqlCommand/SqlStringBuilder.cs
+++ src/NHibernate/SqlCommand/SqlStringBuilder.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections;
 
-namespace NHibernate.SqlCommand {
-
+namespace NHibernate.SqlCommand 
+{
 	/// <summary>
 	/// The SqlStringBuilder is used to construct a SqlString.  The SqlString is a nonmutable
 	/// class so it can't have sql parts added to it.  Instead this class should be used to
@@ -26,7 +26,8 @@ namespace NHibernate.SqlCommand {
 	/// until the very end - making testing dialect indifferent.  Right now all of our test
 	/// to make sure the correct sql is getting built are specific to MsSql2000Dialect.
 	/// </remarks>
-	public class SqlStringBuilder : ISqlStringBuilder {
+	public class SqlStringBuilder : ISqlStringBuilder 
+	{
 
 		// this holds the strings and parameters that make up the full sql statement.
 		ArrayList sqlParts;
@@ -34,14 +35,16 @@ namespace NHibernate.SqlCommand {
 		/// <summary>
 		/// Create an empty StringBuilder with the default capacity.  
 		/// </summary>
-		public SqlStringBuilder() : this(16){
+		public SqlStringBuilder() : this(16)
+		{
 		}
 
 		/// <summary>
 		/// Create a StringBuilder with a specific capacity.
 		/// </summary>
 		/// <param name="partsCapacity">The number of parts expected.</param>
-		public SqlStringBuilder(int partsCapacity) {
+		public SqlStringBuilder(int partsCapacity) 
+		{
 			sqlParts = new ArrayList(partsCapacity);
 		}
 
@@ -49,9 +52,11 @@ namespace NHibernate.SqlCommand {
 		/// Create a StringBuilder to modify the SqlString
 		/// </summary>
 		/// <param name="sqlString">The SqlString to modify.</param>
-		public SqlStringBuilder(SqlString sqlString) {
+		public SqlStringBuilder(SqlString sqlString) 
+		{
 			sqlParts = new ArrayList(sqlString.SqlParts.Length);
-			foreach(object part in sqlString.SqlParts) {
+			foreach(object part in sqlString.SqlParts) 
+			{
 				sqlParts.Add(part);
 			}
 		}
@@ -59,10 +64,11 @@ namespace NHibernate.SqlCommand {
 		/// <summary>
 		/// Adds the preformatted sql to the SqlString that is being built.
 		/// </summary>
-		/// <param name="sqlString">The string to add.</param>
+		/// <param name="sql">The string to add.</param>
 		/// <returns>This SqlStringBuilder</returns>
-		public SqlStringBuilder Add(string sqlString) {
-			sqlParts.Add(sqlString);
+		public SqlStringBuilder Add(string sql) 
+		{
+			sqlParts.Add(sql);
 			return this;
 		}
 
@@ -74,7 +80,8 @@ namespace NHibernate.SqlCommand {
 		/// </summary>
 		/// <param name="parameter">The Parameter to add.</param>
 		/// <returns>This SqlStringBuilder</returns>
-		public SqlStringBuilder Add(Parameter parameter) {
+		public SqlStringBuilder Add(Parameter parameter) 
+		{
 			sqlParts.Add(parameter);
 			return this;
 		}
@@ -86,17 +93,22 @@ namespace NHibernate.SqlCommand {
 		/// <param name="part">The part to add when it is not known if it is a Parameter, String, or SqlString.</param>
 		/// <returns>This SqlStringBuilder.</returns>
 		/// <exception cref="ArgumentException">Thrown when the part is not a Parameter, String, or SqlString.</exception>
-		public SqlStringBuilder AddObject(object part) {
-			if(part is Parameter) {
+		public SqlStringBuilder AddObject(object part) 
+		{
+			if(part is Parameter) 
+			{
 				return this.Add((Parameter)part);
 			}
-			else if (part is String) {
+			else if (part is String) 
+			{
 				return this.Add((String)part);
 			}
-			else if (part is SqlString) {
+			else if (part is SqlString) 
+			{
 				return this.Add((SqlString)part, null, null, null);
 			}
-			else {
+			else 
+			{
 				throw new ArgumentException("Part was not a Parameter, String, or SqlString.");
 			}
 		}
@@ -109,7 +121,8 @@ namespace NHibernate.SqlCommand {
 		/// <param name="sqlString">The SqlString to add to this SqlStringBuilder</param>
 		/// <returns>This SqlStringBuilder</returns>
 		/// <remarks>This calls the overloaded Add(sqlString, null, null, null, false)</remarks>
-		public SqlStringBuilder Add(SqlString sqlString) {
+		public SqlStringBuilder Add(SqlString sqlString) 
+		{
 			return Add(sqlString, null, null, null, false);
 		}
 
@@ -123,7 +136,8 @@ namespace NHibernate.SqlCommand {
 		/// <param name="postfix">String to put at the end of the combined SqlString.</param>
 		/// <returns>This SqlStringBuilder</returns>
 		/// <remarks>This calls the overloaded Add method with an array of SqlStrings</remarks>
-		public SqlStringBuilder Add(SqlString sqlString, string prefix, string op, string postfix) {
+		public SqlStringBuilder Add(SqlString sqlString, string prefix, string op, string postfix) 
+		{
 			return Add(new SqlString[] {sqlString}, prefix, op, postfix);
 		}
 
@@ -136,7 +150,8 @@ namespace NHibernate.SqlCommand {
 		/// <param name="postfix">String to put at the end of the combined SqlString.</param>
 		/// <param name="wrapStatement">Wrap each SqlStrings with "(" and ")"</param>
 		/// <returns>This SqlStringBuilder</returns>
-		public SqlStringBuilder Add(SqlString sqlString, string prefix, string op, string postfix, bool wrapStatement) {
+		public SqlStringBuilder Add(SqlString sqlString, string prefix, string op, string postfix, bool wrapStatement) 
+		{
 			return Add(new SqlString[] {sqlString}, prefix, op, postfix, wrapStatement);
 		}
 
@@ -149,7 +164,8 @@ namespace NHibernate.SqlCommand {
 		/// <param name="postfix">String to put at the end of the combined SqlStrings.</param>
 		/// <returns>This SqlStringBuilder</returns>
 		/// <remarks>This calls the overloaded Add method with wrapStatement=true</remarks>
-		public SqlStringBuilder Add(SqlString[] sqlStrings, string prefix, string op, string postfix) {
+		public SqlStringBuilder Add(SqlString[] sqlStrings, string prefix, string op, string postfix) 
+		{
 			return Add(sqlStrings, prefix, op, postfix, true);
 		}
 
@@ -162,23 +178,23 @@ namespace NHibernate.SqlCommand {
 		/// <param name="postfix">String to put at the end of the combined SqlStrings.</param>
 		/// <param name="wrapStatement">Wrap each SqlStrings with "(" and ")"</param>
 		/// <returns>This SqlStringBuilder</returns>
-		public SqlStringBuilder Add(SqlString[] sqlStrings, string prefix, string op, string postfix, bool wrapStatement) {
+		public SqlStringBuilder Add(SqlString[] sqlStrings, string prefix, string op, string postfix, bool wrapStatement) 
+		{
 			
 			if(prefix!=null && prefix!=String.Empty) sqlParts.Add(prefix);
 			
 			bool opNeeded = false;
 			
-			foreach(SqlString sqlString in sqlStrings) {
-				
-				if(opNeeded) {
-					sqlParts.Add(" " + op + " ");
-				}
+			foreach(SqlString sqlString in sqlStrings) 
+			{
+				if(opNeeded) sqlParts.Add(" " + op + " ");
 				
 				opNeeded=true;
 
 				if(wrapStatement) sqlParts.Add("(");
 				
-				foreach(object sqlPart in sqlString.SqlParts) {
+				foreach(object sqlPart in sqlString.SqlParts) 
+				{
 					sqlParts.Add(sqlPart);
 				}
 
@@ -197,7 +213,8 @@ namespace NHibernate.SqlCommand {
 		/// Converts the mutable SqlStringBuilder into the immutable SqlString.
 		/// </summary>
 		/// <returns>The SqlString that was built.</returns>
-		public SqlString ToSqlString() {
+		public SqlString ToSqlString() 
+		{
 			return new SqlString((object[])sqlParts.ToArray(typeof(object)));
 		}
 	}
