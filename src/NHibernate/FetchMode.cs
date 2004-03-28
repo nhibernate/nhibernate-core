1b6221c9866dafc24d020694f4b3176ab7a804c3
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-03-28 06:10:26 +0000

    Synch with H2.0.3 - did not exist before then.
    SVN: trunk@328

---
+++ src/NHibernate/FetchMode.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace NHibernate 
+{
+	
+	/// <summary>
+	/// Represents a fetching strategy.
+	/// </summary>
+	/// <remarks>
+	/// This is used together with the <see cref="ICriteria"/> API to specify
+	/// runtime fetching strategies.
+	/// <para>
+	/// For Hql queries, use the <c>FETCH</c> keyword instead.
+	/// </para>
+	/// </remarks>
+	public enum FetchMode 
+	{
+		
+		/// <summary>
+		/// Default to the setting configured in the mapping file.
+		/// </summary>
+		Default = 0,
+		/// <summary>
+		/// Fetch lazily.  Equivalent to <c>outer-join="false"</c>
+		/// </summary>
+		Lazy = 1,
+		/// <summary>
+		/// Fetch eagerly, using an outer join.  Equivalent to <c>outer-join="true"</c>
+		/// </summary>
+		Eager = 2
+	}
+}
