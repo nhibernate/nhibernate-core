6bbd1bc9846b0373da1ee26f7a5fb7fb131212c2
Peter Smulovics <szoke@users.sourceforge.net>
2004-04-08 07:43:29 +0000

    I think we may need this for later use :)
    SVN: trunk@379

--- src/NHibernate.Test/TestCase.cs
+++ src/NHibernate.Test/TestCase.cs
@@ -42,6 +42,38 @@ namespace NHibernate.Test
 		{
 			new SchemaExport(cfg).Drop(true, true);
 		}
+		public void ExecuteStatement(string sql)
+		{
+			ExecuteStatement(sql, true);
+		}
+
+		public void ExecuteStatement(string sql, bool error)
+		{
+			SqlConnection conn = null;
+			SqlTransaction tran = null;
+			try
+			{
+				conn = new SqlConnection("Server=localhost;initial catalog=nhibernate;User ID=someuser;Password=somepwd");
+				conn.Open();
+				tran = conn.BeginTransaction();
+				System.Data.SqlClient.SqlCommand comm = conn.CreateCommand();
+				comm.CommandText = sql;
+				comm.Transaction = tran;
+				comm.CommandType = CommandType.Text;
+				comm.ExecuteNonQuery();
+				tran.Commit();
+			}
+			catch
+			{
+				tran.Rollback();
+				if (error)
+					throw;
+			}
+			finally
+			{
+				conn.Close();
+			}
+		}
 
 	}
 }
