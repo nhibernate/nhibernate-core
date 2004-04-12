a0c43d691a118d7181c1c35a940dd1dc017a618b
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-04-12 05:39:10 +0000

    Test Fixture for Template class.
    SVN: trunk@403

---
+++ src/NHibernate.Test/SqlCommandTest/TemplateFixture.cs
@@ -0,0 +1,36 @@
+using System;
+
+using NHibernate.Dialect;
+using NHibernate.SqlCommand;
+
+using NUnit.Framework;
+
+namespace NHibernate.Test.SqlCommandTest 
+{
+
+	[TestFixture]
+	public class TemplateFixture 
+	{
+
+		public TemplateFixture() 
+		{
+		}
+
+		/// <summary>
+		/// Tests that a column enclosed by <c>`</c> is enclosed by the Dialect.OpenQuote 
+		/// and Dialect.CloseQuote after the Template Renders the Where String.
+		/// </summary>
+		[Test]
+		public void ReplaceWithDialectQuote() 
+		{
+			Dialect.Dialect dialect = new Dialect.MsSql2000Dialect();
+			string whereFragment = "column_name = 'string value' and `backtick` = 1";
+
+			string expectedFragment = "$PlaceHolder.column_name = 'string value' and $PlaceHolder.[backtick] = 1";
+
+			Assert.AreEqual( expectedFragment, Template.RenderWhereStringTemplate(whereFragment, dialect) );
+
+		}
+
+	}
+}
