0204868aaf2961866583524ae8ddacad39f18de2
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-04-14 18:09:18 +0000

    Added Guid column to the MockReader.
    SVN: trunk@430

--- src/NHibernate.Test/TypesTest/BaseTypeFixture.cs
+++ src/NHibernate.Test/TypesTest/BaseTypeFixture.cs
@@ -18,7 +18,7 @@ namespace NHibernate.Test.TypesTest
 		protected const int ByteTypeColumnIndex = 1;
 		protected const int Int32TypeColumnIndex = 2;
 		protected const int DecimalTypeColumnIndex = 3;
-
+		protected const int GuidTypeColumnIndex = 4;
 		[SetUp]
 		public void SetUp() 
 		{
@@ -29,17 +29,21 @@ namespace NHibernate.Test.TypesTest
 			dataTable.Columns.Add(new DataColumn("ByteType", typeof(byte)));
 			dataTable.Columns.Add(new DataColumn("Int32Type", typeof(int)));
 			dataTable.Columns.Add(new DataColumn("DecimalType", typeof(decimal)));
+			dataTable.Columns.Add(new DataColumn("GuidType", typeof(Guid)));
 			mockReader.SetSchemaTable(dataTable);
 
-			object[,] rowValues = new object[2,4];
+			object[,] rowValues = new object[2,5];
 			rowValues[0,BooleanTypeColumnIndex] = true;
 			rowValues[0,ByteTypeColumnIndex] = 5;
 			rowValues[0,Int32TypeColumnIndex] = 0;
 			rowValues[0,DecimalTypeColumnIndex] = 5.64351M;
+			rowValues[0,GuidTypeColumnIndex] = new Guid("{01234567-abcd-abcd-abcd-0123456789ab}");
+
 			rowValues[1,BooleanTypeColumnIndex] = false;
 			rowValues[1,ByteTypeColumnIndex] = 6;
 			rowValues[1,Int32TypeColumnIndex] = 1;
 			rowValues[1,DecimalTypeColumnIndex] = 5.6435101M;
+			rowValues[1,GuidTypeColumnIndex] = new Guid("{76543210-dcba-dcba-dcba-ba9876543210}");
 			
 			mockReader.SetRows(rowValues);
 
