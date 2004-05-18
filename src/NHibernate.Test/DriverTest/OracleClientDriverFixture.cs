7892e278759f6303710148a6ab92c04a3a2de925
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-05-18 05:06:08 +0000

    Added test to ensure the correct Oracle System.Types were being loaded
    SVN: trunk@527

---
+++ src/NHibernate.Test/DriverTest/OracleClientDriverFixture.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Data;
+
+using NHibernate.Driver;
+using NUnit.Framework;
+
+namespace NHibernate.Test.DriverTest
+{
+	/// <summary>
+	/// Summary description for OracleClientDriverFixture.
+	/// </summary>
+	[TestFixture]
+	public class OracleClientDriverFixture
+	{
+		
+		/// <summary>
+		/// Verify that the correct Connection Class is being loaded.
+		/// </summary>
+		[Test]
+		public void ConnectionClassName() 
+		{
+			Driver.IDriver driver = new Driver.OracleClientDriver();
+			IDbConnection conn = driver.CreateConnection();
+
+			Assert.AreEqual( "System.Data.OracleClient.OracleConnection", conn.GetType().FullName);
+		}
+
+		/// <summary>
+		/// Verify that the correct Command class is being loaded.
+		/// </summary>
+		[Test]
+		public void CommandClassName() 
+		{
+			Driver.IDriver driver = new Driver.OracleClientDriver();
+			IDbCommand cmd = driver.CreateCommand();
+
+			Assert.AreEqual( "System.Data.OracleClient.OracleCommand", cmd.GetType().FullName );
+
+		}
+	}
+}
