b96b9c97a1b65df05a53f83d43248865db3cc5f7
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-07-12 01:32:18 +0000

    Added TestFixtures for configuring NHibernate from a seperate xml file.
    SVN: trunk@666

---
+++ src/NHibernate.Test/CfgTest/ConfigurationFixture.cs
@@ -0,0 +1,41 @@
+using System;
+
+using NHibernate.Cfg;
+
+using NUnit.Framework;
+
+namespace NHibernate.Test.CfgTest
+{
+	/// <summary>
+	/// Summary description for ConfigurationFixture.
+	/// </summary>
+	[TestFixture]
+	public class ConfigurationFixture
+	{
+		[SetUp]
+		public void SetUp() 
+		{
+			System.IO.File.Copy("..\\..\\hibernate.cfg.xml", "hibernate.cfg.xml");
+		}
+
+		[TearDown]
+		public void TearDown() 
+		{
+			System.IO.File.Delete("hibernate.cfg.xml");
+		}
+		
+		/// <summary>
+		/// Verify that NHibernate can read the configuration from a hibernate.cfg.xml
+		/// file and that the values override what is in the app.config.
+		/// </summary>
+		[Test]
+		public void ReadCfgXmlFromDefaultFile() 
+		{
+			Configuration cfg = new Configuration();
+			cfg.Configure();
+			
+			Assert.AreEqual( "true 1, false 0, yes 'Y', no 'N'", cfg.Properties[Cfg.Environment.QuerySubstitutions]);
+			Assert.AreEqual( "Server=localhost;initial catalog=nhibernate;Integrated Security=SSPI", cfg.Properties[Cfg.Environment.ConnectionString]);
+		}
+	}
+}
