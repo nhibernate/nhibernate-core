4756ef4d6e1647b529164f1eedb8e07b003c5ba3
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-07-18 03:06:13 +0000

    Added comments about needing to set sql-type or length in the hbm.xml for the correct column type to be generated by schema export.
    SVN: trunk@693

--- src/NHibernate/Type/TrueFalseType.cs
+++ src/NHibernate/Type/TrueFalseType.cs
@@ -8,6 +8,14 @@ namespace NHibernate.Type
 	/// Maps a System.Boolean to a single character column that stores a 
 	/// "T"/F" to indicate true/false.
 	/// </summary>
+	/// <remarks>
+	/// If you are using schema-export to generate your tables then you need
+	/// to set the column attributes: <c>length=1</c> or <c>sql-type="char(1)"</c>.
+	/// 
+	/// This needs to be done because in Java's JDBC there is a type for CHAR and 
+	/// in ADO.NET there is not one specifically for char, so you need to tell schema
+	/// export to create a char(1) column.
+	/// </remarks>
 	public class TrueFalseType: CharBooleanType 
 	{
 
--- src/NHibernate/Type/YesNoType.cs
+++ src/NHibernate/Type/YesNoType.cs
@@ -8,20 +8,33 @@ namespace NHibernate.Type
 	/// Maps a System.Boolean to a single character column that stores a 
 	/// "Y"/"N" to indicate true/false.
 	/// </summary>
-	public class YesNoType : CharBooleanType {
+	/// <remarks>
+	/// If you are using schema-export to generate your tables then you need
+	/// to set the column attributes: <c>length=1</c> or <c>sql-type="char(1)"</c>.
+	/// 
+	/// This needs to be done because in Java's JDBC there is a type for CHAR and 
+	/// in ADO.NET there is not one specifically for char, so you need to tell schema
+	/// export to create a char(1) column.
+	/// </remarks>
+	public class YesNoType : CharBooleanType 
+	{
 
-		internal YesNoType(AnsiStringFixedLengthSqlType sqlType) : base(sqlType) {
+		internal YesNoType(AnsiStringFixedLengthSqlType sqlType) : base(sqlType) 
+		{
 		}
 
-		protected override sealed string TrueString {
+		protected override sealed string TrueString 
+		{
 			get { return "Y"; }
 		}
 
-		protected override sealed string FalseString {
+		protected override sealed string FalseString 
+		{
 			get { return "N"; }
 		}
 
-		public override string Name {
+		public override string Name 
+		{
 			get { return "YesNo"; }
 		}
 	}
