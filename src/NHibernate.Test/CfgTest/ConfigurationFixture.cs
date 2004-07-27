dd209c66540cb2d96b18348feb5f0352b93eaad0
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-07-27 03:23:58 +0000

    Added fixture for manual configuration.
    SVN: trunk@734

--- src/NHibernate.Test/CfgTest/ConfigurationFixture.cs
+++ src/NHibernate.Test/CfgTest/ConfigurationFixture.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Collections;
 
 using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+using NHibernate.DomainModel;
 
 using NUnit.Framework;
 
@@ -26,5 +30,35 @@ namespace NHibernate.Test.CfgTest
 			Assert.AreEqual( "true 1, false 0, yes 'Y', no 'N'", cfg.Properties[Cfg.Environment.QuerySubstitutions]);
 			Assert.AreEqual( "Server=localhost;initial catalog=nhibernate;Integrated Security=SSPI", cfg.Properties[Cfg.Environment.ConnectionString]);
 		}
+
+		/// <summary>
+		/// Recieved sample code that Configuration could not be configured manually.  It can be configured
+		/// manually just need to set all of the properties before adding classes
+		/// </summary>
+		[Test]
+		public void ManualConfiguration() 
+		{
+			//log4net.Config.DOMConfigurator.ConfigureAndWatch( new FileInfo("log4net.cfg.xml") ); //use xml file instead of config
+			Configuration cfg = new Configuration();
+			IDictionary props = new Hashtable();
+
+			props["hibernate.connection.provider"] = "NHibernate.Connection.DriverConnectionProvider"; 
+			props["hibernate.dialect" ] = "NHibernate.Dialect.MsSql2000Dialect"; 
+			props["hibernate.connection.driver_class" ] = "NHibernate.Driver.SqlClientDriver" ;
+			props["hibernate.connection.connection_string"] = "Server=localhost;initial catalog=nhibernate;Integrated Security=SSPI" ;
+		
+			foreach( DictionaryEntry de in props ) 
+			{
+				cfg.SetProperty( de.Key.ToString(), de.Value.ToString() );
+			}
+
+			cfg.AddClass( typeof(Simple) );
+
+			new SchemaExport( cfg ).Create( true, true );
+
+			ISessionFactory factory = cfg.BuildSessionFactory();
+
+		}
+
 	}
 }
