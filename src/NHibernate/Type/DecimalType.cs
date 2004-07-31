cf387eff40e5f7a49dd3bab7ef200c28104d4be1
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-07-31 14:50:32 +0000

    Removed IVersionType interface from DecimalType.
    SVN: trunk@752

--- src/NHibernate/Type/DecimalType.cs
+++ src/NHibernate/Type/DecimalType.cs
@@ -3,54 +3,57 @@ using System.Data;
 
 using NHibernate.SqlTypes;
 
-namespace NHibernate.Type {
+namespace NHibernate.Type 
+{
 
 	/// <summary>
 	/// DecimalType
 	/// </summary>
-	public class DecimalType : PrimitiveType, IIdentifierType, IVersionType {
+	public class DecimalType : PrimitiveType, IIdentifierType
+	{
 	
-		internal DecimalType(DecimalSqlType sqlType) : base(sqlType) {
+		internal DecimalType(DecimalSqlType sqlType) : base(sqlType) 
+		{
 		}
 
-		public override object Get(IDataReader rs, int index) {
+		public override object Get(IDataReader rs, int index) 
+		{
 			return Convert.ToDecimal(rs[index]);
 		}
 
-		public override object Get(IDataReader rs, string name) {
+		public override object Get(IDataReader rs, string name) 
+		{
 			return Convert.ToDecimal(rs[name]);
 		}
 
-		public override System.Type PrimitiveClass {
+		public override System.Type PrimitiveClass 
+		{
 			get { return typeof(Decimal); }
 		}
 
-		public override System.Type ReturnedClass {
+		public override System.Type ReturnedClass 
+		{
 			get { return typeof(Decimal); }
 		}
 
-		public override void Set(IDbCommand st, object value, int index) {
+		public override void Set(IDbCommand st, object value, int index) 
+		{
 			IDataParameter parm = st.Parameters[index] as IDataParameter;
 			parm.Value = value;
 		}
 
-		public override string Name {
+		public override string Name 
+		{
 			get { return "Decimal"; }
 		}
 
-		public object StringToObject(string xml) {
+		public object StringToObject(string xml) 
+		{
 			return long.Parse(xml);
 		}
 
-		public object Next(object current) {
-			return ((Decimal)current) + 1;
-		}
-
-		public object Seed {
-			get { return 0; }
-		}
-
-		public override string ObjectToSQLString(object value) {
+		public override string ObjectToSQLString(object value) 
+		{
 			return value.ToString();
 		}
 	}
