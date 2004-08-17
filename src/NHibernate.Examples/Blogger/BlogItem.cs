7c4787bcac05a1a9a8834a468f770b5e0fb0388d
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-08-17 17:40:43 +0000

    Added Blogger example to cvs.
    SVN: trunk@841

---
+++ src/NHibernate.Examples/Blogger/BlogItem.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+
+namespace NHibernate.Examples.Blogger
+{
+	public class BlogItem
+	{
+		private long id;
+		private DateTime dateTime;
+		private string text;
+		private string title;
+		private Blog blog;
+
+		public Blog ParentBlog
+		{
+			get
+			{
+				return blog;
+			}
+			set
+			{
+				blog = value;
+			}
+		}
+
+		public DateTime ItemDate
+		{
+			get
+			{
+				return dateTime;
+			}
+			set
+			{
+				dateTime = value;
+			}
+		}
+
+		public long Id
+		{
+			get
+			{
+				return id;
+			}
+			set
+			{
+				id=value;
+			}
+		}
+		public string Text
+		{
+			get
+			{
+				return text;
+			}
+			set
+			{
+				text=value;
+			}
+		}
+		public string Title
+		{
+			get
+			{
+				return title;
+			}
+			set
+			{
+				title=value;
+			}
+		}
+	}
+}
