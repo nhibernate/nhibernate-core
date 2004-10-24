49f35e9660e83543038267c9261f0b82e557c269
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-10-24 21:42:40 +0000

    Fixed up Timestamper to be more like hibernate.  Not sure the old impl was at all correct.
    SVN: trunk@1045

--- src/NHibernate/Cache/Timestamper.cs
+++ src/NHibernate/Cache/Timestamper.cs
@@ -12,25 +12,36 @@ namespace NHibernate.Cache
 	///	</remarks>
 	public sealed class Timestamper 
 	{
+		private static object lockObject = new object();
+		
+		// hibernate is using System.currentMilliSeconds which is calculated
+		// from jan 1, 1970
+		private static long baseDateMs = (new DateTime(1970, 1, 1).Ticks)/10000;
+		
 		private static short counter = 0;
 		private static long time;
 		private const int BinDigits = 12;
-		public const short OneMs = 1<<BinDigits;
+		public const short OneMs = 1<<BinDigits; //(4096 is the value)
 
-		[MethodImpl(MethodImplOptions.Synchronized)]
 		public static long Next() 
 		{
-			long newTime = System.DateTime.Now.Ticks << BinDigits; //is this right?
-			if (time < newTime) 
+			lock( lockObject ) 
 			{
-				time = newTime;
-				counter = 0;
-			} 
-			else if (counter < OneMs - 1) 
-			{
-				counter++;
+				// Ticks is accurate down to 100 nanoseconds - hibernate uses milliseconds
+				// to help calculate next time so drop the nanoseconds portion.(1ms==1000000ns)
+				long newTime = ( (System.DateTime.Now.Ticks/10000) - baseDateMs ) << BinDigits; 
+				if (time < newTime) 
+				{
+					time = newTime;
+					counter = 0;
+				} 
+				else if (counter < OneMs - 1) 
+				{
+					counter++;
+				}
+				return time + counter;
+
 			}
-			return time + counter;
 		}
 
 		private Timestamper()
