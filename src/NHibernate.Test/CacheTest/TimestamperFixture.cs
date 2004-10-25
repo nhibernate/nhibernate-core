9de8693d20c13c8b4a4c739015431d0025fb3e55
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2004-10-25 06:48:04 +0000

    Added some test for Timestampper cache problems and a test to verify NH-132 is not a problem.
    SVN: trunk@1049

---
+++ src/NHibernate.Test/CacheTest/TimestamperFixture.cs
@@ -0,0 +1,33 @@
+using System;
+
+using NHibernate.Cache;
+
+using NUnit.Framework;
+
+namespace NHibernate.Test.CacheTest
+{
+	/// <summary>
+	/// Summary description for TimestamperFixture.
+	/// </summary>
+	[TestFixture]
+	public class TimestamperFixture
+	{
+		[Test]
+		public void VerifyIncrease() 
+		{
+			long currentTicks = 0;
+			long newTicks = 0;
+
+			// the Timestampper will only generate 4095 increasing identifiers per millisecond.
+			for( int i=0; i<4095; i++ ) 
+			{
+				newTicks = Timestamper.Next();
+				if( (newTicks - currentTicks) == 0 ) 
+				{
+					Assert.Fail( "diff was " + (newTicks - currentTicks) + ".  It should always increase.  Loop i=" + i + " with currentTicks = " + currentTicks + " and newTicks = " + newTicks );
+				}
+				currentTicks = newTicks;
+			}
+		}
+	}
+}
---
+++ src/NHibernate.Test/MappingTest/TableFixture.cs
@@ -0,0 +1,55 @@
+using System;
+
+using NHibernate.Mapping;
+
+using NUnit.Framework;
+
+namespace NHibernate.Test.MappingTest
+{
+	/// <summary>
+	/// Summary description for TableFixture.
+	/// </summary>
+	[TestFixture]
+	public class TableFixture
+	{
+		[Test]
+		public void TableNameQuoted() 
+		{
+			Table tbl = new Table();
+			tbl.Name = "`keyword`";
+
+			Dialect.Dialect dialect = new Dialect.MsSql2000Dialect();
+
+			Assert.AreEqual( "[keyword]", tbl.GetQuotedName( dialect ) );
+
+			Assert.AreEqual( "dbo.[keyword]", tbl.GetQualifiedName( dialect, "dbo" ) );
+
+			Assert.AreEqual( "[keyword]", tbl.GetQualifiedName( dialect, null ) );
+
+			tbl.Schema = "sch";
+
+			Assert.AreEqual( "sch.[keyword]", tbl.GetQualifiedName( dialect ) );
+		}
+
+		[Test]
+		public void TableNameNotQuoted() 
+		{
+			Table tbl = new Table();
+			tbl.Name = "notkeyword";
+
+			Dialect.Dialect dialect = new Dialect.MsSql2000Dialect();
+
+			Assert.AreEqual( "notkeyword", tbl.GetQuotedName( dialect ) );
+
+			Assert.AreEqual( "dbo.notkeyword", tbl.GetQualifiedName( dialect, "dbo" ) );
+
+			Assert.AreEqual( "notkeyword", tbl.GetQualifiedName( dialect, null ) );
+
+			tbl.Schema = "sch";
+
+			Assert.AreEqual( "sch.notkeyword", tbl.GetQualifiedName( dialect ) );
+		}
+
+
+	}
+}
