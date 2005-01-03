87a6f855cec053eb9459e921e67da3658eae5919
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2005-01-03 03:43:53 +0000

    Added a fixture to verify the exception thrown when a class is missing a default ctor is easy to read.
    SVN: trunk@1193

---
+++ src/NHibernate.Test/MappingExceptions/MissingDefCtorFixture.cs
@@ -0,0 +1,38 @@
+using System;
+
+using NHibernate.Cfg;
+
+using NUnit.Framework;
+
+namespace NHibernate.Test.MappingExceptions
+{
+	/// <summary>
+	/// A TestFixture to verify the Exception thrown when a mapped class is missing the default
+	/// ctor is readable and understandable.
+	/// </summary>
+	[TestFixture]
+	public class MissingDefCtorFixture
+	{
+		[Test]
+		public void ClassMissingDefaultCtor() 
+		{
+			bool excCaught = false;
+
+			// add a resource that doesn't exist
+			string resource = "NHibernate.Test.MappingExceptions.MissingDefCtor.hbm.xml";
+			Configuration cfg = new Configuration();
+			try 
+			{
+				cfg.AddResource( resource, this.GetType().Assembly );
+				cfg.BuildSessionFactory();
+			}
+			catch( MappingException me ) 
+			{
+				Assert.AreEqual( "The mapped class NHibernate.Test.MappingExceptions.MissingDefCtor must declare a default (no-arg) constructor.", me.Message );
+				excCaught = true;
+			}
+
+			Assert.IsTrue( excCaught, "Should have caught the MappingException about default ctor being missing." );
+		}
+	}
+}
