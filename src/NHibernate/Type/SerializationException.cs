196bbbe801a9df1be2809201fec869c90f0600ab
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2005-02-14 03:39:07 +0000

    Followed FxCops rules on creating custom exceptions and supporting serialization.  However, they still inherit from ApplicationException.
    SVN: trunk@1279

--- src/NHibernate/Type/SerializationException.cs
+++ src/NHibernate/Type/SerializationException.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace NHibernate.Type
 {
@@ -9,11 +10,46 @@ namespace NHibernate.Type
 	public class SerializationException : HibernateException
 	{
 		/// <summary>
-		/// 
+		/// Initializes a new instance of the <see cref="SerializationException"/> class.
 		/// </summary>
-		/// <param name="message"></param>
-		/// <param name="root"></param>
-		public SerializationException( string message, Exception root ) : base( message, root )
+		public SerializationException(  ) : base( "The Property associated with a SerializableType threw an Exception during serialization or deserialization." )
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SerializationException"/> class.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		public SerializationException( string message ) : base( message )
+		{
+		
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SerializationException"/> class.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		/// <param name="e">
+		/// The exception that is the cause of the current exception. If the innerException parameter 
+		/// is not a null reference, the current exception is raised in a catch block that handles 
+		/// the inner exception.
+		/// </param>
+		public SerializationException( string message, Exception e ) : base( message, e )
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SerializationException"/> class
+		/// with serialized data.
+		/// </summary>
+		/// <param name="info">
+		/// The <see cref="SerializationInfo"/> that holds the serialized object 
+		/// data about the exception being thrown.
+		/// </param>
+		/// <param name="context">
+		/// The <see cref="StreamingContext"/> that contains contextual information about the source or destination.
+		/// </param>
+		protected SerializationException( SerializationInfo info, StreamingContext context ) : base( info, context )
 		{
 		}
 	}
