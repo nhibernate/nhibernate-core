5b08dea947f6eda2a3812452d3a99ca245b61ae8
Sergey Koshcheyev <justme84@users.sourceforge.net>
2005-04-09 12:48:06 +0000

    Abort transactions on failure
    SVN: trunk@1379

--- src/NHibernate.Test/ExpressionTest/QueryByExampleTest.cs
+++ src/NHibernate.Test/ExpressionTest/QueryByExampleTest.cs
@@ -31,90 +31,102 @@ namespace NHibernate.Test.ExpressionTest
 		[Test]
 		public void TestSimpleQBE()
 		{
-			ISession s = sessions.OpenSession();
-			ITransaction t = s.BeginTransaction();
-			Componentizable master = GetMaster("hibernate", null, "ope%");
-			ICriteria crit = s.CreateCriteria(typeof(Componentizable));
-			Example ex = Example.create(master).EnableLike();
-			crit.Add(ex);
-			IList result = crit.List();
-			Assertion.AssertNotNull(result);
-			Assertion.AssertEquals(1, result.Count);
-			t.Commit();
-			s.Close();
+			using( ISession s = sessions.OpenSession() )
+			using( ITransaction t = s.BeginTransaction() )
+			{
+				Componentizable master = GetMaster("hibernate", null, "ope%");
+				ICriteria crit = s.CreateCriteria(typeof(Componentizable));
+				Example ex = Example.create(master).EnableLike();
+				crit.Add(ex);
+				IList result = crit.List();
+				Assert.IsNotNull(result);
+				Assert.AreEqual(1, result.Count);
+				t.Commit();
+			}
 		}
 
 		[Test]
 		[Ignore("Test Fails with Exception - do to with Criteria expression/parameter handling")]
 		public void TestJunctionNotExpressionQBE()
 		{
-			ISession s = sessions.OpenSession();
-			ITransaction t = s.BeginTransaction();
-			Componentizable master = GetMaster("hibernate", null, "ope%");
-			ICriteria crit = s.CreateCriteria(typeof(Componentizable));
-			Example ex = Example.create(master).EnableLike();
+			using( ISession s = sessions.OpenSession() )
+			using( ITransaction t = s.BeginTransaction() )
+			{
+				Componentizable master = GetMaster("hibernate", null, "ope%");
+				ICriteria crit = s.CreateCriteria(typeof(Componentizable));
+				Example ex = Example.create(master).EnableLike();
 
 			
-			crit.Add(Expression.Expression.Or(Expression.Expression.Not(ex), ex));
+				crit.Add(Expression.Expression.Or(Expression.Expression.Not(ex), ex));
 
-			IList result = crit.List();
-			Assertion.AssertNotNull(result);
-//			if (!(GetDialect()
-//			instanceof HSQLDialect) )
-//			assertEquals(2, result.size());
-			t.Commit();
-			s.Close();
+				IList result = crit.List();
+				Assert.IsNotNull(result);
+//				if (!(GetDialect()
+//					instanceof HSQLDialect) )
+//					assertEquals(2, result.size());
+				t.Commit();
+			}
 		}
 
 		[Test]
 		public void TestExcludingQBE()
 		{
-			ISession s = sessions.OpenSession();
-			ITransaction t = s.BeginTransaction();
-			Componentizable master = GetMaster("hibernate", null, "ope%");
-			ICriteria crit = s.CreateCriteria(typeof(Componentizable));
-			Example ex = Example.create(master).EnableLike()
-				.ExcludeProperty("Component.SubComponent");
-			crit.Add(ex);
-			IList result = crit.List();
-			Assertion.AssertNotNull(result);
-			Assertion.AssertEquals(3, result.Count);
+			using( ISession s = sessions.OpenSession() )
+			using( ITransaction t = s.BeginTransaction() )
+			{
+				Componentizable master = GetMaster("hibernate", null, "ope%");
+				ICriteria crit = s.CreateCriteria(typeof(Componentizable));
+				Example ex = Example.create(master).EnableLike()
+					.ExcludeProperty("Component.SubComponent");
+				crit.Add(ex);
+				IList result = crit.List();
+				Assert.IsNotNull(result);
+				Assert.AreEqual(3, result.Count);
 
-			master = GetMaster("hibernate", "ORM tool", "fake stuff");
-			crit = s.CreateCriteria(typeof(Componentizable));
-			ex = Example.create(master).EnableLike()
-				.ExcludeProperty("Component.SubComponent.SubName1");
-			crit.Add(ex);
-			result = crit.List();
-			Assertion.AssertNotNull(result);
-			Assertion.AssertEquals(1, result.Count);
-			t.Commit();
-			s.Close();
+				master = GetMaster("hibernate", "ORM tool", "fake stuff");
+				crit = s.CreateCriteria(typeof(Componentizable));
+				ex = Example.create(master).EnableLike()
+					.ExcludeProperty("Component.SubComponent.SubName1");
+				crit.Add(ex);
+				result = crit.List();
+				Assert.IsNotNull(result);
+				Assert.AreEqual(1, result.Count);
+				t.Commit();
+			}
 		}
 
 		private void initData()
 		{
-			ISession s = sessions.OpenSession();
-			Componentizable master = GetMaster("hibernate", "ORM tool", "ORM tool1");
-			s.Save(master);
-			s.Close();
-			s = sessions.OpenSession();
-			master = GetMaster("hibernate", "open source", "open source1");
-			s.Save(master);
-			s.Close();
-			s = sessions.OpenSession();
-			master = GetMaster("hibernate", null, null);
-			s.Save(master);
-			s.Close();
+			using( ISession s = sessions.OpenSession() )
+			{
+				Componentizable master = GetMaster("hibernate", "ORM tool", "ORM tool1");
+				s.Save(master);
+				s.Flush();
+			}
+
+			using( ISession s = sessions.OpenSession() )
+			{
+				Componentizable master = GetMaster("hibernate", "open source", "open source1");
+				s.Save(master);
+				s.Flush();
+			}
+
+			using( ISession s = sessions.OpenSession() )
+			{
+				Componentizable master = GetMaster("hibernate", null, null);
+				s.Save(master);
+				s.Flush();
+			}
 		}
 
 		private void deleteData()
 		{
-			ISession s = sessions.OpenSession();
-			ITransaction t = s.BeginTransaction();
-			s.Delete("from Componentizable");
-			t.Commit();
-			s.Close();
+			using( ISession s = sessions.OpenSession() )
+			using( ITransaction t = s.BeginTransaction() )
+			{
+				s.Delete("from Componentizable");
+				t.Commit();
+			}
 		}
 
 		private Componentizable GetMaster(String name, String subName, String subName1)
