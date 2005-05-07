78dd7caddad69fe8daaf154573fde7c031a302b9
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2005-05-07 14:16:47 +0000

    nunit reports not installed yet since nant upgrade.
    SVN: trunk@1429

--- release-package.bat
+++ release-package.bat
@@ -1,2 +1,2 @@
 NAnt -D:project.config=release -D:sign=true clean build-with-clover test >release-with-clover.log
-NAnt -D:project.config=release -D:sign=true -D:vshik.installed=true -D:nunit2report.installed=true clean-bin package >release-package.log
+NAnt -D:project.config=release -D:sign=true -D:vshik.installed=true -D:nunit2report.installed=false clean-bin package >release-package.log
