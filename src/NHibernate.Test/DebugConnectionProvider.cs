9fe183856dea59fcdd005f41f111ff63fc82cedc
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2005-05-15 17:22:06 +0000

    Fixed HasOpenConnections so it works with ms sql and disposing of the ISession.
    SVN: trunk@1474

--- src/NHibernate.Test/DebugConnectionProvider.cs
+++ src/NHibernate.Test/DebugConnectionProvider.cs
@@ -34,7 +34,32 @@ namespace NHibernate.Test
 		{
 			get
 			{
-				return !connections.IsEmpty;
+				// check to see if all connections that were at one point opened
+				// have been closed through the CloseConnection
+				// method
+				if( connections.IsEmpty )
+				{
+					// there are no connections, either none were opened or
+					// all of the closings went through CloseConnection.
+					return false;
+				}
+				else 
+				{
+					// Disposing of an ISession does not call CloseConnection (should it???)
+					// so a Diposed of ISession will leave an IDbConnection in the list but
+					// the IDbConnection will be closed (atleast with MsSql it works this way).
+					foreach( IDbConnection conn in connections )
+					{
+						if( conn.State!=ConnectionState.Closed )
+						{
+							return true;
+						}	
+					}
+					
+					// all of the connections have been Disposed and were closed that way
+					// or they were Closed through the CloseConnection method.
+					return false;
+				}
 			}
 		}
 
