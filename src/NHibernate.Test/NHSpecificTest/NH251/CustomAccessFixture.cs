52d73992ff4a279e9a575741536c96c22241826f
Sergey Koshcheyev <justme84@users.sourceforge.net>
2005-06-18 18:39:06 +0000

    Removed incorrect comment
    SVN: trunk@1530

--- src/NHibernate.Test/NHSpecificTest/NH251/CustomAccessFixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH251/CustomAccessFixture.cs
@@ -5,27 +5,6 @@ using NUnit.Framework;
 
 namespace NHibernate.Test.NHSpecificTest.NH251
 {
-	// NH-251: Custom access strategy does not work for components
-	// SetUp blows up...
-	// A quick & dirty workaround is to change the code in Binder.PropertiesFromXML()
-	// from:
-	// 		else if ( "component".Equals(name) ) 
-	//		{
-	//			string subpath = path + StringHelper.Dot + propertyName;
-	//			System.Type reflectedClass = ReflectHelper.GetGetter( model.PersistentClazz, propertyName ).ReturnType;
-	//			value = new Component(model);
-	//			BindComponent(subnode, (Component) value, reflectedClass, subpath, true, mappings);
-	//		} 
-	//
-	// ... to:
-	//		else if ( "component".Equals(name) ) 
-	//		{
-	//			string subpath = path + StringHelper.Dot + propertyName;
-	//			System.Type reflectedClass = null;
-	//			value = new Component(model);
-	//			BindComponent(subnode, (Component) value, reflectedClass, subpath, true, mappings);
-	//		} 
-
 	[TestFixture]
 	public class CustomAccessFixture
 	{
