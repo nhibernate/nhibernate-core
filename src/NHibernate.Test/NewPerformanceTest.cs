62956b3d5ea69717e1e6bcd93c8fa6b3c14922c0
Demetris Manikas <demanic@users.sourceforge.net>
2005-07-14 12:37:13 +0000

    ported test from Hibernate
    SVN: trunk@1579

--- src/NHibernate.Test/NewPerformanceTest.cs
+++ src/NHibernate.Test/NewPerformanceTest.cs
@@ -1,5 +1,6 @@
 using System;
-
+using System.Collections;
+using NHibernate.DomainModel;
 using NUnit.Framework;
 
 namespace NHibernate.Test
@@ -18,20 +19,64 @@ namespace NHibernate.Test
 			}
 		}
 
-		[Test]
-		[Ignore("Test not written yet.")]
+		[Test, Explicit]
 		public void Performance() 
 		{
+			for ( int n=2; n<4000; n*=2 ) 
+			{
+			
+				Simple[] simples = new Simple[n];
+				object[] ids = new object[n];
+				for ( int i=0; i<n; i++ ) 
+				{
+					simples[i] = new Simple();
+					simples[i].Init();
+					simples[i].Count = i;
+					ids[i] = (long) i;
+				}
+			
+				ISession s = OpenSession();
+				Prepare(s, simples, ids, n);
+				s.Close();
+			
+				long find = 0;
+				long flush = 0;
+			
+				for ( int i=0; i<100; i++ ) 
+				{
+					s = OpenSession();
+					long time = DateTime.Now.Ticks;
+					IList list = s.Find("from s in class Simple where not s.Name='osama bin laden' and s.Other is null");
+					find += DateTime.Now.Ticks - time;
+					Assert.IsTrue( list.Count == n );
+					time = DateTime.Now.Ticks;
+					s.Flush();
+					flush += DateTime.Now.Ticks - time;
+					s.Close();
+				}
+			
+				Console.WriteLine( "Objects: " + n + " - find(): " + find + "ms / flush(): " + flush + "ms / Ratio: " + ( (float) flush )/find );
+				Console.WriteLine( "Objects: " + n + " flush time per object: " + flush / 100.0 / n );
+				Console.WriteLine( "Objects: " + n + " load time per object: " + find / 100.0 / n );
+				s = OpenSession();
+				Delete(s);
+				s.Close();
+			}
 		}
 
-		private void Prepare() 
+		private void Prepare(ISession s, Simple[] simples, object[] ids, int N)  
 		{
-			//TODO: add details - not a test method
+			for ( int i=0; i<N; i++ ) 
+			{
+				s.Save( simples[i], ids[i] );
+			}
+			s.Flush();
 		}
 
-		private void Delete() 
+		private void Delete(ISession s) 
 		{
-			//TODO: add details - not a test method
+			s.Delete("from s in class Simple");
+			s.Flush();
 		}
 
 	}
