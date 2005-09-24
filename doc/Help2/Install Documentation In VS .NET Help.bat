c693b8c9a376b6222f8cd882908bbd083dbf44e0
Pierre Henri Kuate <kpixel@users.sourceforge.net>
2005-09-24 12:18:48 +0000

    - Add an "installer" to integrate the documentation in VS .NET Help - Update NAnt .build files to make them more "similar"
    SVN: trunk@1709

---
+++ doc/Help2/Install Documentation In VS .NET Help.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ------------------------------------------------------
+echo Install the documentation in Visual Studio .NET help
+echo Make sure that NAnt and H2Reg are correctly installed!
+echo ------------------------------------------------------
+echo ...
+NAnt install
+echo on
