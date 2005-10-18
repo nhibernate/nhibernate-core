162c4087b1b8ebc767e8a1d0d830a607a87edb13
Sergey Koshcheyev <justme84@users.sourceforge.net>
2005-10-18 15:50:06 +0000

    Version update
    SVN: trunk@1754

--- src/NHibernate/Cfg/Environment.cs
+++ src/NHibernate/Cfg/Environment.cs
@@ -28,7 +28,7 @@ namespace NHibernate.Cfg
 	public sealed class Environment
 	{
 		/// <summary></summary>
-		public const string Version = "0.9.0.0";
+		public const string Version = "1.0.0.0";
 
 		/// <summary></summary>
 		public const string ConnectionProvider = "hibernate.connection.provider";
