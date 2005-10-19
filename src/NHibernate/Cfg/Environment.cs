1e819d88abe9f54fe77876ba23bc3cc5ea75d146
Sergey Koshcheyev <justme84@users.sourceforge.net>
2005-10-19 17:32:04 +0000

    Set version to 1.0.1
    SVN: trunk@1756

--- src/NHibernate/Cfg/Environment.cs
+++ src/NHibernate/Cfg/Environment.cs
@@ -28,7 +28,7 @@ namespace NHibernate.Cfg
 	public sealed class Environment
 	{
 		/// <summary></summary>
-		public const string Version = "1.0.0.0";
+		public const string Version = "1.0.1.0";
 
 		/// <summary></summary>
 		public const string ConnectionProvider = "hibernate.connection.provider";
