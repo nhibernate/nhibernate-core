5eb1a6260225538b7ff37fc3d2276df9f74d33bd
Sergey Koshcheyev <justme84@users.sourceforge.net>
2005-11-10 13:04:40 +0000

    Fixed misspelling in configuration node name
    SVN: trunk@1793

--- src/NHibernate/Cfg/Configuration.cs
+++ src/NHibernate/Cfg/Configuration.cs
@@ -1552,7 +1552,7 @@ namespace NHibernate.Cfg
 
 		private XmlNode GetAppConfigConfigurationNode()
 		{
-			XmlNode node = ConfigurationSettings.GetConfig( "hibernate-config" ) as XmlNode;
+			XmlNode node = ConfigurationSettings.GetConfig( "hibernate-configuration" ) as XmlNode;
 			return node;
 		}
 
