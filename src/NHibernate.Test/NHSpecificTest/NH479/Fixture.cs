7e27648edd43fda005c9e6bd3fbf3eef8745d501
Sergey Koshcheyev <justme84@users.sourceforge.net>
2005-12-08 12:36:37 +0000

    NH-479 test and fix
    SVN: branches/v10branch@1832

---
+++ src/NHibernate.Test/NHSpecificTest/NH479/Fixture.cs
@@ -0,0 +1,48 @@
+using System;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH479
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		public override string BugNumber
+		{
+			get { return "NH479"; }
+		}
+
+		[Test]
+		public void SaveOrUpdateCopyTest()
+		{
+			Main main = new Main();
+			Aggregate aggregate = new Aggregate();
+
+			main.Aggregate = aggregate;
+			aggregate.Main = main;
+
+			using( ISession s = OpenSession() )
+			using( ITransaction t = s.BeginTransaction() )
+			{
+				s.Save( main );
+				t.Commit();
+			}
+
+			using( ISession s = OpenSession() )
+			{
+				using( ITransaction t = s.BeginTransaction() )
+				{
+					s.SaveOrUpdateCopy( main );
+					s.SaveOrUpdateCopy( aggregate );
+					t.Commit();
+				}
+
+				using( ITransaction t = s.BeginTransaction() )
+				{
+					s.Delete( "from Aggregate" );
+					s.Delete( "from Main" );
+					t.Commit();
+				}
+			}
+		}
+	}
+}
