2ce6a45f951c7ed57dffe7e6431180b72ae88ab3
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2005-12-29 18:50:28 +0000

    2 batch files to clean out the obj directory under each csproj for the sln. VS2003 will not compile if VS2005 left files behind in the obj directory.
    SVN: trunk@1855

---
+++ src/NHibernate-2.0.sln.bat
@@ -0,0 +1,5 @@
+del /F /S /Q NHibernate\obj\Debug
+del /F /S /Q NHibernate.DomainModel\obj\Debug
+del /F /S /Q NHibernate.Test\obj\Debug
+del /F /S /Q NHibernate.Test.Performance\obj\Debug
+NHibernate-2.0.sln
