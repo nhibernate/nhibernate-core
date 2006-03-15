ef626684fb511893d8894d017e768c435e17f8a6
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2006-03-15 21:34:11 +0000

    Found on a blog how to configure vs.net 2005 to not use the same folders as vs2003 so now we can have both csproj files open in diff versions at the same time.
    SVN: trunk@1988

--- src/NHibernate-1.1.sln.bat
+++
@@ -1,5 +0,0 @@
-del /F /S /Q NHibernate\obj\Debug NHibernate\bin
-del /F /S /Q NHibernate.DomainModel\obj\Debug NHibernate.DomainModel\bin
-del /F /S /Q NHibernate.Test\obj\Debug NHibernate.Test\bin
-del /F /S /Q NHibernate.Test.Performance\obj\Debug NHibernate.Test.Performance\bin
-NHibernate-1.1.sln
