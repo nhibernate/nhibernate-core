404eed806b08ef7fa012afc3af41114ee1aa0457
Mike Doerfler <mikedoerfler@users.sourceforge.net>
2006-03-26 19:59:05 +0000

    fixed up with name of documentation nant build file.
    SVN: trunk@2000

--- doc/Build.bat
+++ doc/Build.bat
@@ -1,3 +1,3 @@
 rem Open the file "build\build.log" to read NAnt's output
 md build
-NAnt   -D:vshik.installed=true   clean  build > build\build.log
+NAnt -buildfile:reference\Documentation.build  -D:vshik.installed=true   clean  build> build\build.log
