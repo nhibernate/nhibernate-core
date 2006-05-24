5d0628f1079426c50ec5681924207d4b2e939685
Sergey Koshcheyev <justme84@users.sourceforge.net>
2006-05-24 22:41:19 +0000

    Make class public and all methods virtual so that it can be used as a base class for interceptors.
    SVN: trunk@2077

--- src/NHibernate/Cfg/EmptyInterceptor.cs
+++ src/NHibernate/Cfg/EmptyInterceptor.cs
@@ -5,55 +5,46 @@ using NHibernate.Type;
 namespace NHibernate.Cfg
 {
 	[Serializable]
-	internal class EmptyInterceptor : IInterceptor
+	public class EmptyInterceptor : IInterceptor
 	{
-
-		public EmptyInterceptor()
-		{	
-		}
-
-		public void OnDelete( object entity, object id, object[ ] state, string[ ] propertyNames, IType[ ] types )
+		public virtual void OnDelete( object entity, object id, object[ ] state, string[ ] propertyNames, IType[ ] types )
 		{
 		}
 
-		public bool OnFlushDirty( object entity, object id, object[ ] currentState, object[ ] previousState, string[ ] propertyNames, IType[ ] types )
+		public virtual bool OnFlushDirty( object entity, object id, object[ ] currentState, object[ ] previousState, string[ ] propertyNames, IType[ ] types )
 		{
 			return false;
 		}
 
-		public bool OnLoad( object entity, object id, object[ ] state, string[ ] propertyNames, IType[ ] types )
+		public virtual bool OnLoad( object entity, object id, object[ ] state, string[ ] propertyNames, IType[ ] types )
 		{
 			return false;
 		}
 
-		public bool OnSave( object entity, object id, object[ ] state, string[ ] propertyNames, IType[ ] types )
+		public virtual bool OnSave( object entity, object id, object[ ] state, string[ ] propertyNames, IType[ ] types )
 		{
 			return false;
 		}
 
-		public void OnPostFlush( object entity, object id, object[ ] currentState, string[ ] propertyNames, IType[ ] types )
+		public virtual void PostFlush( ICollection entities )
 		{
 		}
 
-		public void PostFlush( ICollection entities )
+		public virtual void PreFlush( ICollection entitites )
 		{
 		}
 
-		public void PreFlush( ICollection entitites )
-		{
-		}
-
-		public object IsUnsaved( object entity )
+		public virtual object IsUnsaved( object entity )
 		{
 			return null;
 		}
 
-		public object Instantiate( System.Type clazz, object id )
+		public virtual object Instantiate( System.Type clazz, object id )
 		{
 			return null;
 		}
 
-		public int[ ] FindDirty( object entity, object id, object[ ] currentState, object[ ] previousState, string[ ] propertyNames, IType[ ] types )
+		public virtual int[] FindDirty( object entity, object id, object[] currentState, object[] previousState, string[] propertyNames, IType[] types )
 		{
 			return null;
 		}
