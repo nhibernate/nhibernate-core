b5f97abfe85627fd51f249dcc5b84566263eae3e
Sergey Koshcheyev <justme84@users.sourceforge.net>
2006-05-25 10:40:51 +0000

    NH-602 - FirebirdClientDriver
    SVN: trunk@2083

--- src/NHibernate/Driver/FirebirdDriver.cs
+++ src/NHibernate/Driver/FirebirdDriver.cs
@@ -1,9 +1,10 @@
 namespace NHibernate.Driver
 {
 	/// <summary>
-	/// A NHibernate Driver for using the FirebirdSql.Data.Firebird DataProvider.
+	/// A NHibernate Driver for using the Firebird data provider located in
+	/// <c>FirebirdSql.Data.FirebirdClient</c> assembly.
 	/// </summary>
-	public class FirebirdDriver : ReflectionBasedDriver
+	public class FirebirdClientDriver : ReflectionBasedDriver
 	{
 		/// <summary>
 		/// Initializes a new instance of the <see cref="FirebirdDriver"/> class.
@@ -11,10 +12,10 @@ namespace NHibernate.Driver
 		/// <exception cref="HibernateException">
 		/// Thrown when the <c>FirebirdSql.Data.Firebird</c> assembly can not be loaded.
 		/// </exception>
-		public FirebirdDriver() : base(
-			"FirebirdSql.Data.Firebird",
-			"FirebirdSql.Data.Firebird.FbConnection",
-			"FirebirdSql.Data.Firebird.FbCommand" )
+		public FirebirdClientDriver() : base(
+			"FirebirdSql.Data.FirebirdClient",
+			"FirebirdSql.Data.FirebirdClient.FbConnection",
+			"FirebirdSql.Data.FirebirdClient.FbCommand" )
 		{
 		}
 
