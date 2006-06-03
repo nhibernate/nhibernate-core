2635969a3a421091d4f8d3c1c1286158220d936e
Sergey Koshcheyev <justme84@users.sourceforge.net>
2006-06-03 17:31:48 +0000

    NH-548
    SVN: branches/v10branch@2133

---
+++ src/NHibernate.Test/NHSpecificTest/NH548/MainObject.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH548
+{
+	public class MainObject
+	{
+		private int _id;
+		private string _name;
+		private ComponentObject _component;
+
+		public MainObject()
+		{
+			_component = new ComponentObject( this );
+		}
+
+		public int ID
+		{
+			get { return _id; }
+		}
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		public ComponentObject Component
+		{
+			get { return _component; }
+		}
+
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+}
