fec5a709206dd108b83403d28d44738d143fcd98
Sergey Koshcheyev <justme84@users.sourceforge.net>
2006-09-21 06:51:44 +0000

    Forgot to add NH-719 tests to SVN.
    SVN: trunk@2248

---
+++ src/NHibernate.Test/NHSpecificTest/NH719/Domain.cs
@@ -0,0 +1,115 @@
+namespace NHibernate.Test.NHSpecificTest.NH719
+{
+    public class A
+    {
+        private int id;
+        private string foo;
+
+        protected A()
+        {
+        }
+
+        public A(int id, string foo)
+        {
+            this.id = id;
+            this.foo = foo;
+        }
+
+        public virtual int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public virtual string Foo
+        {
+            get { return foo; }
+            set { foo = value; }
+        }
+    }
+
+    public class B
+    {
+        private int id;
+        private string foo;
+
+        protected B()
+        {
+        }
+
+        public B(int id, string foo)
+        {
+            this.id = id;
+            this.foo = foo;
+        }
+
+        public virtual int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+        public virtual string Foo
+        {
+            get { return foo; }
+            set { foo = value; }
+        }
+    }
+
+    public class NotCached
+    {
+        private int id;
+        private object owner;
+
+        protected NotCached()
+        {
+        }
+
+        public NotCached(int id, object owner)
+        {
+            this.id = id;
+            this.owner = owner;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public object Owner
+        {
+            get { return owner; }
+            set { owner = value; }
+        }
+    }
+
+    public class Cached
+    {
+        private int id;
+        private object owner;
+
+        protected Cached()
+        {
+        }
+
+        public Cached(int id, object owner)
+        {
+            this.id = id;
+            this.owner = owner;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public object Owner
+        {
+            get { return owner; }
+            set { owner = value; }
+        }
+    }
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH719/Fixture.cs
@@ -0,0 +1,70 @@
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH719
+{
+    [TestFixture]
+    public class Fixture : BugTestCase
+    {
+        public override string BugNumber
+        {
+            get { return "NH719"; }
+        }
+
+        [Test]
+        public void CacheLoadTest()
+        {
+            //Instantiate and setup associations (all needed to generate the error);
+            A a = new A(1, "aaa");
+            B b = new B(2, "bbb");
+            NotCached notCached = new NotCached(1, b);
+            Cached cached = new Cached(1, a);
+
+            try
+            {
+                using (ISession session = sessions.OpenSession())
+                {
+                    session.Save(a);
+                    session.Save(b);
+                    session.Save(notCached);
+                    session.Save(cached);
+
+                    session.Flush();
+                }
+
+                using (ISession session = sessions.OpenSession())
+                {
+                    // runs OK, since it's not cached
+                    NotCached nc = (NotCached)session.Load(typeof(NotCached), 1);
+                    Assert.AreEqual("bbb", ((B)nc.Owner).Foo);
+
+                    // 1st run OK, not yet in cache
+                    Cached ca = (Cached)session.Load(typeof(Cached), 1);
+                    Assert.AreEqual("aaa", ((A)ca.Owner).Foo);
+                }
+
+                // 2nd run fails, when data is read from the cache
+                using (ISession session = sessions.OpenSession())
+                {
+                    // runs OK, since it's not cached
+                    NotCached nc = (NotCached)session.Load(typeof(NotCached), 1);
+                    Assert.AreEqual("bbb", ((B)nc.Owner).Foo);
+
+                    // 2nd run fails, when loaded from in cache
+                    Cached ca = (Cached)session.Load(typeof(Cached), 1);
+                    Assert.AreEqual("aaa", ((A)ca.Owner).Foo);
+                }
+            }
+            finally
+            {
+                using (ISession session = OpenSession())
+                {
+                    session.Delete(notCached);
+                    session.Delete(cached);
+                    session.Delete(a);
+                    session.Delete(b);
+                    session.Flush();
+                }
+            }
+        }
+    }
+}
