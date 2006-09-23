373fd0b4de9e8650132ebcbd9a071b345407a139
Sergey Koshcheyev <justme84@users.sourceforge.net>
2006-09-23 04:36:44 +0000

    Minor changes
    SVN: trunk@2259

--- src/NHibernate.Test/FilterTest/FilterConfig.cs
+++ src/NHibernate.Test/FilterTest/FilterConfig.cs
@@ -5,69 +5,67 @@ using NUnit.Framework;
 
 namespace NHibernate.Test.FilterTest
 {
-    [TestFixture]
-    public class FilterConfig 
-    {
-        string mappingCfg = "NHibernate.Test.FilterTest.FilterMapping.hbm.xml";
+	[TestFixture]
+	public class FilterConfig
+	{
+		string mappingCfg = "NHibernate.Test.FilterTest.FilterMapping.hbm.xml";
+
+		[Test]
+		public void FilterDefinitionsLoadedCorrectly()
+		{
+			Configuration cfg = new Configuration();
+			cfg.AddResource(mappingCfg, this.GetType().Assembly);
+			Assert.AreEqual(cfg.FilterDefinitions.Count, 2);
+
+			Assert.IsTrue(cfg.FilterDefinitions.Contains("LiveFilter"));
+
+			FilterDefinition f = cfg.FilterDefinitions["LiveFilter"] as FilterDefinition;
+
+			Assert.AreEqual(f.ParameterTypes.Count, 1);
+
+			BooleanType t = f.ParameterTypes["LiveParam"] as BooleanType;
+
+			Assert.IsNotNull(t); //ensure that the parameter is the correct type. 
+		}
+
+		[Test]
+		public void FiltersLoaded()
+		{
+			Configuration cfg = new Configuration();
+			cfg.AddResource(mappingCfg, this.GetType().Assembly);
+
+			ISessionFactory factory = cfg.BuildSessionFactory();
+
+			ISession session = factory.OpenSession();
+
+			IFilter filter = session.EnableFilter("LiveFilter");
+
+			Assert.AreEqual(filter.FilterDefinition.FilterName, "LiveFilter");
+
+			filter.SetParameter("LiveParam", true);
+
+			filter.Validate(); // make sure that everything is set up right. 
+
+			IFilter filter2 = session.EnableFilter("LiveFilter2");
+
+			filter2.SetParameter("LiveParam2", "somename");
+
+			filter2.Validate();
+		}
 
 
-        [Test]
-        public void TestFilterDefinitionsLoadedCorrectly() {
-            Configuration cfg = new Configuration();
-            cfg.AddResource(mappingCfg, this.GetType().Assembly);
-            Assert.AreEqual(cfg.FilterDefinitions.Count, 2);
+		[Test]
+		[ExpectedException(typeof(HibernateException))]
+		public void TestFilterThrowsWithNoParameterSet()
+		{
+			Configuration cfg = new Configuration();
+			cfg.AddResource(mappingCfg, this.GetType().Assembly);
 
-            Assert.IsTrue(cfg.FilterDefinitions.Contains("LiveFilter"));
+			ISessionFactory factory = cfg.BuildSessionFactory();
 
-            FilterDefinition f = cfg.FilterDefinitions["LiveFilter"] as FilterDefinition;
-
-            Assert.AreEqual(f.ParameterTypes.Count,1);
-
-            BooleanType t = f.ParameterTypes["LiveParam"] as BooleanType;
-
-            Assert.IsNotNull(t); //ensure that the parameter is the correct type. 
-
-        }
-
-
-        [Test]
-        public void TestFiltersLoaded() {
-            Configuration cfg = new Configuration();
-            cfg.AddResource(mappingCfg, this.GetType().Assembly);
-
-            ISessionFactory factory = cfg.BuildSessionFactory();
-
-            ISession session = factory.OpenSession();
-
-            IFilter filter = session.EnableFilter("LiveFilter");
-
-            Assert.AreEqual(filter.FilterDefinition.FilterName, "LiveFilter");
-
-            filter.SetParameter("LiveParam", true);
-
-            filter.Validate(); // make sure that everything is set up right. 
-
-            IFilter filter2 = session.EnableFilter("LiveFilter2");
-
-            filter2.SetParameter("LiveParam2", "somename");
-
-            filter2.Validate();
-
-        }
-
-
-        [Test]
-        [ExpectedException(typeof(HibernateException))]
-        public void TestFilterThrowsWithNoParameterSet() {
-            Configuration cfg = new Configuration();
-            cfg.AddResource(mappingCfg, this.GetType().Assembly);
-
-            ISessionFactory factory = cfg.BuildSessionFactory();
-
-            ISession session = factory.OpenSession();
-            IFilter filter = session.EnableFilter("LiveFilter");
-            filter.Validate();
-        }
-
-    }
+			ISession session = factory.OpenSession();
+			IFilter filter = session.EnableFilter("LiveFilter");
+			filter.Validate();
+		}
+	}
 }
