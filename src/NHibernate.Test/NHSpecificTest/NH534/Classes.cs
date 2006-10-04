7e3e7c6a91a4e69f8d759a7d392bd8b0773b754f
Sergey Koshcheyev <justme84@users.sourceforge.net>
2006-10-04 06:57:37 +0000

    NH-534 test (just in case the bug comes back in the future)
    SVN: trunk@2309

---
+++ src/NHibernate.Test/NHSpecificTest/NH534/Classes.cs
@@ -0,0 +1,63 @@
+using System;
+using Iesi.Collections;
+
+namespace NHibernate.Test.NHSpecificTest.NH534
+{
+	public class Base
+	{
+		private int objectId;
+		private int versionCounter;
+
+		public virtual int ObjectId
+		{
+			get { return objectId; }
+			set { objectId = value; }
+		}
+
+		public virtual int VersionCounter
+		{
+			get { return versionCounter; }
+			set { versionCounter = value; }
+		}
+	}
+	
+	public class Parent : Base
+	{
+		private ISet children;
+
+		public virtual ISet Children
+		{
+			get { return children; }
+			set { children = value; }
+		}
+
+		public virtual void AddChild(Child child)
+		{
+			if (children == null)
+			{
+				children = new HashedSet();
+			}
+			children.Add(child);
+		}
+	}
+	
+	public class Child : Base
+	{
+		private Parent owner;
+		
+		public Child()
+		{
+		}
+
+		public Child(Parent parent)
+		{
+			parent.AddChild(this);
+		}
+
+		public virtual Parent Owner
+		{
+			get { return owner; }
+			set { owner = value; }
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH534/Fixture.cs
@@ -0,0 +1,36 @@
+using System;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH534
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		public override string BugNumber
+		{
+			get { return "NH534"; }
+		}
+		
+		[Test]
+		public void Bug()
+		{
+			Parent p;
+			Child c;
+			using (ISession s = OpenSession())
+			{
+				p = new Parent();
+				s.Save(p);
+				c = new Child(p);
+				s.Save(c);
+				s.Flush();
+			}
+			
+			using (ISession s = OpenSession())
+			{
+				s.Delete(c);
+				s.Delete(p);
+				s.Flush();
+			}
+		}
+	}
+}
