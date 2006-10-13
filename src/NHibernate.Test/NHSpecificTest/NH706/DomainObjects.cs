94973aea6b7d704bd114d0261456295ef686cdd9
Sergey Koshcheyev <justme84@users.sourceforge.net>
2006-10-13 11:46:50 +0000

    NH-706 test case
    SVN: trunk@2348

---
+++ src/NHibernate.Test/NHSpecificTest/NH706/DomainObjects.cs
@@ -0,0 +1,234 @@
+using System;
+using System.Collections;
+using Iesi.Collections;
+
+namespace NHibernate.Test.NHSpecificTest.NH706
+{
+
+    public class Parent
+    {
+
+        private int _id;
+        private string _name;
+        private ISet _children;
+        private ISet _differentChildren;
+
+        public virtual int ID
+        {
+            get
+            {
+                return this._id;
+            }
+            set
+            {
+                this._id = value;
+            }
+        }
+
+        public virtual string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        public virtual ISet Children
+        {
+            get
+            {
+                return this._children;
+            }
+            set
+            {
+                this._children = value;
+            }
+        }
+
+        public virtual ISet DifferentChildren
+        {
+            get
+            {
+                return this._differentChildren;
+            }
+            set
+            {
+                this._differentChildren = value;
+            }
+        }
+
+        public Parent()
+        {
+            this._children = new HashedSet();
+            this._differentChildren = new HashedSet();
+        }
+
+    }
+
+    public class Child
+    {
+
+        private int _id;
+        private string _name;
+        private Parent _parent;
+        private RelatedObject _relatedObject;
+
+        public virtual int ID
+        {
+            get
+            {
+                return this._id;
+            }
+            set
+            {
+                this._id = value;
+            }
+        }
+
+        public virtual string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        public virtual Parent Parent
+        {
+            get
+            {
+                return this._parent;
+            }
+            set
+            {
+                this._parent = value;
+            }
+        }
+
+        public virtual RelatedObject RelatedObject
+        {
+            get
+            {
+                return this._relatedObject;
+            }
+            set
+            {
+                this._relatedObject = value;
+            }
+        }
+
+    }
+
+    public class DifferentChild
+    {
+
+        private int _id;
+        private string _name;
+        private Parent _parent;
+        private Child _child;
+
+        public virtual int ID
+        {
+            get
+            {
+                return this._id;
+            }
+            set
+            {
+                this._id = value;
+            }
+        }
+
+        public virtual string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        public virtual Parent Parent
+        {
+            get
+            {
+                return this._parent;
+            }
+            set
+            {
+                this._parent = value;
+            }
+        }
+
+        public virtual Child Child
+        {
+            get
+            {
+                return this._child;
+            }
+            set
+            {
+                this._child = value;
+            }
+        }
+
+    }
+
+    public class RelatedObject
+    {
+
+        private int _id;
+        private string _name;
+
+        public virtual int ID
+        {
+            get
+            {
+                return this._id;
+            }
+            set
+            {
+                this._id = value;
+            }
+        }
+
+        public virtual string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+
+    }
+
+    public class ChildComparer : IComparer
+    {
+
+        public int Compare(object x, object y)
+        {
+			RelatedObject relX = ((Child) x).RelatedObject;
+			RelatedObject relY = ((Child) y).RelatedObject;
+
+			int result = relX.Name.CompareTo(relY.Name);
+			return result;
+        }
+
+    }
+
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH706/Fixture.cs
@@ -0,0 +1,95 @@
+using System;
+using Iesi.Collections;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH706
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		[Test]
+		public void Bug()
+		{
+			Parent parent = new Parent();
+			parent.Name = "Parent";
+
+			RelatedObject obj1 = new RelatedObject();
+			obj1.Name = "Related Object 1";
+
+			RelatedObject obj2 = new RelatedObject();
+			obj2.Name = "Related Object 2";
+
+			Child child1 = new Child();
+			child1.Parent = parent;
+			child1.RelatedObject = obj1;
+			child1.Name = "Child 1";
+
+			Child child2 = new Child();
+			child2.Parent = parent;
+			child2.RelatedObject = obj2;
+			child2.Name = "Child 2";
+
+			parent.Children = new SortedSet(new ChildComparer());
+			parent.Children.Add(child1);
+			parent.Children.Add(child2);
+
+			DifferentChild dc1 = new DifferentChild();
+			dc1.Parent = parent;
+			dc1.Name = "Different Child 1";
+			dc1.Child = child1;
+			
+			DifferentChild dc2 = new DifferentChild();
+			dc2.Parent = parent;
+			dc2.Name = "Different Child 2";
+			dc2.Child = child2;
+
+			parent.DifferentChildren = new HashedSet();
+			parent.DifferentChildren.Add(dc1);
+			parent.DifferentChildren.Add(dc2);
+
+			using (ISession session = OpenSession())
+			{
+				session.Save(obj1);
+				session.Save(obj2);
+
+				session.Save(parent);
+				
+				session.Save(child1);
+				session.Save(child2);
+
+				session.Save(dc1);
+				session.Save(dc2);
+				session.Flush();
+			}
+
+			int dcId = 0;
+			using (ISession session = OpenSession())
+			{
+				Parent loadedParent = (Parent) session.Get(typeof(Parent), 1);
+				NHibernateUtil.Initialize(loadedParent.DifferentChildren);
+				foreach (DifferentChild dc in loadedParent.DifferentChildren)
+				{
+					dcId = dc.ID;
+					break;
+				}
+			}
+			
+			using (ISession session = OpenSession())
+			{
+				DifferentChild dc = (DifferentChild) session.Get(typeof (DifferentChild), dcId);
+			}
+			
+			using (ISession session = OpenSession())
+			{
+				session.Delete(dc1);
+				session.Delete(dc2);
+				session.Delete(child1);
+				session.Delete(child2);
+				session.Delete(parent);
+				session.Delete(obj1);
+				session.Delete(obj2);
+				session.Flush();
+			}
+		}
+	}
+}
