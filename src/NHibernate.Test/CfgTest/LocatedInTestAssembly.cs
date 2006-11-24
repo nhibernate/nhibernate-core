d0455f226bc2cff83636c01649db928e09a266c2
Sergey Koshcheyev <justme84@users.sourceforge.net>
2006-11-24 14:33:03 +0000

    Committing forgotten file
    SVN: trunk@2499

---
+++ src/NHibernate.Test/CfgTest/LocatedInTestAssembly.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace NHibernate.Test.CfgTest
+{
+	/// <summary>
+	/// This class should be located in NHibernate.Test assembly
+	/// (used from ConfigurationFixture)
+	/// </summary>
+	public class LocatedInTestAssembly
+	{
+		private int id;
+		
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+	}
+}
