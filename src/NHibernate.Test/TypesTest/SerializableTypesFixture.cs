2d361238f11af7e1fc0838b81c98451866fd9eb5
Ayende Rahien <ayende@ayende.com>
2006-11-25 22:23:45 +0000

    Simplified Test
    SVN: trunk@2502

--- src/NHibernate.Test/TypesTest/SerializableTypesFixture.cs
+++ src/NHibernate.Test/TypesTest/SerializableTypesFixture.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Reflection;
 using NUnit.Framework;
 using NHibernate.Type;
 
@@ -17,70 +17,12 @@ namespace NHibernate.Test.TypesTest
 		[Test]
 		public void EachEmbeddedBasicTypeIsSerializable()
 		{
-			IType ntp = NHibernateUtil.AnsiChar;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.AnsiString;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Binary;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.BinaryBlob;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Boolean;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Byte;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Character;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Class;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.CultureInfo;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Date;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.DateTime;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Decimal;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Double;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Guid;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Int16;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Int32;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Int64;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Object;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.SByte;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Serializable;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Single;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.String;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.StringClob;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Ticks;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Time;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.TimeSpan;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.Timestamp;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.TrueFalse;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.UInt16;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.UInt32;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.UInt64;
-			NHAssert.IsSerializable(ntp);
-			ntp = NHibernateUtil.YesNo;
-			NHAssert.IsSerializable(ntp);
+			FieldInfo[] builtInCustomTypes = typeof(NHibernateUtil).GetFields(BindingFlags.Public | BindingFlags.Static);
+			foreach (FieldInfo fieldInfo in builtInCustomTypes)
+			{
+				IType ntp = (IType)fieldInfo.GetValue(null);
+				NHAssert.IsSerializable(ntp, fieldInfo.Name + " is not serializable");
+			}
 		}
 	}
 }
