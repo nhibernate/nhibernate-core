2108c9bebd24ffdf5a71be8d0f82229733c8c3bd
Sergey Koshcheyev <justme84@users.sourceforge.net>
2007-01-26 09:01:42 +0000

    NH-876 test
    SVN: trunk@2593

---
+++ src/NHibernate.Test/CacheTest/QueryCacheFixture.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using NHibernate.Cfg;
+using NHibernate.DomainModel;
+using NUnit.Framework;
+
+namespace NHibernate.Test.CacheTest
+{
+	[TestFixture]
+	public class QueryCacheFixture : TestCase
+	{
+		protected override IList Mappings
+		{
+			get { return new string[] { "Simple.hbm.xml" }; }
+		}
+		protected override void Configure(Configuration cfg)
+		{
+			cfg.SetProperty(Cfg.Environment.UseQueryCache, "true");
+		}
+
+		[Test]
+		public void QueryCacheWithNullParameters()
+		{
+			Simple simple = new Simple();
+			
+			using (ISession s = OpenSession())
+			{
+				s.Save(simple, 1L);
+				s.Flush();
+			}
+
+			using (ISession s = OpenSession())
+			{
+				s
+					.CreateQuery("from Simple s where s = :s or s.Name = :name or s.Address = :address")
+					.SetEntity("s", s.Load(typeof(Simple), 1L))
+					.SetString("name", null)
+					.SetString("address", null)
+					.SetCacheable(true)
+					.UniqueResult();
+
+				// Run a second time, just to test the query cache
+				object result = s
+					.CreateQuery("from Simple s where s = :s or s.Name = :name or s.Address = :address")
+					.SetEntity("s", simple)
+					.SetString("name", null)
+					.SetString("address", null)
+					.SetCacheable(true)
+					.UniqueResult();
+
+				Assert.IsNotNull(result);
+				Assert.AreEqual(1L, (long) s.GetIdentifier(result));
+			}
+
+			using (ISession s = OpenSession())
+			{
+				s.Delete("from Simple");
+				s.Flush();
+			}
+		}
+	}
+}
