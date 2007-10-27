a9686b18644883cf6292d104047086fe7a1723e1
Fabio Maulo <fabiomaulo@gmail.com>
2007-10-27 15:45:25 +0000

    Minor
    SVN: trunk@3075

--- src/NHibernate.Test/NHSpecificTest/NH892/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH892/Fixture.cs
@@ -14,46 +14,42 @@ namespace NHibernate.Test.NHSpecificTest.NH892
 			get { return "NH892"; }
 		}
 
-		protected override void OnSetUp()
-		{
-			session = OpenSession();
-		}
-
-		protected override void OnTearDown()
-		{
-			session.Close();
-		}
-
-		[Test]
-		[Ignore("Not fixed yet. Test currently fails")]
+		[Test, Ignore("Not Fixed yet")]
 		public void SelectWithWhereClause()
 		{
-			User user1 = new User();
-			user1.UserName = "User1";
-			session.Save(user1);
+			using (session = OpenSession())
+			{
+				User user1 = new User();
+				user1.UserName = "User1";
+				session.Save(user1);
 
-			User user2 = new User();
-			user2.UserName = "User2";
-			session.Save(user2);
+				User user2 = new User();
+				user2.UserName = "User2";
+				session.Save(user2);
 
-			BlogPost post = new BlogPost();
-			post.Title = "Post 1";
-			post.Poster = user1;
-			session.Save(post);
+				BlogPost post = new BlogPost();
+				post.Title = "Post 1";
+				post.Poster = user1;
+				session.Save(post);
 
-			session.Flush();
-			session.Clear();
+				session.Flush();
+				session.Clear();
 
-			User poster = (User)session.Get(typeof(User), user1.ID);
+				User poster = (User) session.Get(typeof (User), user1.ID);
 
-			string hql = "from BlogPost b where b.Poster = :poster";
-			IList list = session.CreateQuery(hql)
-				.SetParameter("poster", poster)
-				.List();
-			Assert.AreEqual(1, list.Count);
-			BlogPost retrievedPost = (BlogPost)list[0];
-			Assert.AreEqual(post.ID, retrievedPost.ID);
-			Assert.AreEqual(user1.ID, retrievedPost.Poster.ID);
+				string hql = "from BlogPost b where b.Poster = :poster";
+				IList list = session.CreateQuery(hql).SetParameter("poster", poster).List();
+				Assert.AreEqual(1, list.Count);
+				BlogPost retrievedPost = (BlogPost) list[0];
+				Assert.AreEqual(post.ID, retrievedPost.ID);
+				Assert.AreEqual(user1.ID, retrievedPost.Poster.ID);
+
+				session.Delete(post);
+				session.Delete(user1);
+				session.Delete(user2);
+				session.Flush();
+				session.Close();
+			}
 		}
 	}
 }
