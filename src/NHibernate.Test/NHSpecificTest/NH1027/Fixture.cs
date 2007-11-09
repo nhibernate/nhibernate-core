694ac2e9214411300981ad299e47103f72addb00
Karl Chu <karlchu@users.sourceforge.net>
2007-11-09 22:44:14 +0000

    Added test for NH-1027 and NH-1028 (posted by Ayende Rahien)
    SVN: trunk@3088

---
+++ src/NHibernate.Test/NHSpecificTest/NH1027/Fixture.cs
@@ -0,0 +1,38 @@
+using NHibernate.Dialect;
+using NHibernate.SqlCommand;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1027
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		public override string BugNumber
+		{
+            get { return "NH1027"; }
+		}
+
+        private void AssertDialect()
+        {
+            if (!(Dialect is MsSql2005Dialect))
+                Assert.Ignore("This test is specific for MsSql2005Dialect");
+        }
+
+	    [Test]
+		[Ignore("Not yet fixed")]
+	    public void CanMakeCriteriaQueryAcrossBothAssociations()
+	    {
+	        AssertDialect();
+	        using(ISession s = OpenSession())
+	        {
+	            ICriteria criteria = s.CreateCriteria(typeof(Item));
+	            criteria.CreateCriteria("Ships", "s", JoinType.InnerJoin)
+                    .Add(Expression.Expression.Eq("s.Id", 15));
+                criteria.CreateCriteria("Containers", "c", JoinType.LeftOuterJoin)
+                 .Add(Expression.Expression.Eq("c.Id", 15));
+	            criteria.SetMaxResults(2);
+	            criteria.List();
+	        }
+	    }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1027/ManyToManyAssoc.cs
@@ -0,0 +1,117 @@
+using System;
+using Iesi.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH1027
+{
+	public class Item
+	{
+	    private int id;
+	    private string name;
+        private ISet<Container> containers = new HashedSet<Container>();
+        private ISet<Ship> ships = new HashedSet<Ship>();
+
+	    public virtual int Id
+	    {
+	        get { return id; }
+	        set { id = value; }
+	    }
+
+        public virtual string Name
+	    {
+	        get { return name; }
+	        set { name = value; }
+	    }
+
+        public virtual string Name1
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual string Name2
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual string Name3
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual string Name4
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual string Name5
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual string Name6
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual string Name7
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+	    public virtual ISet<Container> Containers
+	    {
+	        get { return containers; }
+	        set { containers = value; }
+	    }
+
+        public virtual ISet<Ship> Ships
+	    {
+	        get { return ships; }
+	        set { ships = value; }
+	    }
+	}
+
+    public class Container
+    {
+        private int id;
+        private string name;
+
+        public virtual string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+
+    public class Ship
+    {
+        private int id;
+        private string name;
+
+        public virtual string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+    }
+}
