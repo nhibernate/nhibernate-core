f0985448c6244fa28683d22089d22a2a6d42f3d9
Fabio Maulo <fabiomaulo@gmail.com>
2008-03-02 22:08:25 +0000

    Minor (reformatted)
    SVN: trunk@3335

--- src/NHibernate.Test/NHSpecificTest/NH1027/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH1027/Fixture.cs
@@ -9,30 +9,30 @@ namespace NHibernate.Test.NHSpecificTest.NH1027
 	{
 		public override string BugNumber
 		{
-            get { return "NH1027"; }
+			get { return "NH1027"; }
 		}
 
-        private void AssertDialect()
-        {
-            if (!(Dialect is MsSql2005Dialect))
-                Assert.Ignore("This test is specific for MsSql2005Dialect");
-        }
+		private void AssertDialect()
+		{
+			if (!(Dialect is MsSql2005Dialect))
+				Assert.Ignore("This test is specific for MsSql2005Dialect");
+		}
 
-	    [Test]
+		[Test]
 		[Ignore("Not completely fixed yet.")]
-	    public void CanMakeCriteriaQueryAcrossBothAssociations()
-	    {
-	        AssertDialect();
-	        using(ISession s = OpenSession())
-	        {
-	            ICriteria criteria = s.CreateCriteria(typeof(Item));
-	            criteria.CreateCriteria("Ships", "s", JoinType.InnerJoin)
-                    .Add(Expressions.Expression.Eq("s.Id", 15));
-                criteria.CreateCriteria("Containers", "c", JoinType.LeftOuterJoin)
-                 .Add(Expressions.Expression.Eq("c.Id", 15));
-	            criteria.SetMaxResults(2);
-	            criteria.List();
-	        }
-	    }
+		public void CanMakeCriteriaQueryAcrossBothAssociations()
+		{
+			AssertDialect();
+			using (ISession s = OpenSession())
+			{
+				ICriteria criteria = s.CreateCriteria(typeof(Item));
+				criteria.CreateCriteria("Ships", "s", JoinType.InnerJoin)
+							.Add(Expressions.Expression.Eq("s.Id", 15));
+				criteria.CreateCriteria("Containers", "c", JoinType.LeftOuterJoin)
+				 .Add(Expressions.Expression.Eq("c.Id", 15));
+				criteria.SetMaxResults(2);
+				criteria.List();
+			}
+		}
 	}
 }
