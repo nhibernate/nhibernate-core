c1be700ce8057c5152285e90d01bb565437d8f56
Fabio Maulo <fabiomaulo@gmail.com>
2008-03-17 14:50:09 +0000

    Test for on-delete in a simple parent-child relationship
    SVN: trunk@3395

---
+++ src/NHibernate.Test/Ondelete/ParentChildFixture.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using NHibernate.Cfg;
+using NHibernate.Stat;
+using NUnit.Framework;
+
+namespace NHibernate.Test.Ondelete
+{
+	[TestFixture, Ignore("Not supported yet.")]
+	public class ParentChildFixture : TestCase
+	{
+		protected override string MappingsAssembly
+		{
+			get { return "NHibernate.Test"; }
+		}
+
+		protected override IList Mappings
+		{
+			get { return new string[] { "Ondelete.ParentChild.hbm.xml" }; }
+		}
+
+		protected override void Configure(Configuration cfg)
+		{
+			cfg.SetProperty(Environment.GenerateStatistics, "true");
+		}
+
+		[Test]
+		public void ParentChildCascade()
+		{
+			ISession s = OpenSession();
+			ITransaction t = s.BeginTransaction();
+			Parent p = new Parent("foo");
+			p.Children.Add(new Child(p, "foo1"));
+			p.Children.Add(new Child(p, "foo2"));
+			Parent q = new Parent("bar");
+			q.Children.Add(new Child(q, "bar1"));
+			q.Children.Add(new Child(q, "bar2"));
+			s.Save(p);
+			s.Save(q);
+			t.Commit();
+			s.Close();
+
+			IStatistics statistics = sessions.Statistics;
+			statistics.Clear();
+
+			s = OpenSession();
+			t = s.BeginTransaction();
+			IList<Parent> l = s.CreateQuery("from Parent").List<Parent>();
+			p = l[0];
+			q = l[1];
+			statistics.Clear();
+
+			s.Delete(p);
+			s.Delete(q);
+			t.Commit();
+
+			Assert.AreEqual(2, statistics.EntityDeleteCount);
+			Assert.AreEqual(2, statistics.PrepareStatementCount);
+
+			t = s.BeginTransaction();
+			IList names = s.CreateQuery("from Parent p").List();
+			Assert.AreEqual(0, names.Count);
+
+			t.Commit();
+			s.Close();
+		}
+	}
+}
