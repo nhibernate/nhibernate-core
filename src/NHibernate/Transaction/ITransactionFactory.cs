e7046fe9ee22d40490501b9ed9ae42a0abfae288
Dario Quintana <darioquintana@users.sourceforge.net>
2008-04-03 03:23:58 +0000

    minors
    SVN: trunk@3461

--- src/NHibernate/Transaction/ITransactionFactory.cs
+++ src/NHibernate/Transaction/ITransactionFactory.cs
@@ -1,10 +1,15 @@
 using System.Collections;
+using NHibernate;
 using NHibernate.Engine;
 
 namespace NHibernate.Transaction
 {
 	/// <summary>
-	/// An abstract factory for <c>ITransaction</c> instances.
+	/// An abstract factory for <see cref="ITransaction"/> instances.
+	/// Concrete implementations are specified by <c>transaction.factory_class</c> 
+	/// configuration property.
+	/// 
+	/// Implementors must be threadsafe and should declare a public default constructor. 
 	/// <seealso cref="ITransactionContext"/>
 	/// </summary>
 	public interface ITransactionFactory
