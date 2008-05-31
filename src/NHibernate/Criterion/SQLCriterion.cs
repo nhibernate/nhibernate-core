5c1b4d4eab57ccf243d7199fd32109e4b429e740
Ayende Rahien <ayende@ayende.com>
2008-05-31 10:45:15 +0000

    Part of NH-1329 doesn't make sense in the branch, reverted.
    SVN: branches/2.0.x@3515

--- src/NHibernate/Criterion/SQLCriterion.cs
+++ src/NHibernate/Criterion/SQLCriterion.cs
@@ -32,7 +32,6 @@ namespace NHibernate.Criterion
 
 		public override SqlString ToSqlString(ICriteria criteria, ICriteriaQuery criteriaQuery, IDictionary<string, IFilter> enabledFilters)
 		{
-			criteriaQuery.AddUsedTypedValues(GetTypedValues(criteria, criteriaQuery));
 			return _sql.Replace("{alias}", criteriaQuery.GetSQLAlias(criteria));
 		}
 
