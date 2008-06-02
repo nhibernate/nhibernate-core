c509eed17be0c0e681d01fd36213a8956d6bc9a7
Fabio Maulo <fabiomaulo@gmail.com>
2008-06-02 13:18:45 +0000

    Reformatted and commented different behavior from Hb
    SVN: trunk@3519

--- src/NHibernate/Action/CollectionUpdateAction.cs
+++ src/NHibernate/Action/CollectionUpdateAction.cs
@@ -1,20 +1,19 @@
+using System;
+using NHibernate.Cache;
+using NHibernate.Collection;
+using NHibernate.Engine;
+using NHibernate.Impl;
+using NHibernate.Persister.Collection;
+
 namespace NHibernate.Action
 {
-	using System;
-	using Cache;
-	using Collection;
-	using Engine;
-	using Impl;
-	using NHibernate.Persister.Collection;
-
 	[Serializable]
 	public sealed class CollectionUpdateAction : CollectionAction
 	{
 		private readonly bool emptySnapshot;
 
-		public CollectionUpdateAction(
-			IPersistentCollection collection, ICollectionPersister persister,
-			object key, bool emptySnapshot, ISessionImplementor session)
+		public CollectionUpdateAction(IPersistentCollection collection, ICollectionPersister persister, object key,
+		                              bool emptySnapshot, ISessionImplementor session)
 			: base(persister, collection, key, session)
 		{
 			this.emptySnapshot = emptySnapshot;
@@ -31,22 +30,29 @@ namespace NHibernate.Action
 			if (!collection.WasInitialized)
 			{
 				if (!collection.HasQueuedAdds)
+				{
 					throw new AssertionFailure("no queued adds");
+				}
 				//do nothing - we only need to notify the cache...
 			}
 			else if (!affectedByFilters && collection.Empty)
 			{
 				if (!emptySnapshot)
+				{
 					persister.Remove(id, session);
+				}
 			}
 			else if (collection.NeedsRecreate(persister))
 			{
 				if (affectedByFilters)
 				{
-					throw new HibernateException("cannot recreate collection while filter is enabled: " + MessageHelper.InfoString(persister, id, persister.Factory));
+					throw new HibernateException("cannot recreate collection while filter is enabled: "
+					                             + MessageHelper.InfoString(persister, id, persister.Factory));
 				}
 				if (!emptySnapshot)
+				{
 					persister.Remove(id, session);
+				}
 				persister.Recreate(collection, id, session);
 			}
 			else
@@ -68,16 +74,16 @@ namespace NHibernate.Action
 
 		public override void AfterTransactionCompletion(bool success)
 		{
+			// NH Different behavior: to support unlocking collections from the cache.(r3260)
 			if (Persister.HasCache)
 			{
 				CacheKey ck = new CacheKey(Key, Persister.KeyType, Persister.Role, Session.EntityMode, Session.Factory);
 
-				if (success )
+				if (success)
 				{
 					// we can't disassemble a collection if it was uninitialized 
 					// or detached from the session
-					if (Collection.WasInitialized 
-						&& Session.PersistenceContext.ContainsCollection(Collection))
+					if (Collection.WasInitialized && Session.PersistenceContext.ContainsCollection(Collection))
 					{
 						bool put = Persister.Cache.AfterUpdate(ck, Collection.Disassemble(Persister), null, Lock);
 
