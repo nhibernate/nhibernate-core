d843498c077dfb3603dc18f3efcc39ccb7a8e70a
Fabio Maulo <fabiomaulo@gmail.com>
2008-06-07 20:16:13 +0000

    - Added ThreadSafeDictionary generic dictionary - Added base class to test multi-threads
    SVN: trunk@3524

---
+++ src/NHibernate.Test/MultiThreadRunner.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Threading;
+
+namespace NHibernate.Test
+{
+	public class MultiThreadRunner<T>
+	{
+		public delegate void ExecuteAction(T subject);
+		private readonly int numThreads;
+		private readonly ExecuteAction[] actions;
+		private readonly Random rnd = new Random();
+		private bool running;
+
+		public MultiThreadRunner(int numThreads, ExecuteAction[] actions)
+		{
+			if(numThreads < 1)
+			{
+				throw new ArgumentOutOfRangeException("numThreads",numThreads,"Must be GT 1");
+			}
+			if (actions == null || actions.Length == 0)
+			{
+				throw new ArgumentNullException("actions");
+			}
+			foreach (ExecuteAction action in actions)
+			{
+				if(action==null)
+					throw new ArgumentNullException("actions", "null delegate");
+			}
+			this.numThreads = numThreads;
+			this.actions = actions;
+		}
+
+		public void Run(T subjectInstance)
+		{
+			running = true;
+			Thread[] t = new Thread[numThreads];
+			for (int i = 0; i < numThreads; i++)
+			{
+				t[i] = new Thread(ThreadProc);
+				t[i].Name = i.ToString();
+				t[i].Start(subjectInstance);
+				if (i > 2)
+					Thread.Sleep(30);
+			}
+
+			Thread.Sleep(1000);
+
+			// Tell the threads to shut down, then wait until they all
+			// finish.
+			running = false;
+			for (int i = 0; i < numThreads; i++)
+			{
+				t[i].Join();
+			}
+		}
+
+		private void ThreadProc(object arg)
+		{
+			T subjectInstance = (T) arg;
+			while (running)
+			{
+				int actionIdx = rnd.Next(0, actions.Length);
+				actions[actionIdx](subjectInstance);
+			}
+		}
+	}
+}
---
+++ src/NHibernate.Test/UtilityTest/ThreadSafeDictionaryFixture.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using log4net;
+using NHibernate.Util;
+using NUnit.Framework;
+
+namespace NHibernate.Test.UtilityTest
+{
+	[TestFixture]
+	public class ThreadSafeDictionaryFixture
+	{
+		private static readonly ILog log = LogManager.GetLogger(typeof(ThreadSafeDictionaryFixture));
+
+		private readonly Random rnd = new Random();
+		private int read, write;
+
+		[Test, Explicit]
+		public void MultiThreadAccess()
+		{
+			MultiThreadRunner<IDictionary<int, int>>.ExecuteAction[] actions =
+				new MultiThreadRunner<IDictionary<int, int>>.ExecuteAction[]
+					{
+						delegate(IDictionary<int, int> d)
+							{
+								try
+								{
+									log.DebugFormat("T{0} Add", Thread.CurrentThread.Name);
+									write++;
+									d.Add(rnd.Next(), rnd.Next());
+								}
+								catch (ArgumentException)
+								{
+									// duplicated key
+								}
+							}, 
+						delegate(IDictionary<int, int> d)
+						 	{
+								log.DebugFormat("T{0} ContainsKey", Thread.CurrentThread.Name);
+					   		read++;
+					   		d.ContainsKey(rnd.Next());
+					   	}, 
+						delegate(IDictionary<int, int> d)
+			   	   	{
+								log.DebugFormat("T{0} Remove", Thread.CurrentThread.Name);
+			   	   		write++;
+			   	   		d.Remove(rnd.Next());
+			   	   	}, 
+						delegate(IDictionary<int, int> d)
+	   	   	   	{
+								log.DebugFormat("T{0} TryGetValue", Thread.CurrentThread.Name);
+	   	   	   		read++;
+	   	   	   		int val;
+	   	   	   		d.TryGetValue(rnd.Next(), out val);
+	   	   	   	}, 
+						delegate(IDictionary<int, int> d)
+ 	   	   	   	{
+ 	   	   	   		try
+ 	   	   	   		{
+									log.DebugFormat("T{0} get_this[]", Thread.CurrentThread.Name);
+ 	   	   	   			read++;
+ 	   	   	   			int val = d[rnd.Next()];
+ 	   	   	   		}
+ 	   	   	   		catch (KeyNotFoundException)
+ 	   	   	   		{
+ 	   	   	   			// not foud key
+ 	   	   	   		}
+ 	   	   	   	}, 
+						delegate(IDictionary<int, int> d)
+ 	   	   	   	{
+								log.DebugFormat("T{0} set_this[]", Thread.CurrentThread.Name);
+ 	   	   	   		write++;
+ 	   	   	   		d[rnd.Next()] = rnd.Next();
+ 	   	   	   	},
+						delegate(IDictionary<int, int> d)
+							{
+								log.DebugFormat("T{0} Keys", Thread.CurrentThread.Name);
+								read++;
+								IEnumerable<int> e = d.Keys;
+							},
+						delegate(IDictionary<int, int> d)
+					   	{
+								log.DebugFormat("T{0} Values", Thread.CurrentThread.Name);
+					   		read++;
+					   		IEnumerable<int> e = d.Values;
+					   	}, 
+						delegate(IDictionary<int, int> d)
+			   	   	{
+								log.DebugFormat("T{0} GetEnumerator", Thread.CurrentThread.Name);
+			   	   		read++;
+			   	   		foreach (KeyValuePair<int, int> pair in d)
+			   	   		{
+			   	   			
+			   	   		}
+			   	   	},
+					};
+			MultiThreadRunner<IDictionary<int, int>> mtr = new MultiThreadRunner<IDictionary<int, int>>(20, actions);
+			IDictionary<int, int> wrapper = new ThreadSafeDictionary<int, int>(new Dictionary<int, int>());
+			mtr.Run(wrapper);
+			log.DebugFormat("{0} reads, {1} writes -- elements {2}", read, write, wrapper.Count);
+		}
+	}
+}
