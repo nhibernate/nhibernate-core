2ed495fd054b24ce1a2415c91dd2fb642190b8d8
Fabio Maulo <fabiomaulo@gmail.com>
2008-06-27 23:01:58 +0000

    Added forgotten test in the solution (NH1347) Source reformatted and cleaned.
    SVN: trunk@3582

--- src/NHibernate/Dialect/SQLiteDialect.cs
+++ src/NHibernate/Dialect/SQLiteDialect.cs
@@ -1,140 +1,138 @@
-using System;
 using System.Data;
 using System.Text;
-using System.Text.RegularExpressions;
 using NHibernate.SqlCommand;
 using NHibernate.Util;
 
 namespace NHibernate.Dialect
 {
-    /// <summary>
-    /// A SQL dialect for SQLite.
-    /// </summary>
-    /// <remarks>
-    /// <p>
-    /// Author: <a href="mailto:ib@stalker.ro"> Ioan Bizau </a>
-    /// </p>
-    /// </remarks>
-    public class SQLiteDialect : Dialect
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public SQLiteDialect()
-        {
-            RegisterColumnType(DbType.Binary, "BLOB");
-            RegisterColumnType(DbType.Byte, "INTEGER");
-            RegisterColumnType(DbType.Int16, "INTEGER");
-            RegisterColumnType(DbType.Int32, "INTEGER");
-            RegisterColumnType(DbType.Int64, "INTEGER");
-            RegisterColumnType(DbType.SByte, "INTEGER");
-            RegisterColumnType(DbType.UInt16, "INTEGER");
-            RegisterColumnType(DbType.UInt32, "INTEGER");
-            RegisterColumnType(DbType.UInt64, "INTEGER");
-            RegisterColumnType(DbType.Currency, "NUMERIC");
-            RegisterColumnType(DbType.Decimal, "NUMERIC");
-            RegisterColumnType(DbType.Double, "NUMERIC");
-            RegisterColumnType(DbType.Single, "NUMERIC");
-            RegisterColumnType(DbType.VarNumeric, "NUMERIC");
-            RegisterColumnType(DbType.String, "TEXT");
-            RegisterColumnType(DbType.AnsiStringFixedLength, "TEXT");
-            RegisterColumnType(DbType.StringFixedLength, "TEXT");
+	/// <summary>
+	/// A SQL dialect for SQLite.
+	/// </summary>
+	/// <remarks>
+	/// <p>
+	/// Author: <a href="mailto:ib@stalker.ro"> Ioan Bizau </a>
+	/// </p>
+	/// </remarks>
+	public class SQLiteDialect : Dialect
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public SQLiteDialect()
+		{
+			RegisterColumnType(DbType.Binary, "BLOB");
+			RegisterColumnType(DbType.Byte, "INTEGER");
+			RegisterColumnType(DbType.Int16, "INTEGER");
+			RegisterColumnType(DbType.Int32, "INTEGER");
+			RegisterColumnType(DbType.Int64, "INTEGER");
+			RegisterColumnType(DbType.SByte, "INTEGER");
+			RegisterColumnType(DbType.UInt16, "INTEGER");
+			RegisterColumnType(DbType.UInt32, "INTEGER");
+			RegisterColumnType(DbType.UInt64, "INTEGER");
+			RegisterColumnType(DbType.Currency, "NUMERIC");
+			RegisterColumnType(DbType.Decimal, "NUMERIC");
+			RegisterColumnType(DbType.Double, "NUMERIC");
+			RegisterColumnType(DbType.Single, "NUMERIC");
+			RegisterColumnType(DbType.VarNumeric, "NUMERIC");
+			RegisterColumnType(DbType.String, "TEXT");
+			RegisterColumnType(DbType.AnsiStringFixedLength, "TEXT");
+			RegisterColumnType(DbType.StringFixedLength, "TEXT");
 
-            RegisterColumnType(DbType.DateTime, "DATETIME");
-            RegisterColumnType(DbType.Time, "DATETIME");
-            RegisterColumnType(DbType.Boolean, "INTEGER");
-            RegisterColumnType(DbType.Guid, "UNIQUEIDENTIFIER");
-        }
+			RegisterColumnType(DbType.DateTime, "DATETIME");
+			RegisterColumnType(DbType.Time, "DATETIME");
+			RegisterColumnType(DbType.Boolean, "INTEGER");
+			RegisterColumnType(DbType.Guid, "UNIQUEIDENTIFIER");
+		}
 
-        public override string IdentitySelectString
-        {
-            get { return "select last_insert_rowid()"; }
-        }
+		public override string IdentitySelectString
+		{
+			get { return "select last_insert_rowid()"; }
+		}
 
-        public override SqlString AppendIdentitySelectToInsert(SqlString insertSql)
-        {
-            return insertSql.Append("; " + IdentitySelectString);
-        }
+		public override SqlString AppendIdentitySelectToInsert(SqlString insertSql)
+		{
+			return insertSql.Append("; " + IdentitySelectString);
+		}
 
-        public override bool SupportsInsertSelectIdentity
-        {
-            get { return true; }
-        }
+		public override bool SupportsInsertSelectIdentity
+		{
+			get { return true; }
+		}
 
-        public override bool HasAlterTable
-        {
-            get { return false; }
-        }
+		public override bool HasAlterTable
+		{
+			get { return false; }
+		}
 
-        public override bool DropConstraints
-        {
-            get { return false; }
-        }
+		public override bool DropConstraints
+		{
+			get { return false; }
+		}
 
-        public override string ForUpdateString
-        {
-            get { return string.Empty; }
-        }
+		public override string ForUpdateString
+		{
+			get { return string.Empty; }
+		}
 
-        public override bool SupportsSubSelects
-        {
-            get { return true; }
-        }
+		public override bool SupportsSubSelects
+		{
+			get { return true; }
+		}
 
-        protected override bool SupportsIfExistsBeforeTableName
-        {
-            get { return true; }
-        }
+		protected override bool SupportsIfExistsBeforeTableName
+		{
+			get { return true; }
+		}
 
-        public override bool HasDataTypeInIdentityColumn
-        {
-            get { return false; }
-        }
+		public override bool HasDataTypeInIdentityColumn
+		{
+			get { return false; }
+		}
 
-        public override bool SupportsIdentityColumns
-        {
-            get { return true; }
-        }
+		public override bool SupportsIdentityColumns
+		{
+			get { return true; }
+		}
 
-    	public override bool SupportsLimit
-    	{
-    		get { return true; }
-    	}
+		public override bool SupportsLimit
+		{
+			get { return true; }
+		}
 
-        public override string IdentityColumnString
-        {
-            get
-            {
-                // identity columns in sqlite are marked as being integer primary key
-                // the primary key part will be put in at the end of the create table,
-                // so just the integer part is needed here
-                return "integer";
-            }
-        }
+		public override string IdentityColumnString
+		{
+			get
+			{
+				// identity columns in sqlite are marked as being integer primary key
+				// the primary key part will be put in at the end of the create table,
+				// so just the integer part is needed here
+				return "integer";
+			}
+		}
 
-        public override string Qualify(string catalog, string schema, string table)
-        {
-            StringBuilder qualifiedName = new StringBuilder();
+		public override string Qualify(string catalog, string schema, string table)
+		{
+			StringBuilder qualifiedName = new StringBuilder();
 
-            if (!string.IsNullOrEmpty(catalog))
-            {
-                if (catalog.EndsWith(CloseQuote.ToString()))
-                    catalog = catalog.Substring(0, catalog.Length - 1);
-                qualifiedName.Append(catalog).Append(StringHelper.Underscore);
-            }
-            if (!string.IsNullOrEmpty(schema))
-            {
-                if (schema.StartsWith(OpenQuote.ToString()))
-                    schema = schema.Substring(0, schema.Length-1);
-                qualifiedName.Append(schema).Append(StringHelper.Underscore);
-            }
+			if (!string.IsNullOrEmpty(catalog))
+			{
+				if (catalog.EndsWith(CloseQuote.ToString()))
+					catalog = catalog.Substring(0, catalog.Length - 1);
+				qualifiedName.Append(catalog).Append(StringHelper.Underscore);
+			}
+			if (!string.IsNullOrEmpty(schema))
+			{
+				if (schema.StartsWith(OpenQuote.ToString()))
+					schema = schema.Substring(0, schema.Length - 1);
+				qualifiedName.Append(schema).Append(StringHelper.Underscore);
+			}
 
-            if (table.StartsWith(OpenQuote.ToString()))
-                table = table.Substring(1, table.Length - 1);
+			if (table.StartsWith(OpenQuote.ToString()))
+				table = table.Substring(1, table.Length - 1);
 
-            return qualifiedName.Append(table).ToString();
-	
-        }
+			return qualifiedName.Append(table).ToString();
+
+		}
 
 		public override string NoColumnsInsertString
 		{
@@ -148,20 +146,20 @@ namespace NHibernate.Dialect
 		/// <param name="limit">Maximum number of rows to be returned by the query</param>
 		/// <param name="offset">Offset of the first row to process in the result set</param>
 		/// <returns>A new SqlString that contains the <c>LIMIT</c> clause.</returns>
-        public override SqlString GetLimitString(SqlString querySqlString, int offset, int limit)
-        {
-            SqlStringBuilder pagingBuilder = new SqlStringBuilder();
-            pagingBuilder.Add(querySqlString);
-            pagingBuilder.Add(" limit ");
-            pagingBuilder.Add(limit.ToString());
+		public override SqlString GetLimitString(SqlString querySqlString, int offset, int limit)
+		{
+			SqlStringBuilder pagingBuilder = new SqlStringBuilder();
+			pagingBuilder.Add(querySqlString);
+			pagingBuilder.Add(" limit ");
+			pagingBuilder.Add(limit.ToString());
 
-            if (offset > 0)
-            {
-                pagingBuilder.Add(", ");
-                pagingBuilder.Add(offset.ToString());
-            }
+			if (offset > 0)
+			{
+				pagingBuilder.Add(", ");
+				pagingBuilder.Add(offset.ToString());
+			}
 
-            return pagingBuilder.ToSqlString();
-        }
-    }
+			return pagingBuilder.ToSqlString();
+		}
+	}
 }
