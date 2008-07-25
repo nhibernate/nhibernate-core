438e5e4121b39a44213a5c8f98248d4c015aa197
Fabio Maulo <fabiomaulo@gmail.com>
2008-07-25 06:15:55 +0000

    NH-1408 not an issue in Trunk
    SVN: trunk@3663

---
+++ src/NHibernate.Test/NHSpecificTest/NH1408/DbResourceKey.cs
@@ -0,0 +1,37 @@
+namespace NHibernate.Test.NHSpecificTest.NH1408
+{
+	public class DbResourceKey : Entity
+	{
+		private readonly object[] keys = new object[2];
+		private DbResource resource;
+
+		protected DbResourceKey() {}
+
+		public DbResourceKey(string resourceId) : this(resourceId, null) {}
+
+		public DbResourceKey(string resourceId, string language)
+		{
+			keys[0] = resourceId;
+			keys[1] = language;
+		}
+
+		// used just for hibernate mappings
+		public virtual object Key0
+		{
+			get { return keys[0]; }
+			set { keys[0] = value; }
+		}
+
+		public virtual object Key1
+		{
+			get { return keys[1]; }
+			set { keys[1] = value; }
+		}
+
+		public virtual DbResource Resource
+		{
+			get { return resource; }
+			internal set { resource = value; }
+		}
+	}
+}
