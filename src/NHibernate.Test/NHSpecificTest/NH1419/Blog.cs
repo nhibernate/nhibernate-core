5f834ae9107da7de9e146f5ff12e18d96980a348
Fabio Maulo <fabiomaulo@gmail.com>
2008-07-31 15:59:20 +0000

    Demonstration NH-1419 (not an issue)
    SVN: branches/2.0.x@3692

---
+++ src/NHibernate.Test/NHSpecificTest/NH1419/Blog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH1419
+{
+	[Serializable]
+	public class Blog
+	{
+		private Guid id;
+		private string name;
+		private IList<Entry> entries;
+
+		public Guid ID
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public IList<Entry> Entries
+		{
+			get { return entries; }
+			set { entries = value; }
+		}
+
+		public void AddEntry(Entry entry)
+		{
+			if (entries == null)
+			{
+				entries = new List<Entry>();
+			}
+			entry.Blog = this;
+			entries.Add(entry);
+		}
+	}
+}
