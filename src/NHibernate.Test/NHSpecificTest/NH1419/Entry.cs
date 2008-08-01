0d7bc590604d335e16fbbd39c322063d53d36d64
Fabio Maulo <fabiomaulo@gmail.com>
2008-08-01 04:26:00 +0000

    Included test only to be sure we have the same behavior (in the trunk we have a different implementation of collections and cascade). Same behavior confirmed.
    SVN: trunk@3694

---
+++ src/NHibernate.Test/NHSpecificTest/NH1419/Entry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH1419
+{
+	[Serializable]
+	public class Entry
+	{
+		private Guid id;
+		private Blog blog;
+		private string subject;
+
+		public Guid ID
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		public Blog Blog
+		{
+			get { return blog; }
+			set { blog = value; }
+		}
+
+		public string Subject
+		{
+			get { return subject; }
+			set { subject = value; }
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1419/Fixture.cs
@@ -0,0 +1,58 @@
+﻿using NHibernate;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1419
+{
+	[TestFixture]
+	public class Tests : BugTestCase
+	{
+		[Test]
+		public void Bug()
+		{
+			using (ISession session = OpenSession())
+			{
+				ITransaction transaction = session.BeginTransaction();
+
+				Blog blog = new Blog();
+				blog.Name = "Test Blog 1";
+
+				Entry entry = new Entry();
+				entry.Subject = "Test Entry 1";
+
+				blog.AddEntry(entry);
+
+				session.SaveOrUpdate(blog);
+
+				transaction.Commit();
+			}
+			using (ISession session = OpenSession())
+			{
+				ITransaction transaction = session.BeginTransaction();
+				session.Delete("from Blog");
+				transaction.Commit();
+			}
+		}
+
+		[Test]
+		public void WithEmptyCollection()
+		{
+			using (ISession session = OpenSession())
+			{
+				ITransaction transaction = session.BeginTransaction();
+
+				Blog blog = new Blog();
+				blog.Name = "Test Blog 1";
+
+				session.SaveOrUpdate(blog);
+
+				transaction.Commit();
+			}
+			using (ISession session = OpenSession())
+			{
+				ITransaction transaction = session.BeginTransaction();
+				session.Delete("from Blog");
+				transaction.Commit();
+			}
+		}
+	}
+}
