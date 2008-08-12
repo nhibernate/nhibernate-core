c18dbd2fc8a15aa4291fb2edf5a980fe1cc80ae0
Chad Lee <chadly69@users.sourceforge.net>
2008-08-12 15:40:30 +0000

    Adding skeleton for NHibernate.Linq project to NHibernate-3.5.sln.
    SVN: trunk@3698

---
+++ src/NHibernate.Linq/NHibernateExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace NHibernate.Linq
+{
+	/// <summary>
+	/// Provides a static method that enables LINQ syntax for NHibernate Queries.
+	/// </summary>
+	public static class NHibernateExtensions
+	{
+		/// <summary>
+		/// Creates a new <see cref="T:NHibernate.Linq.NHibernateQueryProvider"/> object used to evaluate an expression tree.
+		/// </summary>
+		/// <typeparam name="T">An NHibernate entity type.</typeparam>
+		/// <param name="session">An initialized <see cref="T:NHibernate.ISession"/> object.</param>
+		/// <returns>An <see cref="T:NHibernate.Linq.NHibernateQueryProvider"/> used to evaluate an expression tree.</returns>
+		public static IQueryable<T> Linq<T>(this ISession session)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
