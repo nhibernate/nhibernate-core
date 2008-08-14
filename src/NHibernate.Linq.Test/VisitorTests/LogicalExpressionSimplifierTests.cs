3d577978cf8cafd1d8603587799c2ff3bd83cd5b
Tuna Toksoz <tehlike@users.sourceforge.net>
2008-08-14 17:04:36 +0000

    Added logical expression reducer.
    SVN: trunk@3702

---
+++ src/NHibernate.Linq.Test/VisitorTests/LogicalExpressionSimplifierTests.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using NHibernate.Criterion;
+using NHibernate.Linq.Visitors;
+using NUnit.Framework;
+using Expression=System.Linq.Expressions.Expression;
+
+namespace NHibernate.Linq.Test.VisitorTests
+{
+	[TestFixture]
+	public class LogicalExpressionSimplifierTests
+	{
+		[Test]
+		public void CanReduceAndAlsoWithFalse()
+		{
+			var left = Expression.Constant(false);
+			var right = Expression.GreaterThan(Expression.Constant(3), Expression.Constant(1));
+			var ex1 = Expression.AndAlso(left, right);
+			var ex2 = Expression.AndAlso(right, left);
+			var reduced1 = Reduce(ex1) as ConstantExpression;
+			var reduced2 = Reduce(ex2) as ConstantExpression;
+			Assert.IsNotNull(reduced1);
+			Assert.IsNotNull(reduced2);
+			Assert.IsFalse((bool)reduced1.Value);
+			Assert.IsFalse((bool)reduced2.Value);
+
+			Console.WriteLine("Original expressions\n");
+			Console.WriteLine(ex1.ToString());
+			Console.WriteLine(ex2.ToString());
+			Console.WriteLine("");
+			Console.WriteLine("Reduced expressions\n");
+			Console.WriteLine(reduced1.ToString());
+			Console.WriteLine(reduced2.ToString());
+		}
+
+		[Test]
+		public void CanReduceOrWithTrue()
+		{
+			var left = Expression.Constant(true);
+			var right = Expression.GreaterThan(Expression.Constant(3), Expression.Constant(1));
+			var ex1 = Expression.OrElse(left, right);
+			var ex2 = Expression.OrElse(right, left);
+			var reduced1 = Reduce(ex1) as ConstantExpression;
+			var reduced2 = Reduce(ex2) as ConstantExpression;
+			Assert.IsNotNull(reduced1);
+			Assert.IsNotNull(reduced2);
+			Assert.IsTrue((bool)reduced1.Value);
+			Assert.IsTrue((bool)reduced2.Value);
+
+			Console.WriteLine("Original expressions");
+			Console.WriteLine("");
+			Console.WriteLine(ex1.ToString());
+			Console.WriteLine(ex2.ToString());
+			Console.WriteLine("");
+			Console.WriteLine("Reduced expressions");
+			Console.WriteLine("");
+			Console.WriteLine(reduced1.ToString());
+			Console.WriteLine(reduced2.ToString());
+		}
+
+		[Test]
+		public void CanReduceEqualWithTrue()
+		{
+			var left = Expression.Constant(true);
+			var right = Expression.GreaterThan(Expression.Constant(3), Expression.Constant(1));
+			var ex1 = Expression.Equal(left, right);
+			var ex2 = Expression.Equal(right, left);
+			var reduced1 = Reduce(ex1);
+			var reduced2 = Reduce(ex2);
+			Assert.IsNotNull(reduced1);
+			Assert.IsNotNull(reduced2);
+			Assert.AreEqual(reduced1,right);
+			Assert.AreEqual(reduced2, right);
+
+			Console.WriteLine("Original expressions");
+			Console.WriteLine("");
+			Console.WriteLine(ex1.ToString());
+			Console.WriteLine(ex2.ToString());
+			Console.WriteLine("");
+			Console.WriteLine("Reduced expressions");
+			Console.WriteLine("");
+			Console.WriteLine(reduced1.ToString());
+			Console.WriteLine(reduced2.ToString());
+		}
+		[Test]
+		public void CanReduceEqualWithFalse()
+		{
+			var left = Expression.Constant(false);
+			var right = Expression.GreaterThan(Expression.Constant(3), Expression.Constant(1));
+			var ex1 = Expression.Equal(left, right);
+			var ex2 = Expression.Equal(right, left);
+			var reduced1 = Reduce(ex1) as UnaryExpression;
+			var reduced2 = Reduce(ex2) as UnaryExpression;
+			Assert.IsNotNull(reduced1);
+			Assert.IsNotNull(reduced2);
+			Assert.AreEqual(ExpressionType.Not, reduced1.NodeType);
+			Assert.AreEqual(ExpressionType.Not, reduced2.NodeType);
+			Assert.AreEqual(right,reduced1.Operand);
+			Assert.AreEqual(right, reduced2.Operand);
+
+			Console.WriteLine("Original expressions");
+			Console.WriteLine("");
+			Console.WriteLine(ex1.ToString());
+			Console.WriteLine(ex2.ToString());
+			Console.WriteLine("");
+			Console.WriteLine("Reduced expressions");
+			Console.WriteLine("");
+			Console.WriteLine(reduced1.ToString());
+			Console.WriteLine(reduced2.ToString());
+		}
+
+		[Test]
+		public void CanReduceNotEqualWithTrue()
+		{
+			var left = Expression.Constant(true);
+			var right = Expression.GreaterThan(Expression.Constant(3), Expression.Constant(1));
+			var ex1 = Expression.NotEqual(left, right);
+			var ex2 = Expression.NotEqual(right, left);
+			var reduced1 = Reduce(ex1) as UnaryExpression;
+			var reduced2 = Reduce(ex2) as UnaryExpression;
+	
+			Assert.IsNotNull(reduced1);
+			Assert.IsNotNull(reduced2);
+			Assert.AreEqual(ExpressionType.Not, reduced1.NodeType);
+			Assert.AreEqual(ExpressionType.Not, reduced2.NodeType);
+			Assert.AreEqual(right, reduced1.Operand);
+			Assert.AreEqual(right, reduced2.Operand);
+
+			Console.WriteLine("Original expressions");
+			Console.WriteLine("");
+			Console.WriteLine(ex1.ToString());
+			Console.WriteLine(ex2.ToString());
+			Console.WriteLine("");
+			Console.WriteLine("Reduced expressions");
+			Console.WriteLine(reduced1.ToString());
+			Console.WriteLine(reduced2.ToString());
+		}
+		[Test]
+		public void CanReduceNotEqualWithFalse()
+		{
+			var left = Expression.Constant(false);
+			var right = Expression.GreaterThan(Expression.Constant(3), Expression.Constant(1));
+			var ex1 = Expression.NotEqual(left, right);
+			var ex2 = Expression.NotEqual(right, left);
+			var reduced1 = Reduce(ex1);
+			var reduced2 = Reduce(ex2);
+			Assert.IsNotNull(reduced1);
+			Assert.IsNotNull(reduced2);
+			Assert.AreEqual(reduced1, right);
+			Assert.AreEqual(reduced2, right);
+
+			Console.WriteLine("Original expressions\n");
+			Console.WriteLine("");
+			Console.WriteLine(ex1.ToString());
+			Console.WriteLine(ex2.ToString());
+			Console.WriteLine("");
+			Console.WriteLine("Reduced expressions\n");
+			Console.WriteLine("");
+			Console.WriteLine(reduced1.ToString());
+			Console.WriteLine(reduced2.ToString());
+		}		
+		
+
+		[Test]
+		public void CanReduceNested()
+		{
+			var trueExpression = Expression.Constant(true);
+			var falseExpression = Expression.Constant(false);
+			var testExpression = Expression.Equal(Expression.Equal(Expression.Equal(falseExpression, falseExpression), falseExpression),falseExpression);
+			var reduced = Reduce(testExpression) as ConstantExpression;
+			Assert.IsNotNull(reduced);
+			Assert.IsTrue((bool)reduced.Value);
+
+			Console.WriteLine("Original expressions\n");
+			Console.WriteLine(testExpression.ToString());
+
+			Console.WriteLine("Reduced expressions\n");
+			Console.WriteLine(reduced.ToString());
+		}
+		[Test]
+		public void CanReduceNested2()
+		{
+			var trueExpression = Expression.Constant(true);
+			var falseExpression = Expression.Constant(false);
+			var testExpression = Expression.Equal(Expression.Equal(Expression.NotEqual(trueExpression, falseExpression), falseExpression), falseExpression);
+			var reduced = Reduce(testExpression) as ConstantExpression;
+			Assert.IsNotNull(reduced);
+			Assert.IsTrue((bool)reduced.Value);
+
+			Console.WriteLine("Original expressions\n");
+			Console.WriteLine(testExpression.ToString());
+
+			Console.WriteLine("Reduced expressions\n");
+			Console.WriteLine(reduced.ToString());
+		}
+
+		protected Expression Reduce(Expression expr)
+		{
+			var reducer = new LogicalExpressionReducer();
+			return reducer.Visit(expr);
+		}
+
+	}
+}
---
+++ src/NHibernate.Linq/Visitors/LogicalExpressionReducer.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace NHibernate.Linq.Visitors
+{
+	/// <summary>
+	/// Reduces the expression using logical simplification
+	/// Please note that it doesn't evaluate the binary expressions with constant operands
+	/// </summary>
+
+	//May be used for animal.Offspring.Any() == true
+	//Eventhough it is almost unnecessary to reduce not(not(true)) kind of expression, but simplified expressions are gold.
+	public class LogicalExpressionReducer:ExpressionVisitor
+	{
+		protected override Expression VisitBinary(BinaryExpression expr)
+		{
+			bool modified;
+			Expression e = ProcessBinaryExpression(expr.Left, expr.Right, expr.NodeType, out modified);
+			if (modified)
+				return Visit(e);
+			e = ProcessBinaryExpression(expr.Right, expr.Left, expr.NodeType, out modified);
+			if(modified)
+				return Visit(e);
+			return expr;
+
+		}
+
+		protected override Expression VisitUnary(UnaryExpression u)
+		{
+			switch(u.NodeType)
+			{
+				case ExpressionType.Not:
+					if (u.Operand.NodeType == ExpressionType.Not)
+						return Visit(((UnaryExpression) (u.Operand)).Operand);
+					else if (u.Operand.NodeType == ExpressionType.Equal)
+					{
+						var binaryExpression = u.Operand as BinaryExpression;
+						return Visit(Expression.NotEqual(binaryExpression.Left, binaryExpression.Right));
+					}
+					else if (u.Operand.NodeType == ExpressionType.Equal)
+					{
+						var binaryExpression = u.Operand as BinaryExpression;
+						return Visit(Expression.Equal(binaryExpression.Left, binaryExpression.Right));
+					}
+					else if(u.Operand.NodeType==ExpressionType.Constant)
+					{
+						var constantExpression = u.Operand as ConstantExpression;
+						return Visit(Expression.Constant(!((bool)constantExpression.Value)));
+					}
+					break;
+			}
+			return u;
+		}
+
+		private Expression ProcessBinaryExpression(Expression exprToCompare, Expression exprToReturn,
+														ExpressionType nodeType, out bool modified)
+		{
+			modified = false;
+
+			var visitor = new BooleanConstantFinder();
+			visitor.Visit(exprToCompare);
+
+			if (!visitor.Constant.HasValue)
+				return null;
+
+			bool constantValue = visitor.Constant.Value;
+			switch (nodeType)
+			{
+				case ExpressionType.Equal:
+					modified = true;
+					return constantValue ? exprToReturn : Expression.Not(exprToReturn);
+				case ExpressionType.NotEqual:
+					modified = true;
+					return constantValue ? Expression.Not(exprToReturn) : exprToReturn;
+				case ExpressionType.Or:
+				case ExpressionType.OrElse:
+					modified = true;
+					return constantValue ? Expression.Constant(true) : exprToReturn;
+				case ExpressionType.And:
+				case ExpressionType.AndAlso:
+					modified = true;
+					return constantValue ? exprToReturn : Expression.Constant(false);
+				default:
+					return null;
+			}
+
+		}
+
+		class BooleanConstantFinder : ExpressionVisitor
+		{
+			private bool isNestedBinaryExpression;
+
+			public bool? Constant { get; private set; }
+
+			protected override Expression VisitConstant(ConstantExpression c)
+			{
+				if (c.Type == typeof(bool) && !isNestedBinaryExpression)
+					Constant = (bool)c.Value;
+				return c;
+			}
+
+			protected override Expression VisitBinary(BinaryExpression b)
+			{
+				isNestedBinaryExpression = true;
+				return b;
+			}
+		}
+	}
+}
