bdd60632d7c16b7ba4b3986c0687eac3dd267a9e
Fabio Maulo <fabiomaulo@gmail.com>
2008-10-10 04:33:22 +0000

    another initial test to support Xml entityMode
    SVN: trunk@3827

---
+++ src/NHibernate.Test/EntityModeTest/Xml/Basic/XmlFixture.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Xml;
+using NHibernate.Cfg;
+using NUnit.Framework;
+
+namespace NHibernate.Test.EntityModeTest.Xml.Basic
+{
+	[TestFixture, Ignore("Not supported yet.")]
+	public class XmlFixture : TestCase
+	{
+		protected override string MappingsAssembly
+		{
+			get { return "NHibernate.Test"; }
+		}
+
+		#region Overrides of TestCase
+
+		protected override IList Mappings
+		{
+			get
+			{
+				return new[]
+				       	{
+				       		"EntityModeTest.Xml.Basic.Account.hbm.xml", "EntityModeTest.Xml.Basic.AB.hbm.xml",
+				       		"EntityModeTest.Xml.Basic.Employer.hbm.xml"
+				       	};
+			}
+		}
+
+		#endregion
+
+		protected override void Configure(Configuration configuration)
+		{
+			cfg.SetProperty(Environment.DefaultEntityMode, EntityModeHelper.ToString(EntityMode.Xml));
+		}
+
+		[Test]
+		public void CompositeId()
+		{
+			const string xml =
+@"<a id='1'>
+	<x>foo bar</x>
+	<b bId='1' aId='1>foo foo</b>
+	<b bId='2' aId='1>bar bar</b>
+</a>";
+
+			XmlElement a=null;
+			ISession s = OpenSession();
+			ITransaction t = s.BeginTransaction();
+			s.Persist("A", a);
+			t.Commit();
+			s.Close();
+
+			s = OpenSession();
+			t = s.BeginTransaction();
+			//a = (XmlElement)s.CreateCriteria("A").UniqueResult();
+			Assert.AreEqual(a.GetElementsByTagName("b").Count, 2);
+			Print(a);
+			//s.Delete("A", a);
+			t.Commit();
+			s.Close();
+		}
+
+		public static void Print(XmlElement elt)
+		{
+			//XMLHelper.dump( elt );
+		}
+	}
+}
