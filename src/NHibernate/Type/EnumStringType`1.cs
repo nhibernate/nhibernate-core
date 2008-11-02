228106b3649075d728b844f6279a7e21bcd57852
Tuna Toksoz <tehlike@users.sourceforge.net>
2008-11-02 17:16:59 +0000

    Adding EnumStringType with generic arguments in order developer not to write specific classes for enums. (Fix NH-1544)
    SVN: trunk@3882

---
+++ src/NHibernate/Type/EnumStringType`1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NHibernate.Type
+{
+	public class EnumStringType<T> : EnumStringType
+	{
+		public EnumStringType():base(typeof(T))
+		{
+			
+		}
+	}
+}
