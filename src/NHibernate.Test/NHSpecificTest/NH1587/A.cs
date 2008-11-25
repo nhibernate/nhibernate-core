3c4b1137305b8e88844e3bd921a90fc6dad346cd
Fabio Maulo <fabiomaulo@gmail.com>
2008-11-25 13:30:17 +0000

    Test for NH-1587 (only test because there are some existing failing tests to fix)
    SVN: trunk@3924

---
+++ src/NHibernate.Test/NHSpecificTest/NH1587/A.cs
@@ -0,0 +1,7 @@
+namespace NHibernate.Test.NHSpecificTest.NH1587
+{
+	public class A
+	{
+		public virtual string Description { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1587/Fixture.cs
@@ -0,0 +1,37 @@
+using log4net.Config;
+using log4net.Core;
+using NHibernate.Cfg;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1587
+{
+	[TestFixture]
+	public class Fixture
+	{
+		[Test, Ignore("Not fixed yet")]
+		public void Bug()
+		{
+			XmlConfigurator.Configure();
+			var cfg = new Configuration();
+			if (TestConfigurationHelper.hibernateConfigFile != null)
+				cfg.Configure(TestConfigurationHelper.hibernateConfigFile);
+			cfg.AddResource("NHibernate.Test.NHSpecificTest.NH1587.Mappings.hbm.xml", GetType().Assembly);
+			cfg.Configure();
+
+			bool useOptimizer= false;
+			using (var ls = new LogSpy("NHibernate.Tuple.Entity.PocoEntityTuplizer"))
+			{
+				cfg.BuildSessionFactory();
+				foreach (LoggingEvent loggingEvent in ls.Appender.GetEvents())
+				{
+					if (((string)(loggingEvent.MessageObject)).StartsWith("Create Instantiator using optimizer"))
+					{
+						useOptimizer = true;
+						break;
+					}
+				}
+			}
+			Assert.That(useOptimizer);
+		}
+	}
+}
