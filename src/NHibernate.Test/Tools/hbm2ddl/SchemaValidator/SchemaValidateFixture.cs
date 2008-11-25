abfc5e8a1a5c1686541885cde059cfff8d512d06
Fabio Maulo <fabiomaulo@gmail.com>
2008-11-25 21:31:27 +0000

    avoid the name of catalog in test (each one can use a different configuration)
    SVN: trunk@3926

--- src/NHibernate.Test/Tools/hbm2ddl/SchemaValidator/SchemaValidateFixture.cs
+++ src/NHibernate.Test/Tools/hbm2ddl/SchemaValidator/SchemaValidateFixture.cs
@@ -6,6 +6,7 @@ using System.Text;
 using NHibernate.Cfg;
 using NHibernate.Tool.hbm2ddl;
 using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
 
 namespace NHibernate.Test.Tools.hbm2ddl.SchemaValidator
 {
@@ -26,7 +27,6 @@ namespace NHibernate.Test.Tools.hbm2ddl.SchemaValidator
 		}
 
 		[Test]
-		[ExpectedException(typeof(HibernateException), ExpectedMessage = "Missing column: Name in nhibernate.dbo.Version")]
 		public void ShouldNotVerifyModifiedTable()
 		{
 			string resource1 = "NHibernate.Test.Tools.hbm2ddl.SchemaValidator.1_Version.hbm.xml";
@@ -39,7 +39,14 @@ namespace NHibernate.Test.Tools.hbm2ddl.SchemaValidator
 				v2cfg.AddInputStream(stream);
 			new NHibernate.Tool.hbm2ddl.SchemaExport(v1cfg).Execute(true, true, false, true);
 			var v2schemaValidator = new NHibernate.Tool.hbm2ddl.SchemaValidator((v2cfg));
-			v2schemaValidator.Validate();
+			try
+			{
+				v2schemaValidator.Validate();				
+			}
+			catch (HibernateException e)
+			{
+				Assert.That(e.Message, Text.StartsWith("Missing column: Name"));
+			}
 		}
 	}
 }
