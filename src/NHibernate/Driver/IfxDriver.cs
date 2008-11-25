6cc47ea8485fbc8cbe2b2c0a0dd59c9c95fcce12
Fabio Maulo <fabiomaulo@gmail.com>
2008-11-25 21:50:57 +0000

    Fix NH-1586 (Informix drive by Robert Sosnowski)
    SVN: trunk@3928

---
+++ src/NHibernate/Driver/IfxDriver.cs
@@ -0,0 +1,37 @@
+namespace NHibernate.Driver
+{
+	/// <summary>
+	/// A NHibernate Driver for using the Informix DataProvider
+	/// </summary>
+	public class IfxDriver : ReflectionBasedDriver
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="IfxDriver"/> class.
+		/// </summary>
+		/// <exception cref="HibernateException">
+		/// Thrown when the <c>IBM.Data.Informix</c> assembly can not be loaded.
+		/// </exception>
+		public IfxDriver()
+			: base(
+				"IBM.Data.Informix",
+				"IBM.Data.Informix.IfxConnection",
+				"IBM.Data.Informix.IfxCommand")
+		{
+		}
+
+		public override bool UseNamedPrefixInSql
+		{
+			get { return false; }
+		}
+
+		public override bool UseNamedPrefixInParameter
+		{
+			get { return false; }
+		}
+
+		public override string NamedPrefix
+		{
+			get { return ":"; }
+		}
+	}
+}
