ec9d9cfbd336e2773837b7fb3f859d6c8afc40fa
Tuna Toksoz <tehlike@users.sourceforge.net>
2008-12-12 14:40:38 +0000

    Added tests for NH1349 to support it is not an issue.
    SVN: trunk@3945

---
+++ src/NHibernate.Test/NHSpecificTest/NH1349/Fixture.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace NHibernate.Test.NHSpecificTest.NH1349
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			using(var session=this.OpenSession())
+			{
+				using(var tran=session.BeginTransaction())
+				{
+					string name = "fabio";
+					string accNum = DateTime.Now.Ticks.ToString(); ;
+					Services newServ = new Services();
+					newServ.AccountNumber = accNum;
+					newServ.Name = name + " person";
+					newServ.Type = (new Random()).Next(0, 9).ToString();
+
+					session.Save(newServ);
+					tran.Commit();
+				}
+			}
+		}
+		protected override void OnTearDown()
+		{
+			using (var session = this.OpenSession())
+			{
+				using (var tran = session.BeginTransaction())
+				{
+					session.Delete("from Services");
+					tran.Commit();
+				}
+			}
+		}
+
+		[Test]
+		public void Can_page_with_formula_property()
+		{
+			using (var session = this.OpenSession())
+			{
+				using(var tran=session.BeginTransaction())
+				{
+					IList ret = session.CreateCriteria(typeof(Services)).SetMaxResults(5).List(); //this breaks
+					Assert.That(ret.Count,Is.EqualTo(1));
+				}
+			}
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1349/Services.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NHibernate.Test.NHSpecificTest.NH1349
+{
+	public class Services
+	{
+		private int id;
+		private int companyCount;
+		private string accountNumber;
+		private string name;
+		private string type;
+
+		public Services()
+		{ }
+
+		public virtual int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		public virtual string AccountNumber
+		{
+			get { return accountNumber; }
+			set { accountNumber = value; }
+		}
+		public virtual string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		public virtual string Type
+		{
+			get { return type; }
+			set { type = value; }
+		}
+		public virtual int CompanyCount
+		{
+			get { return companyCount; }
+			set { companyCount = value; }
+		}
+		public virtual string ToString()
+		{
+			return (this.id + "] [" + this.accountNumber + "]  [" + this.name + "] [" + this.type + "]  [" + this.CompanyCount + "]");
+
+		}
+	}
+
+}
