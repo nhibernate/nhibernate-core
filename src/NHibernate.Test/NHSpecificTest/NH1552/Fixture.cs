fb5348c4b95811fe88b39ed7030efaedd0aca204
Tuna Toksoz <tehlike@users.sourceforge.net>
2008-12-12 15:00:59 +0000

    Added tests to support NH-1552 is no longer an issue after applying NH-1603(MSSQL 2005 Paging patch)
    SVN: trunk@3947

---
+++ src/NHibernate.Test/NHSpecificTest/NH1552/Fixture.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace NHibernate.Test.NHSpecificTest.NH1552
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			using (var session = this.OpenSession())
+			{
+				using (var tran = session.BeginTransaction())
+				{
+					MyClass newServ = new MyClass();
+					newServ.Name = "tuna";
+					session.Save(newServ);
+					tran.Commit();
+				}
+			}
+		}
+		protected override void OnTearDown()
+		{
+			using (var session = this.OpenSession())
+			{
+				using (var tran = session.BeginTransaction())
+				{
+					session.Delete("from MyClass");
+					tran.Commit();
+				}
+			}
+		}
+
+		[Test]
+		public void Paging_with_sql_works_as_expected()
+		{
+			using (var session = this.OpenSession())
+			{
+				using (var tran = session.BeginTransaction())
+				{
+					string sql = "select * from MyClass";
+					IList list = session.CreateSQLQuery(sql)
+						.AddEntity(typeof(MyClass))
+						.SetFirstResult(0)
+						.SetMaxResults(50)
+						.List();
+					Assert.That(list.Count, Is.EqualTo(1));
+				}
+			}
+		}
+	}
+}
