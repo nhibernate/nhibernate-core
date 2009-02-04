80ab22487ebd18180378f57bbc09ab1dd7464f40
Dario Quintana <darioquintana@users.sourceforge.net>
2009-02-04 12:59:40 +0000

    adding test for NH-1654
    SVN: trunk@4034

---
+++ src/NHibernate.Test/NHSpecificTest/NH1654/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace NHibernate.Test.NHSpecificTest.NH1654
+{
+	public class Employee
+	{
+		public virtual int Id { get; set; }
+
+		public virtual string FirstName { get; set; }
+
+		public virtual string FirstNameFormula { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1654/Fixture.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1654
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		public override string BugNumber
+		{
+			get { return "NH1654"; }
+		}
+
+		[Test,Ignore]
+		public void Test()
+		{
+			int employeeId;
+			using (ISession sess = OpenSession())
+			using (ITransaction tx = sess.BeginTransaction())
+			{
+				var emp = new Employee();
+				emp.Id = 1;
+				emp.FirstName = "John";
+
+				sess.Save(emp);
+
+				tx.Commit();
+
+				employeeId = emp.Id;
+			}
+
+			using (ISession sess = OpenSession())
+			using (ITransaction tx = sess.BeginTransaction())
+			{
+				var load = sess.Load<Employee>(employeeId);
+				Assert.AreEqual("John", load.FirstNameFormula);
+
+				tx.Commit();
+			}
+
+			using (ISession sess = OpenSession())
+			using (ITransaction tx = sess.BeginTransaction())
+			{
+				sess.Delete("from Employee");
+				tx.Commit();
+			}
+		}
+	}
+}
