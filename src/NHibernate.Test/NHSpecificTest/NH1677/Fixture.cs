c0e1d8f0218c1ed350c5a2702dd06fa59445b029
Fabio Maulo <fabiomaulo@gmail.com>
2009-02-21 03:16:40 +0000

    The other test for the same fixed issue (fixed in the last commit)
    SVN: trunk@4092

--- src/NHibernate.Test/NHSpecificTest/NH1677/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH1677/Fixture.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using System.Data;
-using System.Text;
+using NHibernate.Cfg;
 using NUnit.Framework;
 
 namespace NHibernate.Test.NHSpecificTest.NH1677
 {
-	[TestFixture,Ignore]
-	public class EntityModeMapCriteria:BugTestCase
+	[TestFixture]
+	public class EntityModeMapCriteria : BugTestCase
 	{
 		private const int NumberOfRecordPerEntity = 10;
 		private const string Entity1Name = "Entity1";
@@ -17,18 +15,17 @@ namespace NHibernate.Test.NHSpecificTest.NH1677
 		private const string Entity2Property = "Entity2Property";
 		private const string EntityPropertyPrefix = "Record";
 
-		protected override void Configure(NHibernate.Cfg.Configuration configuration)
+		protected override void Configure(Configuration configuration)
 		{
 			base.Configure(configuration);
-			configuration.SetProperty("default_entity_mode",
-			                          EntityModeHelper.ToString(EntityMode.Map));
+			configuration.SetProperty("default_entity_mode", EntityModeHelper.ToString(EntityMode.Map));
 		}
 
 		protected override void OnSetUp()
 		{
-			using (var s = OpenSession())
+			using (ISession s = OpenSession())
 			{
-				using (var tx = s.BeginTransaction(IsolationLevel.ReadCommitted))
+				using (ITransaction tx = s.BeginTransaction(IsolationLevel.ReadCommitted))
 				{
 					for (int i = 0; i < NumberOfRecordPerEntity; i++)
 					{
@@ -47,39 +44,39 @@ namespace NHibernate.Test.NHSpecificTest.NH1677
 				}
 			}
 		}
+
 		protected override void OnTearDown()
 		{
-			using(var s=OpenSession())
+			using (ISession s = OpenSession())
 			{
-				using(var tx=s.BeginTransaction())
+				using (ITransaction tx = s.BeginTransaction())
 				{
 					s.Delete(string.Format("from {0}", Entity1Name));
+					s.Delete(string.Format("from {0}", Entity2Name));
 					tx.Commit();
 				}
-
 			}
 		}
+
 		[Test]
 		public void EntityModeMapFailsWithCriteria()
 		{
-			using (var sf = cfg.BuildSessionFactory())
+			using (ISessionFactory sf = cfg.BuildSessionFactory())
 			{
-
-				using (var s = sf.OpenSession())
+				using (ISession s = sf.OpenSession())
 				{
-					var query = s.CreateQuery(string.Format("from {0}", Entity1Name));
-					var entity1List = query.List();
-					Assert.AreEqual(NumberOfRecordPerEntity, entity1List.Count);  // OK, Count == 10
+					IQuery query = s.CreateQuery(string.Format("from {0}", Entity1Name));
+					IList entity1List = query.List();
+					Assert.AreEqual(NumberOfRecordPerEntity, entity1List.Count); // OK, Count == 10
 				}
 
-				using (var s = sf.OpenSession())
+				using (ISession s = sf.OpenSession())
 				{
-					var entity1Criteria = s.CreateCriteria(Entity1Name);
-					var entity1List = entity1Criteria.List();
-					Assert.AreEqual(NumberOfRecordPerEntity, entity1List.Count);  // KO !!! Count == 20 !!!
+					ICriteria entity1Criteria = s.CreateCriteria(Entity1Name);
+					IList entity1List = entity1Criteria.List();
+					Assert.AreEqual(NumberOfRecordPerEntity, entity1List.Count); // KO !!! Count == 20 !!!
 				}
 			}
 		}
 	}
-
 }
