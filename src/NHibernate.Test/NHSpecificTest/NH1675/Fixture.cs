9f1d9eb163f9b8f29d661857673a12a1c183a554
Fabio Maulo <fabiomaulo@gmail.com>
2009-02-21 04:12:45 +0000

    NH-1675 not an issue in trunk but probably a real bug with the old Dialect.
    SVN: trunk@4095

---
+++ src/NHibernate.Test/NHSpecificTest/NH1675/Fixture.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace NHibernate.Test.NHSpecificTest.NH1675
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		[Test]
+		public void ShouldWorkUsingDistinctAndLimits()
+		{
+			using (ISession s = OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				for (int i = 0; i < 5; i++)
+				{
+					s.Save(new Person {FirstName = "Name" + i});
+				}
+				tx.Commit();
+			}
+
+			using (ISession s = OpenSession())
+			{
+				var q =s.CreateQuery("select distinct p from Person p").SetFirstResult(0).SetMaxResults(10);
+				Assert.That(q.List().Count, Is.EqualTo(5));
+			}
+
+			// clean up
+			using (ISession s = OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				s.Delete("from Person");
+				tx.Commit();
+			}
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1675/Person.cs
@@ -0,0 +1,8 @@
+namespace NHibernate.Test.NHSpecificTest.NH1675
+{
+	public class Person
+	{
+		public virtual int Id { get; set; }
+		public virtual string FirstName { get; set; }
+	}
+}
