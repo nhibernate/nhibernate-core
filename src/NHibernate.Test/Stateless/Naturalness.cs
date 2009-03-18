de00334db13f0a79deb0af88e409d6cba8e6bba0
Fabio Maulo <fabiomaulo@gmail.com>
2009-03-18 13:30:33 +0000

    Test to study the StatelessSession behavior in "more complicated" scenario.
    SVN: trunk@4144

---
+++ src/NHibernate.Test/Stateless/Naturalness.cs
@@ -0,0 +1,37 @@
+using System;
+using Iesi.Collections.Generic;
+
+namespace NHibernate.Test.Stateless
+{
+	public abstract class Animal
+	{
+		public virtual int Id { get; private set; }
+		public virtual string Description { get; set; }
+	}
+
+	public class Reptile: Animal
+	{
+		public virtual float BodyTemperature { get; set; }
+	}
+
+	public class Human : Animal
+	{
+		public virtual string Name { get; set; }
+		public virtual string NickName { get; set; }
+		public virtual DateTime Birthdate { get; set; }
+	}
+
+	public class Family<T> where T: Animal
+	{
+		private ISet<T> childs;
+
+		public virtual int Id { get; private set; }
+		public virtual T Father { get; set; }
+		public virtual T Mother { get; set; }
+		public virtual ISet<T> Childs
+		{
+			get { return childs; }
+			set { childs = value; }
+		}
+	}
+}
---
+++ src/NHibernate.Test/Stateless/StatelessWithRelationsFixture.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using Iesi.Collections.Generic;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using System.Collections.Generic;
+
+namespace NHibernate.Test.Stateless
+{
+	[TestFixture]
+	public class StatelessWithRelationsFixture : TestCase
+	{
+		protected override string MappingsAssembly
+		{
+			get { return "NHibernate.Test"; }
+		}
+
+		protected override IList Mappings
+		{
+			get { return new[] { "Stateless.Naturalness.hbm.xml" }; }
+		}
+
+		[Test]
+		public void ShouldWorkLoadingComplexEntities()
+		{
+			const string crocodileFather = "Crocodile father";
+			const string crocodileMother = "Crocodile mother";
+
+			using (ISession s = sessions.OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				var rf = new Reptile { Description = crocodileFather };
+				var rm = new Reptile { Description = crocodileMother };
+				var rc1 = new Reptile { Description = "Crocodile" };
+				var rc2 = new Reptile { Description = "Crocodile" };
+				var rfamily = new Family<Reptile>
+				{
+					Father = rf,
+					Mother = rm,
+					Childs = new HashedSet<Reptile> { rc1, rc2 }
+				};
+				s.Save("ReptilesFamily", rfamily);
+				tx.Commit();
+			}
+
+			const string humanFather = "Fred";
+			const string humanMother = "Wilma";
+			using (ISession s = sessions.OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				var hf = new Human { Description = "Flinstone", Name = humanFather };
+				var hm = new Human { Description = "Flinstone", Name = humanMother };
+				var hc1 = new Human { Description = "Flinstone", Name = "Pebbles" };
+				var hfamily = new Family<Human>
+				{
+					Father = hf,
+					Mother = hm,
+					Childs = new HashedSet<Human> { hc1 }
+				};
+				s.Save("HumanFamily", hfamily);
+				tx.Commit();
+			}
+
+			using (IStatelessSession s = sessions.OpenStatelessSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				IList<Family<Human>> hf = s.CreateQuery("from HumanFamily").List<Family<Human>>();
+				Assert.That(hf.Count, Is.EqualTo(1));
+				Assert.That(hf[0].Father.Name, Is.EqualTo(humanFather));
+				Assert.That(hf[0].Mother.Name, Is.EqualTo(humanMother));
+				Assert.That(hf[0].Childs, Is.Null, "Collections should be ignored by stateless session.");
+
+				IList<Family<Reptile>> rf = s.CreateQuery("from ReptilesFamily").List<Family<Reptile>>();
+				Assert.That(rf.Count, Is.EqualTo(1));
+				Assert.That(rf[0].Father.Description, Is.EqualTo(crocodileFather));
+				Assert.That(rf[0].Mother.Description, Is.EqualTo(crocodileMother));
+				Assert.That(rf[0].Childs, Is.Null, "Collections should be ignored by stateless session.");
+
+				tx.Commit();
+			}
+
+			using (ISession s = sessions.OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				s.Delete("from HumanFamily");
+				s.Delete("from ReptilesFamily");
+				tx.Commit();
+			}
+		}
+
+	}
+}
