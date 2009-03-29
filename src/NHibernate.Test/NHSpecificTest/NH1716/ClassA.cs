fdd012056f77ee1369f9f386511d6a1593fb4d98
Dario Quintana <darioquintana@users.sourceforge.net>
2009-03-29 16:21:04 +0000

    passed test to verify NH-1716.
    SVN: trunk@4169

---
+++ src/NHibernate.Test/NHSpecificTest/NH1716/ClassA.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH1716
+{
+	public class ClassA
+	{
+		public virtual int Id { get; set; }
+		public virtual TimeSpan Time { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1716/Fixture.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1716
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnTearDown()
+		{
+			using (ISession s = OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				s.Delete("from ClassA");
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void TimeSpanLargerThan24h()
+		{
+			var time = new TimeSpan(2, 2, 1, 0);
+			var entity = new ClassA {Time = time};
+			using (ISession s = OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				s.Save(entity);
+				tx.Commit();
+			}
+
+			using (ISession s = OpenSession())
+			{
+				Assert.AreEqual(time, s.Get<ClassA>(entity.Id).Time);
+			}
+		}
+
+		[Test]
+		public void TimeSpanLargerThan2h()
+		{
+			var time = new TimeSpan(0, 2, 1, 0);
+			var entity = new ClassA {Time = time};
+			using (ISession s = OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				s.Save(entity);
+				tx.Commit();
+			}
+
+			using (ISession s = OpenSession())
+			{
+				Assert.AreEqual(time, s.Get<ClassA>(entity.Id).Time);
+			}
+		}
+
+		[Test]
+		public void TimeSpanNegative()
+		{
+			TimeSpan time = TimeSpan.FromDays(-1);
+			var entity = new ClassA {Time = time};
+			using (ISession s = OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				s.Save(entity);
+				tx.Commit();
+			}
+
+			using (ISession s = OpenSession())
+			{
+				Assert.AreEqual(time, s.Get<ClassA>(entity.Id).Time);
+			}
+		}
+
+		[Test]
+		public void VerifyDaysShouldBeZeroInSmallTimeSpan()
+		{
+			var time = new TimeSpan(1, 0, 0);
+			var entity = new ClassA {Time = time};
+			using (ISession s = OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				s.Save(entity);
+				tx.Commit();
+			}
+
+			using (ISession s = OpenSession())
+			{
+				Assert.AreEqual(0, s.Get<ClassA>(entity.Id).Time.Days);
+			}
+		}
+	}
+}
