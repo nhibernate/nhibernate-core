00b21a5197ec93eb172bc71ea6bfdcf67cb6b0e5
Fabio Maulo <fabiomaulo@gmail.com>
2009-05-13 23:14:26 +0000

    Cleaned
    SVN: trunk@4296

--- src/NHibernate.Test/NHSpecificTest/DtcFailures/DtcFailuresFixture.cs
+++ src/NHibernate.Test/NHSpecificTest/DtcFailures/DtcFailuresFixture.cs
@@ -1,156 +1,141 @@
 using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
+using System.Collections;
 using System.Threading;
 using System.Transactions;
-using NHibernate.Criterion;
-using NHibernate.Dialect;
-using NHibernate.Exceptions;
-using NHibernate.Impl;
 using NUnit.Framework;
 
 namespace NHibernate.Test.NHSpecificTest.DtcFailures
 {
-    using System.Collections;
+	[TestFixture]
+	public class DtcFailuresFixture : TestCase
+	{
+		protected override IList Mappings
+		{
+			get { return new string[] {"NHSpecificTest.DtcFailures.Mappings.hbm.xml"}; }
+		}
 
-    [TestFixture]
-    public class DtcFailuresFixture : TestCase
-    {
+		protected override string MappingsAssembly
+		{
+			get { return "NHibernate.Test"; }
+		}
 
-        protected override IList Mappings
-        {
-            get { return new string[] { "NHSpecificTest.DtcFailures.Mappings.hbm.xml" }; }
-        }
+		[Test]
+		public void WillNotCrashOnDtcPrepareFailure()
+		{
+			var tx = new TransactionScope();
+			using (ISession s = sessions.OpenSession())
+			{
+				s.Save(new Person {CreatedAt = DateTime.MinValue // will cause SQL date failure
+				                  });
+			}
 
-        protected override string MappingsAssembly
-        {
-            get { return "NHibernate.Test"; }
-        }
+			new ForceEscalationToDistributedTx();
 
-        [Test]
-        public void WillNotCrashOnDtcPrepareFailure()
-        {
-            var tx = new TransactionScope();
-            using (var s = sessions.OpenSession())
-            {
-                s.Save(new Person
-                {
-                    CreatedAt = DateTime.MinValue // will cause SQL date failure
-                });
-            }
+			tx.Complete();
+			try
+			{
+				tx.Dispose();
+				Assert.Fail("Expected failure");
+			}
+			catch (AssertionException)
+			{
+				throw;
+			}
+			catch (Exception) {}
+		}
 
-            new ForceEscalationToDistributedTx();
+		[Test]
+		public void Can_roll_back_transaction()
+		{
+			var tx = new TransactionScope();
+			using (ISession s = sessions.OpenSession())
+			{
+				new ForceEscalationToDistributedTx(true); //will rollback tx
+				s.Save(new Person {CreatedAt = DateTime.Today});
 
-            tx.Complete();
-            try
-            {
-                tx.Dispose();
-                Assert.Fail("Expected failure");
-            }
-            catch (AssertionException)
-            {
-                throw;
-            }
-            catch (Exception)
-            {
-            }
-        }
+				tx.Complete();
+			}
+			try
+			{
+				tx.Dispose();
+				Assert.Fail("Expected tx abort");
+			}
+			catch (TransactionAbortedException)
+			{
+				//expected   
+			}
+		}
 
-        [Test]
-        public void Can_roll_back_transaction()
-        {
-            var tx = new TransactionScope();
-            using (var s = sessions.OpenSession())
-            {
-                new ForceEscalationToDistributedTx(true);//will rollback tx
-                s.Save(new Person
-                {
-                    CreatedAt = DateTime.Today
-                });
+		[Test]
+		public void CanDeleteItemInDtc()
+		{
+			object id;
+			using (var tx = new TransactionScope())
+			{
+				using (ISession s = sessions.OpenSession())
+				{
+					id = s.Save(new Person {CreatedAt = DateTime.Today});
 
-                tx.Complete();
-            }
-            try
-            {
-                tx.Dispose();
-                Assert.Fail("Expected tx abort");
-            }
-            catch (TransactionAbortedException)
-            {
-             //expected   
-            }
-        }
+					new ForceEscalationToDistributedTx();
 
-        [Test]
-        public void CanDeleteItemInDtc()
-        {
-            object id;
-            using (var tx = new TransactionScope())
-            using (var s = sessions.OpenSession())
-            {
-                id = s.Save(new Person
-                {
-                    CreatedAt = DateTime.Today
-                });
+					tx.Complete();
+				}
+			}
 
-                new ForceEscalationToDistributedTx();
-                
-                tx.Complete();
-            }
+			using (var tx = new TransactionScope())
+			{
+				using (ISession s = sessions.OpenSession())
+				{
+					new ForceEscalationToDistributedTx();
 
-            using (var tx = new TransactionScope())
-            using (var s = sessions.OpenSession())
-            {
-                new ForceEscalationToDistributedTx(); 
-                
-                s.Delete(s.Get<Person>(id));
+					s.Delete(s.Get<Person>(id));
 
-                tx.Complete();
-            }
+					tx.Complete();
+				}
+			}
+		}
 
-        }
+		public class ForceEscalationToDistributedTx : IEnlistmentNotification
+		{
+			private readonly bool shouldRollBack;
+			private readonly int thread;
 
-        public class ForceEscalationToDistributedTx : IEnlistmentNotification
-        {
-            private readonly bool shouldRollBack;
-            private readonly int thread;
+			public ForceEscalationToDistributedTx(bool shouldRollBack)
+			{
+				this.shouldRollBack = shouldRollBack;
+				thread = Thread.CurrentThread.ManagedThreadId;
+				System.Transactions.Transaction.Current.EnlistDurable(Guid.NewGuid(), this, EnlistmentOptions.None);
+			}
 
-            public ForceEscalationToDistributedTx(bool shouldRollBack) 
-            {
-                this.shouldRollBack = shouldRollBack;
-                thread = Thread.CurrentThread.ManagedThreadId;
-                System.Transactions.Transaction.Current.EnlistDurable(Guid.NewGuid(), this, EnlistmentOptions.None);
-            }
+			public ForceEscalationToDistributedTx() : this(false) {}
 
-            public ForceEscalationToDistributedTx():this(false)
-            {
-                
-            }
+			public void Prepare(PreparingEnlistment preparingEnlistment)
+			{
+				Assert.AreNotEqual(thread, Thread.CurrentThread.ManagedThreadId);
+				if (shouldRollBack)
+				{
+					preparingEnlistment.ForceRollback();
+				}
+				else
+				{
+					preparingEnlistment.Prepared();
+				}
+			}
 
-            public void Prepare(PreparingEnlistment preparingEnlistment)
-            {
-                Assert.AreNotEqual(thread, Thread.CurrentThread.ManagedThreadId);
-                if (shouldRollBack)
-                    preparingEnlistment.ForceRollback();
-                else
-                    preparingEnlistment.Prepared();
-            }
+			public void Commit(Enlistment enlistment)
+			{
+				enlistment.Done();
+			}
 
-            public void Commit(Enlistment enlistment)
-            {
-                enlistment.Done();
-            }
+			public void Rollback(Enlistment enlistment)
+			{
+				enlistment.Done();
+			}
 
-            public void Rollback(Enlistment enlistment)
-            {
-                enlistment.Done();
-            }
-
-            public void InDoubt(Enlistment enlistment)
-            {
-                enlistment.Done();
-            }
-        }
-    }
+			public void InDoubt(Enlistment enlistment)
+			{
+				enlistment.Done();
+			}
+		}
+	}
 }
