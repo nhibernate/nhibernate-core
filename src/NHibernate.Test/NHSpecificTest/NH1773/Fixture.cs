398e3d1653b3e802bcbbbb273e59e67c57124748
Fabio Maulo <fabiomaulo@gmail.com>
2009-05-14 14:52:56 +0000

    Passing test
    SVN: trunk@4302

--- src/NHibernate.Test/NHSpecificTest/NH1773/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH1773/Fixture.cs
@@ -1,28 +1,11 @@
-﻿using System;
-using System.Collections;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace NHibernate.Test.NHSpecificTest.NH1773
 {
 	[TestFixture]
 	public class Fixture : BugTestCase
 	{
-		protected override void Configure(NHibernate.Cfg.Configuration configuration)
-		{
-			base.Configure(configuration);
-			configuration.SetProperty(NHibernate.Cfg.Environment.QueryTranslator, typeof(NHibernate.Hql.Classic.ClassicQueryTranslatorFactory).AssemblyQualifiedName);
-		}
-
-		protected override string MappingsAssembly
-		{
-			get { return "NHibernate.Test"; }
-		}
-
-		protected override IList Mappings
-		{
-			get { return new[] { "NHSpecificTest.NH1773.Person.hbm.xml"}; }
-		}
-
 		[Test]
 		public void CustomHQLFunctionsShouldBeRecognizedByTheParser()
 		{
@@ -41,10 +24,10 @@ namespace NHibernate.Test.NHSpecificTest.NH1773
 			using (ISession s = OpenSession())
 			using (ITransaction tx = s.BeginTransaction())
 			{
-				IList result = s.CreateQuery("select new PersonResult(p, current_timestamp()) from Person p left join fetch p.Country").List();
+				IList<PersonResult> result = s.CreateQuery("select new PersonResult(p, current_timestamp()) from Person p left join fetch p.Country").List<PersonResult>();
 
-				Assert.AreEqual("My Name", ((PersonResult)result[0]).Person.Name);
-				Assert.IsTrue(NHibernateUtil.IsInitialized(((PersonResult)result[0]).Person.Country));
+				Assert.AreEqual("My Name", result[0].Person.Name);
+				Assert.IsTrue(NHibernateUtil.IsInitialized(result[0].Person.Country));
 				tx.Commit();
 			}
 		}
@@ -54,8 +37,8 @@ namespace NHibernate.Test.NHSpecificTest.NH1773
 			using (ISession s = OpenSession())
 			using (ITransaction tx = s.BeginTransaction())
 			{
-				s.Delete("from Person");
-				s.Delete("from Country");
+				s.CreateQuery("delete from Person").ExecuteUpdate();
+				s.CreateQuery("delete from Country").ExecuteUpdate();
 				tx.Commit();
 			}
 		}
