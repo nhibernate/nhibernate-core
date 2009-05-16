c9168b84b20010ce683cbc2f2d5e613f7280a523
Ayende Rahien <ayende@ayende.com>
2009-05-16 07:30:51 +0000

    Fixing NH-1169 - Will only throw an error if SqlCommandSet is actually used (will make using NHibernate with Sql Server on Mono easier)
    SVN: trunk@4327

--- src/NHibernate/Impl/SqlClientCommandSet.cs
+++ src/NHibernate/Impl/SqlClientCommandSet.cs
@@ -1,5 +1,6 @@
 
 using System;
+using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Reflection;
@@ -12,13 +13,19 @@ namespace NHibernate.Impl
 
         static SqlClientCommandSet()
         {
-            Assembly sysData = Assembly.Load("System.Data, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
+            Assembly sysData = typeof(IDbConnection).Assembly;
             sqlCmdSetType = sysData.GetType("System.Data.SqlClient.SqlCommandSet");
-            Debug.Assert(sqlCmdSetType != null, "Could not find SqlCommandSet!");
         }
 
         protected override object CreateInternalCommandSet()
         {
+        	if (sqlCmdSetType == null)
+        	{
+        		throw new HibernateException("Could not find SqlCommandSet"+Environment.NewLine +
+				"If you are running on Mono, batching support isn't implemented on Mono" + Environment.NewLine +
+				"If you are running on Microsoft .NET, this probably means that internal details"+Environment.NewLine+
+				"of the BCL that we rely on to allow this have changed, this is a bug. Please inform the developers");
+        	}
             return Activator.CreateInstance(sqlCmdSetType, true);
         }
 
