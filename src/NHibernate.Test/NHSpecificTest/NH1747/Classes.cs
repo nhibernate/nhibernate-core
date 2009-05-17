60c5a44a9d19ce268a8a201e757ecd4da6baabfa
Tuna Toksoz <tehlike@users.sourceforge.net>
2009-05-17 21:30:26 +0000

    Adding tests for NH-1747 by Anne Epstein
    SVN: trunk@4335

---
+++ src/NHibernate.Test/NHSpecificTest/NH1747/Classes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NHibernate.Test.NHSpecificTest.NH1747
+{
+	public class Payment
+	{
+		private Int32 id;
+		public virtual Int32 Id { get { return id; } set { id = value; } }
+
+		private Decimal amount;
+		public virtual Decimal Amount { get { return amount; } set { amount = value; } }
+		private PaymentBatch paymentBatch;
+		public virtual PaymentBatch PaymentBatch { get { return paymentBatch; } set { paymentBatch = value; } }
+	}
+
+	public class PaymentBatch
+	{
+		private Int32 id;
+		public virtual Int32 Id { get { return id; } set { id = value; } }
+
+		private IList<Payment> payments = new List<Payment>();
+		public virtual IList<Payment> Payments { get { return payments; } }
+
+		public virtual void AddPayment(Payment p)
+		{
+			payments.Add(p);
+			p.PaymentBatch = this;
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1747/Fixture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1747
+{
+	[TestFixture,Ignore]
+	public class JoinTraversalTest : BugTestCase
+	{
+
+		protected override void OnSetUp()
+		{
+			base.OnSetUp();
+			using (ISession session = OpenSession())
+			{
+				var payment = new Payment { Amount = 5m, Id = 1 };
+				var paymentBatch = new PaymentBatch { Id = 3 };
+				paymentBatch.AddPayment(payment);
+				session.Save(paymentBatch);
+				session.Save(payment);
+				session.Flush();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			base.OnTearDown();
+			using (ISession session = OpenSession())
+			{
+				var hql = "from System.Object";
+				session.Delete(hql);
+				session.Flush();
+			}
+		}
+
+		[Test]
+		public void TraversingBagToJoinChildElementShouldWork()
+		{
+			using (ISession session = OpenSession())
+			{
+				var paymentBatch = session.Get<PaymentBatch>(3);
+				Assert.AreEqual(1, paymentBatch.Payments.Count);
+
+			}
+		}
+	}
+
+}
