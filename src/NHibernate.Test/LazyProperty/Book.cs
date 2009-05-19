32b2125674c455c87a88fb2381e854f4c55dfc94
Dario Quintana <darioquintana@users.sourceforge.net>
2009-05-19 12:59:37 +0000

    Lazy-Property test, not supported yet in NH. Waiting for a field-interceptor provider to be ready, probably Linfu.
    SVN: trunk@4349

---
+++ src/NHibernate.Test/LazyProperty/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace NHibernate.Test.LazyProperty
+{
+	public class Book
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+
+		private string _aLotOfText;
+
+		public virtual string ALotOfText
+		{
+			get { return _aLotOfText; }
+			set { _aLotOfText = value; }
+		}
+	}
+}
---
+++ src/NHibernate.Test/LazyProperty/LazyPropertyFixture.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace NHibernate.Test.LazyProperty
+{
+	[TestFixture]
+	public class LazyPropertyFixture : TestCase
+	{
+		protected override string MappingsAssembly
+		{
+			get { return "NHibernate.Test"; }
+		}
+
+		protected override IList Mappings
+		{
+			get { return new[] {"LazyProperty.Mappings.hbm.xml"}; }
+		}
+
+		public void LoadData()
+		{
+			Book b = new Book
+			         	{
+			         		Name = "some name",
+			         		ALotOfText = "a lot of text ..."
+			         	};
+
+			using(var s = OpenSession())
+			using(var tx = s.BeginTransaction())
+			{
+				s.Persist(b);
+				tx.Commit();
+			}
+			
+		}
+
+		public void CleanUp()
+		{
+			using (var s = OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+				Assert.That(s.CreateSQLQuery("delete from Book").ExecuteUpdate(), Is.EqualTo(1));
+				tx.Commit();
+			}
+		}
+
+		[Test,Ignore("Not supported yet, waiting for a field-interceptor provider, probably Linfu.")]
+		public void PropertyLoadedNotInitialized()
+		{
+			LoadData();
+
+			using(ISession s = OpenSession())
+			{
+				var book = s.Load<Book>(1);
+
+				Assert.False(NHibernateUtil.IsPropertyInitialized(book, "Id"));
+				Assert.False(NHibernateUtil.IsPropertyInitialized(book, "Name"));
+				Assert.False(NHibernateUtil.IsPropertyInitialized(book, "ALotOfText"));
+				var name = book.Name;
+				Assert.True(NHibernateUtil.IsPropertyInitialized(book, "Id"));
+				Assert.True(NHibernateUtil.IsPropertyInitialized(book, "Name"));
+				Assert.False(NHibernateUtil.IsPropertyInitialized(book, "ALotOfText"));
+			}
+
+			CleanUp();
+		}
+	}
+}
