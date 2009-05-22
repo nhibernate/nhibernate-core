0c99cba8b760e0e158544268510004361faff6d1
Fabio Maulo <fabiomaulo@gmail.com>
2009-05-22 16:29:11 +0000

    Minor
    SVN: trunk@4357

--- src/NHibernate.Example.Web/App_Code/ExampleApplication.cs
+++ src/NHibernate.Example.Web/App_Code/ExampleApplication.cs
@@ -1,22 +1,26 @@
-using System;
 using System.IO;
 using System.Web;
 using System.Web.Hosting;
-using NHibernate;
+using NHibernate.Cfg;
 using NHibernate.Example.Web.Domain;
 
 namespace NHibernate.Example.Web
 {
 	public class ExampleApplication : HttpApplication
 	{
-		public static readonly Cfg.Configuration Configuration;
+		public static readonly Configuration Configuration;
 		public static readonly ISessionFactory SessionFactory;
 
 		static ExampleApplication()
 		{
 			log4net.Config.XmlConfigurator.Configure();
-			Configuration = new NHibernate.Cfg.Configuration()
-				.SetDefaultAssembly(typeof(Item).Assembly.FullName)
+			Configuration = new Configuration();
+			string nhConfigPath = HostingEnvironment.MapPath("~/App_Data/hibernate.cfg.xml");
+			if (File.Exists(nhConfigPath))
+			{
+				Configuration.Configure(nhConfigPath);
+			}
+			Configuration.SetDefaultAssembly(typeof(Item).Assembly.FullName)
 				.SetDefaultNamespace(typeof(Item).Namespace)
 				.AddDirectory(new DirectoryInfo(HostingEnvironment.MapPath("~/App_Data/")));
 			
