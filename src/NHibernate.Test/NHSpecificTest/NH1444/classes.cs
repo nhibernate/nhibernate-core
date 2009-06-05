d158630e132f4ab053c16ffa5586d1ede3406411
Fabio Maulo <fabiomaulo@gmail.com>
2009-06-05 19:55:12 +0000

    Sorry!! 2 files missed in previous commit.
    SVN: trunk@4418

---
+++ src/NHibernate.Test/NHSpecificTest/NH1444/classes.cs
@@ -0,0 +1,18 @@
+using Iesi.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH1444
+{
+	public class xparent
+	{
+		public virtual long Id { get; set; }
+		public virtual long? A { get; set; }
+		public virtual ISet<xchild> Children { get; set; }
+	}
+
+	public class xchild
+	{
+		public virtual long Id { get; set; }
+		public virtual long? B { get; set; }
+		public virtual xparent Parent { get; set; }
+	}
+}
