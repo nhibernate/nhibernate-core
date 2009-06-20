f0f15793ba6aba4a0da063352b66ee689cf25c29
Tuna Toksoz <tehlike@users.sourceforge.net>
2009-06-20 19:24:32 +0000

    Adding tests to show NH-1734 is no longer an issue. Possibly fixed with new AST parser.
    SVN: trunk@4497

---
+++ src/NHibernate.Test/NHSpecificTest/NH1734/Fixture.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1734
+{
+	[TestFixture]
+	public class Fixture:BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			using(var session=this.OpenSession())
+			using(var tran=session.BeginTransaction())
+			{
+				var product = new Product {Amount = 3, Price = 43.2};
+				var product2 = new Product { Amount = 3, Price = 43.2 };
+				session.Save(product);
+				session.Save(product2);
+				tran.Commit();
+			}
+		}
+		protected override void OnTearDown()
+		{
+			using(var session=this.OpenSession())
+			using (var tran = session.BeginTransaction())
+			{
+				session.Delete("from Product");
+				tran.Commit();
+			}
+		}
+
+		[Test]
+		public void ReturnsApropriateTypeWhenSumUsedWithSomeFormula()
+		{
+			using (var session = this.OpenSession())
+			using (var tran = session.BeginTransaction())
+			{
+				var query=session.CreateQuery("select sum(Amount*Price) from Product");
+				var result=query.UniqueResult();
+				Assert.That(result, Is.InstanceOf(typeof (double)));
+				Assert.That(result, Is.EqualTo(43.2*3*2));
+				query = session.CreateQuery("select sum(Price*Amount) from Product");
+				result = query.UniqueResult();
+				Assert.That(result, Is.InstanceOf(typeof(double)));
+				Assert.That(result, Is.EqualTo(43.2 * 3 * 2));
+
+				query = session.CreateQuery("select sum(Price) from Product");
+				result = query.UniqueResult();
+				Assert.That(result, Is.InstanceOf(typeof(double)));
+				Assert.That(result, Is.EqualTo(43.2 * 2));
+
+				query = session.CreateQuery("select sum(Amount) from Product");
+				result = query.UniqueResult();
+				Assert.That(result, Is.InstanceOf(typeof(Int64)));
+				Assert.That(result, Is.EqualTo(6.0));
+			}
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1734/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NHibernate.Test.NHSpecificTest.NH1734
+{
+	public class Product
+	{
+		public virtual int Id { get; set; }
+		public virtual double Price { get; set; }
+		public virtual int Amount { get; set; }
+	}
+}
