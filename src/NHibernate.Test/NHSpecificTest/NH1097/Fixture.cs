d750dd0846f4dd881f078627fa5b2e39d61493a6
Tuna Toksoz <tehlike@users.sourceforge.net>
2009-06-20 18:40:44 +0000

    Adding tests to show that NH-1097 is no longer an issue
    SVN: trunk@4496

---
+++ src/NHibernate.Test/NHSpecificTest/NH1097/Fixture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1097
+{
+	[TestFixture]
+	public class Fixture:BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			using(var session=this.OpenSession())
+			using(var tran=session.BeginTransaction())
+			{
+				session.Save(new Person {Name = "Fabio"});
+				session.Save(new Person { Name = "Dario" });
+				tran.Commit();
+			}
+		}
+		protected override void OnTearDown()
+		{
+			using (var session = this.OpenSession())
+			using (var tran = session.BeginTransaction())
+			{
+				session.Save(new Person { Name = "Fabio" });
+				session.Save(new Person { Name = "Dario" });
+				session.Delete("from Person");
+				tran.Commit();
+			}
+		}
+
+		[Test]
+		public void ThrowsExceptionWhenColumnNameIsUsedInQuery()
+		{
+			using (var session = this.OpenSession())
+			using (var tran = session.BeginTransaction())
+			{
+
+				Assert.Throws<QueryException>(delegate
+				                              	{
+													var query = session.CreateQuery("from Person p where p.namecolumn=:nameOfPerson");
+													query.SetString("nameOfPerson", "Dario");
+				                              		query.List();
+				                              	});
+			}
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1097/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NHibernate.Test.NHSpecificTest.NH1097
+{
+	public class Person
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+
+	}
+}
