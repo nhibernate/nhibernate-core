530cb2e8298ec773d2fcc17c5a0ac0fe327921b5
Tuna Toksoz <tehlike@users.sourceforge.net>
2009-06-21 08:17:14 +0000

    Adding tests for NH-1192 showing it is no longer an issue. Should have been fixed with the new AST parser.
    SVN: trunk@4498

---
+++ src/NHibernate.Test/NHSpecificTest/NH1192/Domain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NHibernate.Test.NHSpecificTest.NH1192
+{
+	public enum Status
+	{
+		None,
+		Bold=1,
+		Italic=2,
+		Underlined=4
+
+	}
+	public class ObjectA
+	{
+		public virtual int Id { get; set; }
+		public virtual Status FontType { get; set; }
+		public virtual string Name { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1192/Fixture.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1192
+{
+	[TestFixture]
+	public class Fixture:BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			using(var session=this.OpenSession())
+			using(var tran=session.BeginTransaction())
+			{
+				session.Save(new ObjectA {FontType = Status.Bold|Status.Italic, Name = "Object1"});
+				session.Save(new ObjectA { FontType = Status.Italic, Name = "Object2" });
+				session.Save(new ObjectA { FontType = Status.Underlined, Name = "Object2" });
+				tran.Commit();
+			}
+		}
+		protected override void OnTearDown()
+		{
+			using (var session = this.OpenSession())
+			using (var tran = session.BeginTransaction())
+			{
+				session.Delete("from ObjectA");
+				tran.Commit();
+			}
+		}
+
+		[Test]
+		public void BitwiseAndWorksCorrectly()
+		{
+			using (var session = this.OpenSession())
+			using (var tran = session.BeginTransaction())
+			{
+				var query = session.CreateQuery("from ObjectA o where (o.FontType & 1) > 0");
+				var result = query.List();
+				Assert.That(result,Has.Count.EqualTo(1));
+				query = session.CreateQuery("from ObjectA o where (o.FontType & 2) > 0");
+				result = query.List();
+				Assert.That(result, Has.Count.EqualTo(2));
+
+				query = session.CreateQuery("from ObjectA o where (o.FontType & 4) > 0");
+				result = query.List();
+				Assert.That(result, Has.Count.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public void BitwiseOrWorksCorrectly()
+		{
+			using (var session = this.OpenSession())
+			using (var tran = session.BeginTransaction())
+			{
+				var query = session.CreateQuery("from ObjectA o where (o.FontType | 2)  = (1|2) ");
+				var result = query.List();
+				Assert.That(result, Has.Count.EqualTo(1));
+			}
+		}
+	}
+}
