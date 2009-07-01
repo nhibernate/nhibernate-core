1063122c65ae82dbaad5627f573058d1ed6fec84
Dario Quintana <darioquintana@users.sourceforge.net>
2009-07-01 18:07:38 +0000

    merge r4552. NH-645 reopened: not working with AST-Hql. Tests rewritten
    SVN: trunk@4554

--- src/NHibernate.Test/NHSpecificTest/NH645/HQLFunctionFixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH645/HQLFunctionFixture.cs
@@ -1,15 +1,37 @@
 using System;
 using System.Collections;
+using Antlr.Runtime.Tree;
 using NHibernate.Cfg;
 using NHibernate.Dialect;
 using NHibernate.Dialect.Function;
+using NHibernate.Hql.Ast.ANTLR;
+using NHibernate.Hql.Classic;
 using NUnit.Framework;
-using Environment=NHibernate.Cfg.Environment;
+using Environment = NHibernate.Cfg.Environment;
 
 namespace NHibernate.Test.NHSpecificTest.NH645
 {
 	[TestFixture]
-	public class HQLFunctionFixture : TestCase
+	public class HqlFunctionWithClassicParser : HQLFunctionFixtureBase
+	{
+		protected override void Configure(Configuration configuration)
+		{
+			base.Configure(configuration);
+			configuration.SetProperty(Environment.QueryTranslator, typeof(ClassicQueryTranslatorFactory).AssemblyQualifiedName);
+		}
+	}
+
+	[TestFixture, Ignore("Not fixed yet in the AST-HQL parser")]
+	public class HqlFunctionWithAstHqlParser : HQLFunctionFixtureBase
+	{
+		protected override void Configure(Configuration configuration)
+		{
+			base.Configure(configuration);
+			configuration.SetProperty(Environment.QueryTranslator, typeof(ASTQueryTranslatorFactory).AssemblyQualifiedName);
+		}
+	}
+
+	public class HQLFunctionFixtureBase : TestCase
 	{
 		private bool appliesToThisDialect = true;
 
@@ -18,28 +40,49 @@ namespace NHibernate.Test.NHSpecificTest.NH645
 			get { return "NHibernate.Test"; }
 		}
 
+		protected override IList Mappings
+		{
+			get { return new[] { "HQL.Animal.hbm.xml", "HQL.MaterialResource.hbm.xml" }; }
+		}
+
 		protected override bool AppliesTo(Dialect.Dialect dialect)
 		{
 			return appliesToThisDialect;
 		}
 
-		protected override IList Mappings
-		{
-			get { return new[] {"HQL.Animal.hbm.xml", "HQL.MaterialResource.hbm.xml"}; }
-		}
-
 		protected override void Configure(Configuration configuration)
 		{
 			if (Dialect is MsSql2005Dialect)
-				configuration.SetProperty(Environment.Dialect, typeof (CustomDialect).AssemblyQualifiedName);
+				configuration.SetProperty(Environment.Dialect, typeof(CustomDialect).AssemblyQualifiedName);
 			else
 				appliesToThisDialect = false;
+		}
+
+		public void Run(string hql)
+		{
+			using (ISession s = OpenSession())
+				try
+				{
+					s.CreateQuery(hql).List();
+				}
+				catch (Exception ex)
+				{
+					if (IsClassicParser)
+					{
+						if (ex is QueryException)
+							Assert.Fail("The parser think that 'freetext' is a boolean function");
+					}
+					else //Hql-Parser
+					{
+						if (ex is RewriteEmptyStreamException || ex is InvalidCastException)
+							Assert.Fail("The parser think that 'freetext' is a boolean function");
+					}
+				}
 		}
 
 		/// <summary>
 		/// Just test the parser can compile, and SqlException is expected.
 		/// </summary>
-		
 		[Test]
 		public void SimpleWhere()
 		{
@@ -56,21 +99,6 @@ namespace NHibernate.Test.NHSpecificTest.NH645
 		public void SimpleWhereWithAnotherClause2()
 		{
 			Run("from Animal a where freetext(a.Description, 'hey apple car') AND a.Description <> 'foo'");
-		}
-
-
-		public void Run(string hql)
-		{
-			using(ISession s = OpenSession())
-				try
-				{
-					s.CreateQuery(hql).List();
-				}
-				catch (Exception ex)
-				{
-					if (ex is QueryException)
-						Assert.Fail("The parser think that 'freetext' is a boolean function");
-				}
 		}
 	}
 
