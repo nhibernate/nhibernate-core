50609dc3337d50ec1d8ab9831108c7df2cb2df4a
Fabio Maulo <fabiomaulo@gmail.com>
2009-07-04 18:17:15 +0000

    Minor (reformatted)
    SVN: trunk@4571

--- src/NHibernate.Test/NHSpecificTest/NH1849/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH1849/Fixture.cs
@@ -1,41 +1,47 @@
+using NHibernate.Cfg;
 using NHibernate.Dialect;
+using NHibernate.Dialect.Function;
 using NUnit.Framework;
 
 namespace NHibernate.Test.NHSpecificTest.NH1849
 {
-	using Criterion;
+	public class CustomDialect : MsSql2005Dialect
+	{
+		public CustomDialect()
+		{
+			RegisterFunction("contains", new StandardSQLFunction("contains", NHibernateUtil.Boolean));
+		}
+	}
 
 	[TestFixture]
-    public class Fixture:BugTestCase
-    {
-        protected override bool AppliesTo(NHibernate.Dialect.Dialect dialect)
-        {
-            return dialect is MsSql2005Dialect;
-        }
-        
-        protected override void Configure(NHibernate.Cfg.Configuration configuration)
-        {
-            base.Configure(configuration);
+	public class Fixture : BugTestCase
+	{
+		protected override bool AppliesTo(Dialect.Dialect dialect)
+		{
+			return dialect is MsSql2005Dialect;
+		}
 
-            configuration.SetProperty("dialect", "NHibernate.Test.NHSpecificTest.NH1849.CustomDialect, NHibernate.Test");
-        }
+		protected override void Configure(Configuration configuration)
+		{
+			base.Configure(configuration);
 
-        /// <summary>
-        /// This test may throw an ado exception due to the absence of a full text index,
-        /// however the query should compile
-        /// </summary>
-        [Test,Ignore]
-        public void ExecutesCustomSqlFunctionContains()
-        {
-            sessions.Statistics.Clear();
-			using (ISession session = this.OpenSession())
+			configuration.SetProperty("dialect", "NHibernate.Test.NHSpecificTest.NH1849.CustomDialect, NHibernate.Test");
+		}
+
+		/// <summary>
+		/// This test may throw an ado exception due to the absence of a full text index,
+		/// however the query should compile
+		/// </summary>
+		[Test, Ignore]
+		public void ExecutesCustomSqlFunctionContains()
+		{
+			sessions.Statistics.Clear();
+			using (ISession session = OpenSession())
 			{
-			    session.CreateQuery("from Customer c where contains(c.Name, :smth)")
-					.SetString("smth","aaaa")
-			        .List();
+				session.CreateQuery("from Customer c where contains(c.Name, :smth)").SetString("smth", "aaaa").List();
 
-                Assert.That(sessions.Statistics.QueryExecutionCount, Is.EqualTo(1));
-            }
-        }
-    }
+				Assert.That(sessions.Statistics.QueryExecutionCount, Is.EqualTo(1));
+			}
+		}
+	}
 }
