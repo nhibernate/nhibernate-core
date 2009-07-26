14002af70af158d8a5850aced09df27a18ffb03e
Davy Brion <davybrion@users.sourceforge.net>
2009-07-26 15:43:11 +0000

    applying patch from 'Dima' to fix NH1899
    SVN: branches/2.1.x@4659

---
+++ src/NHibernate.Test/NHSpecificTest/NH1899/SampleTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+using NHibernate.Dialect;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1899
+{
+    [TestFixture]
+    public class SampleTest : BugTestCase
+    {
+        protected override void OnSetUp()
+        {
+            base.OnSetUp();
+            using (ISession session = OpenSession())
+            {
+                Parent entity = new Parent();
+                entity.Id = 1;
+                entity.Relations = new Dictionary<Key, Value>();
+                entity.Relations.Add(Key.One, Value.ValOne);
+                entity.Relations.Add(Key.Two, Value.ValTwo);
+                session.Save(entity);
+                session.Flush();
+            }
+        }
+
+        protected override void OnTearDown()
+        {
+            base.OnTearDown();
+            using (ISession session = OpenSession())
+            {
+                string hql = "from System.Object";
+                session.Delete(hql);
+                session.Flush();
+            }
+        }
+
+        [Test]
+        public void ShouldNotThrowOnSaveUpdateCopy()
+        {
+            Parent entity;
+
+            using (ISession session = OpenSession())
+            {
+                entity = session.Get<Parent>(1);
+                session.Close();
+                session.Dispose();
+            }
+
+            using (ISession session2 = OpenSession()) 
+			{
+                entity = (Parent)session2.SaveOrUpdateCopy(entity);
+            }
+        }
+    }
+}
