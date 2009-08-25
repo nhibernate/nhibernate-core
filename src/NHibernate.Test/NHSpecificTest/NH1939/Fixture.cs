b7a2caeace5ec0dfc141fca46f6e747d4429de1c
Richard Brown <flukefan@googlemail.com>
2009-08-25 12:44:30 +0000

    Fix NH-1939
    SVN: branches/2.1.x@4699

---
+++ src/NHibernate.Test/NHSpecificTest/NH1939/Fixture.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using NHibernate.Tool.hbm2ddl;
+using System.Text;
+using NHibernate.Cfg;
+
+namespace NHibernate.Test.NHSpecificTest.NH1939
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+
+		private Configuration cfg;
+		private StringBuilder schemaBuilder;
+
+		private void AddString(string sqlString)
+		{
+			schemaBuilder.Append(sqlString);
+		}
+
+		protected override bool AppliesTo(NHibernate.Dialect.Dialect dialect)
+		{
+			return (dialect is Dialect.MsSql2000Dialect);
+		}
+
+		protected override void Configure(Configuration configuration)
+		{
+			base.Configure(configuration);
+			cfg = configuration;
+		}
+
+		[Test] 
+		public void Can_Parameterise_Auxiliary_Database_Objects() 
+		{
+			schemaBuilder = new StringBuilder();
+
+			SchemaExport schemaExport = new SchemaExport(cfg);
+			schemaExport.Execute(AddString, false, false);
+
+			string schema = schemaBuilder.ToString();
+
+			Assert.That(schema.Contains("select 'drop script'"), Is.True,
+				"schema drop script not exported");
+
+			Assert.That(schema.Contains("select 'create script'"), Is.True,
+				"parameterised schema create script not exported");
+		} 
+
+	}
+}
