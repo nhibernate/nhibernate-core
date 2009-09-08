95ef22e90ffeebc4c98987344afea7430a8ad02f
Richard Brown <flukefan@googlemail.com>
2009-09-08 13:15:32 +0000

    Merge r4709 (Fix NH-1926)
    SVN: trunk@4710

---
+++ src/NHibernate.Test/Tools/hbm2ddl/SchemaUpdate/Person.cs
@@ -0,0 +1,10 @@
+namespace NHibernate.Test.Tools.hbm2ddl.SchemaUpdate
+{
+	public class Person
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual string FirstName { get; set; }
+		public virtual string LastName { get; set; }
+	}
+}
