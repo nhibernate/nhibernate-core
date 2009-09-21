5ac5ee2fca0beaffe7ad749477d49f9fa570c7fc
Tuna Toksoz <tehlike@users.sourceforge.net>
2009-09-21 01:57:56 +0000

    Adding tests for NH-1969
    SVN: trunk@4719

---
+++ src/NHibernate.Test/NHSpecificTest/NH1969/DummyEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHibernate.Test.NHSpecificTest.NH1969
+{
+    /// <summary>
+    /// Author : Stephane Verlet
+    /// </summary>
+    public class DummyEntity
+    {
+
+        private int _id;
+
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+    }
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1969/Fixture.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1969
+{
+	using Criterion;
+
+	/// <summary>
+	/// Author : Stephane Verlet
+	/// </summary>
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+
+		protected override void OnSetUp()
+		{
+			using (ISession s = OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+
+				EntityWithTypeProperty entity = new EntityWithTypeProperty();
+				entity.Id = 1;
+				entity.TypeValue = typeof(System.IO.File);  //A random not mapped type
+				s.Save(entity);
+
+				entity = new EntityWithTypeProperty();
+				entity.Id = 2;
+				entity.TypeValue = typeof(DummyEntity); // A mapped entity
+				s.Save(entity);
+
+				tx.Commit();
+			}
+
+		}
+
+		protected override void OnTearDown()
+		{
+			using (ISession session = this.OpenSession())
+			using (ITransaction tx = session.BeginTransaction())
+			{
+				string hql = "from System.Object";
+				session.Delete(hql);
+				tx.Commit();
+			}
+		}
+
+		[Test,Ignore]
+		public void TestMappedTypeCriteria()
+		{
+			using (ISession s = OpenSession())
+			{
+				ICriteria criteria = s.CreateCriteria(typeof(EntityWithTypeProperty));
+				criteria.Add(Restrictions.Eq("TypeValue", typeof(DummyEntity)));
+				IList<EntityWithTypeProperty> results = criteria.List<EntityWithTypeProperty>();
+				Assert.AreEqual(1, results.Count);
+				Assert.AreEqual(2, results[0].Id);
+			}
+		}
+
+
+		[Test]
+		public void TestMappedTypeHQL()
+		{
+			using (ISession s = OpenSession())
+			{
+
+				IQuery q = s.CreateQuery("select t from EntityWithTypeProperty as t where t.TypeValue = :type");
+				q.SetParameter("type", typeof(DummyEntity));
+				IList<EntityWithTypeProperty> results = q.List<EntityWithTypeProperty>();
+				Assert.AreEqual(1, results.Count);
+				Assert.AreEqual(2, results[0].Id);
+
+			}
+		}
+
+
+
+
+		[Test]
+		public void TestNonMappedTypeCriteria()
+		{
+			using (ISession s = OpenSession())
+			{
+				ICriteria criteria = s.CreateCriteria(typeof(EntityWithTypeProperty));
+				criteria.Add(Restrictions.Eq("TypeValue", typeof(System.IO.File)));
+				IList<EntityWithTypeProperty> results = criteria.List<EntityWithTypeProperty>();
+				Assert.AreEqual(1, results.Count);
+				Assert.AreEqual(1, results[0].Id);
+			}
+		}
+
+		[Test]
+		public void TestNonMappedTypeHQL()
+		{
+			using (ISession s = OpenSession())
+			{
+
+				IQuery q = s.CreateQuery("select t from EntityWithTypeProperty as t where t.TypeValue = :type");
+				q.SetParameter("type", typeof(System.IO.File));
+				IList<EntityWithTypeProperty> results = q.List<EntityWithTypeProperty>();
+				Assert.AreEqual(1, results.Count);
+				Assert.AreEqual(1, results[0].Id);
+
+			}
+		}
+
+	}
+}
