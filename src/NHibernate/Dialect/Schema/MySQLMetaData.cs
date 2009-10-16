f9e523eae57694b84359ef7dd347d2928bcb6fa0
Fabio Maulo <fabiomaulo@gmail.com>
2009-10-16 17:58:44 +0000

    Fix NH-1993
    SVN: branches/2.1.x@4762

--- src/NHibernate/Dialect/Schema/MySQLMetaData.cs
+++ src/NHibernate/Dialect/Schema/MySQLMetaData.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Data;
 using System.Data.Common;
-using Iesi.Collections.Generic;
 
 namespace NHibernate.Dialect.Schema
 {
@@ -19,17 +18,6 @@ namespace NHibernate.Dialect.Schema
 		protected override string ForeignKeysSchemaName
 		{
 			get { return "Foreign Keys"; }
-		}
-
-		public override ISet<string> GetReservedWords()
-		{
-			var result = new HashedSet<string>();
-			DataTable dtReservedWords = Connection.GetSchema(DbMetaDataCollectionNames.ReservedWords);
-			foreach (DataRow row in dtReservedWords.Rows)
-			{
-				result.Add(row["Reserved Word"].ToString());
-			}
-			return result;
 		}
 	}
 
