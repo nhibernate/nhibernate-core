af18a85b631e6102c5b4107f4ed25797756fb418
Richard Brown <flukefan@googlemail.com>
2009-10-30 10:55:36 +0000

    Merge r4807 (Fix NH-2003, not null constraint on identifier column)
    SVN: trunk@4808

---
+++ src/NHibernate.Test/NHSpecificTest/NH2003/Fixture.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH2003
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		[Test]
+		public void ShouldCreateNotNullIdColumn()
+		{
+			StringBuilder script = new StringBuilder();
+
+			Configuration cfg = TestConfigurationHelper.GetDefaultConfiguration();
+			using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(MappingsAssembly + "." + (string)Mappings[0]))
+				cfg.AddInputStream(stream);
+			new SchemaExport(cfg).Execute(s => script.AppendLine(s), false, false);
+
+			string wholeScript = script.ToString();
+			Assert.That(wholeScript.ToLower(), Text.Contains("not null"));
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH2003/Model.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH2003
+{
+	public class MyClass
+	{
+		public virtual int Id { get; set; }
+	}
+}
