55e7da4d94aaf4bbb2c73624bb46331b3f64b513
Fabio Maulo <fabiomaulo@gmail.com>
2009-11-20 15:55:07 +0000

    Minor (cleaned)
    SVN: branches/2.1.x@4842

--- src/NHibernate/Impl/SessionIdLoggingContext.cs
+++ src/NHibernate/Impl/SessionIdLoggingContext.cs
@@ -1,13 +1,11 @@
 using System;
-using log4net;
 
 namespace NHibernate.Impl
 {
-	using System.Web;
-
 	public class SessionIdLoggingContext : IDisposable
 	{
-		[ThreadStatic] private static Guid? CurrentSessionId;
+		[ThreadStatic]
+		private static Guid? CurrentSessionId;
 
 		private readonly Guid? oldSessonId;
 
@@ -20,25 +18,23 @@ namespace NHibernate.Impl
 		/// <summary>
 		/// We always set the result to use a thread static variable, on the face of it,
 		/// it looks like it is not a valid choice, since ASP.Net and WCF may decide to switch
-        /// threads on us. But, since SessionIdLoggingContext is only used inside NH calls, and since
-        /// NH calls are never async, this isn't an issue for us.
-        /// In addition to that, attempting to match to the current context has proven to be performance hit.
+		/// threads on us. But, since SessionIdLoggingContext is only used inside NH calls, and since
+		/// NH calls are never async, this isn't an issue for us.
+		/// In addition to that, attempting to match to the current context has proven to be performance hit.
 		/// </summary>
 		public static Guid? SessionId
 		{
-			get
-			{
-				return CurrentSessionId;
-			}
-			set
-			{
-                CurrentSessionId = value;
-			}
+			get { return CurrentSessionId; }
+			set { CurrentSessionId = value; }
 		}
+
+		#region IDisposable Members
 
 		public void Dispose()
 		{
 			SessionId = oldSessonId;
 		}
+
+		#endregion
 	}
 }
