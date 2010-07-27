c9cb79128bdb9278eddfc84b4e507ac94a218b7c
Fabio Maulo <fabiomaulo@gmail.com>
2010-07-27 20:19:40 +0000

    "Decoded" failing test for NH-2208
    SVN: trunk@5064

---
+++ src/NHibernate.Test/NHSpecificTest/NH2208/Filter.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH2208
+{
+	public class Filter : BugTestCase
+	{
+		[Test, Ignore("Not fixed yet")]
+		public void Test()
+		{
+			using (ISession session = OpenSession())
+			{
+				session.EnableFilter("myfilter");
+				session.CreateQuery("from E1 e join fetch e.BO").List();
+			}
+		}
+	}
+}
