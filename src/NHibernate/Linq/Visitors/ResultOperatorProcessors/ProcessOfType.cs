01ba3e37818bb4d1f6d186e773cdccd17b149ab7
Fabio Maulo <fabiomaulo@gmail.com>
2010-08-01 13:49:32 +0000

    Removed unused method
    SVN: trunk@5088

--- src/NHibernate/Linq/Visitors/ResultOperatorProcessors/ProcessOfType.cs
+++ src/NHibernate/Linq/Visitors/ResultOperatorProcessors/ProcessOfType.cs
@@ -1,36 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq.Expressions;
 using NHibernate.Hql.Ast;
 using Remotion.Data.Linq.Clauses.ResultOperators;
 using Remotion.Data.Linq.Clauses.StreamedData;
 
 namespace NHibernate.Linq.Visitors.ResultOperatorProcessors
 {
-    public class ProcessOfType : IResultOperatorProcessor<OfTypeResultOperator>
-    {
-        public void Process(OfTypeResultOperator resultOperator, QueryModelVisitor queryModelVisitor, IntermediateHqlTree tree)
-        {
-            var source =
-                queryModelVisitor.CurrentEvaluationType.As<StreamedSequenceInfo>().ItemExpression;
+	public class ProcessOfType : IResultOperatorProcessor<OfTypeResultOperator>
+	{
+		#region IResultOperatorProcessor<OfTypeResultOperator> Members
 
-            var type = BuildDot(resultOperator.SearchedItemType.FullName.Split('.'), tree.TreeBuilder);
+		public void Process(OfTypeResultOperator resultOperator, QueryModelVisitor queryModelVisitor, IntermediateHqlTree tree)
+		{
+			Expression source =
+				queryModelVisitor.CurrentEvaluationType.As<StreamedSequenceInfo>().ItemExpression;
 
-            tree.AddWhereClause(tree.TreeBuilder.Equality(
-                tree.TreeBuilder.Dot(
-                    HqlGeneratorExpressionTreeVisitor.Visit(source, queryModelVisitor.VisitorParameters).AsExpression(),
-                    tree.TreeBuilder.Class()),
-                tree.TreeBuilder.Ident(resultOperator.SearchedItemType.FullName)));
-        }
+			tree.AddWhereClause(tree.TreeBuilder.Equality(
+				tree.TreeBuilder.Dot(
+					HqlGeneratorExpressionTreeVisitor.Visit(source, queryModelVisitor.VisitorParameters).AsExpression(),
+					tree.TreeBuilder.Class()),
+				tree.TreeBuilder.Ident(resultOperator.SearchedItemType.FullName)));
+		}
 
-        private static HqlExpression BuildDot(IEnumerable<string> split, HqlTreeBuilder builder)
-        {
-            if (split.Count() == 1)
-            {
-                return builder.Ident(split.First());
-            }
-
-            return builder.Dot(builder.Ident(split.First()), BuildDot(split.Skip(1), builder));
-        }
-    }
+		#endregion
+	}
 }
