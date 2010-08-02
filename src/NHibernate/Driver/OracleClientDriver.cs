d6108ab81e84e8370f41d87fc9d1a614cd8bbebc
Fabio Maulo <fabiomaulo@gmail.com>
2010-08-02 19:14:47 +0000

    Removed unneeded breaking change (thanks to my personal advisor Fiamma)
    SVN: trunk@5097

--- src/NHibernate/Driver/OracleClientDriver.cs
+++ src/NHibernate/Driver/OracleClientDriver.cs
@@ -12,7 +12,9 @@ namespace NHibernate.Driver
 		private static readonly SqlType GuidSqlType = new SqlType(DbType.Binary, 16);
 
 		public OracleClientDriver() :
-			base("System.Data.OracleClient", "System.Data.OracleClient.OracleConnection", "System.Data.OracleClient.OracleCommand") { }
+			base("System.Data.OracleClient, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", 
+			"System.Data.OracleClient.OracleConnection", 
+			"System.Data.OracleClient.OracleCommand") { }
 
 		public override bool UseNamedPrefixInSql
 		{
