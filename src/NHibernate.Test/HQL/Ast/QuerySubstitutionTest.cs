07a49ee28f878ba606dc2cdd4ff55d2d30fe209e
Fabio Maulo <fabiomaulo@gmail.com>
2010-08-04 17:30:13 +0000

    Minor (Check NH-2089 with strings substitution)
    SVN: trunk@5108

--- src/NHibernate.Test/HQL/Ast/QuerySubstitutionTest.cs
+++ src/NHibernate.Test/HQL/Ast/QuerySubstitutionTest.cs
@@ -10,13 +10,13 @@ namespace NHibernate.Test.HQL.Ast
 		protected override void Configure(NHibernate.Cfg.Configuration configuration)
 		{
 			base.Configure(configuration);
-			configuration.SessionFactory().Integrate.CreateCommands.WithHqlToSqlSubstitutions("pizza 1");
+			configuration.SessionFactory().Integrate.CreateCommands.WithHqlToSqlSubstitutions("pizza 1, calda 'bobrock'");
 		}
-		const string query = "from SimpleClass s where s.IntValue > pizza";
 
 		[Test]
 		public void WhenSubstitutionsConfiguredThenUseItInTranslation()
 		{
+			const string query = "from SimpleClass s where s.IntValue > pizza";
 			var sql = GetSql(query, new Dictionary<string, string>{{"pizza","1"}});
 			sql.Should().Not.Contain("pizza");
 		}
@@ -24,6 +24,7 @@ namespace NHibernate.Test.HQL.Ast
 		[Test]
 		public void WhenExecutedThroughSessionThenUseSubstitutions()
 		{
+			const string query = "from SimpleClass s where s.IntValue > pizza";
 			using (var s = OpenSession())
 			{
 				using (SqlLogSpy sqlLogSpy = new SqlLogSpy())
@@ -31,6 +32,29 @@ namespace NHibernate.Test.HQL.Ast
 					s.CreateQuery(query).List();
 					string sql = sqlLogSpy.Appender.GetEvents()[0].RenderedMessage;
 					sql.Should().Not.Contain("pizza");
+				}
+			}
+		}
+
+		[Test]
+		public void WhenSubstitutionsWithStringConfiguredThenUseItInTranslation()
+		{
+			const string query = "from SimpleClass s where s.Description > calda";
+			var sql = GetSql(query, new Dictionary<string, string> { { "calda", "'bobrock'" } });
+			sql.Should().Not.Contain("pizza").And.Contain("'bobrock'");
+		}
+
+		[Test]
+		public void WhenExecutedThroughSessionThenUseSubstitutionsWithString()
+		{
+			const string query = "from SimpleClass s where s.Description > calda";
+			using (var s = OpenSession())
+			{
+				using (SqlLogSpy sqlLogSpy = new SqlLogSpy())
+				{
+					s.CreateQuery(query).List();
+					string sql = sqlLogSpy.Appender.GetEvents()[0].RenderedMessage;
+					sql.Should().Not.Contain("pizza").And.Contain("'bobrock'");
 				}
 			}
 		}
