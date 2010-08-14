a5f2b4423f8b566e085f38923d95147887f5b142
Fabio Maulo <fabiomaulo@gmail.com>
2010-08-14 17:44:56 +0000

    OracleClientDriver using DbProviderFactories
    SVN: trunk@5148

--- src/NHibernate/Driver/OracleClientDriver.cs
+++ src/NHibernate/Driver/OracleClientDriver.cs
@@ -12,7 +12,9 @@ namespace NHibernate.Driver
 		private static readonly SqlType GuidSqlType = new SqlType(DbType.Binary, 16);
 
 		public OracleClientDriver() :
-			base("System.Data.OracleClient, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", 
+			base(
+			"System.Data.OracleClient",
+			"System.Data.OracleClient, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", 
 			"System.Data.OracleClient.OracleConnection", 
 			"System.Data.OracleClient.OracleCommand") { }
 
