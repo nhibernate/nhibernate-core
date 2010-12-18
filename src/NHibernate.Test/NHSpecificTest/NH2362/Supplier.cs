faeaf6f76ef28ba4480b1425f0cef3621267d0e8
Fabio Maulo <fabiomaulo@gmail.com>
2010-12-18 16:17:48 +0000

    Tests for NH-2362 (not fixed yet)
    SVN: trunk@5327

---
+++ src/NHibernate.Test/NHSpecificTest/NH2362/Supplier.cs
@@ -0,0 +1,8 @@
+﻿namespace NHibernate.Test.NHSpecificTest.NH2362
+{
+    public class Supplier
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
