56adee434e71d6bb4991623b0420cc7b6f22f281
Ayende Rahien <ayende@ayende.com>
2010-12-27 14:12:24 +0000

    Fixing an issue where filters would not be considered for extra lazy collection queries
    SVN: trunk@5341

---
+++ src/NHibernate.Test/NHSpecificTest/BagWithLazyExtraAndFilter/Domain.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.BagWithLazyExtraAndFilter
+{
+	public class Env
+	{
+		public virtual long Id { get; set; }
+		public virtual IList<MachineRequest> RequestsFailed { get; set; }
+	}
+
+	public class MachineRequest
+	{
+		public virtual long Id { get; set; }
+		public virtual int MachineRequestCompletionStatusInt { get; set; }
+		public virtual long EnvId { get; set; }
+	}
+}
