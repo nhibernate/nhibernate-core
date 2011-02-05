145eb7f388d9fd5686f318837cedf45a270169da
Patrick Earl <patearl@patearl.net>
2011-02-05 23:18:25 +0000

    Linq: Added more tests for the Any() method.
    SVN: trunk@5375

--- src/NHibernate.Test/Linq/ByMethod/AnyTests.cs
+++ src/NHibernate.Test/Linq/ByMethod/AnyTests.cs
@@ -9,11 +9,25 @@ namespace NHibernate.Test.Linq.ByMethod
 	[TestFixture]
 	public class AnyTests : LinqTestCase
 	{
-		[Test]
-		public void AnySublist()
-		{
-			var orders = db.Orders.Where(o => o.OrderLines.Any(ol => ol.Quantity == 5)).ToList();
-			Assert.AreEqual(61, orders.Count);
-		}
+        [Test]
+        public void AnySublist()
+        {
+            var orders = db.Orders.Where(o => o.OrderLines.Any(ol => ol.Quantity == 5)).ToList();
+            Assert.AreEqual(61, orders.Count);
+
+            orders = db.Orders.Where(o => o.OrderLines.Any(ol => ol.Order == null)).ToList();
+            Assert.AreEqual(0, orders.Count);
+        }
+
+        [Test]
+        public void NestedAny()
+        {
+            var test = (from c in db.Customers 
+                       where c.ContactName == "Bob" &&
+                                 (c.CompanyName == "NormalooCorp" ||
+                                  c.Orders.Any(o => o.OrderLines.Any(ol => ol.Discount < 20 && ol.Discount >= 10)))
+                       select c).ToList();
+            Assert.AreEqual(0, test.Count);
+        }
 	}
 }
