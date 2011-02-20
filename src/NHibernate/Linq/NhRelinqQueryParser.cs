d445d7564ba161ed7686a89b028d21b8fec899a8
Patrick Earl <patearl@patearl.net>
2011-02-20 05:11:20 +0000

    Upgraded ReLinq from 1.13.88 to 1.13.93, making required changes.
    SVN: trunk@5386

--- src/NHibernate/Linq/NhRelinqQueryParser.cs
+++ src/NHibernate/Linq/NhRelinqQueryParser.cs
@@ -1,65 +1,87 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using Remotion.Data.Linq;
 using Remotion.Data.Linq.Clauses;
 using Remotion.Data.Linq.Clauses.StreamedData;
 using Remotion.Data.Linq.EagerFetching.Parsing;
+using Remotion.Data.Linq.Parsing.ExpressionTreeVisitors.Transformation;
 using Remotion.Data.Linq.Parsing.Structure;
 using Remotion.Data.Linq.Parsing.Structure.IntermediateModel;
-using AggregateExpressionNode = NHibernate.Linq.Expressions.AggregateExpressionNode;
+using Remotion.Data.Linq.Parsing.Structure.NodeTypeProviders;
 
 namespace NHibernate.Linq
 {
     public static class NhRelinqQueryParser
     {
-        public static readonly MethodCallExpressionNodeTypeRegistry MethodCallRegistry =
-            MethodCallExpressionNodeTypeRegistry.CreateDefault();
+    	private static readonly QueryParser _queryParser;
 
         static NhRelinqQueryParser()
         {
-            MethodCallRegistry.Register(
-                new[]
-                    {
-                        MethodCallExpressionNodeTypeRegistry.GetRegisterableMethodDefinition(
-                            ReflectionHelper.GetMethodDefinition(() => Queryable.Aggregate<object>(null, null))),
-                        MethodCallExpressionNodeTypeRegistry.GetRegisterableMethodDefinition(
-                            ReflectionHelper.GetMethodDefinition(() => Queryable.Aggregate<object, object>(null, null, null)))
-                    },
-                typeof (AggregateExpressionNode));
+        	var nodeTypeProvider = new NHibernateNodeTypeProvider();
 
-					// Here would be useful to have something like our ReflectionHelper.IsMethodOf because it is impossible to know
-					// which is the implementation of ICollection<T> used by our user. Reported to Stefan Wenig as possible (via mail instead of their JIAR)
-        	MethodCallRegistry.Register(
-        		new[]
-        			{
-								MethodCallExpressionNodeTypeRegistry.GetRegisterableMethodDefinition(
-								  ReflectionHelper.GetMethodDefinition((List<object> l) => l.Contains(null))),
-								typeof (ICollection<>).GetMethod("Contains"),
-        			},
-        		typeof (ContainsExpressionNode));
+			var transformerRegistry = ExpressionTransformerRegistry.CreateDefault();
+			// Register custom expression transformers here:
+			// transformerRegistry.Register (new MyExpressionTransformer());
 
-            MethodCallRegistry.Register(new[] { typeof(EagerFetchingExtensionMethods).GetMethod("Fetch") }, typeof(FetchOneExpressionNode));
-            MethodCallRegistry.Register(new[] { typeof(EagerFetchingExtensionMethods).GetMethod("FetchMany") }, typeof(FetchManyExpressionNode));
-            MethodCallRegistry.Register(new[] { typeof(EagerFetchingExtensionMethods).GetMethod("ThenFetch") }, typeof(ThenFetchOneExpressionNode));
-            MethodCallRegistry.Register(new[] { typeof(EagerFetchingExtensionMethods).GetMethod("ThenFetchMany") }, typeof(ThenFetchManyExpressionNode));
+			var processor = ExpressionTreeParser.CreateDefaultProcessor(transformerRegistry);
+			// Add custom processors here:
+			// processor.InnerProcessors.Add (new MyExpressionTreeProcessor());
 
-            MethodCallRegistry.Register(
-                new[]
+			var expressionTreeParser = new ExpressionTreeParser(nodeTypeProvider, processor);
+
+			_queryParser = new QueryParser(expressionTreeParser);			
+        }
+
+        public static QueryModel Parse(Expression expression)
+        {
+            return _queryParser.GetParsedQuery(expression);
+        }
+    }
+
+	public class NHibernateNodeTypeProvider : INodeTypeProvider
+	{
+		private INodeTypeProvider defaultNodeTypeProvider;
+
+		public NHibernateNodeTypeProvider()
+		{
+			var methodInfoRegistry = new MethodInfoBasedNodeTypeRegistry();
+
+			methodInfoRegistry.Register(new[] { typeof(EagerFetchingExtensionMethods).GetMethod("Fetch") }, typeof(FetchOneExpressionNode));
+			methodInfoRegistry.Register(new[] { typeof(EagerFetchingExtensionMethods).GetMethod("FetchMany") }, typeof(FetchManyExpressionNode));
+			methodInfoRegistry.Register(new[] { typeof(EagerFetchingExtensionMethods).GetMethod("ThenFetch") }, typeof(ThenFetchOneExpressionNode));
+			methodInfoRegistry.Register(new[] { typeof(EagerFetchingExtensionMethods).GetMethod("ThenFetchMany") }, typeof(ThenFetchManyExpressionNode));
+
+			methodInfoRegistry.Register(
+				new[]
                     {
                         typeof(LinqExtensionMethods).GetMethod("Cacheable"),
                         typeof(LinqExtensionMethods).GetMethod("CacheMode"),
                         typeof(LinqExtensionMethods).GetMethod("CacheRegion"),
                     }, typeof(CacheableExpressionNode));
 
-        }
+			var nodeTypeProvider = ExpressionTreeParser.CreateDefaultNodeTypeProvider();
+			nodeTypeProvider.InnerProviders.Add(methodInfoRegistry);
+			defaultNodeTypeProvider = nodeTypeProvider;
+		}
 
-        public static QueryModel Parse(Expression expression)
-        {
-            return new QueryParser(new ExpressionTreeParser(MethodCallRegistry)).GetParsedQuery(expression);
-        }
-    }
+		public bool IsRegistered(MethodInfo method)
+		{
+			// Avoid Relinq turning IDictionary.Contains into ContainsResultOperator.  We do our own processing for that method.
+			if (method.DeclaringType == typeof(IDictionary) && method.Name == "Contains")
+				return false;
+
+			return defaultNodeTypeProvider.IsRegistered(method);
+		}
+
+		public System.Type GetNodeType(MethodInfo method)
+		{
+			return defaultNodeTypeProvider.GetNodeType(method);
+		}
+	}
 
     public class CacheableExpressionNode : ResultOperatorExpressionNodeBase
     {
