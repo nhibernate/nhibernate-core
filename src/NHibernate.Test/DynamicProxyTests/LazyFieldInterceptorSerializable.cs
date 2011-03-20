1a056fc48bc878ed154fbba47579e5c4c18435f9
Fabio Maulo <fabiomaulo@gmail.com>
2011-03-20 20:34:55 +0000

    "lazy-field" interceptor proxy Serializable
    SVN: trunk@5482

---
+++ src/NHibernate.Test/DynamicProxyTests/LazyFieldInterceptorSerializable.cs
@@ -0,0 +1,37 @@
+using System;
+using Iesi.Collections.Generic;
+using NHibernate.Proxy;
+using NUnit.Framework;
+using NHibernate.Intercept;
+using SharpTestsEx;
+
+namespace NHibernate.Test.DynamicProxyTests
+{
+	public class LazyFieldInterceptorSerializable
+	{
+		[Serializable]
+		public class MyClass
+		{
+			public virtual int Id { get; set; }
+		}
+
+		[Test]
+		public void LazyFieldInterceptorMarkedAsSerializable()
+		{
+			typeof(DefaultDynamicLazyFieldInterceptor).Should().Have.Attribute<SerializableAttribute>();
+		}
+
+		[Test]
+		public void LazyFieldInterceptorIsBinarySerializable()
+		{
+			var pf = new DefaultProxyFactory();
+			var propertyInfo = typeof(MyClass).GetProperty("Id");
+			pf.PostInstantiate("MyClass", typeof(MyClass), new HashedSet<System.Type>(), propertyInfo.GetGetMethod(), propertyInfo.GetSetMethod(), null);
+			var fieldInterceptionProxy = (IFieldInterceptorAccessor)pf.GetFieldInterceptionProxy(new MyClass());
+			fieldInterceptionProxy.FieldInterceptor = new DefaultFieldInterceptor(null, null, null, "MyClass", typeof(MyClass));
+
+			fieldInterceptionProxy.Should().Be.BinarySerializable();
+		}
+
+	}
+}
