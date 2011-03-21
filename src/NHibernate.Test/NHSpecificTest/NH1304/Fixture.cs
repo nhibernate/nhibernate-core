65414c82d7260ebf64378b1b31d648c25c2b6258
Fabio Maulo <fabiomaulo@gmail.com>
2011-03-21 21:27:07 +0000

    Minor (refactorized test to be sure that it will be executed)
    SVN: trunk@5498

--- src/NHibernate.Test/NHSpecificTest/NH1304/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH1304/Fixture.cs
@@ -1,27 +1,33 @@
 using System.Text;
 using log4net.Core;
+using NHibernate.Cfg;
 using NHibernate.Tuple.Entity;
 using NUnit.Framework;
 namespace NHibernate.Test.NHSpecificTest.NH1304
 {
 	[TestFixture]
-	public class Fixture : BugTestCase
+	public class Fixture
 	{
-		protected override void BuildSessionFactory()
+		[Test]
+		public void WhenNoCustomAccessorIsDefinedThenSholdFindOnlyNoCustom()
 		{
-			using (LogSpy ls = new LogSpy(typeof (AbstractEntityTuplizer)))
+			var cfg = new Configuration();
+			if (TestConfigurationHelper.hibernateConfigFile != null)
+				cfg.Configure(TestConfigurationHelper.hibernateConfigFile);
+			cfg.AddResource("NHibernate.Test.NHSpecificTest.NH1304.Mappings.hbm.xml", GetType().Assembly);
+			using (LogSpy ls = new LogSpy(typeof(AbstractEntityTuplizer)))
 			{
-				base.BuildSessionFactory();
+				cfg.BuildSessionFactory();
 				StringBuilder wholeMessage = new StringBuilder();
 				foreach (LoggingEvent loggingEvent in ls.Appender.GetEvents())
 				{
 					string singleMessage = loggingEvent.RenderedMessage;
 					if (singleMessage.IndexOf("AbstractEntityTuplizer") > 0)
-						Assert.Greater(singleMessage.IndexOf("No custom accessors found"), -1);
+						Assert.Greater(singleMessage.IndexOf("No custom"), -1);
 					wholeMessage.Append(singleMessage);
 				}
 				string logs = wholeMessage.ToString();
-				Assert.AreEqual(-1, logs.IndexOf("Custom accessors found"));
+				Assert.AreEqual(-1, logs.IndexOf("Custom"));
 			}
 		}
 	}
