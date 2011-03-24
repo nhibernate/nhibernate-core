50a04b8e1f887f4a84334f819d79b6a8f0aead75
Fabio Maulo <fabiomaulo@gmail.com>
2011-03-24 21:07:55 +0000

    Passing tests for NH-2527 using no-batcher
    SVN: trunk@5524

---
+++ src/NHibernate.Test/NHSpecificTest/NH2527/FixtureWithNoBatcher.cs
@@ -0,0 +1,78 @@
+﻿using NHibernate.AdoNet;
+using NHibernate.Cfg.Loquacious;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH2527
+{
+	public class FixtureWithNoBatcher : BugTestCase
+	{
+
+		protected override void Configure(Cfg.Configuration configuration)
+		{
+			configuration.DataBaseIntegration(x =>
+																				{
+																					x.BatchSize = 0;
+																					x.Batcher<NonBatchingBatcherFactory>();
+																				});
+		}
+
+		protected override void OnSetUp()
+		{
+			base.OnSetUp();
+
+			using (ISession session = OpenSession())
+			{
+				using (ITransaction tx = session.BeginTransaction())
+				{
+					var parent = new Parent();
+					var childOne = new Child();
+					parent.Childs.Add(childOne);
+					session.Save(parent);
+
+					tx.Commit();
+				}
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			base.OnTearDown();
+
+			using (ISession session = OpenSession())
+			{
+				using (ITransaction tx = session.BeginTransaction())
+				{
+					session.Delete("from Child");
+					session.Delete("from Parent");
+					tx.Commit();
+				}
+			}
+		}
+
+		[Test]
+		public void DisposedCommandShouldNotBeReused()
+		{
+			using (ISession session = OpenSession())
+			{
+				using (ITransaction tx = session.BeginTransaction())
+				{
+					var parent = session.CreateCriteria<Parent>().UniqueResult<Parent>();
+
+					Child childOne = parent.Childs[0];
+
+					var childTwo = new Child();
+					parent.Childs.Add(childTwo);
+
+					Child childToMove = parent.Childs[1];
+					parent.Childs.RemoveAt(1);
+					parent.Childs.Insert(0, childToMove);
+
+					Assert.DoesNotThrow(() => { tx.Commit(); });
+
+					Assert.AreEqual(childTwo.Id, parent.Childs[0].Id);
+					Assert.AreEqual(childOne.Id, parent.Childs[1].Id);
+				}
+			}
+		}
+	}
+}
