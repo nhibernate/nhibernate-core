1f6ae7d79161dd8175ac3bd852dfbe5aeedd6c85
Patrick Earl <patearl@patearl.net>
2011-03-24 02:50:00 +0000

    Improved SQLite type mapping to match the System.Data.SQLite provider, which is by far the dominant provider.
    SVN: trunk@5515

--- src/NHibernate/Dialect/SQLiteDialect.cs
+++ src/NHibernate/Dialect/SQLiteDialect.cs
@@ -24,28 +24,28 @@ namespace NHibernate.Dialect
 		public SQLiteDialect()
 		{
 			RegisterColumnType(DbType.Binary, "BLOB");
-			RegisterColumnType(DbType.Byte, "INTEGER");
-			RegisterColumnType(DbType.Int16, "INTEGER");
-			RegisterColumnType(DbType.Int32, "INTEGER");
-			RegisterColumnType(DbType.Int64, "INTEGER");
+            RegisterColumnType(DbType.Byte, "TINYINT");
+            RegisterColumnType(DbType.Int16, "SMALLINT");
+			RegisterColumnType(DbType.Int32, "INT");
+			RegisterColumnType(DbType.Int64, "BIGINT");
 			RegisterColumnType(DbType.SByte, "INTEGER");
 			RegisterColumnType(DbType.UInt16, "INTEGER");
 			RegisterColumnType(DbType.UInt32, "INTEGER");
 			RegisterColumnType(DbType.UInt64, "INTEGER");
 			RegisterColumnType(DbType.Currency, "NUMERIC");
 			RegisterColumnType(DbType.Decimal, "NUMERIC");
-			RegisterColumnType(DbType.Double, "NUMERIC");
-			RegisterColumnType(DbType.Single, "NUMERIC");
+			RegisterColumnType(DbType.Double, "DOUBLE");
+			RegisterColumnType(DbType.Single, "DOUBLE");
 			RegisterColumnType(DbType.VarNumeric, "NUMERIC");
 			RegisterColumnType(DbType.AnsiString, "TEXT");
 			RegisterColumnType(DbType.String, "TEXT");
 			RegisterColumnType(DbType.AnsiStringFixedLength, "TEXT");
 			RegisterColumnType(DbType.StringFixedLength, "TEXT");
 
-			RegisterColumnType(DbType.Date, "DATETIME"); 
+			RegisterColumnType(DbType.Date, "DATE"); 
 			RegisterColumnType(DbType.DateTime, "DATETIME");
-			RegisterColumnType(DbType.Time, "DATETIME");
-			RegisterColumnType(DbType.Boolean, "INTEGER");
+			RegisterColumnType(DbType.Time, "TIME");
+			RegisterColumnType(DbType.Boolean, "BOOL");
 			RegisterColumnType(DbType.Guid, "UNIQUEIDENTIFIER");
 
             // Using strftime returns 0-padded strings.  '07' <> 7, so it is better to convert to an integer.
