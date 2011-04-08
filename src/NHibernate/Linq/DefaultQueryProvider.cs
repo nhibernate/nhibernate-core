9da06c58694ff7b628b123c562f605d2f9dc5eaa
Fabio Maulo <fabiomaulo@gmail.com>
2011-04-08 05:28:16 +0000

    Minor (reformatting)
    SVN: trunk@5638

--- src/NHibernate/Linq/DefaultQueryProvider.cs
+++ src/NHibernate/Linq/DefaultQueryProvider.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,95 +18,27 @@ namespace NHibernate.Linq
 			_session = session;
 		}
 
+		#region IQueryProvider Members
+
 		public object Execute(Expression expression)
 		{
-		    IQuery query;
-		    NhLinqExpression nhQuery;
-		    NhLinqExpression nhLinqExpression = PrepareQuery(expression, out query, out nhQuery);
-            
+			IQuery query;
+			NhLinqExpression nhQuery;
+			NhLinqExpression nhLinqExpression = PrepareQuery(expression, out query, out nhQuery);
+
 			return ExecuteQuery(nhLinqExpression, query, nhQuery);
 		}
 
-        public object ExecuteFuture(Expression expression)
-        {
-            IQuery query;
-            NhLinqExpression nhQuery;
-            NhLinqExpression nhLinqExpression = PrepareQuery(expression, out query, out nhQuery);
-            return ExecuteFutureQuery(nhLinqExpression, query, nhQuery); 
-        }
-
-	    private NhLinqExpression PrepareQuery(Expression expression, out IQuery query, out NhLinqExpression nhQuery)
-	    {
-	        var nhLinqExpression = new NhLinqExpression(expression);
-
-	        query = _session.CreateQuery(nhLinqExpression);
-
-	        nhQuery = query.As<ExpressionQueryImpl>().QueryExpression.As<NhLinqExpression>();
-
-	        SetParameters(query, nhLinqExpression.ParameterValuesByName);
-	        SetResultTransformerAndAdditionalCriteria(query, nhQuery, nhLinqExpression.ParameterValuesByName);
-	        return nhLinqExpression;
-	    }
-
-	    private object ExecuteFutureQuery(NhLinqExpression nhLinqExpression, IQuery query, NhLinqExpression nhQuery)
-	    {
-	        MethodInfo method;
-            if (nhLinqExpression.ReturnType == NhLinqExpressionReturnType.Sequence)
-	        {
-	            method = typeof (IQuery).GetMethod("Future").MakeGenericMethod(nhQuery.Type);
-	        }
-            else
-            {
-                method = typeof(IQuery).GetMethod("FutureValue").MakeGenericMethod(nhQuery.Type);
-            }
-
-	        var result = method.Invoke(query, new object[0]);
-
-
-            
-	        if (nhQuery.ExpressionToHqlTranslationResults.PostExecuteTransformer != null)
-	        {
-	            ((IDelayedValue) result).ExecuteOnEval = nhQuery.ExpressionToHqlTranslationResults.PostExecuteTransformer;
-	        }
-
-	        return result;
-
-	    }
-
-	    private object ExecuteQuery(NhLinqExpression nhLinqExpression, IQuery query, NhLinqExpression nhQuery)
-	    {
-	        var results = query.List();
-
-	        if (nhQuery.ExpressionToHqlTranslationResults.PostExecuteTransformer != null)
-	        {
-	            try
-	            {
-	                return nhQuery.ExpressionToHqlTranslationResults.PostExecuteTransformer.DynamicInvoke(results.AsQueryable());
-	            }
-	            catch (TargetInvocationException e)
-	            {
-	                throw e.InnerException;
-	            }
-	        }
-
-	        if (nhLinqExpression.ReturnType == NhLinqExpressionReturnType.Sequence)
-	        {
-	            return results.AsQueryable();
-	        }
-
-	        return results[0];
-	    }
-
-	    public TResult Execute<TResult>(Expression expression)
+		public TResult Execute<TResult>(Expression expression)
 		{
 			return (TResult) Execute(expression);
 		}
 
 		public IQueryable CreateQuery(Expression expression)
 		{
-		    var m = ReflectionHelper.GetMethodDefinition((DefaultQueryProvider p) => p.CreateQuery<object>(null)).MakeGenericMethod(expression.Type.GetGenericArguments()[0]);
+			MethodInfo m = ReflectionHelper.GetMethodDefinition((DefaultQueryProvider p) => p.CreateQuery<object>(null)).MakeGenericMethod(expression.Type.GetGenericArguments()[0]);
 
-		    return (IQueryable) m.Invoke(this, new[] {expression});
+			return (IQueryable) m.Invoke(this, new[] {expression});
 		}
 
 		public IQueryable<T> CreateQuery<T>(Expression expression)
@@ -115,63 +46,132 @@ namespace NHibernate.Linq
 			return new NhQueryable<T>(this, expression);
 		}
 
-		static void SetParameters(IQuery query, IDictionary<string, Tuple<object, IType>> parameters)
-		{
-			foreach (var parameterName in query.NamedParameters)
-			{
-			    var param = parameters[parameterName];
+		#endregion
 
-                if (param.First == null)
-                {
-                    if (typeof(ICollection).IsAssignableFrom(param.Second.ReturnedClass))
-                    {
-                        query.SetParameterList(parameterName, null, param.Second);
-                    }
-                    else
-                    {
-                        query.SetParameter(parameterName, null, param.Second);
-                    }
-                }
-                else
-                {
-                    if (param.First is ICollection)
-                    {
-                        query.SetParameterList(parameterName, (ICollection) param.First);
-                    }
-                    else if (param.Second != null)
-                    {
-                        query.SetParameter(parameterName, param.First, param.Second);
-                    }
-                    else
-                    {
-                    	query.SetParameter(parameterName, param.First);
-                    }
-                }
+		public object ExecuteFuture(Expression expression)
+		{
+			IQuery query;
+			NhLinqExpression nhQuery;
+			NhLinqExpression nhLinqExpression = PrepareQuery(expression, out query, out nhQuery);
+			return ExecuteFutureQuery(nhLinqExpression, query, nhQuery);
+		}
+
+		private NhLinqExpression PrepareQuery(Expression expression, out IQuery query, out NhLinqExpression nhQuery)
+		{
+			var nhLinqExpression = new NhLinqExpression(expression);
+
+			query = _session.CreateQuery(nhLinqExpression);
+
+			nhQuery = query.As<ExpressionQueryImpl>().QueryExpression.As<NhLinqExpression>();
+
+			SetParameters(query, nhLinqExpression.ParameterValuesByName);
+			SetResultTransformerAndAdditionalCriteria(query, nhQuery, nhLinqExpression.ParameterValuesByName);
+			return nhLinqExpression;
+		}
+
+		private object ExecuteFutureQuery(NhLinqExpression nhLinqExpression, IQuery query, NhLinqExpression nhQuery)
+		{
+			MethodInfo method;
+			if (nhLinqExpression.ReturnType == NhLinqExpressionReturnType.Sequence)
+			{
+				method = typeof (IQuery).GetMethod("Future").MakeGenericMethod(nhQuery.Type);
+			}
+			else
+			{
+				method = typeof (IQuery).GetMethod("FutureValue").MakeGenericMethod(nhQuery.Type);
+			}
+
+			object result = method.Invoke(query, new object[0]);
+
+
+			if (nhQuery.ExpressionToHqlTranslationResults.PostExecuteTransformer != null)
+			{
+				((IDelayedValue) result).ExecuteOnEval = nhQuery.ExpressionToHqlTranslationResults.PostExecuteTransformer;
+			}
+
+			return result;
+		}
+
+		private object ExecuteQuery(NhLinqExpression nhLinqExpression, IQuery query, NhLinqExpression nhQuery)
+		{
+			IList results = query.List();
+
+			if (nhQuery.ExpressionToHqlTranslationResults.PostExecuteTransformer != null)
+			{
+				try
+				{
+					return nhQuery.ExpressionToHqlTranslationResults.PostExecuteTransformer.DynamicInvoke(results.AsQueryable());
+				}
+				catch (TargetInvocationException e)
+				{
+					throw e.InnerException;
+				}
+			}
+
+			if (nhLinqExpression.ReturnType == NhLinqExpressionReturnType.Sequence)
+			{
+				return results.AsQueryable();
+			}
+
+			return results[0];
+		}
+
+		private static void SetParameters(IQuery query, IDictionary<string, Tuple<object, IType>> parameters)
+		{
+			foreach (string parameterName in query.NamedParameters)
+			{
+				Tuple<object, IType> param = parameters[parameterName];
+
+				if (param.First == null)
+				{
+					if (typeof (ICollection).IsAssignableFrom(param.Second.ReturnedClass))
+					{
+						query.SetParameterList(parameterName, null, param.Second);
+					}
+					else
+					{
+						query.SetParameter(parameterName, null, param.Second);
+					}
+				}
+				else
+				{
+					if (param.First is ICollection)
+					{
+						query.SetParameterList(parameterName, (ICollection) param.First);
+					}
+					else if (param.Second != null)
+					{
+						query.SetParameter(parameterName, param.First, param.Second);
+					}
+					else
+					{
+						query.SetParameter(parameterName, param.First);
+					}
+				}
 			}
 		}
 
-        public void SetResultTransformerAndAdditionalCriteria(IQuery query, NhLinqExpression nhExpression, IDictionary<string, Tuple<object, IType>> parameters)
-        {
-            query.SetResultTransformer(nhExpression.ExpressionToHqlTranslationResults.ResultTransformer);
+		public void SetResultTransformerAndAdditionalCriteria(IQuery query, NhLinqExpression nhExpression, IDictionary<string, Tuple<object, IType>> parameters)
+		{
+			query.SetResultTransformer(nhExpression.ExpressionToHqlTranslationResults.ResultTransformer);
 
-            foreach (var criteria in nhExpression.ExpressionToHqlTranslationResults.AdditionalCriteria)
-            {
-                criteria(query, parameters);
-            }
-        }
+			foreach (var criteria in nhExpression.ExpressionToHqlTranslationResults.AdditionalCriteria)
+			{
+				criteria(query, parameters);
+			}
+		}
 	}
 
-    public class Tuple<T1, T2>
-    {
-        public T1 First { get; set; }
-        public T2 Second { get; set; }
- 
-    }
+	public class Tuple<T1, T2>
+	{
+		public T1 First { get; set; }
+		public T2 Second { get; set; }
+	}
 
-    public class Tuple<T1, T2, T3>
-    {
-        public T1 First { get; set; }
-        public T2 Second { get; set; }
-        public T3 Third { get; set; }
-    }
+	public class Tuple<T1, T2, T3>
+	{
+		public T1 First { get; set; }
+		public T2 Second { get; set; }
+		public T3 Third { get; set; }
+	}
 }
