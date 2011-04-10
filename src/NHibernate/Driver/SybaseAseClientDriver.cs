11573f8d185258d87ff60f3d5d255e03ec32b16e
Julian Maughan <julian.maughan@gmail.com>
2011-04-10 16:12:26 +0000

    Added Sybase ASE 15 support (NH-2526). Predominantly a port from Hibernate, with some refinements.
    SVN: trunk@5649

---
+++ src/NHibernate/Driver/SybaseAseClientDriver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NHibernate.Driver
+{
+	/// <summary>
+	/// This provides a driver for Sybase ASE 15 using the ADO.NET driver.
+	/// </summary>
+	/// <remarks>
+	/// You will need the following libraries available to your application:
+	/// <ul>
+	/// <li>Sybase.AdoNet2.AseClient.dll</li>
+	/// <li>sybdrvado20.dll</li>
+	/// </ul>
+	/// </remarks>
+	public class SybaseAseClientDriver : ReflectionBasedDriver
+	{
+		public SybaseAseClientDriver() : base("Sybase.AdoNet2.AseClient", "Sybase.Data.AseClient.AseConnection", "Sybase.Data.AseClient.AseCommand")
+		{
+		}
+		
+		public override string NamedPrefix
+		{
+			get { return "@"; }
+		}
+		
+		public override bool UseNamedPrefixInParameter
+		{
+			get { return true; }
+		}
+		
+		public override bool UseNamedPrefixInSql
+		{
+			get { return true; }
+		}
+	}
+}
