7fb74c86961d3b22c9d90ac037338bdfca696ec1
Fabio Maulo <fabiomaulo@gmail.com>
2011-04-20 21:46:13 +0000

    DynamicComponentMapper on the road
    SVN: trunk@5729

--- src/NHibernate/Mapping/ByCode/Impl/DynamicComponentMapper.cs
+++ src/NHibernate/Mapping/ByCode/Impl/DynamicComponentMapper.cs
@@ -5,20 +5,18 @@ using NHibernate.Cfg.MappingSchema;
 
 namespace NHibernate.Mapping.ByCode.Impl
 {
-	public class DynamicComponentMapper : IDynamicComponentMapper
+	public class DynamicComponentMapper : AbstractPropertyContainerMapper, IDynamicComponentMapper
 	{
 		private readonly HbmDynamicComponent component;
-		private readonly HbmMapping mapDoc;
 		private readonly IAccessorPropertyMapper accessorPropertyMapper;
 
-		public DynamicComponentMapper(HbmDynamicComponent component, MemberInfo declaringTypeMember, HbmMapping mapDoc)
+		public DynamicComponentMapper(HbmDynamicComponent component, MemberInfo declaringTypeMember, HbmMapping mapDoc) : base(declaringTypeMember.DeclaringType, mapDoc)
 		{
 			this.component = component;
-			this.mapDoc = mapDoc;
 			accessorPropertyMapper = new AccessorPropertyMapper(declaringTypeMember.DeclaringType, declaringTypeMember.Name, x => component.access = x);
 		}
 
-		private void AddProperty(object property)
+		protected override void AddProperty(object property)
 		{
 			if (property == null)
 			{
@@ -26,6 +24,11 @@ namespace NHibernate.Mapping.ByCode.Impl
 			}
 			var toAdd = new[] { property };
 			component.Items = component.Items == null ? toAdd : component.Items.Concat(toAdd).ToArray();
+		}
+
+		protected override bool IsMemberSupportedByMappedContainer(MemberInfo property)
+		{
+			return true;
 		}
 
 		public void Access(Accessor accessor)
@@ -51,63 +54,6 @@ namespace NHibernate.Mapping.ByCode.Impl
 		public void Insert(bool consideredInInsertQuery)
 		{
 			component.insert = consideredInInsertQuery;
-		}
-
-		public void Set(MemberInfo property, Action<ISetPropertiesMapper> collectionMapping, Action<ICollectionElementRelation> mapping)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Bag(MemberInfo property, Action<IBagPropertiesMapper> collectionMapping, Action<ICollectionElementRelation> mapping)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void List(MemberInfo property, Action<IListPropertiesMapper> collectionMapping, Action<ICollectionElementRelation> mapping)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Map(MemberInfo property, Action<IMapPropertiesMapper> collectionMapping, Action<IMapKeyRelation> keyMapping, Action<ICollectionElementRelation> mapping)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void IdBag(MemberInfo property, Action<IIdBagPropertiesMapper> collectionMapping, Action<ICollectionElementRelation> mapping)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Property(MemberInfo property, Action<IPropertyMapper> mapping)
-		{
-			var hbmProperty = new HbmProperty { name = property.Name };
-			mapping(new PropertyMapper(property, hbmProperty));
-			AddProperty(hbmProperty);
-		}
-
-		public void Component(MemberInfo property, Action<IComponentMapper> mapping)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Component(MemberInfo property, Action<IDynamicComponentMapper> mapping)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void ManyToOne(MemberInfo property, Action<IManyToOneMapper> mapping)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Any(MemberInfo property, System.Type idTypeOfMetaType, Action<IAnyMapper> mapping)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void OneToOne(MemberInfo property, Action<IOneToOneMapper> mapping)
-		{
-			throw new NotImplementedException();
 		}
 	}
 }
