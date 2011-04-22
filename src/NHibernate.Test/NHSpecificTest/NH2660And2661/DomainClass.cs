f392f7e2aec4f18cbb0278366260e4d305713be6
Fabio Maulo <fabiomaulo@gmail.com>
2011-04-22 21:50:29 +0000

    Tests for NH-2660 and NH-2661
    SVN: trunk@5750

---
+++ src/NHibernate.Test/NHSpecificTest/NH2660And2661/DomainClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH2660And2661
+{
+    public class DomainClass
+    {
+        private int id;
+        private DateTime data;
+        public int Id { get { return id; } set { id = value; } }
+        public DateTime Data { get { return data; } set { data = value; } }
+    }
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH2660And2661/Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NHibernate.Dialect;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Test.NHSpecificTest.NH2660And2661
+{
+    [TestFixture]
+    public class Test : BugTestCase
+    {
+        protected override void OnSetUp()
+        {
+            base.OnSetUp();
+            using (ISession session = OpenSession())
+            {
+                DomainClass entity = new DomainClass { Id = 1, Data = DateTime.Parse("10:00") };
+                session.Save(entity);
+                session.Flush();
+            }
+        }
+
+        protected override void OnTearDown()
+        {
+            base.OnTearDown();
+            using (ISession session = OpenSession())
+            {
+								session.CreateQuery("delete from DomainClass").ExecuteUpdate();
+                session.Flush();
+            }
+        }
+
+        protected override bool AppliesTo(Dialect.Dialect dialect)
+        {
+            return dialect is MsSql2008Dialect;
+        }
+
+        [Test, Ignore("workaround to sqlserver DP, not fixed yet")]
+        public void ShouldBeAbleToQueryEntity()
+        {
+            using (ISession session = OpenSession())
+            {
+               var query =
+                    session.CreateQuery(
+                        @"from DomainClass entity where Data = :data");
+                query.SetParameter("data", DateTime.Parse("10:00"), NHibernateUtil.Time);
+                query.Executing(x=> x.List()).NotThrows();
+            }
+        }
+    }
+}
