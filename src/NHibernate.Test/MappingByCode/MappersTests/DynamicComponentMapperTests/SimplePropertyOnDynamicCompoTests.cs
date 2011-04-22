8dbaec61597827cb465bddeae8060e54059d0128
Fabio Maulo <fabiomaulo@gmail.com>
2011-04-22 15:36:27 +0000

    Passing tests to add simple property to a dynamic-component
    SVN: trunk@5738

---
+++ src/NHibernate.Test/MappingByCode/MappersTests/DynamicComponentMapperTests/SimplePropertyOnDynamicCompoTests.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Linq;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Impl;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Test.MappingByCode.MappersTests.DynamicComponentMapperTests
+{
+	public class SimplePropertyOnDynamicCompoTests
+	{
+		private class Person
+		{
+			public int Id { get; set; }
+			private IDictionary info;
+			public IDictionary Info
+			{
+				get { return info; }
+			}
+		}
+		
+		[Test]
+		public void WhenAddThenHas()
+		{
+			var mapdoc = new HbmMapping();
+			var component = new HbmDynamicComponent();
+			var mapper = new DynamicComponentMapper(component, For<Person>.Property(p => p.Info), mapdoc);
+			var propertyInfo = (new { A = 5 }).GetType().GetProperty("A");
+			
+			mapper.Property(propertyInfo, x => { });
+			
+			component.Properties.Select(x=> x.Name).Should().Have.SameSequenceAs("A");
+		}
+
+		[Test]
+		public void WhenCustomizeThenCallCustomizer()
+		{
+			var mapdoc = new HbmMapping();
+			var component = new HbmDynamicComponent();
+			var mapper = new DynamicComponentMapper(component, For<Person>.Property(p => p.Info), mapdoc);
+			var propertyInfo = (new { A = 5 }).GetType().GetProperty("A");
+			var called = false;
+			mapper.Property(propertyInfo, x => called = true);
+
+			called.Should().Be.True();
+		}
+
+		[Test]
+		public void WhenCustomizeAccessorThenIgnore()
+		{
+			var mapdoc = new HbmMapping();
+			var component = new HbmDynamicComponent();
+			var mapper = new DynamicComponentMapper(component, For<Person>.Property(p => p.Info), mapdoc);
+			var propertyInfo = (new { A = 5 }).GetType().GetProperty("A");
+
+			mapper.Property(propertyInfo, x => x.Access(Accessor.Field));
+
+			component.Properties.OfType<HbmProperty>().Single().Access.Should().Be.NullOrEmpty();
+		}
+	}
+}
