02abe4350da6e02c591b6ae49e8a5126dfb63166
Fabio Maulo <fabiomaulo@gmail.com>
2011-04-26 19:20:39 +0000

    ComposedIdMapper
    SVN: trunk@5768

---
+++ src/NHibernate.Test/MappingByCode/MappersTests/ComposedIdMapperTests.cs
@@ -0,0 +1,76 @@
+using System.Linq;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode.Impl;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Test.MappingByCode.MappersTests
+{
+	public class ComposedIdMapperTests
+	{
+		private class Person
+		{
+			public string Email { get; set; }
+			public User User { get; set; }
+		}
+
+		private class User
+		{
+			public int Id { get; set; }
+		}
+
+		[Test]
+		public void CanMapProperty()
+		{
+			var mapdoc = new HbmMapping();
+			var compositeId = new HbmCompositeId();
+			var mapper = new ComposedIdMapper(compositeId, mapdoc);
+
+			mapper.Property(For<Person>.Property(ts => ts.Email), x => { });
+
+			compositeId.Items.Should().Have.Count.EqualTo(1);
+			compositeId.Items.First().Should().Be.OfType<HbmKeyProperty>();
+			compositeId.Items.OfType<HbmKeyProperty>().First().Name.Should().Be.EqualTo("Email");
+		}
+
+		[Test]
+		public void CallPropertyMapper()
+		{
+			var mapdoc = new HbmMapping();
+			var compositeId = new HbmCompositeId();
+			var mapper = new ComposedIdMapper(compositeId, mapdoc);
+			var called = false;
+
+			mapper.Property(For<Person>.Property(ts => ts.Email), x => called = true);
+
+			called.Should().Be.True();
+		}
+
+		[Test]
+		public void CanMapManyToOne()
+		{
+			var mapdoc = new HbmMapping();
+			var compositeId = new HbmCompositeId();
+			var mapper = new ComposedIdMapper(compositeId, mapdoc);
+
+			mapper.ManyToOne(For<Person>.Property(ts => ts.User), x => { });
+
+			compositeId.Items.Should().Have.Count.EqualTo(1);
+			compositeId.Items.First().Should().Be.OfType<HbmKeyManyToOne>();
+			compositeId.Items.OfType<HbmKeyManyToOne>().First().Name.Should().Be.EqualTo("User");
+		}
+
+		[Test]
+		public void CallMapManyToOneMapper()
+		{
+			var mapdoc = new HbmMapping();
+			var compositeId = new HbmCompositeId();
+			var mapper = new ComposedIdMapper(compositeId, mapdoc);
+			var called = false;
+
+			mapper.ManyToOne(For<Person>.Property(ts => ts.User), x => called = true);
+
+			called.Should().Be.True();
+		}
+	}
+}
---
+++ src/NHibernate/Mapping/ByCode/Impl/ComposedIdMapper.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using NHibernate.Cfg.MappingSchema;
+
+namespace NHibernate.Mapping.ByCode.Impl
+{
+	public class ComposedIdMapper : IComposedIdMapper
+	{
+		private readonly HbmCompositeId id;
+		private readonly HbmMapping mapDoc;
+
+		public ComposedIdMapper(HbmCompositeId id, HbmMapping mapDoc)
+		{
+			this.id = id;
+			this.mapDoc = mapDoc;
+		}
+
+		public HbmCompositeId ComposedId
+		{
+			get { return id; }
+		}
+
+		#region IComposedIdMapper Members
+
+		public void Property(MemberInfo property, Action<IPropertyMapper> mapping)
+		{
+			var hbmProperty = new HbmKeyProperty {name = property.Name};
+			mapping(new KeyPropertyMapper(property, hbmProperty));
+			AddProperty(hbmProperty);
+		}
+
+		public void ManyToOne(MemberInfo property, Action<IManyToOneMapper> mapping)
+		{
+			var hbm = new HbmKeyManyToOne {name = property.Name};
+			mapping(new KeyManyToOneMapper(property, hbm, mapDoc));
+			AddProperty(hbm);
+		}
+
+		#endregion
+
+		protected void AddProperty(object property)
+		{
+			if (property == null)
+			{
+				throw new ArgumentNullException("property");
+			}
+			var toAdd = new[] {property};
+			id.Items = id.Items == null ? toAdd : id.Items.Concat(toAdd).ToArray();
+		}
+	}
+}
