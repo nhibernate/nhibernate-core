ff6f968307c99d0b2aadcfce2e2f44374b23eca4
Patrick Earl <patearl@patearl.net>
2011-05-15 23:58:33 +0000

    PostgreSQL: Added a comment.
    SVN: trunk@5826

--- src/NHibernate/Driver/NpgsqlDriver.cs
+++ src/NHibernate/Driver/NpgsqlDriver.cs
@@ -79,6 +79,7 @@ namespace NHibernate.Driver
         {
             base.InitializeParameter(dbParam, name, sqlType);
 
+            // Since the .NET currency type has 4 decimal places, we use a decimal type in PostgreSQL instead of its native 2 decimal currency type.
             if (sqlType.DbType == DbType.Currency)
                 dbParam.DbType = DbType.Decimal;
         }
