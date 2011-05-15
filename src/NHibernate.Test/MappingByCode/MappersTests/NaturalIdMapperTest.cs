8ee9bb2ce7e32280a978bfd02c6c41933f12a021
Fabio Maulo <fabiomaulo@gmail.com>
2011-05-15 19:15:09 +0000

    Test ported
    SVN: trunk@5820

---
+++ src/NHibernate.Test/MappingByCode/MappersTests/NaturalIdMapperTest.cs
@@ -0,0 +1,29 @@
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode.Impl;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Test.MappingByCode.MappersTests
+{
+	public class NaturalIdMapperTest
+	{
+		private class EntitySimpleWithNaturalId
+		{
+			public string Something { get; set; }
+		}
+
+		[Test]
+		public void CanSetMutable()
+		{
+			var mapdoc = new HbmMapping();
+			var hbmClass = new HbmClass();
+			var nid = new NaturalIdMapper(typeof(EntitySimpleWithNaturalId), hbmClass, mapdoc);
+			// to have the natural-id assigned ot must have at least a property
+			nid.Property(For<EntitySimpleWithNaturalId>.Property(x => x.Something), pm => { });
+
+			var hbmNaturalId = hbmClass.naturalid;
+			nid.Mutable(true);
+			hbmNaturalId.mutable.Should().Be.True();
+		}
+	}
+}
