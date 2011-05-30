86017f9609c28d57a407baf0e6d6fc455f90459b
Fabio Maulo <fabiomaulo@gmail.com>
2011-05-30 18:59:15 +0000

    trivial relax
    SVN: trunk@5890

--- src/NHibernate/Loader/Criteria/ComponentCollectionCriteriaInfoProvider.cs
+++ src/NHibernate/Loader/Criteria/ComponentCollectionCriteriaInfoProvider.cs
@@ -6,66 +6,64 @@ using NHibernate.Type;
 
 namespace NHibernate.Loader.Criteria
 {
-    public class ComponentCollectionCriteriaInfoProvider : ICriteriaInfoProvider
-    {
-        private readonly IQueryableCollection persister;
-        private readonly IDictionary<String, IType> subTypes = new Dictionary<string, IType>();
+	public class ComponentCollectionCriteriaInfoProvider : ICriteriaInfoProvider
+	{
+		private readonly IQueryableCollection persister;
+		private readonly IDictionary<string, IType> subTypes = new Dictionary<string, IType>();
 
-        public ComponentCollectionCriteriaInfoProvider(IQueryableCollection persister)
-        {
-            this.persister = persister;
-            if (!persister.ElementType.IsComponentType)
-            {
-                throw new ArgumentException("persister for role " + persister.Role + " is not a collection-of-component");
-            }
+		public ComponentCollectionCriteriaInfoProvider(IQueryableCollection persister)
+		{
+			this.persister = persister;
+			if (!persister.ElementType.IsComponentType)
+			{
+				throw new ArgumentException("persister for role " + persister.Role + " is not a collection-of-component");
+			}
 
-            var componentType = (ComponentType)persister.ElementType;
-            var names = componentType.PropertyNames;
-            var types = componentType.Subtypes;
+			var componentType = (IAbstractComponentType) persister.ElementType;
+			string[] names = componentType.PropertyNames;
+			IType[] types = componentType.Subtypes;
 
-            for (var i = 0; i < names.Length; i++)
-            {
-                subTypes.Add(names[i], types[i]);
-            }
+			for (int i = 0; i < names.Length; i++)
+			{
+				subTypes.Add(names[i], types[i]);
+			}
+		}
 
-        }
+		#region ICriteriaInfoProvider Members
 
-        public String Name
-        {
-            get
-            {
-                return persister.Role;
-            }
-        }
+		public String Name
+		{
+			get { return persister.Role; }
+		}
 
-        public string[] Spaces
-        {
-            get
-            {
-                return persister.CollectionSpaces;
-            }
-        }
+		public string[] Spaces
+		{
+			get { return persister.CollectionSpaces; }
+		}
 
-        public IPropertyMapping PropertyMapping
-        {
-            get
-            {
-                return persister;
-            }
-        }
+		public IPropertyMapping PropertyMapping
+		{
+			get { return persister; }
+		}
 
-        public IType GetType(String relativePath)
-        {
-            // TODO: can a component have a nested component? then we may need to do something more here...
-            if (relativePath.IndexOf('.') >= 0)
-                throw new ArgumentException("dotted paths not handled (yet?!) for collection-of-component");
+		public IType GetType(String relativePath)
+		{
+			// TODO: can a component have a nested component? then we may need to do something more here...
+			if (relativePath.IndexOf('.') >= 0)
+			{
+				throw new ArgumentException("dotted paths not handled (yet?!) for collection-of-component");
+			}
 
-            IType type;
+			IType type;
 
-            if (subTypes.TryGetValue(relativePath, out type) == false)
-                throw new ArgumentException("property " + relativePath + " not found in component of collection " + Name);
+			if (subTypes.TryGetValue(relativePath, out type) == false)
+			{
+				throw new ArgumentException("property " + relativePath + " not found in component of collection " + Name);
+			}
 
-            return type;
-        }
-    }
+			return type;
+		}
+
+		#endregion
+	}
 }
