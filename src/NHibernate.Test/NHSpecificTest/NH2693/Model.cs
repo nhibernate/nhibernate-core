2b7345d0e8aca6722b2518377533227bc7a7b3f7
Fabio Maulo <fabiomaulo@gmail.com>
2011-06-19 21:50:00 +0000

    Tests for NH-2693
    SVN: trunk@5963

---
+++ src/NHibernate.Test/NHSpecificTest/NH2693/Model.cs
@@ -0,0 +1,40 @@
+﻿using Iesi.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH2693 {
+   using System;
+   using System.Collections.Generic;
+
+   public class FirstLevel {
+      public FirstLevel() {
+         SecondLevels = new HashedSet<SecondLevelComponent>();
+      }
+
+      public virtual Guid Id { get; set; }
+      public virtual ICollection<SecondLevelComponent> SecondLevels { get; set; }
+   }
+
+   public class SecondLevelComponent {
+      public virtual FirstLevel FirstLevel { get; set; }
+      public virtual ThirdLevel ThirdLevel { get; set; }
+      public virtual SpecificThirdLevel SpecificThirdLevel { get; set; }
+      public virtual bool SomeBool { get; set; }
+   }
+
+   public abstract class ThirdLevel {
+      public virtual Guid Id { get; set; }
+   }
+
+   public class SpecificThirdLevel : ThirdLevel {
+      public SpecificThirdLevel() {
+				FourthLevels = new HashedSet<FourthLevel>();
+      }
+
+      public virtual ICollection<FourthLevel> FourthLevels { get; set; }
+   }
+
+   public class FourthLevel {
+      public virtual Guid Id { get; set; }
+      public virtual SpecificThirdLevel SpecificThirdLevel { get; set; }
+      public virtual string SomeString { get; set; }
+   }
+}
