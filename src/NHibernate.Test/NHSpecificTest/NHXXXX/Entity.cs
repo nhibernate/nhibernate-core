c6ef7b3ff38d4fe6e34a866f8a0b3d0eb7345469
FelicePollano <felice@felicepollano.com>
2011-09-30 16:52:07 +0200

    Created unit test to show CreateFilter not working on collections.

---
+++ src/NHibernate.Test/NHSpecificTest/NHXXXX/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHibernate.Test.NHSpecificTest.NHXXXX
+{
+    class Child
+    {
+        public string Code { get; set; }
+    }
+    class Entity
+    {
+        public Entity()
+        {
+            Children = new List<Child>();
+        }
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual IList<Child> Children { get; set; }
+    }
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NHXXXX/Fixture.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NHXXXX
+{
+    [TestFixture]
+    public class Fixture : BugTestCase
+    {
+        protected override void OnSetUp()
+        {
+            using (ISession session = OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    var e1 = new Entity();
+                    e1.Name = "Bob";
+                    e1.Children.Add(new Child { Code = "c1" });
+                    e1.Children.Add(new Child { Code = "c2" });
+                    e1.Children.Add(new Child { Code = "c3" });
+                    session.Save(e1);
+
+                    
+
+                    session.Flush();
+                    transaction.Commit();
+                }
+            }
+        }
+
+        protected override void OnTearDown()
+        {
+            using (ISession session = OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Delete("from System.Object");
+
+                    session.Flush();
+                    transaction.Commit();
+                }
+            }
+        }
+
+        [Test]
+        public void CreateFilterSelectCountShouldReturnCount()
+        {
+            using (ISession session = OpenSession())
+            {
+                using (session.BeginTransaction())
+                {
+                    var result = session.Query<Entity>()
+                                 .FirstOrDefault();
+                    Assert.IsNotNull(result);
+                    int count = session.CreateFilter(result.Children, "select count(*)").List<int>()[0];
+                    Assert.AreEqual(3,count);
+                }
+            }
+        }
+        [Test]
+        public void CreateFilterShouldAllowCollectionPaging()
+        {
+            using (ISession session = OpenSession())
+            {
+                using (session.BeginTransaction())
+                {
+                    var result = session.Query<Entity>()
+                                 .FirstOrDefault();
+                    Assert.IsNotNull(result);
+                    var children = session.CreateFilter(result.Children,string.Empty)
+                        .SetFirstResult(1)
+                        .SetMaxResults(2)
+                        .List();
+                    Assert.AreEqual(2, children.Count);
+                }
+            }
+        }
+    }
+}
