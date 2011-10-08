5b4e79465c00c989372a2f27affe63ed10d6043b
Julian Maughan <julian.maughan@gmail.com>
2011-10-09 01:25:25 +0800

    Changes XML mapping to use newly implemented <id> 'generator' attribute, and fixes some whitespace issues

--- src/NHibernate.Test/NHSpecificTest/NH0000/Entity.cs
+++ src/NHibernate.Test/NHSpecificTest/NH0000/Entity.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace NHibernate.Test.NHSpecificTest.NH0000
 {
-    class Entity
-    {
-        public virtual Guid Id { get; set; }
-        public virtual string Name { get; set; }
-    }
+	class Entity
+	{
+		public virtual Guid Id { get; set; }
+		public virtual string Name { get; set; }
+	}
 }
