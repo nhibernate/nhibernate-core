cdfb6aaed545440f3ca98bf9806be2f2b569dd5e
Oskar Berggren <oskar.berggren@gmail.com>
2011-11-11 10:25:09 +0100

    Update existing id generator optimizers to reflect current state of Hibernate in most respects. Missing new optimizers LegacyHiLo and PooledLo, and needs review for thread safety.

--- src/NHibernate/Id/Enhanced/OptimizerFactory.cs
+++ src/NHibernate/Id/Enhanced/OptimizerFactory.cs
@@ -7,12 +7,36 @@ namespace NHibernate.Id.Enhanced
 {
 	public class OptimizerFactory
 	{
-		public const string HiLo = "hilo";
 		public const string None = "none";
+		public const string HiLo = "hilo";
+		public const string LegacyHilo = "legacy-hilo";
 		public const string Pool = "pooled";
-		private static readonly System.Type[] CtorSignature = new[] {typeof (System.Type), typeof (int)};
-		private static readonly IInternalLogger log = LoggerProvider.LoggerFor(typeof (OptimizerFactory));
+		public const string PoolLo = "pooled-lo";
 
+		private static readonly System.Type[] CtorSignature = new[] { typeof(System.Type), typeof(int) };
+		private static readonly IInternalLogger log = LoggerProvider.LoggerFor(typeof(OptimizerFactory));
+
+
+		/// <summary>
+		/// Marker interface for optimizer which wish to know the user-specified initial value.
+		/// <p/>
+		/// Used instead of constructor injection since that is already a public understanding and
+		/// because not all optimizers care.
+		/// </summary>
+		public interface IInitialValueAwareOptimizer
+		{
+			/// <summary>
+			/// Reports the user-specified initial value to the optimizer.
+			/// <p/>
+			/// <tt>-1</tt> is used to indicate that the user did not specify.
+			/// <param name="initialValue">The initial value specified by the user, or <tt>-1</tt> to indicate that the
+			/// user did not specify.</param>
+			/// </summary>
+			void InjectInitialValue(long initialValue);
+		}
+
+
+		[Obsolete("Use BuildOptimizer(string type, System.Type returnClass, int incrementSize, long explicitInitialValue) instead.")]
 		public static IOptimizer BuildOptimizer(string type, System.Type returnClass, int incrementSize)
 		{
 			if (string.IsNullOrEmpty(type))
@@ -27,14 +51,20 @@ namespace NHibernate.Id.Enhanced
 			switch (type)
 			{
 				case None:
-					optimizerClassName = typeof (NoopOptimizer).FullName;
+					optimizerClassName = typeof(NoopOptimizer).FullName;
 					break;
 				case HiLo:
-					optimizerClassName = typeof (HiLoOptimizer).FullName;
+					optimizerClassName = typeof(HiLoOptimizer).FullName;
 					break;
+				//case LegacyHilo:  FIXME
+				//    FIXME optimizerClassName = typeof(HiLoOptimizer).FullName;
+				//    break;
 				case Pool:
-					optimizerClassName = typeof (PooledOptimizer).FullName;
+					optimizerClassName = typeof(PooledOptimizer).FullName;
 					break;
+				//case PoolLo:    FIXME
+				//    optimizerClassName = typeof(PooledOptimizer).FullName;
+				//    break;
 				default:
 					optimizerClassName = type;
 					break;
@@ -48,22 +78,36 @@ namespace NHibernate.Id.Enhanced
 			}
 			catch (Exception)
 			{
-				// intentionally empty
+				log.Error("Unable to instantiate id generator optimizer.");  // FIXME: Review log message.
 			}
 
 			// the default...
 			return new NoopOptimizer(returnClass, incrementSize);
 		}
 
+
+		public static IOptimizer BuildOptimizer(string type, System.Type returnClass, int incrementSize, long explicitInitialValue)
+		{
+			// FIXME: Disable this warning, or refactor without the deprecated version.
+			IOptimizer optimizer = BuildOptimizer(type, returnClass, incrementSize);
+
+			if (optimizer is IInitialValueAwareOptimizer)
+				((IInitialValueAwareOptimizer)optimizer).InjectInitialValue(explicitInitialValue);
+
+			return optimizer;
+		}
+
+
 		#region Nested type: HiLoOptimizer
 
 		public class HiLoOptimizer : OptimizerSupport
 		{
-			private long hiValue;
+			private long upperLimit;
 			private long lastSourceValue = -1;
 			private long value_Renamed;
 
-			public HiLoOptimizer(System.Type returnClass, int incrementSize) : base(returnClass, incrementSize)
+			public HiLoOptimizer(System.Type returnClass, int incrementSize)
+				: base(returnClass, incrementSize)
 			{
 				if (incrementSize < 1)
 				{
@@ -71,11 +115,14 @@ namespace NHibernate.Id.Enhanced
 				}
 				if (log.IsDebugEnabled)
 				{
-					log.Debug("creating hilo optimizer with [incrementSize=" + incrementSize + "; returnClass=" + returnClass.FullName
-					          + "]");
+					log.Debug("Creating hilo optimizer with [incrementSize=" + incrementSize + "; returnClass=" + returnClass.FullName
+							  + "]");
 				}
 			}
 
+			/// <summary>
+			/// Exposure intended for testing purposes.
+			/// </summary>
 			public override long LastSourceValue
 			{
 				get { return lastSourceValue; }
@@ -86,9 +133,12 @@ namespace NHibernate.Id.Enhanced
 				get { return value_Renamed - 1; }
 			}
 
+			/// <summary>
+			/// Exposure intended for testing purposes.
+			/// </summary>
 			public long HiValue
 			{
-				get { return hiValue; }
+				get { return upperLimit; }
 			}
 
 			public override bool ApplyIncrementSizeToSourceValues
@@ -105,13 +155,15 @@ namespace NHibernate.Id.Enhanced
 					{
 						lastSourceValue = callback.NextValue;
 					}
-					hiValue = (lastSourceValue * IncrementSize) + 1;
-					value_Renamed = hiValue - IncrementSize;
+					// upperLimit defines the upper end of the bucket values
+					upperLimit = (lastSourceValue * IncrementSize) + 1;
+					// initialize value to the low end of the bucket
+					value_Renamed = upperLimit - IncrementSize;
 				}
-				else if (value_Renamed >= hiValue)
+				else if (upperLimit <= value_Renamed)
 				{
 					lastSourceValue = callback.NextValue;
-					hiValue = (lastSourceValue * IncrementSize) + 1;
+					upperLimit = (lastSourceValue * IncrementSize) + 1;
 				}
 				return Make(value_Renamed++);
 			}
@@ -125,7 +177,7 @@ namespace NHibernate.Id.Enhanced
 		{
 			private long lastSourceValue = -1;
 
-			public NoopOptimizer(System.Type returnClass, int incrementSize) : base(returnClass, incrementSize) {}
+			public NoopOptimizer(System.Type returnClass, int incrementSize) : base(returnClass, incrementSize) { }
 
 			public override long LastSourceValue
 			{
@@ -139,18 +191,15 @@ namespace NHibernate.Id.Enhanced
 
 			public override object Generate(IAccessCallback callback)
 			{
-				if (lastSourceValue == -1)
-				{
-					while (lastSourceValue <= 0)
-					{
-						lastSourceValue = callback.NextValue;
-					}
-				}
-				else
-				{
-					lastSourceValue = callback.NextValue;
-				}
-				return Make(lastSourceValue);
+				// The following note is from Hibernate. I haven't researched it fully. See HHH-3001.
+				// Note: it is incredibly important that the method-local variable be
+				// used here to avoid concurrency issues.
+
+				long val = lastSourceValue;
+				while (val <= 0)
+					val = callback.NextValue;
+				lastSourceValue = val;
+				return Make(val);
 			}
 		}
 
@@ -158,8 +207,16 @@ namespace NHibernate.Id.Enhanced
 
 		#region Nested type: OptimizerSupport
 
+		/// <summary>
+		/// Common support for optimizer implementations.
+		/// </summary>
 		public abstract class OptimizerSupport : IOptimizer
 		{
+			/// <summary>
+			/// Construct an optimizer
+			/// </summary>
+			/// <param name="returnClass">The expected id class.</param>
+			/// <param name="incrementSize">The increment size.</param>
 			protected OptimizerSupport(System.Type returnClass, int incrementSize)
 			{
 				if (returnClass == null)
@@ -193,12 +250,25 @@ namespace NHibernate.Id.Enhanced
 
 		#region Nested type: PooledOptimizer
 
-		public class PooledOptimizer : OptimizerSupport
+		/// <summary>
+		/// Optimizer which uses a pool of values, storing the next low value of the range in the database.
+		/// <para>
+		/// Note that this optimizer works essentially the same as the HiLoOptimizer, except that here the
+		/// bucket ranges are actually encoded into the database structures.
+		/// </para>
+		/// <para>
+		/// Note that if you prefer that the database value be interpreted as the bottom end of our current
+		/// range, the use the PooledLoOptimizer strategy.
+		/// </para>
+		/// </summary>
+		public class PooledOptimizer : OptimizerSupport, IInitialValueAwareOptimizer
 		{
 			private long hiValue = -1;
 			private long value_Renamed;
+			private long initialValue;
 
-			public PooledOptimizer(System.Type returnClass, int incrementSize) : base(returnClass, incrementSize)
+			public PooledOptimizer(System.Type returnClass, int incrementSize)
+				: base(returnClass, incrementSize)
 			{
 				if (incrementSize < 1)
 				{
@@ -206,8 +276,8 @@ namespace NHibernate.Id.Enhanced
 				}
 				if (log.IsDebugEnabled)
 				{
-					log.Debug("creating pooled optimizer with [incrementSize=" + incrementSize + "; returnClass="
-					          + returnClass.FullName + "]");
+					log.Debug("Creating pooled optimizer with [incrementSize=" + incrementSize + "; returnClass="
+							  + returnClass.FullName + "]");
 				}
 			}
 
@@ -216,6 +286,10 @@ namespace NHibernate.Id.Enhanced
 				get { return hiValue; }
 			}
 
+
+			/// <summary>
+			/// Exposure intended for testing purposes.
+			/// </summary>
 			public long LastValue
 			{
 				get { return value_Renamed - 1; }
@@ -225,6 +299,13 @@ namespace NHibernate.Id.Enhanced
 			{
 				get { return true; }
 			}
+
+
+			public void InjectInitialValue(long initialValue)
+			{
+				this.initialValue = initialValue;
+			}
+
 
 			public override object Generate(IAccessCallback callback)
 			{
@@ -238,9 +319,16 @@ namespace NHibernate.Id.Enhanced
 						// because we would not be able to control this if
 						// we are using a sequence...
 						log.Info("pooled optimizer source reported [" + value_Renamed
-						         + "] as the initial value; use of 1 or greater highly recommended");
+								 + "] as the initial value; use of 1 or greater highly recommended");
 					}
-					hiValue = callback.NextValue;
+
+					if ((initialValue == -1 && value_Renamed < IncrementSize) || value_Renamed == initialValue)
+						hiValue = callback.NextValue;
+					else
+					{
+						hiValue = value_Renamed;
+						value_Renamed = hiValue - IncrementSize;
+					}
 				}
 				else if (value_Renamed >= hiValue)
 				{
