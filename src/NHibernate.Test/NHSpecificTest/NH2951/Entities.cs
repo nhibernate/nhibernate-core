4a10db29ad759e9cf4a94389165077a3fd2ac9df
Patrick Earl <pat@pleasantsolutions.com>
2011-11-21 19:14:07 -0700

    NH-2591: Failing unit test.

---
+++ src/NHibernate.Test/NHSpecificTest/NH2951/Entities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH2951
+{
+	class Entity
+	{
+		public virtual Guid Id { get; set; }
+	}
+
+    class Customer : Entity
+    {
+        public virtual string Name { get; set; }
+    }
+
+    class Invoice
+    {
+        public virtual Guid Id { get; set; }
+        public virtual Customer Customer { get; set; }
+        public virtual decimal Amount { get; set; }
+    }
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH2951/Fixture.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using NHibernate.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH2951
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnTearDown()
+		{
+			using (ISession session = OpenSession())
+			using (ITransaction transaction = session.BeginTransaction())
+			{
+				session.Delete("from System.Object");
+
+				session.Flush();
+				transaction.Commit();
+			}
+		}
+
+		[Test]
+        [Ignore("Not working.")]
+		public void UpdateWithSubqueryToJoinedSubclass()
+		{
+            using (ISession session = OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+
+                var c = new Customer { Name = "Bob" };
+                session.Save(c);
+
+                var i = new Invoice { Amount = 10 };
+                session.Save(i);
+
+                session.Flush();
+                transaction.Commit();
+            }
+
+            using (ISession session = OpenSession())
+			using (session.BeginTransaction())
+			{
+                // Using (select c.Id ...) works.
+                string hql = "update Invoice i set i.Customer = (select c from Customer c where c.Name = 'Bob')";
+
+			    int result = session.CreateQuery(hql).ExecuteUpdate();
+
+                Assert.AreEqual(1, result);
+			}
+		}
+	}
+}
