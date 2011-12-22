6ce1cc40e642377342238c25aa8908732880e171
hazzik <hazzik@gmail.com>
2011-12-22 14:26:54 +0600

    Applied tests for NH-2722 from patch

---
+++ src/NHibernate.Test/NHSpecificTest/NH2722/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH2722
+{
+	public class Entity
+	{
+		public virtual Guid Id { get; set; }
+		public virtual int? Property { get; set; }
+	}
+}
