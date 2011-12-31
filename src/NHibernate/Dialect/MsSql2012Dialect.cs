7e40babcb71332b94cd8529352f417ff23c41dff
unknown <Willi@Dell.(none)>
2011-12-31 13:39:50 +0100

    Paging for MsSql2012 changed to the new recommended standard syntax with select xxx from yyy order by zzz offset @p1 rows fetch first @p2 rows only

---
+++ src/NHibernate/Dialect/MsSql2012Dialect.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using NHibernate.Cfg;
+using NHibernate.Dialect.Function;
+using NHibernate.Driver;
+using NHibernate.SqlCommand;
+
+namespace NHibernate.Dialect
+{
+	public class MsSql2012Dialect : MsSql2008Dialect
+	{
+        public override SqlString GetLimitString(SqlString queryString, SqlString offset, SqlString limit)
+        {
+            var result = new SqlStringBuilder(queryString);
+
+            int orderIndex = queryString.LastIndexOfCaseInsensitive(" order by ");
+
+            //don't use the order index if it is contained within a larger statement(assuming
+            //a statement with non matching parenthesis is part of a larger block)
+            if (orderIndex < 0 || !HasMatchingParens(queryString.Substring(orderIndex).ToString()))
+            {
+                // Use order by first column if no explicit ordering is provided
+                result.Add( " ORDER BY ")
+                    .Add("1");
+            }
+
+            result.Add(" OFFSET ")
+                .Add(offset ?? new SqlString("0"))
+                .Add(" ROWS");
+
+            if (limit != null) 
+            {
+                result.Add(" FETCH FIRST ").Add(limit).Add(" ROWS ONLY");
+            }
+
+            return result.ToSqlString();
+        }
+    }
+}
