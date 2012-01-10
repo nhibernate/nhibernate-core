c48b335f94a5ce22754988850bb1ce668a7eb557
hazzik <hazzik@gmail.com>
2012-01-11 00:35:03 +0600

    Add tests for NH-2946 in combination with NH-3003 and NH-2451

--- src/NHibernate.Test/Linq/WhereSubqueryTests.cs
+++ src/NHibernate.Test/Linq/WhereSubqueryTests.cs
@@ -364,7 +364,7 @@ where c.Order.Customer.CustomerId = 'VINET'
 			using (var spy = new SqlLogSpy())
 			{
 				var lines = (from l in db.OrderLines
-							 where l.Order.Customer.CustomerId == "VINET"
+							 where l.Order.Customer.CompanyName == "Vins et alcools Chevalier"
 							 select l).ToList();
 
 				Assert.AreEqual(10, lines.Count);
@@ -374,16 +374,155 @@ where c.Order.Customer.CustomerId = 'VINET'
 		}
 
 		[Test]
-		public void OrderLinesWith2ImpliedJoinShouldProduce2JoinsInSql2()
+		public void OrderLinesWith2ImpliedJoinByIdShouldNotContainsImpliedJoin()
 		{
-            //NH-2451
+			//NH-2946 + NH-3003 = NH-2451
 			using (var spy = new SqlLogSpy())
 			{
 				var lines = (from l in db.OrderLines
-							 where l.Order.RequiredDate < DateTime.Now
+							 where l.Order.Customer.CustomerId == "VINET"
+							 where l.Order.Customer.CompanyName == "Vins et alcools Chevalier"
+							 select l).ToList();
+
+				Assert.AreEqual(10, lines.Count);
+				var countJoins = CountJoins(spy);
+				Assert.That(countJoins, Is.EqualTo(2));
+				Assert.That(Count(spy, "Orders"), Is.EqualTo(1));
+			}
+		}
+		
+		[Test]
+		public void OrderLinesFilterByCustomerCompanyNameAndSelectCustomerIdShouldJoinOrdersOnlyOnce()
+		{
+			//NH-2946 + NH-3003 = NH-2451
+			using (var spy = new SqlLogSpy())
+			{
+				var lines = (from l in db.OrderLines
+							 where l.Order.Customer.CompanyName == "Vins et alcools Chevalier"
+							 select l.Order.Customer.CustomerId).ToList();
+
+				Assert.AreEqual(10, lines.Count);
+				var countJoins = CountJoins(spy);
+				Assert.That(countJoins, Is.EqualTo(2));
+				Assert.That(Count(spy, "Orders"), Is.EqualTo(1));
+			}
+		}
+		
+		[Test]
+		public void OrderLinesFilterByOrderDateAndSelectOrderId()
+		{
+			//NH-2451
+			using (var spy = new SqlLogSpy())
+			{
+				var lines = (from l in db.OrderLines
+							 where l.Order.OrderDate < DateTime.Now
 							 select l.Order.OrderId).ToList();
 
-                Assert.AreEqual(2155, lines.Count);
+				Assert.AreEqual(2155, lines.Count);
+				var countJoins = CountJoins(spy);
+				Assert.That(countJoins, Is.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public void OrderLinesFilterByOrderIdAndSelectOrderDate()
+		{
+			//NH-2451
+			using (var spy = new SqlLogSpy())
+			{
+				var lines = (from l in db.OrderLines
+							 where l.Order.OrderId == 100
+							 select l.Order.OrderDate).ToList();
+
+				var countJoins = CountJoins(spy);
+				Assert.That(countJoins, Is.EqualTo(1));
+				Assert.That(Count(spy, "Orders"), Is.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public void OrderLinesWithFilterByOrderIdShouldNotProduceJoins()
+		{
+			//NH-2946
+			using (var spy = new SqlLogSpy())
+			{
+				(from l in db.OrderLines
+				 where l.Order.OrderId == 1000
+				 select l).ToList();
+
+				var countJoins = CountJoins(spy);
+				Assert.That(countJoins, Is.EqualTo(0));
+			}
+		}
+		
+		[Test]
+		public void OrderLinesWithFilterByOrderIdAndDateShouldProduceOneJoin()
+		{
+			//NH-2946
+			using (var spy = new SqlLogSpy())
+			{
+				(from l in db.OrderLines
+				 where l.Order.OrderId == 1000 && l.Order.OrderDate < DateTime.Now
+				 select l).ToList();
+
+				var countJoins = CountJoins(spy);
+				Assert.That(countJoins, Is.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public void OrderLinesWithOrderByOrderIdShouldNotProduceJoins()
+		{
+			//NH-2946
+			using (var spy = new SqlLogSpy())
+			{
+				(from l in db.OrderLines
+				 orderby l.Order.OrderId
+				 select l).ToList();
+
+				var countJoins = CountJoins(spy);
+				Assert.That(countJoins, Is.EqualTo(0));
+			}
+		}
+
+		[Test]
+		public void OrderLinesWithOrderByOrderIdAndDateShouldProduceOneJoin()
+		{
+			//NH-2946
+			using (var spy = new SqlLogSpy())
+			{
+				(from l in db.OrderLines
+				 orderby l.Order.OrderId, l.Order.OrderDate
+				 select l).ToList();
+
+				var countJoins = CountJoins(spy);
+				Assert.That(countJoins, Is.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public void OrderLinesWithSelectingOrderIdShouldNotProduceJoins()
+		{
+			//NH-2946
+			using (var spy = new SqlLogSpy())
+			{
+				(from l in db.OrderLines
+				 select l.Order.OrderId).ToList();
+
+				var countJoins = CountJoins(spy);
+				Assert.That(countJoins, Is.EqualTo(0));
+			}
+		}
+
+		[Test]
+		public void OrderLinesWithSelectingOrderIdAndDateShouldProduceOneJoin()
+		{
+			//NH-2946
+			using (var spy = new SqlLogSpy())
+			{
+				(from l in db.OrderLines
+				 select new {l.Order.OrderId, l.Order.OrderDate}).ToList();
+
 				var countJoins = CountJoins(spy);
 				Assert.That(countJoins, Is.EqualTo(1));
 			}
@@ -391,8 +530,13 @@ where c.Order.Customer.CustomerId = 'VINET'
 
 		private static int CountJoins(LogSpy sqlLog)
 		{
+			return Count(sqlLog, "join");
+		}
+
+		private static int Count(LogSpy sqlLog, string s)
+		{
 			var log = sqlLog.GetWholeLog();
-			return log.Split(new[] {"join"}, StringSplitOptions.None).Length - 1;
+			return log.Split(new[] {s}, StringSplitOptions.None).Length - 1;
 		}
 	}
 }
