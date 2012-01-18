334ca562e729cb576a35f7f957e7f8e2288a7aaf
hazzik <hazzik@gmail.com>
2012-01-19 01:28:14 +0600

    Implemented support for having clause in linq

---
+++ src/NHibernate/Linq/GroupBy/IsNonAggregatingGroupByDetectionVisitor.cs
@@ -0,0 +1,42 @@
+using System.Linq.Expressions;
+using NHibernate.Linq.Expressions;
+using NHibernate.Linq.Visitors;
+using Remotion.Linq.Clauses.Expressions;
+
+namespace NHibernate.Linq.GroupBy
+{
+	/// <summary>
+	/// Detects if an expression tree contains naked QuerySourceReferenceExpression
+	/// </summary>
+	internal class IsNonAggregatingGroupByDetectionVisitor : NhExpressionTreeVisitor
+	{
+		private bool _containsNakedQuerySourceReferenceExpression;
+
+		public bool IsNonAggregatingGroupBy(Expression expression)
+		{
+			_containsNakedQuerySourceReferenceExpression = false;
+
+			VisitExpression(expression);
+
+			return _containsNakedQuerySourceReferenceExpression;
+		}
+
+		protected override Expression VisitMemberExpression(MemberExpression expression)
+		{
+			return expression.Member.Name == "Key"
+					   ? expression
+					   : base.VisitMemberExpression(expression);
+		}
+
+		protected override Expression VisitNhAggregate(NhAggregatedExpression expression)
+		{
+			return expression;
+		}
+
+		protected override Expression VisitQuerySourceReferenceExpression(QuerySourceReferenceExpression expression)
+		{
+			_containsNakedQuerySourceReferenceExpression = true;
+			return expression;
+		}
+	}
+}
