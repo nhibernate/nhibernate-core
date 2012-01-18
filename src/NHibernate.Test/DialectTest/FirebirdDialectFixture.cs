e37a1969243f2b3aacdba9643630b573074f16ec
Oskar Berggren <oskar.berggren@gmail.com>
2012-01-19 00:33:28 +0100

    Fix indenting and line endings.

--- src/NHibernate.Test/DialectTest/FirebirdDialectFixture.cs
+++ src/NHibernate.Test/DialectTest/FirebirdDialectFixture.cs
@@ -12,19 +12,19 @@ namespace NHibernate.Test.DialectTest
 		{
 			FirebirdDialect d = new FirebirdDialect();
 
-            SqlString str = d.GetLimitString(new SqlString("SELECT * FROM fish"), null, new SqlString("10"));
+			SqlString str = d.GetLimitString(new SqlString("SELECT * FROM fish"), null, new SqlString("10"));
 			Assert.AreEqual("SELECT first 10 * FROM fish", str.ToString());
 
-            str = d.GetLimitString(new SqlString("SELECT * FROM fish ORDER BY name"), new SqlString("5"), new SqlString("15"));
+			str = d.GetLimitString(new SqlString("SELECT * FROM fish ORDER BY name"), new SqlString("5"), new SqlString("15"));
 			Assert.AreEqual("SELECT first 15 skip 5 * FROM fish ORDER BY name", str.ToString());
 
-            str = d.GetLimitString(new SqlString("SELECT * FROM fish ORDER BY name DESC"), new SqlString("7"), new SqlString("28"));
+			str = d.GetLimitString(new SqlString("SELECT * FROM fish ORDER BY name DESC"), new SqlString("7"), new SqlString("28"));
 			Assert.AreEqual("SELECT first 28 skip 7 * FROM fish ORDER BY name DESC", str.ToString());
 
-            str = d.GetLimitString(new SqlString("SELECT DISTINCT fish.family FROM fish ORDER BY name DESC"), null, new SqlString("28"));
+			str = d.GetLimitString(new SqlString("SELECT DISTINCT fish.family FROM fish ORDER BY name DESC"), null, new SqlString("28"));
 			Assert.AreEqual("SELECT first 28 DISTINCT fish.family FROM fish ORDER BY name DESC", str.ToString());
 
-            str = d.GetLimitString(new SqlString("SELECT DISTINCT fish.family FROM fish ORDER BY name DESC"), new SqlString("7"), new SqlString("28"));
+			str = d.GetLimitString(new SqlString("SELECT DISTINCT fish.family FROM fish ORDER BY name DESC"), new SqlString("7"), new SqlString("28"));
 			Assert.AreEqual("SELECT first 28 skip 7 DISTINCT fish.family FROM fish ORDER BY name DESC", str.ToString());
 		}
 	}
