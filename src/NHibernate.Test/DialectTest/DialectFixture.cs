f6062eeb5bc1d5943ddb0b7ed297612b61470d6a
Oskar Berggren <oskar.berggren@gmail.com>
2012-01-19 11:33:27 +0100

    Move the DialectSupportingSequencesMustFullfillSequenceContract test to its own fixture.

--- src/NHibernate.Test/DialectTest/DialectFixture.cs
+++ src/NHibernate.Test/DialectTest/DialectFixture.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.Data;
 using NHibernate.Dialect;
@@ -174,43 +173,5 @@ namespace NHibernate.Test.DialectTest
 			}
 		}
 
-
-		/// <summary>
-		/// Test case data source for DialectSupportingSequencesMustFullfillSequenceContract().
-		/// </summary>
-		private IEnumerable<System.Type> GetAllDialectTypes()
-		{
-			var dialectBaseType = typeof(NHibernate.Dialect.Dialect);
-
-			return dialectBaseType.Assembly.GetExportedTypes()
-				.Where(type => type.IsClass && !type.IsAbstract && type.IsSubclassOf(dialectBaseType))
-				.ToList();
-		}
-
-
-		[TestCaseSource("GetAllDialectTypes")]
-		public void DialectSupportingSequencesMustFullfillSequenceContract(System.Type dialectType)
-		{
-			var dialect = (NHibernate.Dialect.Dialect)Activator.CreateInstance(dialectType);
-
-			if (!dialect.SupportsSequences)
-				Assert.Ignore("This test applies only to dialects that support sequences.");
-
-			// Just call every method that should work if SupportsSequences returns true, to
-			// verify that we at least don't get any exceptions because someone forgot to
-			// override something.
-
-			dialect.GetCreateSequenceString("foo");
-
-			if (dialect.SupportsPooledSequences)
-				dialect.GetCreateSequenceStrings("foo", 3, 3);
-
-			dialect.GetDropSequenceString("foo");
-			dialect.GetDropSequenceStrings("foo");
-			dialect.GetSelectSequenceNextValString("foo");
-			dialect.GetSequenceNextValString("foo");
-			var sql = dialect.QuerySequencesString;
-
-		}
 	}
 }
