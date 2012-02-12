cb8f3c4c39e1e83025f569eac4cc8edadadc7a3b
Scooletz <scooletz@gmail.com>
2012-02-12 20:58:31 +0100

    UpdateTimestampsCache can be queried for timestamps. ReaderWriterLockSlim instead of heavy monitor used

--- src/NHibernate/Cache/UpdateTimestampsCache.cs
+++ src/NHibernate/Cache/UpdateTimestampsCache.cs
@@ -1,111 +1,168 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+using System.Threading;
 using Iesi.Collections.Generic;
 
 using NHibernate.Cfg;
 
 namespace NHibernate.Cache
 {
-	/// <summary>
-	/// Tracks the timestamps of the most recent updates to particular tables. It is
-	/// important that the cache timeout of the underlying cache implementation be set
-	/// to a higher value than the timeouts of any of the query caches. In fact, we 
-	/// recommend that the the underlying cache not be configured for expiry at all.
-	/// Note, in particular, that an LRU cache expiry policy is never appropriate.
-	/// </summary>
-	public class UpdateTimestampsCache
-	{
-		private static readonly IInternalLogger log = LoggerProvider.LoggerFor(typeof(UpdateTimestampsCache));
-		private ICache updateTimestamps;
+    /// <summary>
+    /// Tracks the timestamps of the most recent updates to particular tables. It is
+    /// important that the cache timeout of the underlying cache implementation be set
+    /// to a higher value than the timeouts of any of the query caches. In fact, we 
+    /// recommend that the the underlying cache not be configured for expiry at all.
+    /// Note, in particular, that an LRU cache expiry policy is never appropriate.
+    /// </summary>
+    public class UpdateTimestampsCache
+    {
+        private static readonly IInternalLogger log = LoggerProvider.LoggerFor(typeof(UpdateTimestampsCache));
+        private readonly ReaderWriterLockSlim rwl = new ReaderWriterLockSlim();
+        private readonly ICache updateTimestamps;
 
-		private readonly string regionName = typeof(UpdateTimestampsCache).Name;
+        private readonly string regionName = typeof(UpdateTimestampsCache).Name;
 
-		public void Clear()
-		{
-			updateTimestamps.Clear();
-		}
+        public void Clear()
+        {
+            updateTimestamps.Clear();
+        }
 
-		public UpdateTimestampsCache(Settings settings, IDictionary<string, string> props)
-		{
-			string prefix = settings.CacheRegionPrefix;
-			regionName = prefix == null ? regionName : prefix + '.' + regionName;
-			log.Info("starting update timestamps cache at region: " + regionName);
-			updateTimestamps = settings.CacheProvider.BuildCache(regionName, props);
-		}
+        public UpdateTimestampsCache(Settings settings, IDictionary<string, string> props)
+        {
+            var prefix = settings.CacheRegionPrefix;
+            regionName = prefix == null ? regionName : prefix + '.' + regionName;
+            log.Info("starting update timestamps cache at region: " + regionName);
+            updateTimestamps = settings.CacheProvider.BuildCache(regionName, props);
+        }
 
-		[MethodImpl(MethodImplOptions.Synchronized)]
-		public void PreInvalidate(object[] spaces)
-		{
-			//TODO: to handle concurrent writes correctly, this should return a Lock to the client
-			long ts = updateTimestamps.NextTimestamp() + updateTimestamps.Timeout;
-			for (int i = 0; i < spaces.Length; i++)
-			{
-				updateTimestamps.Put(spaces[i], ts);
-			}
-			//TODO: return new Lock(ts);
-		}
+        public void PreInvalidate(object[] spaces)
+        {
+            rwl.EnterWriteLock();
+            try
+            {
+                //TODO: to handle concurrent writes correctly, this should return a Lock to the client
+                var ts = updateTimestamps.NextTimestamp() + updateTimestamps.Timeout;
+                for (var i = 0; i < spaces.Length; i++)
+                {
+                    updateTimestamps.Put(spaces[i], ts);
+                }
+                //TODO: return new Lock(ts);
+            }
+            finally
+            {
+                rwl.ExitWriteLock();
+            }
+        }
 
-		/// <summary></summary>
-		[MethodImpl(MethodImplOptions.Synchronized)]
-		public void Invalidate(object[] spaces)
-		{
-			//TODO: to handle concurrent writes correctly, the client should pass in a Lock
-			long ts = updateTimestamps.NextTimestamp();
-			//TODO: if lock.getTimestamp().equals(ts)
-			for (int i = 0; i < spaces.Length; i++)
-			{
-				log.Debug(string.Format("Invalidating space [{0}]", spaces[i]));
-				updateTimestamps.Put(spaces[i], ts);
-			}
-		}
+        public void Invalidate(object[] spaces)
+        {
+            rwl.EnterWriteLock();
+            try
+            {
+                //TODO: to handle concurrent writes correctly, the client should pass in a Lock
+                var ts = updateTimestamps.NextTimestamp();
+                //TODO: if lock.getTimestamp().equals(ts)
+                for (var i = 0; i < spaces.Length; i++)
+                {
+                    log.Debug(string.Format("Invalidating space [{0}]", spaces[i]));
+                    updateTimestamps.Put(spaces[i], ts);
+                }
+            }
+            finally
+            {
+                rwl.ExitWriteLock();
+            }
+        }
 
-		[MethodImpl(MethodImplOptions.Synchronized)]
-		public bool IsUpToDate(ISet<string> spaces, long timestamp /* H2.1 has Long here */)
-		{
-			foreach (string space in spaces)
-			{
-				object lastUpdate = updateTimestamps.Get(space);
-				if (lastUpdate == null)
-				{
-					//the last update timestamp was lost from the cache
-					//(or there were no updates since startup!)
+        public bool IsUpToDate(ISet<string> spaces, long timestamp /* H2.1 has Long here */)
+        {
+            rwl.EnterReadLock();
+            try
+            {
+                foreach (var space in spaces)
+                {
+                    var lastUpdate = updateTimestamps.Get(space);
+                    if (lastUpdate == null)
+                    {
+                        //the last update timestamp was lost from the cache
+                        //(or there were no updates since startup!)
 
-					//NOTE: commented out, since users found the "safe" behavior
-					//      counter-intuitive when testing, and we couldn't deal
-					//      with all the forum posts :-(
-					//updateTimestamps.put( space, new Long( updateTimestamps.nextTimestamp() ) );
-					//result = false; // safer
+                        //NOTE: commented out, since users found the "safe" behavior
+                        //      counter-intuitive when testing, and we couldn't deal
+                        //      with all the forum posts :-(
+                        //updateTimestamps.put( space, new Long( updateTimestamps.nextTimestamp() ) );
+                        //result = false; // safer
 
-					//OR: put a timestamp there, to avoid subsequent expensive
-					//    lookups to a distributed cache - this is no good, since
-					//    it is non-threadsafe (could hammer effect of an actual
-					//    invalidation), and because this is not the way our
-					//    preferred distributed caches work (they work by
-					//    replication)
-					//updateTimestamps.put( space, new Long(Long.MIN_VALUE) );
-				}
-				else
-				{
-					if ((long) lastUpdate >= timestamp)
-					{
-						return false;
-					}
-				}
-			}
-			return true;
-		}
+                        //OR: put a timestamp there, to avoid subsequent expensive
+                        //    lookups to a distributed cache - this is no good, since
+                        //    it is non-threadsafe (could hammer effect of an actual
+                        //    invalidation), and because this is not the way our
+                        //    preferred distributed caches work (they work by
+                        //    replication)
+                        //updateTimestamps.put( space, new Long(Long.MIN_VALUE) );
+                    }
+                    else
+                    {
+                        if ((long)lastUpdate >= timestamp)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+            finally
+            {
+                rwl.ExitReadLock();
+            }
+        }
 
-		public void Destroy()
-		{
-			try
-			{
-				updateTimestamps.Destroy();
-			}
-			catch (Exception e)
-			{
-				log.Warn("could not destroy UpdateTimestamps cache", e);
-			}
-		}
-	}
+        /// <summary>
+        /// Gets timestamps of the given spaces.
+        /// </summary>
+        /// <param name="spaces"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// This method may be useful for getting a maximum timestamp of the given set of entities,
+        /// for instance for the http ETag generation.
+        /// </remarks>
+        public long?[] GetTimestamps(string[] spaces)
+        {
+            rwl.EnterReadLock();
+            try
+            {
+                var timestamps = new long?[spaces.Length];
+                for (var i = 0; i < spaces.Length; i++)
+                {
+                    var lastUpdate = updateTimestamps.Get(spaces[i]);
+                    if (lastUpdate == null)
+                    {
+                        timestamps[i] = null;
+                    }
+                    else
+                    {
+                        timestamps[i] = (long)lastUpdate;
+                    }
+                }
+                return timestamps;
+            }
+            finally
+            {
+                rwl.ExitReadLock();
+            }
+        }
+
+        public void Destroy()
+        {
+            try
+            {
+                updateTimestamps.Destroy();
+                rwl.Dispose();
+            }
+            catch (Exception e)
+            {
+                log.Warn("could not destroy UpdateTimestamps cache", e);
+            }
+        }
+    }
 }
