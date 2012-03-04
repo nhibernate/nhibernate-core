10865457dd21fb95393ff617299b05b37484897c
Oskar Berggren <oskar.berggren@gmail.com>
2012-03-04 15:51:07 +0100

    VisitorUtil.cs: Use guard statements to reduce deep nesting.

--- src/NHibernate/Linq/Visitors/VisitorUtil.cs
+++ src/NHibernate/Linq/Visitors/VisitorUtil.cs
@@ -15,30 +15,32 @@ namespace NHibernate.Linq.Visitors
 	{
 		public static bool IsDynamicComponentDictionaryGetter(MethodInfo method, Expression targetObject, ReadOnlyCollection<Expression> arguments, ISessionFactory sessionFactory, out string memberName)
 		{
-			//an IDictionary item getter?
-			if (method.Name == "get_Item" && typeof(IDictionary).IsAssignableFrom(targetObject.Type))
-			{//a  string constant expression as the argument?	
-				ConstantExpression key = arguments.First().As<ConstantExpression>();
-				if (key != null && key.Type == typeof(string))
-				{
-					//The potential member name
-					memberName = (string)key.Value;
-					//need the owning member
-					MemberExpression member = targetObject.As<MemberExpression>();
-					if (member != null)
-					{
-						IClassMetadata metaData = sessionFactory.GetClassMetadata(member.Expression.Type);
-						if (metaData != null)
-						{
-							// is it mapped as a component?
-							IType propertyType = metaData.GetPropertyType(member.Member.Name);
-							return (propertyType != null && propertyType.IsComponentType);
-						}
-					}
-				}
-			}
 			memberName = null;
-			return false;
+
+			// A dynamic component must be an IDictionary with a string key.
+
+			if (method.Name != "get_Item" || !typeof(IDictionary).IsAssignableFrom(targetObject.Type))
+				return false;
+
+			var key = arguments.First().As<ConstantExpression>();
+			if (key == null || key.Type != typeof(string))
+				return false;
+
+			// The potential member name
+			memberName = (string)key.Value;
+
+			// Need the owning member (the dictionary).
+			var member = targetObject.As<MemberExpression>();
+			if (member == null)
+				return false;
+
+			var metaData = sessionFactory.GetClassMetadata(member.Expression.Type);
+			if (metaData == null)
+				return false;
+
+			// IDictionary can be mapped as collection or component - is it mapped as a component?
+			var propertyType = metaData.GetPropertyType(member.Member.Name);
+			return (propertyType != null && propertyType.IsComponentType);
 		}
 
 		public static bool IsDynamicComponentDictionaryGetter(MethodCallExpression expression, ISessionFactory sessionFactory, out string memberName)
