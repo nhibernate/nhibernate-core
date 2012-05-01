3bb3192ff45b6e02f379374f5d2f5578bfc42890
Nicaog <nicaog@gmail.com>
2012-05-01 13:00:19 -0600

    Added failing tests for NH-3132

---
+++ src/NHibernate.Test/NHSpecificTest/NH3132/Fixture.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using NHibernate.Criterion;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH3132
+{
+    [TestFixture]
+    public class Fixture : TestCase
+    {
+        protected override string MappingsAssembly
+        {
+            get { return "NHibernate.Test"; }
+        }
+
+        protected override IList Mappings
+        {
+            get
+            {
+                return new string[]
+					{
+						"NHSpecificTest.NH3132.Mappings.hbm.xml"
+					};
+            }
+        }
+        
+        /// <summary>
+        /// push some data into the database
+        /// Really functions as a save test also 
+        /// </summary>
+        protected override void OnSetUp()
+        {
+            base.OnSetUp();
+
+            using (var session = OpenSession())
+            {
+                using (var tran = session.BeginTransaction())
+                {
+                    Product product = new Product();
+                    product.Name = "First";
+                    product.Lazy = "Lazy";
+                    
+                    session.Save(product);
+
+/*                    Inventory inventory = new Inventory();
+                    inventory.Id = product.Id;
+                    inventory.Quantity = 1;
+
+                    session.Save(inventory);*/
+
+                    tran.Commit();
+                }
+            }
+        }
+
+        protected override void OnTearDown()
+        {
+            base.OnTearDown();
+
+            using (var session = OpenSession())
+            {
+                using (var tran = session.BeginTransaction())
+                {
+                    session.Delete("from Product");
+                    tran.Commit();
+                }                
+            }
+
+        }
+
+        [Test]
+        public void Query_returns_correct_name()
+        {
+            using (var session = OpenSession())
+            {
+                Product product = session.CreateCriteria(typeof (Product))
+                    .Add(Restrictions.Eq("Name", "First"))
+                    .UniqueResult<Product>();
+
+                Assert.IsNotNull(product);
+                Assert.AreEqual("First", product.Name);
+            }
+        }
+    }
+}
