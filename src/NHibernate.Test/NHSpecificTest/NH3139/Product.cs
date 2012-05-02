f2fe8dc43014b219a1e909255684bb75f8718b82
Nicaog <nicaog@gmail.com>
2012-05-02 15:17:04 -0600

    Added failing tests for NH-3139

---
+++ src/NHibernate.Test/NHSpecificTest/NH3139/Product.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH3139
+{
+    public class Product
+    {
+		public virtual Guid Id { get; set; }
+
+        public virtual string Name { get; set; }
+
+		public virtual Inventory Inventory { get; set; }
+    }
+
+	public class Inventory
+	{
+		public virtual Guid Id { get; set; }
+
+		public virtual int Quantity { get; set; }
+	}
+}
