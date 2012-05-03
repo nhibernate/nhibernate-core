183e05f113bbe0f671710e905c48f26b885b692d
Roger Kratz <roger.kratz@gmail.com>
2012-05-03 16:03:18 +0200

    Test and fix for NH-3141

---
+++ src/NHibernate.Test/NHSpecificTest/NH3141/ProxyIdPerformanceTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH3141
+{
+	[TestFixture]
+	public class ProxyIdPerformanceTest : BugTestCase
+	{
+		private int id;
+
+		protected override void OnSetUp()
+		{
+			using (var s = OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+				id = (int) s.Save(new Entity());
+				tx.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var s = OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+				s.CreateQuery("delete from Entity e").ExecuteUpdate();
+				tx.Commit();
+			}
+		}
+
+		[Test, Explicit("No logical test - just to compare before/after fix")]
+		public void ShouldUseIdDirectlyFromProxy()
+		{
+			var proxyEntity = CreateInitializedProxy();
+
+			const int loop = 1000000;
+			var watch = new Stopwatch();
+			watch.Start();
+			const int dummyValue = 0;
+			for (var i = 0; i < loop; i++)
+			{
+				dummyValue.CompareTo(proxyEntity.Id);
+			}
+			watch.Stop();
+
+			//before fix: 2.2s
+			//after fix: 0.8s
+			Console.WriteLine(watch.Elapsed);
+		}
+
+		private Entity CreateInitializedProxy()
+		{
+			using (var s = OpenSession())
+			using (s.BeginTransaction())
+			{
+				var proxyEntity = s.Load<Entity>(id);
+				NHibernateUtil.Initialize(proxyEntity);
+				return proxyEntity;
+			}
+		}
+	}
+}
