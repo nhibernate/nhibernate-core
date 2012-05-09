b5a4588bc623c60ac06a053a11c4a50c2540cb8d
hazzik <hazzik@gmail.com>
2012-05-09 21:14:27 +0600

    Add test and proposed fixes for NH-3126 (thanks to Luke Burgess)

---
+++ src/NHibernate.Test/NHSpecificTest/NH3126/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH3126
+{
+	public class Item
+	{
+		public virtual Guid Id { get; set; }
+
+		public virtual string Name { get; set; }
+
+		public virtual IDictionary<Guid, PropertyValue> PropertyValues { get; set; }
+	}
+}
