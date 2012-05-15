fdc0c1e40df73840a380019482f6148df41864ce
Eamon Hetherton <eamonhetherton@gmail.com>
2012-05-15 15:46:33 +1000

    cleanup
    Signed-off-by: Eamon Hetherton <eamonhetherton@gmail.com>

--- src/NHibernate.Test/NHSpecificTest/NH3148/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH3148/Fixture.cs
@@ -8,14 +8,6 @@ using SharpTestsEx;
 
 namespace NHibernate.Test.NHSpecificTest.NH3148
 {
-	/// <summary>
-	/// Fixture using 'by code' mappings
-	/// </summary>
-	/// <remarks>
-	/// This fixture is identical to <see cref="Fixture" /> except the <see cref="Entity" /> mapping is performed 
-	/// by code in the GetMappings method, and does not require the <c>Mappings.hbm.xml</c> file. Use this approach
-	/// if you prefer.
-	/// </remarks>
 	[TestFixture]
 	public class Fixture
 	{
