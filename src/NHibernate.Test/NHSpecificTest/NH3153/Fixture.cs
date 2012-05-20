a83fdbd346a5c3eddc164291c7b6942d59a636dd
Oskar Berggren <oskar.berggren@gmail.com>
2012-05-20 10:16:50 +0200

    Fix NH-3153, regression regarding schema for id generator tables introduced by NH-2569 in 3.2.

---
+++ src/NHibernate.Test/NHSpecificTest/NH3153/Fixture.cs
@@ -0,0 +1,40 @@
+using NHibernate.Mapping;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Test.NHSpecificTest.NH3153
+{
+	[TestFixture]
+	public class Fixture
+	{
+		// Regardless of whether the schema is specified in the <class> or
+		// the <nhibernate-mapping> element, we should get it into the generator
+		// properties, and it should be _consistently_ unquoted to make sure
+		// that generator from different classes will match up to the same table.
+
+		[Test]
+		public void ShouldGetIdentifierSchemaFromClassElement()
+		{
+			var conf = TestConfigurationHelper.GetDefaultConfiguration();
+			conf.AddResource("NHibernate.Test.NHSpecificTest.NH3153.SchemaInClass.hbm.xml", typeof(A).Assembly);
+
+			var mappings = conf.CreateMappings(Dialect.Dialect.GetDialect());
+
+			var pc = mappings.GetClass(typeof(A).FullName);
+			((SimpleValue)pc.Identifier).IdentifierGeneratorProperties["schema"].Should().Be("Test");
+		}
+
+
+		[Test]
+		public void ShouldGetIdentifierSchemaFromMappingElement()
+		{
+			var conf = TestConfigurationHelper.GetDefaultConfiguration();
+			conf.AddResource("NHibernate.Test.NHSpecificTest.NH3153.SchemaInMapping.hbm.xml", typeof(A).Assembly);
+
+			var mappings = conf.CreateMappings(Dialect.Dialect.GetDialect());
+
+			var pc = mappings.GetClass(typeof(A).FullName);
+			((SimpleValue)pc.Identifier).IdentifierGeneratorProperties["schema"].Should().Be("Test");
+		}
+	}
+}
