a35aaa648b2cbd936a6a514357e8a538d74206be
Oskar Berggren <oskar.berggren@gmail.com>
2012-05-23 20:56:08 +0200

    Fix NH-3142 by having the JoinWalker avoid reusing Parameter instances. Parameter instances are later assigned a value to their BackTrack property. If the same instance is used in multiple positions the BackTracks will be incorrect, since all occurrences will get the same BackTrack and therefore the same value. But in this situation, they really do represent separate values.

--- src/NHibernate/SqlCommand/DisjunctionFragment.cs
+++ src/NHibernate/SqlCommand/DisjunctionFragment.cs
@@ -1,10 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 namespace NHibernate.SqlCommand
 {
 	public class DisjunctionFragment
 	{
 		private SqlStringBuilder buffer = new SqlStringBuilder();
+
+		public DisjunctionFragment()
+		{
+		}
+
+
+		public DisjunctionFragment(IEnumerable<ConditionalFragment> fragments)
+		{
+			foreach (var conditionalFragment in fragments)
+				AddCondition(conditionalFragment);
+		}
+
 
 		public DisjunctionFragment AddCondition(ConditionalFragment fragment)
 		{
