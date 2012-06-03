3cdbb5d4097de26fb667a93bab5a81e244b39648
Oskar Berggren <oskar.berggren@gmail.com>
2012-06-03 15:53:34 +0200

    When saving a byte array, throw an exception if the db parameter's size is set to a value smaller than the length of the array, to prevent silent truncation (NH-3121).

---
+++ src/NHibernate.Test/NHSpecificTest/NH3121/Report.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace NHibernate.Test.NHSpecificTest.NH3121
+{
+	public class Report
+	{
+		public virtual Guid Id { get; set; }
+
+		public virtual Byte[] UnsizedArray { get; set; }
+		public virtual System.Drawing.Image Image { get; set; }
+		public virtual ISerializable SerializableImage { get; set; }
+	}
+}
