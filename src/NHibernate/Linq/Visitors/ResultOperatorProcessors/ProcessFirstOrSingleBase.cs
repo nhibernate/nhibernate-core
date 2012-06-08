cc49fed5cee0c44f6677d97b4986ce2065582c35
hazzik <hazzik@gmail.com>
2012-06-08 17:06:57 +0600

    ProcessFirstOrSingleBase - fix indents

--- src/NHibernate/Linq/Visitors/ResultOperatorProcessors/ProcessFirstOrSingleBase.cs
+++ src/NHibernate/Linq/Visitors/ResultOperatorProcessors/ProcessFirstOrSingleBase.cs
@@ -3,21 +3,21 @@ using System.Reflection;
 
 namespace NHibernate.Linq.Visitors.ResultOperatorProcessors
 {
-    public class ProcessFirstOrSingleBase
-    {
-        protected static void AddClientSideEval(MethodInfo target, QueryModelVisitor queryModelVisitor, IntermediateHqlTree tree)
-        {
-            target = target.MakeGenericMethod(queryModelVisitor.CurrentEvaluationType.DataType);
+	public class ProcessFirstOrSingleBase
+	{
+		protected static void AddClientSideEval(MethodInfo target, QueryModelVisitor queryModelVisitor, IntermediateHqlTree tree)
+		{
+			target = target.MakeGenericMethod(queryModelVisitor.CurrentEvaluationType.DataType);
 
-            var parameter = Expression.Parameter(queryModelVisitor.PreviousEvaluationType.DataType, null);
+			var parameter = Expression.Parameter(queryModelVisitor.PreviousEvaluationType.DataType, null);
 
-            var lambda = Expression.Lambda(
-                Expression.Call(
-                    target,
-                    parameter),
-                parameter);
+			var lambda = Expression.Lambda(
+				Expression.Call(
+					target,
+					parameter),
+				parameter);
 
-            tree.AddPostExecuteTransformer(lambda);
-        }
-    }
+			tree.AddPostExecuteTransformer(lambda);
+		}
+	}
 }
