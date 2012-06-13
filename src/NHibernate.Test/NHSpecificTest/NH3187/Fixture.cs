67b1e8c19de9af1f0feb76955c42ab6dd75f042f
hazzik <hazzik@gmail.com>
2012-06-14 00:27:26 +0600

    Add test for NH-3187 - ability to filter bag with where clause in mapping by inherited property.

---
+++ src/NHibernate.Test/NHSpecificTest/NH3187/Fixture.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using NHibernate.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH3187
+{
+	[TestFixture, Ignore("Not fixed yet.")]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				var station = new Station();
+				session.Save(station);
+
+				session.Save(new Policeman {Name = "2Bob", Station = station, IsDeleted = true});
+
+				session.Save(new Policeman {Name = "1Sally", Station = station});
+
+				session.Flush();
+				transaction.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				session.Delete("from System.Object");
+
+				session.Flush();
+				transaction.Commit();
+			}
+		}
+
+		[Test]
+		public void CanGetCountOfPolicemen()
+		{
+			using (var session = OpenSession())
+			using (session.BeginTransaction())
+			{
+				var station = session.Query<Station>().Single();
+
+				var policemen = station.Policemen;
+
+				Assert.AreEqual(1, station.Policemen.Count());
+				foreach (var policeman in policemen)
+				{
+					Assert.NotNull(policeman);
+				}
+			}
+		}
+	}
+}
