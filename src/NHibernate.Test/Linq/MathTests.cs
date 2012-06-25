6f2b370ba4d19c8d6fd05aa1838ea5d6ba0f2a2b
hazzik <hazzik@gmail.com>
2012-06-25 12:19:07 +0600

    Fix 'sign' test for Firebird and added delta to double numbers comparison.

--- src/NHibernate.Test/Linq/MathTests.cs
+++ src/NHibernate.Test/Linq/MathTests.cs
@@ -10,7 +10,7 @@ namespace NHibernate.Test.Linq
 	public class MathTests : LinqTestCase
 	{
 		private IQueryable<OrderLine> _orderLines;
-		
+
 		private void IgnoreIfNotSupported(string function)
 		{
 			if (!Dialect.Functions.ContainsKey(function))
@@ -38,7 +38,7 @@ namespace NHibernate.Test.Linq
 		{
 			IgnoreIfNotSupported("sign");
 			var signs = (from o in db.OrderLines
-						 select Math.Sign(-1*o.UnitPrice)).ToList();
+						 select Math.Sign(0m - o.UnitPrice)).ToList();
 
 			Assert.True(signs.All(x => x == -1));
 		}
@@ -47,73 +47,73 @@ namespace NHibernate.Test.Linq
 		public void SinTest()
 		{
 			IgnoreIfNotSupported("sin");
-			Test(o => Math.Sin((double)o.UnitPrice));
+			Test(o => Math.Sin((double) o.UnitPrice));
 		}
 
 		[Test]
 		public void CosTest()
 		{
 			IgnoreIfNotSupported("cos");
-			Test(o => Math.Cos((double)o.UnitPrice));
+			Test(o => Math.Cos((double) o.UnitPrice));
 		}
 
 		[Test]
 		public void TanTest()
 		{
 			IgnoreIfNotSupported("tan");
-			Test(o => Math.Tan((double)o.Discount));
+			Test(o => Math.Tan((double) o.Discount));
 		}
 
 		[Test]
 		public void SinhTest()
 		{
 			IgnoreIfNotSupported("sinh");
-			Test(o => Math.Sinh((double)o.Discount));
+			Test(o => Math.Sinh((double) o.Discount));
 		}
 
 		[Test]
 		public void CoshTest()
 		{
 			IgnoreIfNotSupported("cosh");
-			Test(o => Math.Cosh((double)o.Discount));
+			Test(o => Math.Cosh((double) o.Discount));
 		}
 
 		[Test]
 		public void TanhTest()
 		{
 			IgnoreIfNotSupported("tanh");
-			Test(o => Math.Tanh((double)o.Discount));
+			Test(o => Math.Tanh((double) o.Discount));
 		}
 
 		[Test]
 		public void AsinTest()
 		{
 			IgnoreIfNotSupported("asin");
-			Test(o => Math.Asin((double)o.Discount));
+			Test(o => Math.Asin((double) o.Discount));
 		}
 
 		[Test]
 		public void AcosTest()
 		{
 			IgnoreIfNotSupported("acos");
-			Test(o => Math.Acos((double)o.Discount));
+			Test(o => Math.Acos((double) o.Discount));
 		}
 
 		[Test]
 		public void AtanTest()
 		{
 			IgnoreIfNotSupported("atan");
-			Test(o => Math.Atan((double)o.UnitPrice));
+			Test(o => Math.Atan((double) o.UnitPrice));
 		}
 
 		[Test]
 		public void Atan2Test()
 		{
 			IgnoreIfNotSupported("atan2");
-			Test(o => Math.Atan2((double)o.Discount, 0.5d));
+			Test(o => Math.Atan2((double) o.Discount, 0.5d));
 		}
 
-		private void Test<T>(Expression<Func<OrderLine, T>> selector)
+		private void Test(Expression<Func<OrderLine, double>> selector)
 		{
 			var expected = _orderLines
 				.Select(selector)
@@ -123,7 +123,9 @@ namespace NHibernate.Test.Linq
 				.Take(10)
 				.ToList();
 
-			Assert.AreEqual(actual, expected);
+			Assert.AreEqual(expected.Count, actual.Count);
+			for (var i = 0; i < expected.Count; i++)
+				Assert.AreEqual(expected[i], actual[i], 0.000001);
 		}
 	}
 }
