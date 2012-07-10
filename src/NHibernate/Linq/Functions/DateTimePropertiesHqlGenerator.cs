8cd649cb299f9803c8855c4b6886ebafdf272f56
hazzik <hazzik@gmail.com>
2012-07-10 22:44:13 +0600

    Linq - add generators for DateTimeOffset properties (NH-3209)

--- src/NHibernate/Linq/Functions/DateTimePropertiesHqlGenerator.cs
+++ src/NHibernate/Linq/Functions/DateTimePropertiesHqlGenerator.cs
@@ -7,36 +7,34 @@ using NHibernate.Linq.Visitors;
 
 namespace NHibernate.Linq.Functions
 {
-	public class DateTimePropertiesHqlGenerator : IHqlGeneratorForProperty
+	public class DateTimePropertiesHqlGenerator : BaseHqlGeneratorForProperty
 	{
-		private readonly MemberInfo[] supportedProperties;
-
 		public DateTimePropertiesHqlGenerator()
 		{
-			supportedProperties = new[]
-			                      	{
-			                      		ReflectionHelper.GetProperty((DateTime x) => x.Year),
-			                      		ReflectionHelper.GetProperty((DateTime x) => x.Month),
-			                      		ReflectionHelper.GetProperty((DateTime x) => x.Day),
-			                      		ReflectionHelper.GetProperty((DateTime x) => x.Hour),
-			                      		ReflectionHelper.GetProperty((DateTime x) => x.Minute),
-			                      		ReflectionHelper.GetProperty((DateTime x) => x.Second),
-			                      		ReflectionHelper.GetProperty((DateTime x) => x.Date),
-			                      	};
+			SupportedProperties = new[]
+				{
+					ReflectionHelper.GetProperty((DateTime x) => x.Year),
+					ReflectionHelper.GetProperty((DateTime x) => x.Month),
+					ReflectionHelper.GetProperty((DateTime x) => x.Day),
+					ReflectionHelper.GetProperty((DateTime x) => x.Hour),
+					ReflectionHelper.GetProperty((DateTime x) => x.Minute),
+					ReflectionHelper.GetProperty((DateTime x) => x.Second),
+					ReflectionHelper.GetProperty((DateTime x) => x.Date),
+					
+					ReflectionHelper.GetProperty((DateTimeOffset x) => x.Year),
+					ReflectionHelper.GetProperty((DateTimeOffset x) => x.Month),
+					ReflectionHelper.GetProperty((DateTimeOffset x) => x.Day),
+					ReflectionHelper.GetProperty((DateTimeOffset x) => x.Hour),
+					ReflectionHelper.GetProperty((DateTimeOffset x) => x.Minute),
+					ReflectionHelper.GetProperty((DateTimeOffset x) => x.Second),
+					ReflectionHelper.GetProperty((DateTimeOffset x) => x.Date),
+				};
 		}
 
-		public IEnumerable<MemberInfo> SupportedProperties
-		{
-			get
-			{
-				return supportedProperties;
-			}
-		}
-
-		public virtual HqlTreeNode BuildHql(MemberInfo member, Expression expression, HqlTreeBuilder treeBuilder, IHqlExpressionVisitor visitor)
+		public override HqlTreeNode BuildHql(MemberInfo member, Expression expression, HqlTreeBuilder treeBuilder, IHqlExpressionVisitor visitor)
 		{
 			return treeBuilder.MethodCall(member.Name.ToLowerInvariant(),
-			                              visitor.Visit(expression).AsExpression());
+										  visitor.Visit(expression).AsExpression());
 		}
 	}
 }
