94a77942b93e8da6ec791a86abe54a9b74d2def1
kriebb <info@riebbels.be>
2012-07-16 15:55:33 +0200

    Fix NH-3221

---
+++ src/NHibernate.Test/NHSpecificTest/NH3221/Person.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using Iesi.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH3221
+{
+	public class Person : EqualityAndHashCodeProvider<Person, Guid>
+	{
+		#region Instance Variables (4) 
+
+		private readonly ISet<Todo> todos;
+		private readonly IList<Stuff> myStuff;
+
+		#endregion Instance Variables 
+
+		#region Public Properties (8) 
+		
+
+		public override Guid Id { get; protected set; }
+
+
+		public virtual string Name { get; set; }
+
+		public virtual string NickName { get; set; }
+
+		public virtual Person Partner { get; set; }
+
+		public virtual IEnumerable<Todo> Todos { get { return todos; } }
+		public virtual IEnumerable<Stuff> MyStuff { get { return myStuff; } }
+
+		#endregion Public Properties 
+
+		#region Constructors (2) 
+
+		protected Person()
+		{
+			todos = new HashedSet<Todo>();
+			myStuff = new List<Stuff>();
+		}
+
+		public Person(string name):this()
+		{
+			Name = name;
+		}
+
+		#endregion Constructors 
+
+		#region Public Methods (4) 
+
+		public virtual Todo AddTodo(Todo todo)
+		{
+			todos.Add(todo);
+			return todo;
+		}
+
+		#endregion Public Methods 
+
+   
+
+		public virtual Stuff AddStuff(Stuff stuff)
+		{
+			myStuff.Add(stuff);
+			return stuff;
+		}
+
+		public virtual void RemoveTodo(Todo myTodo)
+		{
+			todos.Remove(myTodo);
+		}
+
+		public virtual void RemoveStuff(Stuff stuff)
+		{
+			myStuff.Remove(stuff);
+		}
+	}
+}
