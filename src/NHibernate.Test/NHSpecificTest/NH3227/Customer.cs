b1a2e8c1e86e63131d9d4c134fe562e5ea471a24
Peter Beams <peter.beams@googlemail.com>
2012-07-25 13:20:53 +0100

    Adding test to expose exception in NH3227

---
+++ src/NHibernate.Test/NHSpecificTest/NH3227/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NHibernate.Test.NHSpecificTest.NH3227
+{
+	public class Customer
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+	}
+}
