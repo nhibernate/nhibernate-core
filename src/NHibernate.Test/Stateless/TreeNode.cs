ddc0d4d02ebd491f0606dc0ed697cfa6ae75f33e
CSharper2010 <csharper2010@googlemail.com>
2012-08-16 22:49:11 +0200

    Unit Test for FetchMany and ThenFetchMany in Stateless Session
    Signed-off-by: CSharper2010 <csharper2010@googlemail.com>

---
+++ src/NHibernate.Test/Stateless/TreeNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Iesi.Collections.Generic;
+
+namespace NHibernate.Test.Stateless
+{
+    public class TreeNode
+    {
+        private ISet<TreeNode> children = new HashedSet<TreeNode>();
+
+        public virtual int Id { get; protected set; }
+
+        public virtual string Content { get; set; }
+
+        public virtual ISet<TreeNode> Children
+        {
+            get { return children; }
+            protected set { children = value; }
+        }
+    }
+}
