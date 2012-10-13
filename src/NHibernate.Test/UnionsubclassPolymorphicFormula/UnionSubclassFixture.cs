1a493788585c89cfdd9f3b75e7b0f786ad192f5a
Tatu Teräväinen <tatu.teravainen@gmail.com>
2012-10-13 19:21:13 +0300

    Implemented a couple of tests that demonstrate NH-2354

---
+++ src/NHibernate.Test/UnionsubclassPolymorphicFormula/UnionSubclassFixture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Collections;
+
+namespace NHibernate.Test.UnionsubclassPolymorphicFormula
+{
+	[TestFixture]
+	public class UnionSubclassFixture : TestCase
+	{
+		protected override string MappingsAssembly
+		{
+			get { return "NHibernate.Test"; }
+		}
+
+		protected override IList Mappings
+		{
+			get { return new string[] { "UnionsubclassPolymorphicFormula.Party.hbm.xml" }; }
+		}
+
+		[Test]
+		public void QueryOverPersonTest()
+		{
+			using (ISession s = OpenSession())
+			{
+				using (ITransaction t = s.BeginTransaction())
+				{
+					var person = new Person
+					{
+						FirstName = "Mark",
+						LastName = "Mannson"
+					};
+
+					s.Save(person);
+
+					var result = s.QueryOver<Party>().Where(p => p.Name == "Mark Mannson").SingleOrDefault();
+					
+					Assert.NotNull(result);
+					s.Delete(result);
+					t.Commit();
+				}
+				
+			}
+		}
+
+		[Test]
+		public void QueryOverCompanyTest()
+		{
+			using (ISession s = OpenSession())
+			{
+				using (ITransaction t = s.BeginTransaction())
+				{
+					var company = new Company
+					{
+						CompanyName = "Limited",
+					};
+
+					s.Save(company);
+
+					var result = s.QueryOver<Party>().Where(p => p.Name == "Limited").SingleOrDefault();
+					Assert.NotNull(result);
+				}
+
+			}
+		}
+	}
+}
