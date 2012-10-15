5f2c715e603fb79269a280f7cf03aeb8cb8e5e0f
Christopher McGinnis <cmcginnis@gmail.com>
2012-10-15 16:40:09 -0700

    NH-3291 added method overloads to provide AliasToBeanResultTransformer instance caching by target type.  Instance cache isn't used by default so as to keep backwards compatibility.

--- src/NHibernate/Transform/Transformers.cs
+++ src/NHibernate/Transform/Transformers.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 
 namespace NHibernate.Transform
 {
@@ -16,18 +17,63 @@ namespace NHibernate.Transform
 		/// <summary> Each row of results is a <see cref="IList"/></summary>
 		public static readonly ToListResultTransformer ToList = new ToListResultTransformer();
 
+		private static readonly Dictionary<System.Type, AliasToBeanResultTransformer> aliasToBeanInstances = new Dictionary<System.Type, AliasToBeanResultTransformer>();
+
 		/// <summary>
-		/// Creates a resulttransformer that will inject aliased values into instances
+		/// Creates an IResultTransformer that will inject aliased values into instances
 		/// of <paramref name="target"/> via property methods or fields.
+		/// <param name="target">The class type used to transform the result set</param>
 		/// </summary>
 		public static IResultTransformer AliasToBean(System.Type target)
 		{
-			return new AliasToBeanResultTransformer(target);
+			return AliasToBean(target, false);
 		}
 
+		
+		/// <summary>
+		/// Returns an IResultTransformer that will inject aliased values into instances
+		/// of <paramref name="target"/> via property methods or fields.
+		/// </summary>
+		/// <param name="target">The class type used to transform the result set</param>
+		/// <param name="useInstanceCache">Secifies whether to return a cached or new instance</param>	
+		public static IResultTransformer AliasToBean(System.Type target, bool useInstanceCache)
+		{
+			AliasToBeanResultTransformer transformer = null;
+
+			if (useInstanceCache)
+			{
+				if (aliasToBeanInstances.ContainsKey(target))
+				{
+					return aliasToBeanInstances[target];
+				}
+
+				transformer = new AliasToBeanResultTransformer(target);
+
+				aliasToBeanInstances.Add(target, transformer);
+			}
+
+			return transformer ?? new AliasToBeanResultTransformer(target);
+		}
+
+		/// <summary>
+		/// Creates an IResultTransformer that will inject aliased values into instances
+		/// of <paramref name="target"/> via property methods or fields.
+		/// <param name="target">The class type used to transform the result set</param>
+		/// </summary>
 		public static IResultTransformer AliasToBean<T>()
 		{
 			return AliasToBean(typeof(T));
+		}
+
+		/// <summary>
+		/// Returns an IResultTransformer that will inject aliased values into instances
+		/// of <paramref name="target"/> via property methods or fields.
+		/// </summary>
+		/// <param name="target">The class type used to transform the result set</param>
+		/// <param name="useInstanceCache">Secifies whether to return a cached or new instance</param>	
+		public static IResultTransformer AliasToBean<T>(bool useInstanceCache)
+		{
+			return AliasToBean(typeof(T), useInstanceCache);
 		}
 
 		public static readonly IResultTransformer DistinctRootEntity = new DistinctRootEntityResultTransformer();
