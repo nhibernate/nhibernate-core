99bddd72e884f95a55ee4525396723df1b57e2fd
Oskar Berggren <oskar.berggren@gmail.com>
2012-10-20 23:59:18 +0200

    FIX NH-2898: Fix CastException when assembling object from 2nd level cache with lazy properties (cherry picked from commit d01cdfb0808bfbd8554ef5563f6be54dbc3d5b89) (cherry picked from commit bb1d5fb050ce956744a6eb30bbfd009e8e6eeb33)

--- src/NHibernate/Intercept/ILazyPropertyInitializer.cs
+++ src/NHibernate/Intercept/ILazyPropertyInitializer.cs
@@ -1,3 +1,4 @@
+using System;
 using NHibernate.Engine;
 
 namespace NHibernate.Intercept
@@ -5,7 +6,7 @@ namespace NHibernate.Intercept
 	public struct LazyPropertyInitializer
 	{
 		/// <summary> Marker value for uninitialized properties</summary>
-		public readonly static object UnfetchedProperty= new object();
+		public readonly static object UnfetchedProperty = new Guid(0x8cbd57d5, 0xe58, 0x48a6, 0xa8, 0xf3, 0xec, 0xd4, 0xc2, 0x16, 0x35, 0xee);
 	}
 
 	/// <summary> Contract for controlling how lazy properties get initialized. </summary>
