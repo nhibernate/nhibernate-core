f785eb4dd0581e71c3ca214c8710ee458d843d90
Roger Kratz <roger.kratz@gmail.com>
2012-10-25 13:06:19 +0200

    Failing test for NH3304

---
+++ src/NHibernate.Test/NHSpecificTest/NH3304/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace NHibernate.Test.NHSpecificTest.NH3304
+{
+	public class Entity
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH3304/Fixture.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Test.NHSpecificTest.NH3304
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		[Test]
+		public void NonDbGeneratedPoidEntityShouldRollback()
+		{
+			var rollbackEntity = new Entity {Id=13, Name = "rollback"};
+			var committedEntity = new Entity {Id = 15, Name = "commited"};
+			using (var s = OpenSession())
+			{
+				using (var tx = s.BeginTransaction())
+				{
+					s.Save(rollbackEntity);
+					tx.Rollback();
+				}
+				using (var tx = s.BeginTransaction())
+				{
+					s.Save(committedEntity);
+					tx.Commit();
+				}
+			}
+			using (var s = OpenSession())
+			{
+				using (s.BeginTransaction())
+				{
+					s.CreateQuery("select count(*) from Entity e").UniqueResult<long>()
+						.Should().Be.EqualTo(1);					
+				}
+			}
+		}
+	}
+}
