7a6f0a36249238abe2477660e4a9d0dbdacb9a5d
Alberto Basile <vandalo72@gmail.com>
2012-10-26 17:33:00 +0100

    Add test for SecondLevelCache: Cachable with DistinctRootEntity exception.
    Setting a query cachable when using
    TransformUsing(Transformers.DistinctRootEntity) seems to generate a bug.
    Need to create the database Northwind with the following script:
    MsSql2008DialectLinqReadonlyCreateScript.

---
+++ src/NHibernate.Test/SecondLevelCacheTest/CacheableWithDistinctRootEntityFixture.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+using NHibernate.Stat;
+using NHibernate.Transform;
+using NHibernate.DomainModel.Northwind.Entities;
+using NHibernate.Cfg;
+
+using NUnit.Framework;
+
+namespace NHibernate.Test.SecondLevelCacheTests
+{
+    [TestFixture]
+    public class CacheableWithDistinctRootEntityFixture : ReadonlyTestCase
+    {
+        protected override void Configure(Configuration configuration)
+        {
+            configuration.SetProperty(Cfg.Environment.UseSecondLevelCache, "true");
+        }
+        
+        [Test]
+        public void TestWorkingQueryOverNoCachable()
+        {
+            int resultCount = 0;
+            OrderLine orderLines = null;
+            IList<Order> orders;
+
+            using (ISession s = OpenSession())
+            using (ITransaction tx = s.BeginTransaction())
+            {
+                orders = s.QueryOver<Order>()
+                    .Left.JoinAlias(t => t.OrderLines, () => orderLines)
+                    .Fetch(x => x.OrderLines).Eager
+                    .OrderBy(x => x.OrderDate).Desc
+                    .TransformUsing(Transformers.DistinctRootEntity)
+                    // .Cacheable()
+                    .List();
+
+                tx.Commit();
+            }
+
+            if ((orders != null) && (orders.Count>0))
+            {
+                resultCount = orders.Count;
+            }
+
+            Assert.Greater(resultCount, 0, "Cannot fetch orders.");
+        }
+
+        [Test]
+        public void TestWorkingQueryOverTransformDistinctRootEntity()
+        {
+            int resultCount = 0;
+            OrderLine orderLines = null;
+            IList<Order> orders;
+
+            using (ISession s = OpenSession())
+            using (ITransaction tx = s.BeginTransaction())
+            {
+                orders = s.QueryOver<Order>()
+                    .Left.JoinAlias(t => t.OrderLines, () => orderLines)
+                    .Fetch(x => x.OrderLines).Eager
+                    .OrderBy(x => x.OrderDate).Desc
+                    // .TransformUsing(Transformers.DistinctRootEntity)
+                    .Cacheable()
+                    .List();
+
+                tx.Commit();
+            }
+
+            if ((orders != null) && (orders.Count > 0))
+            {
+                resultCount = orders.Count;
+            }
+
+            Assert.Greater(resultCount, 0, "Cannot fetch orders.");
+        }
+
+        [Test]
+        public void TestNonWorkingQueryOverCachableAndTransformDistinctRootEntity()
+        {
+            int resultCount = 0;
+            OrderLine orderLines = null;
+            IList<Order> orders;
+
+            using (ISession s = OpenSession())
+            using (ITransaction tx = s.BeginTransaction())
+            {
+                orders = s.QueryOver<Order>()
+                    .Left.JoinAlias(t => t.OrderLines, () => orderLines)
+                    .Fetch(x => x.OrderLines).Eager
+                    .OrderBy(x => x.OrderDate).Desc
+                    .TransformUsing(Transformers.DistinctRootEntity)
+                    .Cacheable()
+                    .List();
+
+                tx.Commit();
+            }
+
+            if ((orders != null) && (orders.Count > 0))
+            {
+                resultCount = orders.Count;
+            }
+
+            Assert.Greater(resultCount, 0, "Cannot fetch orders.");
+        }
+    }
+}
