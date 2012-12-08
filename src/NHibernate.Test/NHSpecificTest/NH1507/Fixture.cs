6243dfa62d137731af48fc042cf260ea995a24c0
Alexander I. Zaytsev <hazzik@gmail.com>
2012-12-09 04:10:44 +1300

    Add driver for managed Oracle data provider (NH-3349)

--- src/NHibernate.Test/NHSpecificTest/NH1507/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH1507/Fixture.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using NHibernate.Driver;
 using NUnit.Framework;
 
 namespace NHibernate.Test.NHSpecificTest.NH1507
@@ -7,43 +8,50 @@ namespace NHibernate.Test.NHSpecificTest.NH1507
 	[TestFixture]
 	public class Fixture : BugTestCase
 	{
+		protected override bool AppliesTo(Engine.ISessionFactoryImplementor factory)
+		{
+			return !(factory.ConnectionProvider.Driver is OracleManagedDataClientDriver);
+		}
+
 		protected override void OnSetUp()
 		{
 			CreateData();
 		}
+
 		protected override void OnTearDown()
 		{
 			CleanupData();
 		}
+
 		private void CreateData()
 		{
 			//Employee
 			var emp = new Employee
-			          	{
-			          		Address = "Zombie street",
-			          		City = "Bitonto",
-			          		PostalCode = "66666",
-			          		FirstName = "tomb",
-			          		LastName = "mutilated"
-			          	};
+						{
+							Address = "Zombie street",
+							City = "Bitonto",
+							PostalCode = "66666",
+							FirstName = "tomb",
+							LastName = "mutilated"
+						};
 
 			//and his related orders
 			var order = new Order
-			            	{OrderDate = DateTime.Now, Employee = emp, ShipAddress = "dead zone 1", ShipCountry = "Deadville"};
+							{OrderDate = DateTime.Now, Employee = emp, ShipAddress = "dead zone 1", ShipCountry = "Deadville"};
 
 			var order2 = new Order
-			             	{OrderDate = DateTime.Now, Employee = emp, ShipAddress = "dead zone 2", ShipCountry = "Deadville"};
+							{OrderDate = DateTime.Now, Employee = emp, ShipAddress = "dead zone 2", ShipCountry = "Deadville"};
 
 			//Employee with no related orders but with same PostalCode
 			var emp2 = new Employee
-			           	{
-			           		Address = "Gut street",
-			           		City = "Mariotto",
-			           		Country = "Arised",
-			           		PostalCode = "66666",
-			           		FirstName = "carcass",
-			           		LastName = "purulent"
-			           	};
+						{
+							Address = "Gut street",
+							City = "Mariotto",
+							Country = "Arised",
+							PostalCode = "66666",
+							FirstName = "carcass",
+							LastName = "purulent"
+						};
 
 			//Order with no related employee but with same ShipCountry
 			var order3 = new Order {OrderDate = DateTime.Now, ShipAddress = "dead zone 2", ShipCountry = "Deadville"};
@@ -88,7 +96,7 @@ namespace NHibernate.Test.NHSpecificTest.NH1507
 				//explicit join
 				IList results =
 					session.CreateQuery("select count(*) from Order as entity join entity.Employee ee "
-					                    + "where ee.PostalCode='66666' or entity.ShipCountry='Deadville'").List();
+										+ "where ee.PostalCode='66666' or entity.ShipCountry='Deadville'").List();
 
 				//Debug.Assert(list[0].Equals(191), "Wrong number of orders, returned: " + list[0].ToString());
 				Assert.AreEqual(2, results[0]);
@@ -103,7 +111,7 @@ namespace NHibernate.Test.NHSpecificTest.NH1507
 				//implicit join
 				IList results =
 					session.CreateQuery("select count(*) from Order as entity "
-					                    + "where entity.Employee.PostalCode='66666' or entity.ShipCountry='Deadville'").List();
+										+ "where entity.Employee.PostalCode='66666' or entity.ShipCountry='Deadville'").List();
 
 				Assert.AreEqual(2, results[0]);
 			}
