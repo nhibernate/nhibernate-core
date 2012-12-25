dcf37c3422383cc316f3860570569f8f93d0e49b
Oskar Berggren <oskar.berggren@gmail.com>
2012-12-25 19:23:47 +0100

    MsSqlCeDialect.cs: Respect precision and scale from mapping for decimal (NH-2979).

--- src/NHibernate/Dialect/MsSqlCeDialect.cs
+++ src/NHibernate/Dialect/MsSqlCeDialect.cs
@@ -28,7 +28,7 @@ namespace NHibernate.Dialect
 			RegisterColumnType(DbType.Currency, "MONEY");
 			RegisterColumnType(DbType.DateTime, "DATETIME");
 			RegisterColumnType(DbType.Decimal, "NUMERIC(19,5)");
-			RegisterColumnType(DbType.Decimal, 19, "NUMERIC(19, $l)");
+			RegisterColumnType(DbType.Decimal, 19, "NUMERIC($p, $s)");
 			RegisterColumnType(DbType.Double, "FLOAT");
 			RegisterColumnType(DbType.Guid, "UNIQUEIDENTIFIER");
 			RegisterColumnType(DbType.Int16, "SMALLINT");
