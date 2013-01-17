b26e622df1f834c69c9086e70f212bc460a5ad8e
Alexander I. Zaytsev <hazzik@gmail.com>
2013-01-17 16:34:18 +1300

    Correct call base methods on types with lazy properties (NH-3058)

---
+++ src/NHibernate.Test/NHSpecificTest/NH3058/DomainClass.cs
@@ -0,0 +1,14 @@
+﻿namespace NHibernate.Test.NHSpecificTest.NH3058
+{
+	public class DomainClass
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual string ALotOfText { get; set; }
+
+		public virtual string LoadLazyProperty()
+		{
+			return ALotOfText;
+		}
+	}
+}
