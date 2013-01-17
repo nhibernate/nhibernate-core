574e353fd0a3343bc1e823aa76e80f27b5d57ba3
David Roth <david.roth@fusonic.net>
2013-01-17 17:46:01 +0100

    Fixed Bug NH1165
    Fixed NH-1165 https://nhibernate.jira.com/browse/NH-1165 (Create schemas
    with named unique constraints)

---
+++ src/NHibernate.Test/NHSpecificTest/NH1165/PKNameFixture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using NHibernate.Cfg;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Connection;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Linq;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1165
+{
+    public class ByCodeFixture : TestCaseMappingByCode
+	{
+        protected override HbmMapping GetMappings()
+		{
+			var mapper = new ModelMapper();
+			mapper.Class<Book>(rc =>
+			{
+                rc.Id(x => x.Id, m => m.Generator(Generators.Native));
+                rc.Property(x => x.ISBN_10, map => { map.Unique(true); map.NotNullable(true); });
+                rc.Property(x => x.ISBN_13, map => { map.UniqueKey("UQ_ISBN_13"); map.NotNullable(true); });
+                rc.Property(x => x.Author, map => map.UniqueKey("UQ_Author_Title"));
+                rc.Property(x => x.Title, map => map.UniqueKey("UQ_Author_Title"));
+			});
+
+            return mapper.CompileMappingForAllExplicitlyAddedEntities();
+        }
+
+        protected override void Configure(Configuration configuration)
+        {
+            base.Configure(configuration);
+            this.configuration = configuration;
+        }
+
+        private Configuration configuration;
+
+        [Test]
+        public void SchemaExport_Create_Keys_HaveNames()
+        {
+            StringBuilder builder = new StringBuilder();
+            SchemaExport export = new SchemaExport(configuration);
+            export.Execute(l => builder.AppendLine(l), false, false);
+
+            var value = builder.ToString();
+
+            // Primary key is named
+            Assert.IsTrue(value.Contains("constraint PK_Book primary key (Id)"), "Primary Key should have name.");
+
+            // unique="true" is NOT named
+            Assert.IsTrue(value.Contains("ISBN_10 NVARCHAR(255) not null unique"), "unique should output 'unique' on column.");
+
+            // unique-key="UQ_ISBN_13" is named
+            Assert.IsTrue(value.Contains("ISBN_13 NVARCHAR(255) not null"), "unique-key should output column");
+            Assert.IsFalse(value.Contains("ISBN_13 NVARCHAR(255) not null unique"), "unique-key should NOT output 'unique' on column");
+            Assert.IsTrue(value.Contains("constraint UQ_ISBN_13 unique (ISBN_13)"), "unique-key should output named constraint");
+
+            // compound unique-key is named
+            Assert.IsTrue(value.Contains("constraint UQ_Author_Title unique (Author, Title)"), "unique-key should output named constraint for compound keys");
+        }
+    }
+}
--- src/NHibernate/Mapping/UniqueKey.cs
+++ src/NHibernate/Mapping/UniqueKey.cs
@@ -5,112 +5,123 @@ using NHibernate.Util;
 
 namespace NHibernate.Mapping
 {
-	/// <summary>
-	/// An Unique Key constraint in the database.
-	/// </summary>
-	[Serializable]
-	public class UniqueKey : Constraint
-	{
-		/// <summary>
-		/// Generates the SQL string to create the Unique Key Constraint in the database.
-		/// </summary>
-		/// <param name="dialect">The <see cref="Dialect.Dialect"/> to use for SQL rules.</param>
-		/// <returns> A string that contains the SQL to create the Unique Key Constraint. </returns>
-		public string SqlConstraintString(Dialect.Dialect dialect)
-		{
-			StringBuilder buf = new StringBuilder("unique (");
-			bool commaNeeded = false;
-			bool nullable = false;
-			foreach (Column column in ColumnIterator)
-			{
-				if (!nullable && column.IsNullable)
-					nullable = true;
+    /// <summary>
+    /// An Unique Key constraint in the database.
+    /// </summary>
+    [Serializable]
+    public class UniqueKey : Constraint
+    {
+        /// <summary>
+        /// Generates the SQL string to create the Unique Key Constraint in the database.
+        /// </summary>
+        /// <param name="dialect">The <see cref="Dialect.Dialect"/> to use for SQL rules.</param>
+        /// <returns> A string that contains the SQL to create the Unique Key Constraint. </returns>
+        public string SqlConstraintString(Dialect.Dialect dialect, string suffix, bool needsQuote)
+        {
+            StringBuilder buf = new StringBuilder();
+            if (!string.IsNullOrEmpty(Name))
+            {
+                string constraintName = this.Name + suffix;
+                if (needsQuote)
+                {
+                    constraintName = dialect.QuoteForTableName(constraintName);
+                }
+                buf.Append("constraint ").Append(constraintName);
+            }
+            buf.Append(" unique (");
 
-				if (commaNeeded)
-					buf.Append(StringHelper.CommaSpace);
-				commaNeeded = true;
-				buf.Append(column.GetQuotedName(dialect));
-			}
-			//do not add unique constraint on DB not supporting unique and nullable columns
-			return !nullable || dialect.SupportsNotNullUnique ? buf.Append(StringHelper.ClosedParen).ToString() : null;
-		}
+            bool commaNeeded = false;
+            bool nullable = false;
+            foreach (Column column in ColumnIterator)
+            {
+                if (!nullable && column.IsNullable)
+                    nullable = true;
 
-		/// <summary>
-		/// Generates the SQL string to create the Unique Key Constraint in the database.
-		/// </summary>
-		/// <param name="dialect">The <see cref="Dialect.Dialect"/> to use for SQL rules.</param>
-		/// <param name="constraintName"></param>
-		/// <param name="defaultCatalog"></param>
-		/// <param name="defaultSchema"></param>
-		/// <returns>
-		/// A string that contains the SQL to create the Unique Key Constraint.
-		/// </returns>
-		public override string SqlConstraintString(Dialect.Dialect dialect, string constraintName, string defaultCatalog, string defaultSchema)
-		{
-			StringBuilder buf = new StringBuilder(dialect.GetAddPrimaryKeyConstraintString(constraintName))
-				.Append(StringHelper.OpenParen);
+                if (commaNeeded)
+                    buf.Append(StringHelper.CommaSpace);
+                commaNeeded = true;
+                buf.Append(column.GetQuotedName(dialect));
+            }
+            //do not add unique constraint on DB not supporting unique and nullable columns
+            return !nullable || dialect.SupportsNotNullUnique ? buf.Append(StringHelper.ClosedParen).ToString() : null;
+        }
 
-			bool commaNeeded = false;
-			bool nullable = false;
-			foreach (Column column in ColumnIterator)
-			{
-				if (!nullable && column.IsNullable)
-					nullable = true;
-				if (commaNeeded)
-					buf.Append(StringHelper.CommaSpace);
-				commaNeeded = true;
-				buf.Append(column.GetQuotedName(dialect));
-			}
+        /// <summary>
+        /// Generates the SQL string to create the Unique Key Constraint in the database.
+        /// </summary>
+        /// <param name="dialect">The <see cref="Dialect.Dialect"/> to use for SQL rules.</param>
+        /// <param name="constraintName"></param>
+        /// <param name="defaultCatalog"></param>
+        /// <param name="defaultSchema"></param>
+        /// <returns>
+        /// A string that contains the SQL to create the Unique Key Constraint.
+        /// </returns>
+        public override string SqlConstraintString(Dialect.Dialect dialect, string constraintName, string defaultCatalog, string defaultSchema)
+        {
+            StringBuilder buf = new StringBuilder(dialect.GetAddPrimaryKeyConstraintString(constraintName))
+                .Append(StringHelper.OpenParen);
 
-			return
-				!nullable || dialect.SupportsNotNullUnique
-					? StringHelper.Replace(buf.Append(StringHelper.ClosedParen).ToString(), "primary key", "unique")
-					: null;
-		}
+            bool commaNeeded = false;
+            bool nullable = false;
+            foreach (Column column in ColumnIterator)
+            {
+                if (!nullable && column.IsNullable)
+                    nullable = true;
+                if (commaNeeded)
+                    buf.Append(StringHelper.CommaSpace);
+                commaNeeded = true;
+                buf.Append(column.GetQuotedName(dialect));
+            }
 
-		public override string SqlCreateString(Dialect.Dialect dialect, IMapping p, string defaultCatalog, string defaultSchema)
-		{
-			if (dialect.SupportsUniqueConstraintInCreateAlterTable)
-			{
-				return base.SqlCreateString(dialect, p, defaultCatalog, defaultSchema);
-			}
-			else
-			{
-				return Index.BuildSqlCreateIndexString(dialect, Name, Table, ColumnIterator, true, defaultCatalog, defaultSchema);
-			}
-		}
+            return
+                !nullable || dialect.SupportsNotNullUnique
+                    ? StringHelper.Replace(buf.Append(StringHelper.ClosedParen).ToString(), "primary key", "unique")
+                    : null;
+        }
 
-		#region IRelationalModel Members
+        public override string SqlCreateString(Dialect.Dialect dialect, IMapping p, string defaultCatalog, string defaultSchema)
+        {
+            if (dialect.SupportsUniqueConstraintInCreateAlterTable)
+            {
+                return base.SqlCreateString(dialect, p, defaultCatalog, defaultSchema);
+            }
+            else
+            {
+                return Index.BuildSqlCreateIndexString(dialect, Name, Table, ColumnIterator, true, defaultCatalog, defaultSchema);
+            }
+        }
 
-		/// <summary>
-		/// Get the SQL string to drop this Constraint in the database.
-		/// </summary>
-		/// <param name="dialect">The <see cref="Dialect.Dialect"/> to use for SQL rules.</param>
-		/// <param name="defaultCatalog"></param>
-		/// <param name="defaultSchema"></param>
-		/// <returns>
-		/// A string that contains the SQL to drop this Constraint.
-		/// </returns>
-		public override string SqlDropString(Dialect.Dialect dialect, string defaultCatalog, string defaultSchema)
-		{
-			if (dialect.SupportsUniqueConstraintInCreateAlterTable)
-				return base.SqlDropString(dialect, defaultCatalog, defaultSchema);
-			else
-				return Index.BuildSqlDropIndexString(dialect, Table, Name, defaultCatalog, defaultSchema);
-		}
+        #region IRelationalModel Members
 
-		#endregion
+        /// <summary>
+        /// Get the SQL string to drop this Constraint in the database.
+        /// </summary>
+        /// <param name="dialect">The <see cref="Dialect.Dialect"/> to use for SQL rules.</param>
+        /// <param name="defaultCatalog"></param>
+        /// <param name="defaultSchema"></param>
+        /// <returns>
+        /// A string that contains the SQL to drop this Constraint.
+        /// </returns>
+        public override string SqlDropString(Dialect.Dialect dialect, string defaultCatalog, string defaultSchema)
+        {
+            if (dialect.SupportsUniqueConstraintInCreateAlterTable)
+                return base.SqlDropString(dialect, defaultCatalog, defaultSchema);
+            else
+                return Index.BuildSqlDropIndexString(dialect, Table, Name, defaultCatalog, defaultSchema);
+        }
 
-		public override bool IsGenerated(Dialect.Dialect dialect)
-		{
-			if (dialect.SupportsNotNullUnique)
-				return true;
-			foreach (Column column in ColumnIterator)
-			{
-				if(column.IsNullable)
-					return false;
-			}
-			return true;
-		}
-	}
+        #endregion
+
+        public override bool IsGenerated(Dialect.Dialect dialect)
+        {
+            if (dialect.SupportsNotNullUnique)
+                return true;
+            foreach (Column column in ColumnIterator)
+            {
+                if (column.IsNullable)
+                    return false;
+            }
+            return true;
+        }
+    }
 }
