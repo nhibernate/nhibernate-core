ed07ed45ae555f220a7a9d14da5eb004688dda52
Dmitry Naumov <dimon-naumov@yandex.ru>
2013-01-27 01:15:24 +0400

    Improve Tuple equality
    Let's make clear statement that Tuple can't have null Items and Items
    can't change (as well as Tuple's hash code)

--- src/NHibernate/Linq/NestedSelects/Tuple.cs
+++ src/NHibernate/Linq/NestedSelects/Tuple.cs
@@ -6,18 +6,30 @@ namespace NHibernate.Linq.NestedSelects
 {
 	internal class Tuple : IEquatable<Tuple>
 	{
-		public static readonly FieldInfo ItemsField = typeof (Tuple).GetField("Items");
+		public static readonly ConstructorInfo Constructor = typeof (Tuple).GetConstructor(new[] { typeof (object[]) });
+		public static readonly PropertyInfo ItemsProperty = typeof (Tuple).GetProperty("Items");
+		private readonly object[] _items;
 
-		public object[] Items;
+		public Tuple(object[] items)
+		{
+			if (items == null) throw new ArgumentNullException("items");
+			_items = items;
+		}
+
+		public object[] Items
+		{
+			get { return _items; }
+		}
 
 		public bool Equals(Tuple other)
 		{
 			if (other == null) return false;
+			if (ReferenceEquals(this, other)) return true;
 
-			if (other.Items.Length != Items.Length)
+			if (other._items.Length != _items.Length)
 				return false;
 
-			return !other.Items.Where((t, index) => !Equals(t, Items[index])).Any();
+			return _items.SequenceEqual(other._items);
 		}
 
 		public override bool Equals(object obj)
@@ -27,7 +39,19 @@ namespace NHibernate.Linq.NestedSelects
 
 		public override int GetHashCode()
 		{
-			return (Items != null ? Items.Length.GetHashCode() : 0);
+			unchecked
+			{
+				var length = _items.Length;
+				if (length == 0)
+					return 0;
+
+				var lengthCode = length;
+				var firstElement = _items[0];
+				if (ReferenceEquals(firstElement, null))
+					return lengthCode;
+
+				return firstElement.GetHashCode() * 397 ^ lengthCode;
+			}
 		}
 	}
 }
