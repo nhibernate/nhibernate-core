f790b991780687a4a8309497471b41b956ecc2c2
Manuel Pallier <cremor@gmx.net>
2013-01-28 08:03:16 +0100

    NH-3383: Fix for multiple objects of CascadeStyle in Memory that should be singleton.

--- src/NHibernate/Engine/CascadeStyle.cs
+++ src/NHibernate/Engine/CascadeStyle.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Security;
+using System.Security.Permissions;
 using NHibernate.Util;
 
 namespace NHibernate.Engine
@@ -77,12 +80,53 @@ namespace NHibernate.Engine
 				return style;
 		}
 
+		private static void RunTypeConstructor()
+		{
+			// No code needed.
+		}
+
 		#endregion
 
 		#region The CascadeStyle implementations
 
 		[Serializable]
-		private class AllDeleteOrphanCascadeStyle : CascadeStyle
+		private abstract class SingletonCascadeStyle<TConcrete> : CascadeStyle, ISerializable
+			where TConcrete : class, new()
+		{
+			public static readonly TConcrete Instance = new TConcrete();
+
+#if NET_4_0
+			[SecurityCritical]
+#else
+			[SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.SerializationFormatter)]
+#endif
+			public void GetObjectData(SerializationInfo info, StreamingContext context)
+			{
+				// Don't serialize the real object, that could cause multiple objects of the same
+				// singleton type after deserialization.
+				info.SetType(typeof(CascadeStyleReference));
+			}
+
+			[Serializable]
+			private sealed class CascadeStyleReference : IObjectReference
+			{
+#if NET_4_0
+				[SecurityCritical]
+#endif
+				public Object GetRealObject(StreamingContext context)
+				{
+					// First make sure that the type constructor of the base class was already executed.
+					// If this isn't the case 'Instance' can't be constructed and will stay null.
+					CascadeStyle.RunTypeConstructor();
+
+					// Return the singleton instance of this CascadeStyle.
+					return Instance;
+				}
+			}
+		}
+
+		[Serializable]
+		private class AllDeleteOrphanCascadeStyle : SingletonCascadeStyle<AllDeleteOrphanCascadeStyle>
 		{
 			public override bool DoCascade(CascadingAction action)
 			{
@@ -95,7 +139,7 @@ namespace NHibernate.Engine
 		}
 
 		[Serializable]
-		private class AllCascadeStyle : CascadeStyle
+		private class AllCascadeStyle : SingletonCascadeStyle<AllCascadeStyle>
 		{
 			public override bool DoCascade(CascadingAction action)
 			{
@@ -104,7 +148,7 @@ namespace NHibernate.Engine
 		}
 
 		[Serializable]
-		private class UpdateCascadeStyle : CascadeStyle
+		private class UpdateCascadeStyle : SingletonCascadeStyle<UpdateCascadeStyle>
 		{
 			public override bool DoCascade(CascadingAction action)
 			{
@@ -113,7 +157,7 @@ namespace NHibernate.Engine
 		}
 
 		[Serializable]
-		private class LockCascadeStyle : CascadeStyle
+		private class LockCascadeStyle : SingletonCascadeStyle<LockCascadeStyle>
 		{
 			public override bool DoCascade(CascadingAction action)
 			{
@@ -122,7 +166,7 @@ namespace NHibernate.Engine
 		}
 
 		[Serializable]
-		private class RefreshCascadeStyle : CascadeStyle
+		private class RefreshCascadeStyle : SingletonCascadeStyle<RefreshCascadeStyle>
 		{
 			public override bool DoCascade(CascadingAction action)
 			{
@@ -131,7 +175,7 @@ namespace NHibernate.Engine
 		}
 
 		[Serializable]
-		private class EvictCascadeStyle : CascadeStyle
+		private class EvictCascadeStyle : SingletonCascadeStyle<EvictCascadeStyle>
 		{
 			public override bool DoCascade(CascadingAction action)
 			{
@@ -140,7 +184,7 @@ namespace NHibernate.Engine
 		}
 
 		[Serializable]
-		private class ReplicateCascadeStyle : CascadeStyle
+		private class ReplicateCascadeStyle : SingletonCascadeStyle<ReplicateCascadeStyle>
 		{
 			public override bool DoCascade(CascadingAction action)
 			{
@@ -149,7 +193,7 @@ namespace NHibernate.Engine
 		}
 
 		[Serializable]
-		private class MergeCascadeStyle : CascadeStyle
+		private class MergeCascadeStyle : SingletonCascadeStyle<MergeCascadeStyle>
 		{
 			public override bool DoCascade(CascadingAction action)
 			{
@@ -158,7 +202,7 @@ namespace NHibernate.Engine
 		}
 
 		[Serializable]
-		private class PersistCascadeStyle : CascadeStyle
+		private class PersistCascadeStyle : SingletonCascadeStyle<PersistCascadeStyle>
 		{
 			public override bool DoCascade(CascadingAction action)
 			{
@@ -167,7 +211,7 @@ namespace NHibernate.Engine
 		}
 
 		[Serializable]
-		private class DeleteCascadeStyle : CascadeStyle
+		private class DeleteCascadeStyle : SingletonCascadeStyle<DeleteCascadeStyle>
 		{
 			public override bool DoCascade(CascadingAction action)
 			{
@@ -176,7 +220,7 @@ namespace NHibernate.Engine
 		}
 
 		[Serializable]
-		private class DeleteOrphanCascadeStyle : CascadeStyle
+		private class DeleteOrphanCascadeStyle : SingletonCascadeStyle<DeleteOrphanCascadeStyle>
 		{
 			public override bool DoCascade(CascadingAction action)
 			{
@@ -195,7 +239,7 @@ namespace NHibernate.Engine
 		}
 
 		[Serializable]
-		private class NoneCascadeStyle : CascadeStyle
+		private class NoneCascadeStyle : SingletonCascadeStyle<NoneCascadeStyle>
 		{
 			public override bool DoCascade(CascadingAction action)
 			{
@@ -251,40 +295,40 @@ namespace NHibernate.Engine
 		}
 
 		/// <summary> save / delete / update / evict / lock / replicate / merge / persist + delete orphans</summary>
-		public static readonly CascadeStyle AllDeleteOrphan = new AllDeleteOrphanCascadeStyle();
+		public static readonly CascadeStyle AllDeleteOrphan = AllDeleteOrphanCascadeStyle.Instance;
 
 		/// <summary> save / delete / update / evict / lock / replicate / merge / persist</summary>
-		public static readonly CascadeStyle All = new AllCascadeStyle();
+		public static readonly CascadeStyle All = AllCascadeStyle.Instance;
 
 		/// <summary> save / update</summary>
-		public static readonly CascadeStyle Update = new UpdateCascadeStyle();
+		public static readonly CascadeStyle Update = UpdateCascadeStyle.Instance;
 
 		/// <summary> lock</summary>
-		public static readonly CascadeStyle Lock = new LockCascadeStyle();
+		public static readonly CascadeStyle Lock = LockCascadeStyle.Instance;
 
 		/// <summary> refresh</summary>
-		public static readonly CascadeStyle Refresh = new RefreshCascadeStyle();
+		public static readonly CascadeStyle Refresh = RefreshCascadeStyle.Instance;
 
 		/// <summary> evict</summary>
-		public static readonly CascadeStyle Evict = new EvictCascadeStyle();
+		public static readonly CascadeStyle Evict = EvictCascadeStyle.Instance;
 
 		/// <summary> replicate</summary>
-		public static readonly CascadeStyle Replicate = new ReplicateCascadeStyle();
+		public static readonly CascadeStyle Replicate = ReplicateCascadeStyle.Instance;
 
 		/// <summary> merge</summary>
-		public static readonly CascadeStyle Merge = new MergeCascadeStyle();
+		public static readonly CascadeStyle Merge = MergeCascadeStyle.Instance;
 
 		/// <summary> create</summary>
-		public static readonly CascadeStyle Persist = new PersistCascadeStyle();
+		public static readonly CascadeStyle Persist = PersistCascadeStyle.Instance;
 
 		/// <summary> delete</summary>
-		public static readonly CascadeStyle Delete = new DeleteCascadeStyle();
+		public static readonly CascadeStyle Delete = DeleteCascadeStyle.Instance;
 
 		/// <summary> delete + delete orphans</summary>
-		public static readonly CascadeStyle DeleteOrphan = new DeleteOrphanCascadeStyle();
+		public static readonly CascadeStyle DeleteOrphan = DeleteOrphanCascadeStyle.Instance;
 
 		/// <summary> no cascades</summary>
-		public static readonly CascadeStyle None = new NoneCascadeStyle();
+		public static readonly CascadeStyle None = NoneCascadeStyle.Instance;
 
 		#endregion
 	}
