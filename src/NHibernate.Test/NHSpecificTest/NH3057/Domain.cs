10d11d1831378baef4e17940d0c8a1d0c421dc9b
Alexander I. Zaytsev <hazzik@gmail.com>
2013-02-10 01:13:50 +1300

    Ported back fix for NH-3057

---
+++ src/NHibernate.Test/NHSpecificTest/NH3057/Domain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH3057
+{
+	public abstract class BaseClass
+	{
+		public virtual int Id { get; set; }
+		public virtual string InheritedProperty { get; set; }
+	}
+
+	public class AClass 
+	{
+		private ICollection<BClass> bs = new List<BClass>(); 
+		
+		public virtual int Id { get; set; }
+
+		public virtual ICollection<BClass> Bs
+		{
+			get { return bs; }
+			protected internal set { bs = value; }
+		}
+	}
+
+	public class BClass : BaseClass
+	{
+		public virtual AClass A { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH3057/Fixture.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using NHibernate.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH3057
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				var a = new AClass { Id = 1 };
+				session.Save(a);
+
+				var b = new BClass { Id = 2, A = a, InheritedProperty = "B2" };
+				session.Save(b);
+
+				transaction.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				session.Delete("from System.Object");
+				transaction.Commit();
+			}
+		}
+
+		[Test]
+		public void CollectionQueryOnJoinedSubclassInheritedProperty()
+		{
+			using (var session = OpenSession())
+			using (session.BeginTransaction())
+			{
+				var entities = session.Query<AClass>()
+					.Where(a => a.Bs.Any(b => b.InheritedProperty == "B2"))
+					.ToList();
+
+				Assert.AreEqual(1, entities.Count);
+				Assert.AreEqual(1, entities[0].Id);
+			}
+		}
+
+		[Test]
+		public void CollectionQueryOnJoinedSubclassInheritedPropertyHql()
+		{
+			using (var session = OpenSession())
+			using (session.BeginTransaction())
+			{
+				var entities = session.CreateQuery("from AClass a where exists (from a.Bs b where b.InheritedProperty = 'B2')")
+					.List<AClass>();
+
+				Assert.AreEqual(1, entities.Count);
+				Assert.AreEqual(1, entities[0].Id);
+			}
+		}
+	}
+}
