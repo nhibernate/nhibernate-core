f082cb1291db90b92181003d75523c3a6341cdb0
Oskar Berggren <oskar.berggren@gmail.com>
2013-02-14 19:53:21 +0100

    FirebirdClientDriver.cs, FirebirdDialect.cs: Update references in code documentation.

--- src/NHibernate/Dialect/FirebirdDialect.cs
+++ src/NHibernate/Dialect/FirebirdDialect.cs
@@ -23,7 +23,7 @@ namespace NHibernate.Dialect
 	///		</listheader>
 	///		<item>
 	///			<term>connection.driver_class</term>
-	///			<description><see cref="NHibernate.Driver.FirebirdDriver" /></description>
+	///			<description><see cref="NHibernate.Driver.FirebirdClientDriver" /></description>
 	///		</item>
 	/// </list>
 	/// </remarks>
