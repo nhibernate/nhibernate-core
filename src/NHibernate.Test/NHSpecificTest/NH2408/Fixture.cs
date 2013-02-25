dfa0970b4d5fba575de9778128d0f5443d0d3798
Dmitry Naumov <dnaumov@paladyne.com>
2013-02-25 11:07:30 +0400

    Add test for NH-2408

---
+++ src/NHibernate.Test/NHSpecificTest/NH2408/Fixture.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH2408
+{
+	public class Fixture : BugTestCase
+	{
+		[Test]
+		public void ShouldGenerateCorrectSqlStatement()
+		{
+			using (var session = OpenSession())
+			{
+				var query = session.CreateQuery("from Animal a where a.Name = ?");
+				query.SetParameter(0, "Prince");
+
+				query.SetLockMode("a", LockMode.Upgrade);
+
+				var result = query.List();
+			}
+		}
+	}
+}
