3f1aa0763c02dad012b400330333d8df824aa351
Alexander I. Zaytsev <hazzik@gmail.com>
2013-02-27 01:03:53 +1300

    Add more tests for NH-3408

--- src/NHibernate.Test/NHSpecificTest/NH3408/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH3408/Fixture.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using NHibernate.Linq;
 using NUnit.Framework;
@@ -14,6 +15,36 @@ namespace NHibernate.Test.NHSpecificTest.NH3408
 			{
 				var query = from c in session.Query<Country>()
 							select new { c.Picture, c.NationalHolidays };
+
+				Assert.DoesNotThrow(() => { query.ToList(); });
+			}
+		}
+
+		[Test]
+		public void ProjectAnonymousTypeWithArrayPropertyWhenByteArrayContains()
+		{
+			using (var session = OpenSession())
+			using (session.BeginTransaction())
+			{
+				var pictures = new List<byte[]>();
+				var query = from c in session.Query<Country>()
+							where pictures.Contains(c.Picture)
+							select new { c.Picture, c.NationalHolidays };
+
+				Assert.DoesNotThrow(() => { query.ToList(); });
+			}
+		}
+
+		[Test]
+		public void SelectBytePropertyWithArrayPropertyWhenByteArrayContains()
+		{
+			using (var session = OpenSession())
+			using (session.BeginTransaction())
+			{
+				var pictures = new List<byte[]>();
+				var query = from c in session.Query<Country>()
+							where pictures.Contains(c.Picture)
+							select c.Picture;
 
 				Assert.DoesNotThrow(() => { query.ToList(); });
 			}
