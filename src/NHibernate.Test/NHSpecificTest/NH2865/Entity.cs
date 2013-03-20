87288599474dbb6b736bbb6aefbcb0680f1220a7
Léonard Labat <leonard.labat@live.fr>
2013-03-20 12:14:23 +0100

    Add support for Convert.ToInt32 / int.Parse
    Also support for decimal and double.
    
    NH-2865 : Test case added
    NH-3377 : Test case added

---
+++ src/NHibernate.Test/NHSpecificTest/NH2865/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH2865
+{
+	class OrderLine
+	{
+		public virtual Guid Id { get; set; }
+		public virtual string Count { get; set; }
+	}
+}
