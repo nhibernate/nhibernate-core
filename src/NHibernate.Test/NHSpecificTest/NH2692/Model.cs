d10c549469f9d439394038b416e24df0fffc772f
Darren Kopp <darrenkopp@gmail.com>
2013-04-15 16:04:53 -0600

    Add test case
    add the test case code from jira
    
    https://nhibernate.jira.com/browse/NH-2692

---
+++ src/NHibernate.Test/NHSpecificTest/NH2692/Model.cs
@@ -0,0 +1,19 @@
+﻿namespace NHibernate.Test.NHSpecificTest.NH2692 {
+   using System;
+   using System.Collections.Generic;
+
+   public class Parent {
+      public Parent() {
+         ChildComponents = new List<ChildComponent>();
+      }
+
+      public virtual Guid Id { get; set; }
+      public virtual ICollection<ChildComponent> ChildComponents { get; set; }
+   }
+
+   public class ChildComponent {
+      public virtual Parent Parent { get; set; }
+      public virtual bool SomeBool { get; set; }
+      public virtual string SomeString { get; set; }
+   }
+}
