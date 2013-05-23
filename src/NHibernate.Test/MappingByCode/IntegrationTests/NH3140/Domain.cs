36af1d47ccb8ce205efd0a970958e586d32a424d
Diego Mijelshon <diego@mijelshon.com.ar>
2013-05-24 08:51:21 +1200

    NH-3140 - Fix incorrect mapping-by-code of many-to-many collection of elements when a column name matches a class name

---
+++ src/NHibernate.Test/MappingByCode/IntegrationTests/NH3140/Domain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NHibernate.Test.MappingByCode.IntegrationTests.NH3140
+{
+	public class Foo
+	{
+		public virtual Guid Id { get; set; }
+		public virtual ICollection<Bar> Bars { get; set; }
+	}
+
+	public class Bar
+	{
+		public virtual Guid Id { get; set; }
+	}
+}
