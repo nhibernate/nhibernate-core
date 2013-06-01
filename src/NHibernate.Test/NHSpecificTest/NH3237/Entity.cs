c7bbd7136210b71f17836db5a7e5e504f9e10c86
Alexander I. Zaytsev <hazzik@gmail.com>
2013-06-01 22:08:39 +1200

    NH-3237 - Port-back removing cast operator in max and min aggregates

---
+++ src/NHibernate.Test/NHSpecificTest/NH3237/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH3237
+{
+	class Entity
+	{
+		public virtual Guid Id { get; set; }
+		public virtual DateTimeOffset DateTimeOffsetValue { get; set; }
+		public virtual TestEnum EnumValue { get; set; }
+		public virtual int IntValue { get; set; }
+		public virtual long LongValue { get; set; }
+		public virtual decimal DecimalValue { get; set; }
+		public virtual double DoubleValue { get; set; }
+		public virtual float FloatValue { get; set; }
+		public virtual string StringValue { get; set; }
+		public virtual DateTime DateTimeValue { get; set; }
+	}
+
+	public enum TestEnum
+	{
+		Zero = 0,
+		One = 1,
+		Two = 2
+	}
+}
