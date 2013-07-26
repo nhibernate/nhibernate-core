5d3ddcfc6688758175fe4100ef35a8b52702cce8
haugjan <haugjan>
2013-07-26 22:12:05 +0200

    Fixed Issue 2963: Excpetion when map an IList(Of T) by Code in VB.NET

---
+++ src/NHibernate.Test.VisualBasic/Issues/NH2963/Fixture.vb
@@ -0,0 +1,14 @@
+﻿Imports NHibernate.Mapping.ByCode
+Imports NUnit.Framework
+Namespace Issues.NH2963
+
+  <TestFixture()>
+  Public Class Fixture
+
+    <Test>
+    Public Sub GenericDecodeMemberAccessExpressionForCollectionAsEnumerableShouldReturnMemberOfDeclaringClass()
+      Mapping.ByCode.TypeExtensions.DecodeMemberAccessExpression(Of Entity, IEnumerable(Of String))(Function(mc) mc.Childs)
+    End Sub
+
+  End Class
+End Namespace
