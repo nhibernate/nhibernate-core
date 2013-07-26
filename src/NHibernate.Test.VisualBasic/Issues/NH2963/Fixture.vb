06b0fafdc864251986dcb7b30e9dda58d13c8330
haugjan <haugjan>
2013-07-26 22:03:09 +0200

    NH-2963 - Exception when map an IList(Of T) by Code in VB.NET

---
+++ src/NHibernate.Test.VisualBasic/Issues/NH2963/Fixture.vb
@@ -0,0 +1,17 @@
+﻿Imports NHibernate.Mapping.ByCode
+Imports NUnit.Framework
+
+Namespace Issues.NH2963
+    <TestFixture()>
+    Public Class Fixture
+        <Test>
+        Public Sub GenericDecodeMemberAccessExpressionForCollectionAsEnumerableShouldReturnMemberOfDeclaringClass()
+            TypeExtensions.DecodeMemberAccessExpression (Of Entity, IEnumerable(Of String))(Function(mc) mc.Childs)
+        End Sub
+
+        <Test>
+        Public Sub GenericDecodeMemberAccessExpressionOfForCollectionAsEnumerableShouldReturnMemberOfDeclaringClass()
+            TypeExtensions.DecodeMemberAccessExpressionOf (Of Entity, IEnumerable(Of String))(Function(mc) mc.Childs)
+        End Sub
+    End Class
+End Namespace
