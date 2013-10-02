7310c5277aee6f6d15d29ffa5b280157f90915ed
Alexander I. Zaytsev <hazzik@gmail.com>
2013-10-02 22:14:31 +1300

    NH-1978 - Fix tests for dialects with delayed save (for ex. sequence based IDs)

--- src/NHibernate.Test/NHSpecificTest/NH1978/AliasTest.cs
+++ src/NHibernate.Test/NHSpecificTest/NH1978/AliasTest.cs
@@ -3,23 +3,24 @@ using NUnit.Framework;
 
 namespace NHibernate.Test.NHSpecificTest.NH1978
 {
-    [TestFixture]
-    public class AliasTest : BugTestCase
-    {
-        [Test]
-        public void ShouldReturnPlanFromEmployee()
-        {
-            using(var s = OpenSession())
-            using (var trans = s.BeginTransaction())
-            {
-                var plan = new _401k {PlanName = "test"};
-                s.Save(plan);
-                s.Refresh(plan);
-                var emp = new Employee {EmpName = "name", PlanParent = plan};
-                s.Save(emp);
+	[TestFixture]
+	public class AliasTest : BugTestCase
+	{
+		[Test]
+		public void ShouldReturnPlanFromEmployee()
+		{
+			using(var s = OpenSession())
+			using (var trans = s.BeginTransaction())
+			{
+				var plan = new _401k {PlanName = "test"};
+				s.Save(plan);
+				s.Flush();
+				s.Refresh(plan);
+				var emp = new Employee {EmpName = "name", PlanParent = plan};
+				s.Save(emp);
 
-                trans.Rollback();
-            }
-        }
-    }
+				trans.Rollback();
+			}
+		}
+	}
 }
