9c3a4c71b649e0db02b707dcb0aa3dae744df9b3
Alexander I. Zaytsev <hazzik@gmail.com>
2013-10-02 21:58:14 +1300

    NH-1831 - Ignore tests for Oracle as it does not support &, |, ^ as bitwise operators.

--- src/NHibernate.Test/NHSpecificTest/NH1831/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH1831/Fixture.cs
@@ -1,19 +1,26 @@
-﻿using NUnit.Framework;
+﻿using NHibernate.Dialect;
+using NUnit.Framework;
 
 namespace NHibernate.Test.NHSpecificTest.NH1831
 {
-   [TestFixture]
-   public class Fixture : BugTestCase
-   {
-      [Test]
-      public void CorrectPrecedenceForBitwiseOperators()
-      {
-         using (var s = OpenSession())
-         {
-            const string hql = @"SELECT dt FROM DocumentType dt WHERE dt.systemAction & :sysAct = :sysAct ";
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override bool AppliesTo(Dialect.Dialect dialect)
+		{
+			return !(dialect is Oracle8iDialect);
+		}
 
-            s.CreateQuery(hql).SetParameter("sysAct", SystemAction.Denunciation).List();
-         }
-      }
-   }
+		[Test]
+		public void CorrectPrecedenceForBitwiseOperators()
+		{
+			using (var s = OpenSession())
+			using (s.BeginTransaction())
+			{
+				const string hql = @"SELECT dt FROM DocumentType dt WHERE dt.systemAction & :sysAct = :sysAct ";
+
+				s.CreateQuery(hql).SetParameter("sysAct", SystemAction.Denunciation).List();
+			}
+		}
+	}
 }
