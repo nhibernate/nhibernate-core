366ae0445f41ea13c2425005e99b8296987e8834
Alexander I. Zaytsev <hazzik@gmail.com>
2013-10-02 21:28:05 +1300

    Split QueryOver integration fixture.

---
+++ src/NHibernate.Test/Criteria/Lambda/FunctionsIntegrationFixture.cs
@@ -0,0 +1,187 @@
+using System;
+using System.Collections;
+using NHibernate.Criterion;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Test.Criteria.Lambda
+{
+	[TestFixture]
+	public class FunctionsIntegrationFixture : TestCase
+	{
+		protected override string MappingsAssembly
+		{
+			get { return "NHibernate.Test"; }
+		}
+
+		protected override IList Mappings
+		{
+			get { return new[] { "Criteria.Lambda.Mappings.hbm.xml" }; }
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
+			{
+				s.Delete("from Person");
+				t.Commit();
+			}
+		}
+
+		protected override void OnSetUp()
+		{
+			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
+			{
+				s.Save(new Person { Name = "p2", BirthDate = new DateTime(2008, 07, 07) });
+				s.Save(new Person { Name = "p1", BirthDate = new DateTime(2009, 08, 07), Age = 90 });
+				s.Save(new Person { Name = "pP3", BirthDate = new DateTime(2007, 06, 05) });
+
+				t.Commit();
+			}
+		}
+
+		[Test]
+		public void YearPartEqual()
+		{
+			using (var s = OpenSession())
+			using (s.BeginTransaction())
+			{
+				var persons = s.QueryOver<Person>()
+					.Where(p => p.BirthDate.YearPart() == 2008)
+					.List();
+
+				persons.Count.Should().Be(1);
+				persons[0].Name.Should().Be("p2");
+			}
+		}
+
+		[Test]
+		public void YearPartIsIn()
+		{
+			using (var s = OpenSession())
+			using (s.BeginTransaction())
+			{
+				var persons = s.QueryOver<Person>()
+					.Where(p => p.BirthDate.YearPart().IsIn(new[] { 2008, 2009 }))
+					.OrderBy(p => p.Name).Asc
+					.List();
+
+				persons.Count.Should().Be(2);
+				persons[0].Name.Should().Be("p1");
+				persons[1].Name.Should().Be("p2");
+			}
+		}
+
+		[Test]
+		public void YearPartSingleOrDefault()
+		{
+			using (var s = OpenSession())
+			using (s.BeginTransaction())
+			{
+				var yearOfBirth = s.QueryOver<Person>()
+					.Where(p => p.Name == "p2")
+					.Select(p => p.BirthDate.YearPart())
+					.SingleOrDefault<object>();
+
+				yearOfBirth.GetType().Should().Be(typeof (int));
+				yearOfBirth.Should().Be(2008);
+			}
+		}
+
+		[Test]
+		public void SelectAvg()
+		{
+			using (var s = OpenSession())
+			using (s.BeginTransaction())
+			{
+				var avgYear = s.QueryOver<Person>()
+					.SelectList(list => list.SelectAvg(p => p.BirthDate.YearPart()))
+					.SingleOrDefault<object>();
+
+				avgYear.GetType().Should().Be(typeof (double));
+				string.Format("{0:0}", avgYear).Should().Be("2008");
+			}
+		}
+
+		[Test]
+		public void SqrtSingleOrDefault()
+		{
+			using (var s = OpenSession())
+			using (s.BeginTransaction())
+			{
+				var sqrtOfAge = s.QueryOver<Person>()
+					.Where(p => p.Name == "p1")
+					.Select(p => p.Age.Sqrt())
+					.SingleOrDefault<object>();
+
+				sqrtOfAge.Should().Be.InstanceOf<double>();
+				string.Format("{0:0.00}", sqrtOfAge).Should().Be((9.49).ToString());
+			}
+		}
+
+		[Test]
+		public void FunctionsToLowerToUpper()
+		{
+			using (var s = OpenSession())
+			using (s.BeginTransaction())
+			{
+				var names = s.QueryOver<Person>()
+					.Where(p => p.Name == "pP3")
+					.Select(p => p.Name.Lower(), p => p.Name.Upper())
+					.SingleOrDefault<object[]>();
+
+				names[0].Should().Be("pp3");
+				names[1].Should().Be("PP3");
+			}
+		}
+
+		[Test]
+		public void Concat()
+		{
+			using (var s = OpenSession())
+			using (s.BeginTransaction())
+			{
+				var name = s.QueryOver<Person>()
+					.Where(p => p.Name == "p1")
+					.Select(p => Projections.Concat(p.Name, ", ", p.Name))
+					.SingleOrDefault<string>();
+
+				name.Should().Be("p1, p1");
+			}
+		}
+
+		[Test]
+		public void MonthPartEqualsDayPart()
+		{
+			using (var s = OpenSession())
+			using (s.BeginTransaction())
+			{
+				var persons = s.QueryOver<Person>()
+					.Where(p => p.BirthDate.MonthPart() == p.BirthDate.DayPart())
+					.List();
+
+				persons.Count.Should().Be(1);
+				persons[0].Name.Should().Be("p2");
+			}
+		}
+
+		[Test]
+		public void Order()
+		{
+			using (var s = OpenSession())
+			using (s.BeginTransaction())
+			{
+				var persons = s.QueryOver<Person>()
+					.OrderBy(p => p.BirthDate.YearPart()).Desc
+					.List();
+
+				persons.Count.Should().Be(3);
+				persons[0].Name.Should().Be("p1");
+				persons[1].Name.Should().Be("p2");
+				persons[2].Name.Should().Be("pP3");
+			}
+		}
+	}
+}
--- src/NHibernate.Test/Criteria/Lambda/IntegrationFixture.cs
+++ src/NHibernate.Test/Criteria/Lambda/IntegrationFixture.cs
@@ -7,36 +7,23 @@ using NUnit.Framework;
 using SharpTestsEx;
 
 using NHibernate.Criterion;
-using NHibernate.Transform;
-using NHibernate.Type;
-using NHibernate.Util;
 
 namespace NHibernate.Test.Criteria.Lambda
 {
-
 	[TestFixture]
 	public class IntegrationFixture : TestCase
 	{
-
 		protected override string MappingsAssembly { get { return "NHibernate.Test"; } }
 
 		protected override IList Mappings
 		{
-			get
-			{
-				return new string[]
-					{
-						"Criteria.Lambda.Mappings.hbm.xml",
-					};
-			}
+			get { return new[] { "Criteria.Lambda.Mappings.hbm.xml" }; }
 		}
 
 		protected override void OnTearDown()
 		{
-			base.OnTearDown();
-
-			using (ISession s = OpenSession())
-			using (ITransaction t = s.BeginTransaction())
+			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
 			{
 				s.CreateQuery("delete from Child").ExecuteUpdate();
 				s.CreateQuery("update Person p set p.Father = null").ExecuteUpdate();
@@ -44,44 +31,6 @@ namespace NHibernate.Test.Criteria.Lambda
 				s.CreateQuery("delete from JoinedChild").ExecuteUpdate();
 				s.CreateQuery("delete from Parent").ExecuteUpdate();
 				t.Commit();
-			}
-		}
-
-		[Test]
-		public void ICriteriaOfT_SimpleCriterion()
-		{
-			using (ISession s = OpenSession())
-			using (ITransaction t = s.BeginTransaction())
-			{
-				s.Save(new Person() { Name = "test person 1", Age = 20 });
-				s.Save(new Person() { Name = "test person 2", Age = 30 });
-				s.Save(new Person() { Name = "test person 3", Age = 40 });
-
-				t.Commit();
-			}
-
-			using (ISession s = OpenSession())
-			{
-				IList<Person> actual =
-					s.QueryOver<Person>()
-						.Where(p => p.Name == "test person 2")
-						.And(p => p.Age == 30)
-						.List();
-
-				Assert.That(actual.Count, Is.EqualTo(1));
-			}
-
-			using (ISession s = OpenSession())
-			{
-				Person personAlias = null;
-
-				IList<Person> actual =
-					s.QueryOver<Person>(() => personAlias)
-						.Where(() => personAlias.Name == "test person 2")
-						.And(() => personAlias.Age == 30)
-						.List();
-
-				Assert.That(actual.Count, Is.EqualTo(1));
 			}
 		}
 
@@ -106,91 +55,6 @@ namespace NHibernate.Test.Criteria.Lambda
 						.List();
 
 				Assert.That(actual[0].Age, Is.EqualTo(20));
-			}
-		}
-
-		[Test]
-		public void Project_SingleProperty()
-		{
-			using (ISession s = OpenSession())
-			using (ITransaction t = s.BeginTransaction())
-			{
-				s.Save(new Person() { Name = "test person 1", Age = 20 });
-				s.Save(new Person() { Name = "test person 2", Age = 30 });
-				t.Commit();
-			}
-
-			using (ISession s = OpenSession())
-			{
-				var actual =
-					s.QueryOver<Person>()
-						.Select(p => p.Age)
-						.OrderBy(p => p.Age).Asc
-						.List<int>();
-
-				Assert.That(actual[0], Is.EqualTo(20));
-			}
-		}
-
-		[Test]
-		public void Project_MultipleProperties()
-		{
-			using (ISession s = OpenSession())
-			using (ITransaction t = s.BeginTransaction())
-			{
-				s.Save(new Person() { Name = "test person 1", Age = 20 });
-				s.Save(new Person() { Name = "test person 2", Age = 30 });
-				t.Commit();
-			}
-
-			using (ISession s = OpenSession())
-			{
-				Person personAlias = null;
-				var actual =
-					s.QueryOver<Person>(() => personAlias)
-						.Select(p => p.Name,
-								p => personAlias.Age)
-						.OrderBy(p => p.Age).Asc
-						.List<object[]>()
-						.Select(props => new {
-							TestName = (string)props[0],
-							TestAge = (int)props[1],
-							});
-
-				Assert.That(actual.ElementAt(0).TestName, Is.EqualTo("test person 1"));
-				Assert.That(actual.ElementAt(1).TestAge, Is.EqualTo(30));
-			}
-		}
-
-		[Test]
-		public void Project_TransformToDto()
-		{
-			using (ISession s = OpenSession())
-			using (ITransaction t = s.BeginTransaction())
-			{
-				s.Save(new Person() { Name = "test person 1", Age = 20 });
-				s.Save(new Person() { Name = "test person 1", Age = 30 });
-				s.Save(new Person() { Name = "test person 2", Age = 40 });
-				t.Commit();
-			}
-
-			using (ISession s = OpenSession())
-			{
-				PersonSummary summary = null;
-				var actual =
-					s.QueryOver<Person>()
-						.SelectList(list => list
-							.SelectGroup(p => p.Name).WithAlias(() => summary.Name)
-							.Select(Projections.RowCount()).WithAlias(() => summary.Count))
-						.OrderByAlias(() => summary.Name).Asc
-						.TransformUsing(Transformers.AliasToBean<PersonSummary>())
-						.List<PersonSummary>();
-
-				Assert.That(actual.Count, Is.EqualTo(2));
-				Assert.That(actual[0].Name, Is.EqualTo("test person 1"));
-				Assert.That(actual[0].Count, Is.EqualTo(2));
-				Assert.That(actual[1].Name, Is.EqualTo("test person 2"));
-				Assert.That(actual[1].Count, Is.EqualTo(1));
 			}
 		}
 
@@ -392,144 +256,6 @@ namespace NHibernate.Test.Criteria.Lambda
 		}
 
 		[Test]
-		public void SubCriteria()
-		{
-			using (ISession s = OpenSession())
-			using (ITransaction t = s.BeginTransaction())
-			{
-				s.Save(new Person() { Name = "Name 1" }
-						.AddChild(new Child() { Nickname = "Name 1.1"})
-						.AddChild(new Child() { Nickname = "Name 1.2"}));
-
-				s.Save(new Person() { Name = "Name 2" }
-						.AddChild(new Child() { Nickname = "Name 2.1"})
-						.AddChild(new Child() { Nickname = "Name 2.2"}));
-
-				t.Commit();
-			}
-
-			using (ISession s = OpenSession())
-			{
-				var persons =
-					s.QueryOver<Person>()
-						.JoinQueryOver(p => p.Children)
-							.Where(c => c.Nickname == "Name 2.1")
-							.List();
-
-				Assert.That(persons.Count, Is.EqualTo(1));
-				Assert.That(persons[0].Name, Is.EqualTo("Name 2"));
-			}
-		}
-
-		[Test]
-		public void SubCriteriaProjections()
-		{
-			using (ISession s = OpenSession())
-			using (ITransaction t = s.BeginTransaction())
-			{
-				s.Save(new Person() { Name = "Name 1", Age = 33 }
-						.AddChild(new Child() { Nickname = "Name 1.1", Age = 3}));
-
-				t.Commit();
-			}
-
-			using (ISession s = OpenSession())
-			{
-				var simpleProjection =
-					s.QueryOver<Child>()
-						.JoinQueryOver(c => c.Parent)
-							.Where(p => p.Name == "Name 1" && p.Age == 33)
-							.Select(c => c.Nickname, c => c.Age)
-							.List<object[]>()
-							.Select(props => new
-								{
-									Name = (string)props[0],
-									Age = (int)props[1],
-								});
-
-				Assert.That(simpleProjection.Count(), Is.EqualTo(1));
-				Assert.That(simpleProjection.First().Name, Is.EqualTo("Name 1.1"));
-				Assert.That(simpleProjection.First().Age, Is.EqualTo(3));
-
-				Child childAlias = null;
-				var listProjection =
-					s.QueryOver<Child>(() => childAlias)
-						.JoinQueryOver(c => c.Parent)
-							.Where(p => p.Name == "Name 1" && p.Age == 33)
-							.SelectList(list => list
-								.Select(c => childAlias.Nickname)
-								.Select(c => c.Age))
-							.List<object[]>()
-							.Select(props => new
-								{
-									Name = (string)props[0],
-									Age = (int)props[1],
-								});
-
-				Assert.That(listProjection.Count(), Is.EqualTo(1));
-				Assert.That(listProjection.First().Name, Is.EqualTo("Name 1.1"));
-				Assert.That(listProjection.First().Age, Is.EqualTo(3));
-			}
-		}
-
-		[Test]
-		public void SubQuery()
-		{
-			using (ISession s = OpenSession())
-			using (ITransaction t = s.BeginTransaction())
-			{
-				s.Save(new Person() { Name = "Name 1", Age = 1 }
-						.AddChild(new Child() { Nickname = "Name 1.1", Age = 1}));
-
-				s.Save(new Person() { Name = "Name 2", Age = 2 }
-						.AddChild(new Child() { Nickname = "Name 2.1", Age = 2})
-						.AddChild(new Child() { Nickname = "Name 2.2", Age = 2}));
-
-				s.Save(new Person() { Name = "Name 3", Age = 3 }
-						.AddChild(new Child() { Nickname = "Name 3.1", Age = 3}));
-
-				t.Commit();
-			}
-
-			using (ISession s = OpenSession())
-			{
-				Person personAlias = null;
-				object childCountAlias = null;
-
-				QueryOver<Child> averageChildAge =
-					QueryOver.Of<Child>()
-						.SelectList(list => list.SelectAvg(c => c.Age));
-
-				QueryOver<Child> childCountQuery =
-					QueryOver.Of<Child>()
-						.Where(c => c.Parent.Id == personAlias.Id)
-						.Select(Projections.RowCount());
-
-				var nameAndChildCount =
-					s.QueryOver<Person>(() => personAlias)
-						.WithSubquery.Where(p => p.Age <= averageChildAge.As<int>())
-						.SelectList(list => list
-							.Select(p => p.Name)
-							.SelectSubQuery(childCountQuery).WithAlias(() => childCountAlias))
-						.OrderByAlias(() => childCountAlias).Desc
-						.List<object[]>()
-						.Select(props => new {
-							Name = (string)props[0],
-							ChildCount = (int)props[1],
-							})
-						.ToList();
-
-				Assert.That(nameAndChildCount.Count, Is.EqualTo(2));
-
-				Assert.That(nameAndChildCount[0].Name, Is.EqualTo("Name 2"));
-				Assert.That(nameAndChildCount[0].ChildCount, Is.EqualTo(2));
-
-				Assert.That(nameAndChildCount[1].Name, Is.EqualTo("Name 1"));
-				Assert.That(nameAndChildCount[1].ChildCount, Is.EqualTo(1));
-			}
-		}
-
-		[Test]
 		public void OverrideEagerJoin()
 		{
 			using (ISession s = OpenSession())
@@ -586,155 +312,6 @@ namespace NHibernate.Test.Criteria.Lambda
 				Assert.That(rowCount, Is.EqualTo(4));
 				Assert.That(bigRowCount, Is.TypeOf<long>());
 				Assert.That(bigRowCount, Is.EqualTo(4));
-			}
-		}
-
-		[Test]
-		public void Functions()
-		{
-			using (ISession s = OpenSession())
-			using (ITransaction t = s.BeginTransaction())
-			{
-				s.Save(new Person() { Name = "p1", BirthDate = new DateTime(2009, 08, 07), Age = 90 });
-				s.Save(new Person() { Name = "p2", BirthDate = new DateTime(2008, 07, 06) });
-				s.Save(new Person() { Name = "pP3", BirthDate = new DateTime(2007, 06, 05) });
-
-				t.Commit();
-			}
-
-			using (ISession s = OpenSession())
-			{
-				var persons =
-					s.QueryOver<Person>()
-						.Where(p => p.BirthDate.YearPart() == 2008)
-						.List();
-
-				persons.Count.Should().Be(1);
-				persons[0].Name.Should().Be("p2");
-			}
-
-			using (ISession s = OpenSession())
-			{
-				var persons =
-					s.QueryOver<Person>()
-						.Where(p => p.BirthDate.YearPart().IsIn(new [] { 2008, 2009 }))
-						.OrderBy(p => p.Name).Asc
-						.List();
-
-				persons.Count.Should().Be(2);
-				persons[0].Name.Should().Be("p1");
-				persons[1].Name.Should().Be("p2");
-			}
-
-			using (ISession s = OpenSession())
-			{
-				var yearOfBirth =
-					s.QueryOver<Person>()
-						.Where(p => p.Name == "p2")
-						.Select(p => p.BirthDate.YearPart())
-						.SingleOrDefault<object>();
-
-				yearOfBirth.GetType().Should().Be(typeof(int));
-				yearOfBirth.Should().Be(2008);
-			}
-
-			using (ISession s = OpenSession())
-			{
-				var avgYear =
-					s.QueryOver<Person>()
-						.SelectList(list => list.SelectAvg(p => p.BirthDate.YearPart()))
-						.SingleOrDefault<object>();
-
-				avgYear.GetType().Should().Be(typeof(double));
-				string.Format("{0:0}", avgYear).Should().Be("2008");
-			}
-
-			using (ISession s = OpenSession())
-			{
-				var sqrtOfAge =
-					s.QueryOver<Person>()
-						.Where(p => p.Name == "p1")
-						.Select(p => p.Age.Sqrt())
-						.SingleOrDefault<object>();
-
-				sqrtOfAge.Should().Be.InstanceOf<double>();
-				string.Format("{0:0.00}", sqrtOfAge).Should().Be((9.49).ToString());
-			}
-
-			using (ISession s = OpenSession())
-			{
-				var names =
-					s.QueryOver<Person>()
-						.Where(p => p.Name == "pP3")
-						.Select(p => p.Name.Lower(), p => p.Name.Upper())
-						.SingleOrDefault<object[]>();
-
-				names[0].Should().Be("pp3");
-				names[1].Should().Be("PP3");
-			}
-
-			using (ISession s = OpenSession())
-			{
-				var name =
-					s.QueryOver<Person>()
-						.Where(p => p.Name == "p1")
-						.Select(p => Projections.Concat(p.Name, ", ", p.Name))
-						.SingleOrDefault<string>();
-
-				name.Should().Be("p1, p1");
-			}
-		}
-
-		[Test]
-		public void FunctionsProperty()
-		{
-			using (ISession s = OpenSession())
-			using (ITransaction t = s.BeginTransaction())
-			{
-				s.Save(new Person() { Name = "p1", BirthDate = new DateTime(2009, 08, 07) });
-				s.Save(new Person() { Name = "p2", BirthDate = new DateTime(2008, 07, 07) });
-				s.Save(new Person() { Name = "p3", BirthDate = new DateTime(2007, 06, 07) });
-
-				t.Commit();
-			}
-
-			using (ISession s = OpenSession())
-			{
-				var persons =
-					s.QueryOver<Person>()
-						.Where(p => p.BirthDate.MonthPart() == p.BirthDate.DayPart())
-						.List();
-
-				persons.Count.Should().Be(1);
-				persons[0].Name.Should().Be("p2");
-			}
-		}
-
-		[Test]
-		public void FunctionsOrder()
-		{
-			using (ISession s = OpenSession())
-			using (ITransaction t = s.BeginTransaction())
-			{
-				s.Save(new Person() { Name = "p2", BirthDate = new DateTime(2008, 07, 06) });
-				s.Save(new Person() { Name = "p1", BirthDate = new DateTime(2009, 08, 07) });
-				s.Save(new Person() { Name = "p3", BirthDate = new DateTime(2007, 06, 05) });
-
-				t.Commit();
-			}
-
-			using (ISession s = OpenSession())
-			using (ITransaction t = s.BeginTransaction())
-			{
-				var persons =
-					s.QueryOver<Person>()
-						.OrderBy(p => p.BirthDate.YearPart()).Desc
-						.List();
-
-				persons.Count.Should().Be(3);
-				persons[0].Name.Should().Be("p1");
-				persons[1].Name.Should().Be("p2");
-				persons[2].Name.Should().Be("p3");
 			}
 		}
 
@@ -818,31 +395,30 @@ namespace NHibernate.Test.Criteria.Lambda
 		[Test]
 		public void StatelessSession()
 		{
+			int personId;
 			using (var ss = sessions.OpenStatelessSession())
+			using (var t = ss.BeginTransaction())
 			{
-				using (var tx = ss.BeginTransaction())
-				{
-					var person = new Person() { Name = "test1" };
-					ss.Insert(person);
+				var person = new Person { Name = "test1" };
+				ss.Insert(person);
+				personId = person.Id;
+				t.Commit();
+			}
 
-					var statelessPerson1 =
-						ss.QueryOver<Person>()
-							.List()
-							[0];
+			using (var ss = sessions.OpenStatelessSession())
+			using (ss.BeginTransaction())
+			{
+				var statelessPerson1 = ss.QueryOver<Person>()
+					.List()[0];
 
-					Assert.That(statelessPerson1.Id, Is.EqualTo(person.Id));
+				Assert.That(statelessPerson1.Id, Is.EqualTo(personId));
 
-					var statelessPerson2 =
-						QueryOver.Of<Person>()
-							.GetExecutableQueryOver(ss)
-							.List()
-							[0];
+				var statelessPerson2 = QueryOver.Of<Person>()
+					.GetExecutableQueryOver(ss)
+					.List()[0];
 
-					Assert.That(statelessPerson2.Id, Is.EqualTo(person.Id));
-				}
+				Assert.That(statelessPerson2.Id, Is.EqualTo(personId));
 			}
 		}
-
 	}
-
 }
