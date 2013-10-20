d2218cc06d42760a76ea4b797a6a42a4ef2cb946
Roger Kratz <roger.kratz@gmail.com>
2013-10-20 22:43:05 +0200

    Failing test for NH-3555

---
+++ src/NHibernate.Test/NHSpecificTest/NH3555/Model.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH3555
+{
+	public class MapEntity
+	{
+		public MapEntity()
+		{
+			Dic = new Dictionary<KeyEntity, ValueEntity>();
+		}
+
+		public virtual int Id { get; set; }
+		public virtual IDictionary<KeyEntity, ValueEntity> Dic { get; set; }
+	}
+
+	public class KeyEntity
+	{
+		public virtual int Id { get; set; }
+		public virtual int Number { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var casted = obj as KeyEntity;
+			if (casted == null)
+				return false;
+			return (Id == casted.Id && Number == casted.Number);
+		}
+
+		public override int GetHashCode()
+		{
+			return Id ^ Number;
+		}
+	}
+
+	public class ValueEntity
+	{
+		public virtual int Id { get; set; }
+		public virtual int Number { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var casted = obj as KeyEntity;
+			if (casted == null)
+				return false;
+			return (Id == casted.Id && Number == casted.Number);
+		}
+
+		public override int GetHashCode()
+		{
+			return Id ^ Number;
+		}
+	}
+}
