9758768be683010c493789427c80bc881de31728
Roger Kratz <roger.kratz@gmail.com>
2013-11-12 13:05:00 +0100

    Failing test for NH3570. One test for unidirectional onetomany and one test for bidirectional onetomany - both fails.

---
+++ src/NHibernate.Test/NHSpecificTest/NH3570/BiFixture.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Test.NHSpecificTest.NH3570
+{
+	[TestFixture]
+	public class BiFixture : BugTestCase
+	{
+		private Guid id;
+
+		[Test]
+		[KnownBug("NH-3570")]
+		public void ShouldNotSaveRemoveChild()
+		{
+			var parent = new BiParent();
+			parent.AddChild(new BiChild());
+			using (var s = OpenSession())
+			{
+				using (var tx = s.BeginTransaction())
+				{
+					id = (Guid)s.Save(parent);
+					parent.Children.Clear();
+					parent.AddChild(new BiChild());
+					tx.Commit();
+				}
+			}
+			using (var s = OpenSession())
+			{
+				using (s.BeginTransaction())
+				{
+					s.Get<BiParent>(id).Children.Count.Should().Be.EqualTo(1);
+					s.CreateCriteria<BiChild>().List().Count.Should().Be.EqualTo(1);
+				}
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var s = OpenSession())
+			{
+				using (var tx = s.BeginTransaction())
+				{
+					s.CreateQuery("delete from BiChild").ExecuteUpdate();
+					s.CreateQuery("delete from BiParent").ExecuteUpdate();
+					tx.Commit();
+				}
+			}
+		}
+	}
+}
