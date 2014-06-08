ad3689ff985c9c5c332cf7811a12b6fd81e894c4
Jake Levitt <jlevitt@vt.edu>
2014-06-08 01:01:55 -0400

    Add test case for NH3455.

---
+++ src/NHibernate.Test/NHSpecificTest/NH3455/Fixture.cs
@@ -0,0 +1,77 @@
+﻿using NHibernate.Transform;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH3455
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				var e1 = new Person { Name = "Bob", Age = 31, Weight = 185, Address = new Address
+					{
+						City = "Abington",
+						State = "VA",
+						Street = "Avenue",
+						Zip = "11121"
+					}};
+				session.Save(e1);
+
+				var e2 = new Person
+				{
+					Name = "Sally",
+					Age = 22,
+					Weight = 122,
+					Address = new Address
+					{
+						City = "Olympia",
+						State = "WA",
+						Street = "Broad",
+						Zip = "99989"
+					}
+				};
+				session.Save(e2);
+
+				session.Flush();
+				transaction.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				session.Delete("from System.Object");
+
+				session.Flush();
+				transaction.Commit();
+			}
+		}
+
+		[Test]
+		public void OrderBySpecifiedProperty()
+		{
+			using (var session = OpenSession())
+			using (session.BeginTransaction())
+			{
+				PersonDto dto = null;
+				var people = session.QueryOver<Person>()
+									.SelectList(b => b.Select(p => p.Id).WithAlias(() => dto.Id)
+													  .Select(p => p.Name).WithAlias(() => dto.Name)
+													  .Select(p => p.Address).WithAlias(() => dto.Address)
+													  .Select(p => p.Age).WithAlias(() => dto.Age))
+									.OrderBy(p => p.Age)
+									.Desc
+									.TransformUsing(Transformers.AliasToBean<PersonDto>())
+									.List<PersonDto>();
+
+				Assert.That(people.Count, Is.EqualTo(2));
+				Assert.That(people, Is.Ordered.By("Age").Descending);
+			}
+		}
+	}
+}
