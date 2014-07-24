733d6b06e964550a021f886396b338d93603bf8a
Oskar Berggren <oskar.berggren@gmail.com>
2014-07-25 00:41:56 +0200

    Linq/DateTimeTests.cs: Fix indenting.

--- src/NHibernate.Test/Linq/DateTimeTests.cs
+++ src/NHibernate.Test/Linq/DateTimeTests.cs
@@ -4,47 +4,47 @@ using NUnit.Framework;
 
 namespace NHibernate.Test.Linq
 {
-    [TestFixture]
-    public class DateTimeTests : LinqTestCase
-    {
-        [Test]
-        public void CanQueryByYear()
-        {
-            var x = (from o in db.Orders
-                     where o.OrderDate.Value.Year == 1998
-                     select o).ToList();
+	[TestFixture]
+	public class DateTimeTests : LinqTestCase
+	{
+		[Test]
+		public void CanQueryByYear()
+		{
+			var x = (from o in db.Orders
+				where o.OrderDate.Value.Year == 1998
+				select o).ToList();
 
-            Assert.AreEqual(270, x.Count());
-        }
+			Assert.AreEqual(270, x.Count());
+		}
 
-        [Test]
-        public void CanQueryByDate()
-        {
-            var x = (from o in db.Orders
-                    where o.OrderDate.Value.Date == new DateTime(1998, 02, 26)
-                    select o).ToList();
+		[Test]
+		public void CanQueryByDate()
+		{
+			var x = (from o in db.Orders
+				where o.OrderDate.Value.Date == new DateTime(1998, 02, 26)
+				select o).ToList();
 
-            Assert.AreEqual(6, x.Count());
-        }
+			Assert.AreEqual(6, x.Count());
+		}
 
-        [Test]
-        public void CanQueryByDateTime()
-        {
-            var x = (from o in db.Orders
-                     where o.OrderDate.Value == new DateTime(1998, 02, 26)
-                     select o).ToList();
+		[Test]
+		public void CanQueryByDateTime()
+		{
+			var x = (from o in db.Orders
+				where o.OrderDate.Value == new DateTime(1998, 02, 26)
+				select o).ToList();
 
-            Assert.AreEqual(5, x.Count());
-        }
+			Assert.AreEqual(5, x.Count());
+		}
 
-        [Test]
-        public void CanQueryByDateTime2()
-        {
-            var x = (from o in db.Orders
-                     where o.OrderDate.Value == new DateTime(1998, 02, 26, 0, 1, 0)
-                     select o).ToList();
+		[Test]
+		public void CanQueryByDateTime2()
+		{
+			var x = (from o in db.Orders
+				where o.OrderDate.Value == new DateTime(1998, 02, 26, 0, 1, 0)
+				select o).ToList();
 
-            Assert.AreEqual(1, x.Count());
-        }
-    }
+			Assert.AreEqual(1, x.Count());
+		}
+	}
 }
