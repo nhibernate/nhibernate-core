3c01e6122301b3ef6e39773d23c5e28c1224c617
Oskar Berggren <oskar.berggren@gmail.com>
2014-07-26 00:13:25 +0200

    Make the testcase a bit more interested and avoid fix warnings.

--- src/NHibernate.Test/NHSpecificTest/NH3641/TestFixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH3641/TestFixture.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
+﻿using System.Linq;
 using NHibernate.Linq;
 using NUnit.Framework;
 
@@ -16,7 +11,7 @@ namespace NHibernate.Test.NHSpecificTest.NH3641
             using (var session = OpenSession())
             using (var tx = session.BeginTransaction())
             {
-                var child = new Entity { Id = 1, Flag = false };
+                var child = new Entity { Id = 1, Flag = true };
                 var parent = new Entity { Id = 2, ChildInterface = child, ChildConcrete = child };
 
                 session.Save(child);
@@ -41,24 +36,28 @@ namespace NHibernate.Test.NHSpecificTest.NH3641
             session.CreateQuery("delete from " + typeof(T).Name).ExecuteUpdate();
         }
 
-        [Test]
-        public void TrueOrChildPropertyConcrete()
-        {
-            using (var session = OpenSession())
-            {
-                var result = session.Query<IEntity>().Where(x => true || x.ChildConcrete.Flag).ToList();
-                Assert.That(result, Has.Count.EqualTo(2));
-            }
-        }
+	    [Test]
+	    public void TrueOrChildPropertyConcrete()
+	    {
+		    using (var session = OpenSession())
+		    {
+			    var result = session.Query<IEntity>()
+				    .Where(x => x.ChildConcrete == null || x.ChildConcrete.Flag)
+				    .ToList();
+			    Assert.That(result, Has.Count.EqualTo(2));
+		    }
+	    }
 
-        [Test]
-        public void TrueOrChildPropertyInterface()
-        {
-            using (var session = OpenSession())
-            {
-                var result = session.Query<IEntity>().Where(x => true || ((Entity)x.ChildInterface).Flag).ToList();
-                Assert.That(result, Has.Count.EqualTo(2));
-            }
-        }
+	    [Test]
+	    public void TrueOrChildPropertyInterface()
+	    {
+		    using (var session = OpenSession())
+		    {
+			    var result = session.Query<IEntity>()
+				    .Where(x => x.ChildInterface == null || ((Entity) x.ChildInterface).Flag)
+				    .ToList();
+			    Assert.That(result, Has.Count.EqualTo(2));
+		    }
+	    }
     }
 }
