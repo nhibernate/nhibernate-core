7240908de2ab17dbf44b46f1c69919f4d6c11682
Oskar Berggren <oskar.berggren@gmail.com>
2014-07-27 13:42:58 +0200

    Add test case from NH-1863. Already working since at least 3.3.3.GA.

---
+++ src/NHibernate.Test/NHSpecificTest/NH1863/Domain.cs
@@ -0,0 +1,27 @@
+﻿using Iesi.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH1863
+{
+	public class Customer
+	{
+		private ISet<Category> _categories = new HashedSet<Category>();
+
+		public virtual int Id { get; set; }
+
+		public virtual string Name { get; set; }
+
+		public virtual ISet<Category> Categories
+		{
+			get { return _categories; }
+			set { _categories = value; }
+		}
+	}
+
+	public class Category
+	{
+		public virtual int Id { get; set; }
+
+		public virtual bool IsActive { get; set; }
+	}
+
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH1863/Fixture.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using NHibernate.Criterion;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1863
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			using (ISession s = OpenSession())
+			{
+				using (ITransaction tx = s.BeginTransaction())
+				{
+					var category = new Category {IsActive = true};
+					s.Save(category);
+
+					var customerWithCategory = new Customer {Name = "HasCategory"};
+					customerWithCategory.Categories.Add(category);
+					s.Save(customerWithCategory);
+
+					var customerWithNoCategory = new Customer {Name = "HasNoCategory"};
+					s.Save(customerWithNoCategory);
+
+					tx.Commit();
+				}
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (ISession s = OpenSession())
+			{
+				using (ITransaction tx = s.BeginTransaction())
+				{
+					s.Delete("from Customer");
+					s.Delete("from Category");
+					tx.Commit();
+				}
+			}
+		}
+
+		[Test]
+		public void CanGetCustomerWithCategoryWhenFilterIsEnabled()
+		{
+			using (ISession session = OpenSession())
+			{
+				IFilter filter = session.EnableFilter("onlyActive");
+				filter.SetParameter("activeFlag", 1);
+
+				ICriteria hasCategoryCriteria = session.CreateCriteria(typeof (Customer));
+				hasCategoryCriteria.Add(Restrictions.Eq("Name", "HasCategory"));
+				IList<Customer> hasCategoryResult = hasCategoryCriteria.List<Customer>();
+
+				Assert.That(hasCategoryResult.Count, Is.EqualTo(1));
+			}
+
+		}
+
+		[Test]
+		public void CanGetCustomerWithCategoryWhenFilterIsDisabled()
+		{
+			using (ISession session = OpenSession())
+			{
+				session.DisableFilter("onlyActive");
+
+				ICriteria hasCategoryCriteria = session.CreateCriteria(typeof (Customer));
+				hasCategoryCriteria.Add(Restrictions.Eq("Name", "HasCategory"));
+				IList<Customer> hasCategoryResult = hasCategoryCriteria.List<Customer>();
+
+				Assert.That(hasCategoryResult.Count, Is.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public void CanGetCustomerWithNoCategoryWhenFilterIsEnabled()
+		{
+			using (ISession session = OpenSession())
+			{
+				IFilter filter = session.EnableFilter("onlyActive");
+				filter.SetParameter("activeFlag", 1);
+
+				ICriteria hasNoCategoryCriteria = session.CreateCriteria(typeof (Customer));
+				hasNoCategoryCriteria.Add(Restrictions.Eq("Name", "HasNoCategory"));
+				IList<Customer> hasNoCategoryResult = hasNoCategoryCriteria.List<Customer>();
+
+				Assert.That(hasNoCategoryResult.Count, Is.EqualTo(1));
+			}
+
+		}
+
+		[Test]
+		public void CanGetCustomerWithNoCategoryWhenFilterIsDisabled()
+		{
+			using (ISession session = OpenSession())
+			{
+				session.DisableFilter("onlyActive");
+
+				ICriteria hasNoCategoryCriteria = session.CreateCriteria(typeof (Customer));
+				hasNoCategoryCriteria.Add(Restrictions.Eq("Name", "HasNoCategory"));
+				IList<Customer> hasNoCategoryResult = hasNoCategoryCriteria.List<Customer>();
+
+				Assert.That(hasNoCategoryResult.Count, Is.EqualTo(1));
+			}
+
+		}
+	}
+}
