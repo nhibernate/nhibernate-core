b6e5136574726abda39f7624703677a3fd7d7ff3
Oskar Berggren <oskar.berggren@gmail.com>
2014-08-03 18:40:52 +0200

    Add ignored embryo for NH-3401 test case.

---
+++ src/NHibernate.Test/NHSpecificTest/NH3401/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH3401
+{
+	class Entity
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual bool YesNo { get; set; }
+	}
+}
