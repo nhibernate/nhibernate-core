d1629f9795d6af94a41daa98c3b269220b9efcf8
Ricardo Peres <rjperes@hotmail.com>
2014-08-14 10:42:37 +0100

    Added unit test

---
+++ src/NHibernate.Test/MappingByCode/IntegrationTests/NH3657/OneToOneToPropertyReferenceWithExplicitClassSet.cs
@@ -0,0 +1,101 @@
+﻿using System.Linq;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Linq;
+using NHibernate.Mapping.ByCode;
+using NUnit.Framework;
+using NHibernate.Test.MappingByCode.IntegrationTests.NH3041;
+
+namespace NHibernate.Test.MappingByCode.IntegrationTests.NH3657
+{
+	[TestFixture]
+	public class OneToOneToPropertyReferenceWithExplicitClassSet : TestCaseMappingByCode
+	{
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var tx = session.BeginTransaction())
+			{
+				var person1 = new Person { FirstName = "Jack" };
+				session.Save(person1);
+
+				var person2 = new Person { FirstName = "Robert" };
+				session.Save(person2);
+
+				var personDetail = new PersonDetail { LastName = "Smith", Person = person1 };
+				session.Save(personDetail);
+
+				tx.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var tx = session.BeginTransaction())
+			{
+				session.Delete("from System.Object");
+				tx.Commit();
+			}
+		}
+
+		protected override HbmMapping GetMappings()
+		{
+			var mapper = new ModelMapper();
+			mapper.Class<PersonDetail>(m =>
+				{
+					m.Id(t => t.PersonDetailId, a => a.Generator(Generators.Identity));
+					m.Property(t => t.LastName,
+							   c =>
+								   {
+									   c.NotNullable(true);
+									   c.Length(32);
+								   });
+					m.ManyToOne(t => t.Person,
+								c =>
+									{
+										c.Column("PersonId");
+										c.Unique(true);
+										c.NotNullable(false);
+										c.NotFound(NotFoundMode.Ignore);
+									});
+				});
+
+			mapper.Class<Person>(m =>
+				{
+					m.Id(t => t.PersonId, a => a.Generator(Generators.Identity));
+					m.Property(t => t.FirstName,
+							   c =>
+								   {
+									   c.NotNullable(true);
+									   c.Length(32);
+								   });
+					m.OneToOne(t => t.PersonDetail,
+							   oo =>
+								   {
+									   //NH-3657
+									   oo.Class(typeof(PersonDetail));
+									   oo.PropertyReference(x => x.Person);
+									   oo.Cascade(Mapping.ByCode.Cascade.All);
+								   });
+				});
+
+			return mapper.CompileMappingForAllExplicitlyAddedEntities();
+		}
+
+		[Test]
+		public void ShouldConfigureSessionCorrectly()
+		{
+			using (var session = OpenSession())
+			{
+				var person1 = session.Get<Person>(1);
+				var person2 = session.Get<Person>(2);
+				var personDetail = session.Query<PersonDetail>().Single();
+
+				Assert.IsNull(person2.PersonDetail);
+				Assert.IsNotNull(person1.PersonDetail);
+				Assert.AreEqual(person1.PersonDetail.LastName, personDetail.LastName);
+				Assert.AreEqual(person1.FirstName, personDetail.Person.FirstName);
+			}
+		}
+	}
+}
