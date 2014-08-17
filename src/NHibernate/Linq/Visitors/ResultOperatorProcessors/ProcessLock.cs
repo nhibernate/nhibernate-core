2e9f8e331904019b7d410ae0f4be35afd5323627
Onur Gumus <onur@outlook.com.tr>
2014-08-17 15:49:27 +0300

    fixes NH-2285

---
+++ src/NHibernate/Linq/Visitors/ResultOperatorProcessors/ProcessLock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHibernate.Linq.Visitors.ResultOperatorProcessors
+{
+	internal class ProcessLock : IResultOperatorProcessor<LockResultOperator>
+	{
+		public void Process(LockResultOperator resultOperator, QueryModelVisitor queryModelVisitor, IntermediateHqlTree tree)
+		{
+			tree.AddAdditionalCriteria((q, p) => q.SetLockMode(queryModelVisitor.Model.MainFromClause.ItemName, (LockMode)resultOperator.LockMode.Value));
+		}
+	}
+}
+
