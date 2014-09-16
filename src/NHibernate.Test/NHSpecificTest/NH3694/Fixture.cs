ba610413b55ad15213ec549b934b14947302ef58
Pawel-sm <pawel_sm@wp.pl>
2014-09-16 13:57:09 +0200

    NH-3694 Added failing test case

---
+++ src/NHibernate.Test/NHSpecificTest/NH3694/Fixture.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using NHibernate.SqlCommand;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH3694
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			//noop
+		}
+
+		protected override void OnTearDown()
+		{
+			using (ISession session = OpenSession())
+			using (ITransaction transaction = session.BeginTransaction())
+			{
+				session.Delete("from System.Object");
+
+				session.Flush();
+				transaction.Commit();
+			}
+		}
+
+        [Test]
+        public void ListEntitiesWithComponentsCollectionPropertyAndRestricionAppliedToThem()
+        {
+            const string val1 = "one";
+            var entity = new EntityWithComponentsCollection
+            {
+                Components = new HashSet<Component>
+                        {
+                            new Component()
+                                {
+                                    Val1 = val1,
+                                    Val2 = "two",
+                                },
+                            new Component()
+                                {
+                                    Val1 = "I",
+                                    Val2 = "II"
+                                }
+                        }
+            };
+            using (ISession session = this.OpenSession())
+            {
+                session.Save(entity);
+                session.Flush();
+            }
+
+            using (ISession session = this.OpenSession())
+            {
+                Component component = null;
+                var foundEntity = session.QueryOver<EntityWithComponentsCollection>()
+                    .JoinAlias(x => x.Components, () => component, JoinType.LeftOuterJoin)
+                    .Where(g => component.Val1 == val1)
+                    .SingleOrDefault();
+
+                Assert.IsNotNull(foundEntity);
+            }
+
+            using (ISession session = this.OpenSession())
+            {
+                session.Delete(entity);
+                session.Flush();
+            }
+        }
+    }
+}
