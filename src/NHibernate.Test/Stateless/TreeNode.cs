62348a06882f10497c289fbb1001e12028b06cc9
Oskar Berggren <oskar.berggren@gmail.com>
2014-09-21 22:59:50 +0200

    Code style cleanup.

--- src/NHibernate.Test/Stateless/TreeNode.cs
+++ src/NHibernate.Test/Stateless/TreeNode.cs
@@ -3,18 +3,18 @@
 
 namespace NHibernate.Test.Stateless
 {
-    public class TreeNode
-    {
-        private ISet<TreeNode> children = new HashSet<TreeNode>();
+	public class TreeNode
+	{
+		private ISet<TreeNode> _children = new HashSet<TreeNode>();
 
-        public virtual int Id { get; protected set; }
+		public virtual int Id { get; protected set; }
 
-        public virtual string Content { get; set; }
+		public virtual string Content { get; set; }
 
-        public virtual ISet<TreeNode> Children
-        {
-            get { return children; }
-            protected set { children = value; }
-        }
-    }
+		public virtual ISet<TreeNode> Children
+		{
+			get { return _children; }
+			protected set { _children = value; }
+		}
+	}
 }
