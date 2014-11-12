1feddd8a24fbc6b9dd00e22dbd4018f718c0439e
Ricardo Peres <rjperes@hotmail.com>
2014-11-12 19:29:59 +0000

    NH-3735 and unit tests Fixed mapping of class Animal: a calculated column was returning a string but was being stored in a bool

---
+++ src/NHibernate.Test/Criteria/DeleteTests.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using NHibernate.Criterion;
+using NUnit.Framework;
+
+namespace NHibernate.Test.Criteria
+{
+	[TestFixture]
+	public class DeleteTests : TestCase
+	{
+		protected override string MappingsAssembly
+		{
+			get { return "NHibernate.Test"; }
+		}
+
+		protected override IList Mappings
+		{
+			get
+			{
+				return new string[]
+					{
+						"Criteria.Enrolment.hbm.xml",
+						"Criteria.Animal.hbm.xml",
+						"Criteria.MaterialResource.hbm.xml"
+					};
+			}
+		}
+
+		[Test]
+		public void CanDeleteSimpleExpression()
+		{
+			//NH-3735
+			using (var session = this.OpenSession())
+			using (session.BeginTransaction())
+			{
+				var mother = new Animal
+				{
+					BodyWeight = 200f,
+					Description = "Mother animal",
+					SerialNumber = "Mother"
+				};
+
+				var father = new Animal
+				{
+					BodyWeight = 250f,
+					Description = "Father animal",
+					SerialNumber = "Father"
+				};
+
+				var child = new Animal
+				{
+					BodyWeight = 100f,
+					Description = "Child animal",
+					SerialNumber = "Child",
+					Mother = mother,
+					Father = father
+				};
+
+				session.Save(mother);
+				session.Save(father);
+				session.Save(child);
+				session.Flush();
+
+				session.Clear();
+
+				var beforeDeleteCount = session.CreateCriteria(typeof(Animal)).Add(Restrictions.IsNotNull("father")).SetProjection(Projections.RowCount()).UniqueResult<int>();
+
+				var deleteCount = session.CreateCriteria(typeof(Animal)).Add(Restrictions.IsNotNull("father")).Delete();
+
+				var afterDeleteCount = session.CreateCriteria(typeof(Animal)).Add(Restrictions.IsNotNull("father")).SetProjection(Projections.RowCount()).UniqueResult<int>();
+
+				Assert.AreEqual(beforeDeleteCount, deleteCount);
+				Assert.AreEqual(0, afterDeleteCount);
+			}
+		}
+
+		[Test]
+		public void CanDeleteComplexExpression()
+		{
+			//NH-3735
+			using (var session = this.OpenSession())
+			using (session.BeginTransaction())
+			{
+				var mother = new Animal
+				{
+					BodyWeight = 200f,
+					Description = "Mother animal",
+					SerialNumber = "Mother"
+				};
+
+				var father = new Animal
+				{
+					BodyWeight = 250f,
+					Description = "Father animal",
+					SerialNumber = "Father"
+				};
+
+				var child = new Animal
+				{
+					BodyWeight = 100f,
+					Description = "Child animal",
+					SerialNumber = "Child",
+					Mother = mother,
+					Father = father
+				};
+
+				session.Save(mother);
+				session.Save(father);
+				session.Save(child);
+				session.Flush();
+
+				session.Clear();
+
+				var beforeDeleteCount = session.CreateCriteria(typeof(Animal)).Add(Restrictions.Conjunction().Add(Restrictions.IsNotNull("father")).Add(Restrictions.Ge("bodyWeight", 100f)).Add(Restrictions.IsEmpty("offspring"))).SetProjection(Projections.RowCount()).UniqueResult<int>();
+
+				var deleteCount = session.CreateCriteria(typeof(Animal)).Add(Restrictions.Conjunction().Add(Restrictions.IsNotNull("father")).Add(Restrictions.Ge("bodyWeight", 100f)).Add(Restrictions.IsEmpty("offspring"))).Delete();
+
+				var afterDeleteCount = session.CreateCriteria(typeof(Animal)).Add(Restrictions.Conjunction().Add(Restrictions.IsNotNull("father")).Add(Restrictions.Ge("bodyWeight", 100f)).Add(Restrictions.IsEmpty("offspring"))).SetProjection(Projections.RowCount()).UniqueResult<int>();
+
+				Assert.AreEqual(beforeDeleteCount, deleteCount);
+				Assert.AreEqual(0, afterDeleteCount);
+			}
+		}
+	}
+}
---
+++ src/NHibernate/Criterion/CriteriaExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using NHibernate.Exceptions;
+using NHibernate.Impl;
+using NHibernate.Loader.Criteria;
+using NHibernate.Persister.Entity;
+using NHibernate.SqlCommand;
+
+namespace NHibernate.Criterion
+{
+	public static class CriteriaExtensions
+	{
+		public static Int32 Delete(this IQueryOver queryOver)
+		{
+			return Delete(queryOver.UnderlyingCriteria);
+		}
+
+		public static Int32 Delete(this ICriteria criteria)
+		{
+			//NH-3735
+			criteria.ClearOrders();
+			criteria = criteria.SetProjection(Projections.Id());
+			var criteriaImpl = criteria as CriteriaImpl;
+			var sessionImpl = criteriaImpl.Session;
+			var session = sessionImpl as ISession;
+			var factory = sessionImpl.Factory;
+			var implementors = factory.GetImplementors(criteriaImpl.EntityOrClassName);
+			var loader = new CriteriaLoader(factory.GetEntityPersister(implementors[0]) as IOuterJoinLoadable, factory, criteriaImpl, implementors[0], sessionImpl.EnabledFilters);
+			var parameters = loader.Translator.GetQueryParameters().NamedParameters.Select(x => x.Value.Value).ToArray();
+
+			
+			var aliasRegex = new Regex(" from (\\w+) (\\w+) ", RegexOptions.IgnoreCase);
+			var parameterTokensRegex = new Regex("\\?");
+			//we need to turn positional parameters into named parameters because of SetParameterList
+			var count = 0;
+			var replacedSql = parameterTokensRegex.Replace(loader.SqlString.ToString(), m => ":p" + count++);
+			var sql = new StringBuilder(replacedSql);
+			//find from
+			var fromIndex = sql.ToString().IndexOf(" from ", StringComparison.InvariantCultureIgnoreCase);
+			//find alias
+			var alias = aliasRegex.Match(sql.ToString()).Groups[2].Value;
+
+			//make a string in the form DELETE alias FROM table alias WHERE condition
+			sql.Remove(0, fromIndex);
+			sql.Insert(0, string.Concat("delete ", alias, " "));
+
+			using (var childSession = session.GetSession(session.ActiveEntityMode))
+			{
+				try
+				{
+					var query = childSession.CreateSQLQuery(sql.ToString());
+
+					for (var i = 0; i < parameters.Length; ++i)
+					{
+						var parameter = parameters[i];
+
+						if (!(parameter is IEnumerable) || (parameter is string) || (parameter is byte[]))
+						{
+							query.SetParameter(String.Format("p{0}", i), parameter);
+						}
+						else
+						{
+							query.SetParameterList(String.Format("p{0}", i), parameter as IEnumerable);
+						}
+					}
+
+					return query.ExecuteUpdate();
+				}
+				catch (Exception ex)
+				{
+					throw ADOExceptionHelper.Convert(sessionImpl.Factory.SQLExceptionConverter, ex, "Error deleting records.", new SqlString(sql.ToString()), parameters, loader.Translator.GetQueryParameters().NamedParameters.ToDictionary(x => x.Key, x => x.Value));
+				}
+			}
+		}
+	}
+}
