297a9004b02d179ebf48b42449a76709540fc277
Jake Levitt <jlevitt@vt.edu>
2014-12-24 18:50:53 -0500

    Implement fix and test cases for NH-3634.

---
+++ src/NHibernate.Test/NHSpecificTest/NH3634/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace NHibernate.Test.NHSpecificTest.NH3634
+{
+	class Person
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual Connection Connection { get; set; }
+	}
+}
