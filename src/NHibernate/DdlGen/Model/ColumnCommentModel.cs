73d5290360a71f9b234b99e4745a17e549b4e67e
Jeffrey Becker <jeffrey.a.becker@gmail.com>
2015-02-16 13:55:34 -0500

    added a constructor for easier migration

--- src/NHibernate/DdlGen/Model/ColumnCommentModel.cs
+++ src/NHibernate/DdlGen/Model/ColumnCommentModel.cs
@@ -2,6 +2,14 @@
 {
     public class ColumnCommentModel
     {
+        public ColumnCommentModel(){}
+
+        public ColumnCommentModel(string columnName, string comment)
+        {
+            ColumnName = columnName;
+            Comment = comment;
+        }
+
         public string Comment { get; set; }
 
         public string ColumnName { get; set; }
