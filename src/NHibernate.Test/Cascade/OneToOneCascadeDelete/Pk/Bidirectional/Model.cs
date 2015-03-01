edcf92865f40e99412feed7a1fb4159158ec4843
Alexander Zaytsev <hazzik@gmail.com>
2015-03-01 15:49:54 +1300

    NH-1262 - Test refactored to use common code base and to be more stable on some dialects

---
+++ src/NHibernate.Test/Cascade/OneToOneCascadeDelete/Pk/Bidirectional/Model.cs
@@ -0,0 +1,30 @@
+﻿namespace NHibernate.Test.Cascade.OneToOneCascadeDelete.Pk.Bidirectional
+{
+	public class Employee
+	{
+		public virtual long Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual EmployeeInfo Info { get; set; }
+
+		public Employee()
+		{
+
+		}
+	}
+
+	public class EmployeeInfo
+	{
+		public virtual long Id { get; set; }
+		public virtual Employee EmployeeDetails { get; set; }
+
+		public EmployeeInfo()
+		{
+
+		}
+
+		public EmployeeInfo(Employee employee)
+		{
+			this.EmployeeDetails = employee;
+		}
+	}
+}
