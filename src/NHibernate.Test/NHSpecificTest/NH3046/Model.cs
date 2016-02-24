b64bafbed49653c86c6115bcdb574f3164fe257c
Tikuna <montagutcarlos@gmail.com>
2016-02-24 12:55:47 -0300

    Adjusts in the test nh3046
    Adjusts in the test

--- src/NHibernate.Test/NHSpecificTest/NH3046/Model.cs
+++ src/NHibernate.Test/NHSpecificTest/NH3046/Model.cs
@@ -1,4 +1,4 @@
-﻿namespace NHibernate.Test.NHSpecificTest.NH3010
+﻿namespace NHibernate.Test.NHSpecificTest.NH3046
 {
 	using System;
 	using System.Collections.Generic;
@@ -10,12 +10,12 @@
 			Childs = new List<Child>();
 		}
 
-		public virtual Guid Id { get; set; }
+		public virtual long Id { get; set; }
 		public virtual IList<Child> Childs { get; set; }
 	}
 
 	public class Child
 	{
-		public virtual Guid Id { get; set; }
+		public virtual long Id { get; set; }
 	}
 }
