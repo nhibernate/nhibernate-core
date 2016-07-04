c0df1238f6e576d841bde2767b7ac7d6c0eaa33b
podzyuban <podzyuban@gmail.com>
2016-07-04 19:19:56 +0300

    fix NHibernate.Test.DialectTest.SchemaTests.ColumnMetaDataFixture.SetColumnSizeAndNumericalAndScalePrecision

--- src/NHibernate.Test/DialectTest/SchemaTests/ColumnMetaDataFixture.cs
+++ src/NHibernate.Test/DialectTest/SchemaTests/ColumnMetaDataFixture.cs
@@ -25,7 +25,7 @@ namespace NHibernate.Test.DialectTest.SchemaTests
         [TestCase("13", null, null, 13, 0, 0)] // Size, but no precision.
         [TestCase("5000000000", null, null, int.MaxValue, 0, 0)]  // Oversize column should be bounded to int.MaxValue.
         [TestCase("13", "7", "5", 13, 7, 5)]  // Can handle both size and precision together.
-        public void SetColumnSizeAndNumericalPrecision(
+        public void SetColumnSizeAndNumericalAndScalePrecision(
             object columnSizeInput,
             object precisionInput,
             object scaleInput,
@@ -44,7 +44,7 @@ namespace NHibernate.Test.DialectTest.SchemaTests
 
             Assert.AreEqual(expectedColumnSize, column.ColumnSize);
             Assert.AreEqual(expectedPrecision, column.NumericalPrecision);
-            Assert.AreEqual(expectedPrecision, column.NumericalScale);
+            Assert.AreEqual(expectedScale, column.NumericalScale);
         }
     }
 }
