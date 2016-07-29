88bdad0a78dbb21732334a986f91aeaeaea15ad6
Mark Perry <mark.perry@bevalued.co.uk>
2016-07-29 15:05:33 +0100

    NH3500 - Unwrap TIE errors for classes with lazy properties

---
+++ src/NHibernate.Test/NHSpecificTest/NH3500/Fixture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Intercept;
+using NHibernate.Proxy;
+using NHibernate.Test.DynamicProxyTests;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH3500
+{
+    [Serializable]
+    public class MyClass
+    {
+        public virtual int Id { get; set; }
+
+        public virtual void ThrowError()
+        {
+            throw new Exception("test");
+        }
+    }
+
+    [TestFixture]
+    public class Fixture
+    {
+        [Test]
+        public void DefaultDynamicLazyFieldInterceptorUnWrapsTIEExceptions()
+        {
+            var pf = new DefaultProxyFactory();
+            var propertyInfo = typeof(MyClass).GetProperty("Id");
+            pf.PostInstantiate("MyClass", typeof(MyClass), new HashSet<System.Type>(), propertyInfo.GetGetMethod(), propertyInfo.GetSetMethod(), null);
+            var myClassProxied = (MyClass)pf.GetFieldInterceptionProxy(new MyClass());
+            Assert.Throws<Exception>(() => myClassProxied.ThrowError(), "test");
+        }
+    }
+}
