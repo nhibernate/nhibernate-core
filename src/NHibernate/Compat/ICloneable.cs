6a8dc5d5844ec9057de1329f3e4ee0b721855472
Nathan Brown <nathan@nkbrown.us>
2016-08-15 00:55:37 -0700

    NH-3807 - Add compatibility layer of Attributes for CoreClr. So we don't have to #if #endif around every single usage.
    Adds minimal implementation of:
    DesignerCategoryAttribute
    IntrospectionExtensions
    NonSerializedAttribute
    SerializableAttribute
    
    RuntimeReflectionExtensions
    IntrospectionExtensions
    
    InvalidExpressionException

---
+++ src/NHibernate/Compat/ICloneable.cs
@@ -0,0 +1,11 @@
+﻿#if FEATURE_NETCORE_ICLONEABLE_API
+// ReSharper disable once CheckNamespace
+namespace System
+{
+	public interface ICloneable
+	{
+		object Clone();
+	}
+}
+#endif
+
---
+++ src/NHibernate/Compat/NonSerializedAttribute.cs
@@ -0,0 +1,10 @@
+﻿#if !FEATURE_SERIALIZATION && NETSTANDARD
+// ReSharper disable once CheckNamespace
+namespace System
+{
+	[AttributeUsage(AttributeTargets.Field, Inherited = false)]
+	internal sealed class NonSerializedAttribute : Attribute
+	{
+	}
+}
+#endif
