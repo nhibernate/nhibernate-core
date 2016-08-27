4887f6b4c3dd006253bc5c657b2294856e28ffd6
Nathan Brown <nathan@nkbrown.us>
2016-08-27 13:11:50 -0700

    NH-3900 - Splitting NHibernate.Test.Linq namespace into separate project because NUnit 3 doesn't guarantee order of SetUpFixture in relation to other tests.

---
+++ src/NHibernate.Test.Linq/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliantAttribute(false)]
+[assembly: AssemblyTitleAttribute("NHibernate.Test.Linq")]
+[assembly: AssemblyDescriptionAttribute("The Unit Tests for NHibernate.")]
+[assembly: AssemblyCompanyAttribute("NHibernate.info")]
+[assembly: AssemblyProductAttribute("NHibernate.Test.Linq")]
+[assembly: AssemblyCopyrightAttribute("Licensed under LGPL.")]
+[assembly: AssemblyDelaySignAttribute(false)]
+
