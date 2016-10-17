45b27941366b82d8fdfb79d5703948b155b59659
LordJZ <LordJZ@outlook.com>
2016-10-17 14:00:09 +0300

    NH-3911 test

---
+++ src/NHibernate.Test/NHSpecificTest/NH3911/Derived1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHibernate.Test.NHSpecificTest.NH3911
+{
+    public class Derived1 : Base
+    {
+        public virtual new Derived2 Ref
+        {
+            get { return (Derived2)base.Ref.Unwrap(); }
+            set { base.Ref = value; }
+        }
+    }
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH3911/Derived2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHibernate.Test.NHSpecificTest.NH3911
+{
+    public class Derived2 : Base
+    {
+        public virtual new Derived1 Ref
+        {
+            get { return (Derived1)base.Ref.Unwrap(); }
+            set { base.Ref = value; }
+        }
+    }
+}
