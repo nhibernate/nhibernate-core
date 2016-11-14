4934507204fd447d8b06a152f75cabbb4d11a423
Alexander Zaytsev <hazzik@gmail.com>
2016-11-14 13:28:34 +1300

    NH-3874 - Make test pass for SqlServer CE.
    SqlServer CE could not handle tables with single column which is an identity primary key.

--- src/NHibernate.Test/NHSpecificTest/NH3874/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH3874/Fixture.cs
@@ -18,7 +18,7 @@ namespace NHibernate.Test.NHSpecificTest.NH3874
 			using (var session = OpenSession())
 			using (var tx = session.BeginTransaction())
 			{
-				var one = new One();
+				var one = new One { Name = "One" };
 				var two = new Two { One = one };
 				two.One.Twos = new[] { two };
 				_id = session.Save(one);
--- src/NHibernate.Test/NHSpecificTest/NH3874/One.cs
+++ src/NHibernate.Test/NHSpecificTest/NH3874/One.cs
@@ -6,6 +6,8 @@ namespace NHibernate.Test.NHSpecificTest.NH3874
 	{
 		public virtual IntWrapper Id { get; set; }
 
+		public virtual string Name { get; set; }
+
 		public virtual IList<Two> Twos { get; set; }
 	}
 }
