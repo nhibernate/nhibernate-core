fb75d7d19234ce5e9880e21461ce22a8a215254f
Oskar Berggren <oskar.berggren@gmail.com>
2016-11-14 02:18:10 +0100

    SqlStatementLogger: Minor cleanup to formatting and spelling.

--- src/NHibernate/AdoNet/Util/SqlStatementLogger.cs
+++ src/NHibernate/AdoNet/Util/SqlStatementLogger.cs
@@ -8,10 +8,12 @@ namespace NHibernate.AdoNet.Util
 	/// <summary> Centralize logging handling for SQL statements. </summary>
 	public class SqlStatementLogger
 	{
-		private static readonly IInternalLogger log = LoggerProvider.LoggerFor("NHibernate.SQL");
+		private static readonly IInternalLogger Logger = LoggerProvider.LoggerFor("NHibernate.SQL");
 
 		/// <summary> Constructs a new SqlStatementLogger instance.</summary>
-		public SqlStatementLogger() : this(false, false) { }
+		public SqlStatementLogger() : this(false, false)
+		{
+		}
 
 		/// <summary> Constructs a new SqlStatementLogger instance. </summary>
 		/// <param name="logToStdout">Should we log to STDOUT in addition to our internal logger. </param>
@@ -28,7 +30,7 @@ namespace NHibernate.AdoNet.Util
 
 		public bool IsDebugEnabled
 		{
-			get { return log.IsDebugEnabled; }
+			get { return Logger.IsDebugEnabled; }
 		}
 
 		/// <summary> Log a IDbCommand. </summary>
@@ -37,7 +39,7 @@ namespace NHibernate.AdoNet.Util
 		/// <param name="style">The requested formatting style. </param>
 		public virtual void LogCommand(string message, IDbCommand command, FormatStyle style)
 		{
-			if (!log.IsDebugEnabled && !LogToStdout || string.IsNullOrEmpty(command.CommandText))
+			if (!Logger.IsDebugEnabled && !LogToStdout || string.IsNullOrEmpty(command.CommandText))
 			{
 				return;
 			}
@@ -53,7 +55,7 @@ namespace NHibernate.AdoNet.Util
 			{
 				logMessage = message + statement;
 			}
-			log.Debug(logMessage);
+			Logger.Debug(logMessage);
 			if (LogToStdout)
 			{
 				Console.Out.WriteLine("NHibernate: " + statement);
@@ -78,7 +80,7 @@ namespace NHibernate.AdoNet.Util
 			}
 			else
 			{
-				var output = new StringBuilder(command.CommandText.Length + (command.Parameters.Count * 20));
+				var output = new StringBuilder(command.CommandText.Length + (command.Parameters.Count*20));
 				output.Append(command.CommandText.TrimEnd(' ', ';', '\n'));
 				output.Append(";");
 
@@ -92,54 +94,65 @@ namespace NHibernate.AdoNet.Util
 						output.Append(", ");
 					}
 					appendComma = true;
-					p = (IDataParameter)command.Parameters[i];
-					output.Append(string.Format("{0} = {1} [Type: {2}]", p.ParameterName, GetParameterLogableValue(p), GetParameterLogableType(p)));
+					p = (IDataParameter) command.Parameters[i];
+					output.Append(
+						string.Format("{0} = {1} [Type: {2}]", p.ParameterName, GetParameterLoggableValue(p), GetParameterLoggableType(p)));
 				}
 				outputText = output.ToString();
 			}
 			return outputText;
 		}
 
-	    private static string GetParameterLogableType(IDataParameter dataParameter)
-	    {
-            var p = dataParameter as IDbDataParameter;
-            if (p != null)
-                return p.DbType + " (" + p.Size + ")";
-	        return p.DbType.ToString();
+		private static string GetParameterLoggableType(IDataParameter dataParameter)
+		{
+			var p = dataParameter as IDbDataParameter;
+			if (p != null)
+				return p.DbType + " (" + p.Size + ")";
+			return p.DbType.ToString();
 
-	    }
+		}
 
-			public string GetParameterLogableValue(IDataParameter parameter)
+
+		public string GetParameterLoggableValue(IDataParameter parameter)
+		{
+			const int maxLoggableStringLength = 1000;
+			if (parameter.Value == null || DBNull.Value.Equals(parameter.Value))
 			{
-				const int maxLogableStringLength = 1000;
-				if (parameter.Value == null || DBNull.Value.Equals(parameter.Value))
-				{
-					return "NULL";
-				}
-				if (IsStringType(parameter.DbType))
-				{
-					return string.Concat("'", TruncateWithEllipsis(parameter.Value.ToString(), maxLogableStringLength), "'");
-				}
-				var buffer = parameter.Value as byte[];
-				if (buffer != null)
-				{
-					return GetBufferAsHexString(buffer);
-				}
-				return parameter.Value.ToString();
+				return "NULL";
 			}
+			if (IsStringType(parameter.DbType))
+			{
+				return string.Concat("'", TruncateWithEllipsis(parameter.Value.ToString(), maxLoggableStringLength), "'");
+			}
+			var buffer = parameter.Value as byte[];
+			if (buffer != null)
+			{
+				return GetBufferAsHexString(buffer);
+			}
+			return parameter.Value.ToString();
+
+		}
+
+
+		[Obsolete("Use GetParameterLoggableValue(parameter) instead.")]
+		public string GetParameterLogableValue(IDataParameter parameter)
+		{
+			return GetParameterLoggableValue(parameter);
+		}
+
 
 		private static string GetBufferAsHexString(byte[] buffer)
 		{
 			const int maxBytes = 128;
 			int bufferLength = buffer.Length;
 
-			var sb = new StringBuilder(maxBytes * 2 + 8);
+			var sb = new StringBuilder(maxBytes*2 + 8);
 			sb.Append("0x");
 			for (int i = 0; i < bufferLength && i < maxBytes; i++)
 			{
 				sb.Append(buffer[i].ToString("X2"));
 			}
-			if(bufferLength > maxBytes)
+			if (bufferLength > maxBytes)
 			{
 				sb.Append("...");
 			}
@@ -149,7 +162,7 @@ namespace NHibernate.AdoNet.Util
 		private static bool IsStringType(DbType dbType)
 		{
 			return DbType.String.Equals(dbType) || DbType.AnsiString.Equals(dbType)
-						 || DbType.AnsiStringFixedLength.Equals(dbType) || DbType.StringFixedLength.Equals(dbType);
+			       || DbType.AnsiStringFixedLength.Equals(dbType) || DbType.StringFixedLength.Equals(dbType);
 		}
 
 		public FormatStyle DetermineActualStyle(FormatStyle style)
@@ -159,8 +172,8 @@ namespace NHibernate.AdoNet.Util
 
 		public void LogBatchCommand(string batchCommand)
 		{
-			log.Debug(batchCommand);
-			if(LogToStdout)
+			Logger.Debug(batchCommand);
+			if (LogToStdout)
 			{
 				Console.Out.WriteLine("NHibernate: " + batchCommand);
 			}
