16498402c8b71b86a45c4cd79d0e1f367d4507f1
Oskar Berggren <oskar.berggren@gmail.com>
2016-11-20 16:12:48 +0100

    QueryLock.cs: Strengthen asserts to verify actual locking has taken place.

--- src/NHibernate.Test/Linq/QueryLock.cs
+++ src/NHibernate.Test/Linq/QueryLock.cs
@@ -1,9 +1,10 @@
 ﻿using System.Linq;
-using NHibernate.AdoNet;
-using NHibernate.Cfg;
-using NHibernate.Engine;
+using System.Transactions;
+using NHibernate.DomainModel.Northwind.Entities;
+using NHibernate.Exceptions;
 using NHibernate.Linq;
 using NUnit.Framework;
+
 
 namespace NHibernate.Test.Linq
 {
@@ -13,35 +14,69 @@ namespace NHibernate.Test.Linq
 		[Test]
 		public void CanSetLockLinqQueries()
 		{
-			var result = (from e in db.Customers
-						  where e.CompanyName == "Corp"
-						  select e).SetLockMode(LockMode.Upgrade).ToList();
+			using (session.BeginTransaction())
+			{
+				var result = (from e in db.Customers
+				              select e).SetLockMode(LockMode.Upgrade).ToList();
 
+				Assert.That(result, Has.Count.EqualTo(91));
+				Assert.That(session.GetCurrentLockMode(result[0]), Is.EqualTo(LockMode.Upgrade));
+				AssertSeparateTransactionIsLockedOut(result[0].CustomerId);
+			}
 		}
 
 
 		[Test]
 		public void CanSetLockOnLinqPagingQuery()
 		{
-			var result = (from e in db.Customers
-						  where e.CompanyName == "Corp"
-						  select e).Skip(5).Take(5).SetLockMode(LockMode.Upgrade).ToList();
+			using (session.BeginTransaction())
+			{
+				var result = (from e in db.Customers
+				              select e).Skip(5).Take(5).SetLockMode(LockMode.Upgrade).ToList();
+
+				Assert.That(result, Has.Count.EqualTo(5));
+				Assert.That(session.GetCurrentLockMode(result[0]), Is.EqualTo(LockMode.Upgrade));
+				AssertSeparateTransactionIsLockedOut(result[0].CustomerId);
+			}
 		}
 
 
 		[Test]
 		public void CanLockBeforeSkipOnLinqOrderedPageQuery()
 		{
-			var result = (from e in db.Customers
-						  orderby e.CompanyName
-						  select e)
-				.SetLockMode(LockMode.Upgrade).Skip(5).Take(5).ToList();
+			using (session.BeginTransaction())
+			{
+				var result = (from e in db.Customers
+				              orderby e.CompanyName
+				              select e)
+					.SetLockMode(LockMode.Upgrade).Skip(5).Take(5).ToList();
 
-
+				Assert.That(result, Has.Count.EqualTo(5));
+				Assert.That(session.GetCurrentLockMode(result[0]), Is.EqualTo(LockMode.Upgrade));
+				AssertSeparateTransactionIsLockedOut(result[0].CustomerId);
+			}
 		}
 
 
+		private void AssertSeparateTransactionIsLockedOut(string customerId)
+		{
+			using (new TransactionScope(TransactionScopeOption.Suppress))
+			using (var s2 = OpenSession())
+			using (s2.BeginTransaction())
+			{
+				// TODO: We should try to verify that the exception actually IS a locking failure and not something unrelated.
+				Assert.Throws<GenericADOException>(
+					() =>
+					{
+						var result2 = (from e in s2.Query<Customer>()
+									   where e.CustomerId == customerId
+									   select e).SetLockMode(LockMode.UpgradeNoWait)
+												.Timeout(5).ToList();
+						Assert.IsNotNull(result2);
+					});
+			}
+		}
 	}
-
 }
+
 
