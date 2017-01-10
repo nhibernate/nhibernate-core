a37a83993cdd088c1784ebd9ce14cd8afb1e8b64
Alexander Zaytsev <hazzik@gmail.com>
2017-01-10 14:39:26 +1300

    NH-3929 - Revert NH-3904
    * Users required to explicitly specify custom user type via MappedAs method if they want to use IUserType/ICompositeUserType type parameters in Linq queries with generators custom generators, or properly implement generators to take the type into account.
    * Fix MappedAs method (NH-2401).

--- src/NHibernate.Test/NHSpecificTest/EntityWithUserTypeCanHaveLinqGenerators/EntityWithUserTypeProperty.cs
+++ src/NHibernate.Test/NHSpecificTest/EntityWithUserTypeCanHaveLinqGenerators/EntityWithUserTypeProperty.cs
@@ -6,6 +6,7 @@ namespace NHibernate.Test.NHSpecificTest.EntityWithUserTypeCanHaveLinqGenerators
 		public virtual int Id { get; set; }
 		public virtual string Name { get; set; }
 		public virtual IExample Example { get; set; }
+		public virtual double DoubleStoredAsString { get; set; }
 	}
 
 
