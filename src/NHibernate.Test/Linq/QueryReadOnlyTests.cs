1b01cc6c9a95b4c14d32183b09b0a5c4beae17b3
Alexander Zaytsev <hazzik@gmail.com>
2017-02-13 01:13:21 +1300

    Copy test from #260

--- src/NHibernate.Test/Linq/QueryReadOnlyTests.cs
+++ src/NHibernate.Test/Linq/QueryReadOnlyTests.cs
@@ -7,11 +7,6 @@ namespace NHibernate.Test.Linq
 {
 	public class QueryReadOnlyTests : LinqTestCase
 	{
-		protected override void Configure(Configuration configuration)
-		{
-			base.Configure(configuration);
-		}
-
 		[Test]
 		public void CanSetReadOnlyOnLinqQueries()
 		{
@@ -40,6 +35,23 @@ namespace NHibernate.Test.Linq
 						  orderby e.CompanyName
 						  select e)
 				.AsReadOnly().Skip(5).Take(5).ToList();
+
+			Assert.That(result.All(x => this.session.IsReadOnly(x)), Is.True);
+		}
+		
+		[Test]
+		public void CanSetReadOnlyOnLinqGroupPageQuery()
+		{
+			var subQuery = db.Customers.Where(e2 => e2.CompanyName.Contains("a")).Select(e2 => e2.CustomerId)
+							 .AsReadOnly(); // This AsReadOnly() should not cause trouble, and be ignored.
+
+			var result = (from e in db.Customers
+						  where subQuery.Contains(e.CustomerId)
+						  group e by e.CompanyName
+							  into g
+							  select new { g.Key, Count = g.Count() })
+				.Skip(5).Take(5)
+				.AsReadOnly().ToList();
 
 			Assert.That(result.All(x => this.session.IsReadOnly(x)), Is.True);
 		}
