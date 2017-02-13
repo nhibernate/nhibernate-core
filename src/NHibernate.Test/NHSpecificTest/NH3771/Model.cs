4534c031cbb08a363f2294c736ba7e199bced763
Alexander Zaytsev <hazzik@gmail.com>
2017-02-13 13:04:07 +1300

    Fix indents & code cleanup

--- src/NHibernate.Test/NHSpecificTest/NH3771/Model.cs
+++ src/NHibernate.Test/NHSpecificTest/NH3771/Model.cs
@@ -1,17 +1,12 @@
-﻿namespace NHibernate.Test.NHSpecificTest.NH3771
-{
-	using System;
-	using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
+namespace NHibernate.Test.NHSpecificTest.NH3771
+{
 	public class Singer
 	{
-        public Singer()
-		{
-		}
-
 		public virtual long Id { get; set; }
-        public virtual string Name { get; set; }
-        public virtual int Version { get; set; }
+		public virtual string Name { get; set; }
+		public virtual int Version { get; set; }
 	}
-
 }
