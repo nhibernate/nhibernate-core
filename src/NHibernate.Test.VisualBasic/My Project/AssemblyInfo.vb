c996d4fac473532d8105af7087fb04869cfefe32
Alexander Zaytsev <hazzik@gmail.com>
2017-03-17 13:55:18 +1300

    Convert to VS2017 projects

--- src/NHibernate.Test.VisualBasic/My Project/AssemblyInfo.vb
+++ src/NHibernate.Test.VisualBasic/My Project/AssemblyInfo.vb
@@ -1,34 +1,36 @@
 ﻿Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
+Imports NUnit.Framework
 
-' General Information about an assembly is controlled through the following 
+' General Information about an assembly is controlled through the following
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("NHibernate.Test.VisualBasic")> 
-<Assembly: AssemblyDescription("The Visual Basic Unit Tests for NHibernate.")> 
-<Assembly: AssemblyCompany("Microsoft")> 
-<Assembly: AssemblyProduct("NHibernate.Test.VisualBasic")> 
-<Assembly: AssemblyCopyright("Copyright © Microsoft 2011")> 
-<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyTitle("NHibernate.Test.VisualBasic")>
+<Assembly: AssemblyDescription("The Visual Basic Unit Tests for NHibernate.")>
+<Assembly: AssemblyCompany("NHibernate Community")>
+<Assembly: AssemblyProduct("NHibernate.Test.VisualBasic")>
+<Assembly: AssemblyCopyright("Copyright © NHibernate Community 2011 - 2017")>
+<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("f3bf0316-dff3-4f0c-918a-1f278d626d85")> 
+<Assembly: Guid("34060097-6a6b-43c8-b952-3a2e10f2c7a2")>
 
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
-'      Minor Version 
+'      Minor Version
 '      Build Number
 '      Revision
 '
-' You can specify all the values or you can default the Build and Revision Numbers 
+' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
+' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: CLSCompliant(False)> 
+<assembly: LevelOfParallelism(0)>
