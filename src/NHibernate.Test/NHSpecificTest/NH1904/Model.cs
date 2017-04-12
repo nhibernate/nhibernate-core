cc9ff7d061377ee05478ef3841bbbde416c3b2db
Frédéric Delaporte <fredericdelaporte@free.fr>
2017-04-12 12:27:13 +0200

    NH-1904 - public properties cannot have the same name with different case, test case and fix for struct components

--- src/NHibernate.Test/NHSpecificTest/NH1904/Model.cs
+++ src/NHibernate.Test/NHSpecificTest/NH1904/Model.cs
@@ -9,4 +9,19 @@ namespace NHibernate.Test.NHSpecificTest.NH1904
 
 		protected virtual DateTime issued { get; set; }
 	}
+
+	public class InvoiceWithAddress : Invoice
+	{
+		public virtual Address BillingAddress { get; set; }
+	}
+
+	public struct Address
+	{
+		public string Line { get; set; }
+		public string line { get; set; }
+		public string Line2 { get; set; }
+		public string City { get; set; }
+		public string ZipCode { get; set; }
+		public string Country { get; set; }
+	}
 }
