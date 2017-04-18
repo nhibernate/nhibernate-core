5614c270b18e5daeb79e4dc1ca15ef43c0dbe45b
Ralf Kornelius <r.kornelius@vescon.com>
2017-04-18 12:59:41 +0200

    fixed remaining indendations

--- src/NHibernate.Test/NHSpecificTest/NH3984/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH3984/Fixture.cs
@@ -6,55 +6,55 @@ using NUnit.Framework;
 
 namespace NHibernate.Test.NHSpecificTest.NH3984
 {
-    [TestFixture]
-    public class Fixture : BugTestCase
-    {
-        protected override void OnTearDown()
-        {
-            using (var s = OpenSession())
-            using (var tx = s.BeginTransaction())
-            {
-                s.CreateSQLQuery("delete from LogEntry").ExecuteUpdate();
-                tx.Commit();
-            }
-        }
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnTearDown()
+		{
+			using (var s = OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+				s.CreateSQLQuery("delete from LogEntry").ExecuteUpdate();
+				tx.Commit();
+			}
+		}
 
-        protected override bool AppliesTo(Dialect.Dialect dialect)
-        {
-            return dialect is MsSql2008Dialect;
-        }
+		protected override bool AppliesTo(Dialect.Dialect dialect)
+		{
+			return dialect is MsSql2008Dialect;
+		}
 
-        [Test]
-        public void ShouldBePossibleToReadMillisecondsFromDatetime2ViaCreateSQLQuery()
-        {
-            var now = DateTime.UtcNow;
+		[Test]
+		public void ShouldBePossibleToReadMillisecondsFromDatetime2ViaCreateSQLQuery()
+		{
+			var now = DateTime.UtcNow;
 
-            using (var session = OpenSession())
-            using (var transaction = session.BeginTransaction())
-            {
-                var entry = new LogEntry {Text = "Test", CreatedAt = now};
-                session.Save(entry);
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				var entry = new LogEntry {Text = "Test", CreatedAt = now};
+				session.Save(entry);
 
-                session.Flush();
-                transaction.Commit();
-            }
+				session.Flush();
+				transaction.Commit();
+			}
 
-            using (var session = OpenSession())
-            {
-                var datetimeViaQuery = session
-                    .Query<LogEntry>()
-                    .Select(x => x.CreatedAt)
-                    .Single();
+			using (var session = OpenSession())
+			{
+				var datetimeViaQuery = session
+					.Query<LogEntry>()
+					.Select(x => x.CreatedAt)
+					.Single();
 
-                Assert.That(datetimeViaQuery.Millisecond, Is.EqualTo(now.Millisecond));
+				Assert.That(datetimeViaQuery.Millisecond, Is.EqualTo(now.Millisecond));
 
-                var datetimeViaCreateSqlQuery = session.CreateSQLQuery("SELECT CreatedAt FROM LogEntry")
-                                                       .List()
-                                                       .Cast<DateTime>()
-                                                       .Single();
+				var datetimeViaCreateSqlQuery = session.CreateSQLQuery("SELECT CreatedAt FROM LogEntry")
+														.List()
+														.Cast<DateTime>()
+														.Single();
 
-                Assert.That(datetimeViaCreateSqlQuery.Millisecond, Is.EqualTo(now.Millisecond));
-            }
-        }
-    }
+				Assert.That(datetimeViaCreateSqlQuery.Millisecond, Is.EqualTo(now.Millisecond));
+			}
+		}
+	}
 }
--- src/NHibernate.Test/NHSpecificTest/NH3984/LogEntry.cs
+++ src/NHibernate.Test/NHSpecificTest/NH3984/LogEntry.cs
@@ -2,10 +2,10 @@
 
 namespace NHibernate.Test.NHSpecificTest.NH3984
 {
-    public class LogEntry
-    {
-        public virtual int Id { get; set; }
-        public virtual string Text { get; set; }
-        public virtual DateTime CreatedAt { get; set; }
-    }
+	public class LogEntry
+	{
+		public virtual int Id { get; set; }
+		public virtual string Text { get; set; }
+		public virtual DateTime CreatedAt { get; set; }
+	}
 }
