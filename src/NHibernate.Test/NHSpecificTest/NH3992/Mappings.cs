f46ebe625aa09a42e8340b1cbf6f68d8ab2a704d
Byron Pogson <byron.pogson@manufacturingintelligence.com.au>
2017-04-27 12:01:50 +0800

    Unit test for issue

---
+++ src/NHibernate.Test/NHSpecificTest/NH3992/Mappings.cs
@@ -0,0 +1,27 @@
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace NHibernate.Test.NHSpecificTest.NH3992
+{
+    public class BaseEntityMapping : ClassMapping<BaseEntity>
+    {
+        public BaseEntityMapping()
+        {
+            Id(i => i.Id, m => m.Generator(Generators.GuidComb));
+	        Property(p => p.BaseField,
+		        map => { map.Column("BaseField"); });
+        }
+    }
+
+	public class MappedEntityMapping : SubclassMapping<MappedEntity>
+	{
+		public MappedEntityMapping()
+		{
+			Extends(typeof(BaseEntity));
+			Property(p => p.ExtendedField,
+				map => { map.Column("ExtendedField"); });
+			Property(p => p.TopLevelField,
+				map => { map.Column("TopLevelField"); });
+		}
+	}
+}
