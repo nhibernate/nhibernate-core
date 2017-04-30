e662e7c618187eb86e44c7106329a9986ab70f7f
Alexander Zaytsev <hazzik@gmail.com>
2017-04-30 13:49:27 +1200

    NH-3997 - SqlServer CE: Make native generator to be TableHiLoGenerator
    * Adjust tests wrongly assuming the behaviour of id generator

--- src/NHibernate/Dialect/MsSqlCeDialect.cs
+++ src/NHibernate/Dialect/MsSqlCeDialect.cs
@@ -4,6 +4,7 @@ using System.Data.Common;
 using System.Text;
 using NHibernate.Dialect.Function;
 using NHibernate.Dialect.Schema;
+using NHibernate.Id;
 using NHibernate.SqlCommand;
 using NHibernate.Util;
 using Environment = NHibernate.Cfg.Environment;
@@ -123,6 +124,10 @@ namespace NHibernate.Dialect
 		{
 			get { return false; }
 		}
+
+		public override System.Type NativeIdentifierGeneratorClass => typeof(TableHiLoGenerator);
+
+		public override bool SupportsCircularCascadeDeleteConstraints => false;
 
 		public override IDataBaseSchema GetDataBaseSchema(DbConnection connection)
 		{
