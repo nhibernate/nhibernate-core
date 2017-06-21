5e78969fa1feea3ec451c2129c5a324c24b4607b
Frédéric Delaporte <fredericdelaporte@free.fr>
2017-06-21 18:05:27 +0200

    Xml comment, forgotten covariance.

--- src/NHibernate/IFutureEnumerable.cs
+++ src/NHibernate/IFutureEnumerable.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace NHibernate
 {
-	public interface IFutureEnumerable<T> : IEnumerable<T>
+	/// <summary>
+	/// A deferred enumerable, which enumeration will trigger execution of all other pending futures.
+	/// </summary>
+	/// <typeparam name="T">The type of the enumerated elements.</typeparam>
+	public interface IFutureEnumerable<out T> : IEnumerable<T>
 	{
+		/// <summary>
+		/// An <see cref="IAsyncEnumerable{T}"/> for enumerating the future asynchronously.
+		/// </summary>
 		IAsyncEnumerable<T> AsyncEnumerable { get; }
 	}
 }
