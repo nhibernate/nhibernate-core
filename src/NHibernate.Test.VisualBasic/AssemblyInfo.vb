02a631586558d09a917cde77a86bdfd9b09009c6
Alexander Zaytsev <hazzik@gmail.com>
2017-08-15 12:42:30 +1200

    NH-3990 - Install Microsoft.VisualBasic package to enable compilation of NH-3302 tests
    - Also remove auto-generated junk

--- src/NHibernate.Test.VisualBasic/AssemblyInfo.vb
+++ src/NHibernate.Test.VisualBasic/AssemblyInfo.vb
@@ -1,3 +1,15 @@
-﻿Imports NUnit.Framework
+﻿Imports System
+Imports System.Reflection
+Imports NUnit.Framework
 
-<assembly: LevelOfParallelism(0)>
+<Assembly: CLSCompliant(False)>
+<Assembly: AssemblyTitle("NHibernate.Test.VisualBasic")>
+<Assembly: AssemblyDescription("The Visual Basic Unit Tests for NHibernate.")>
+<Assembly: AssemblyCompany("NHibernate.info")>
+<Assembly: AssemblyProduct("NHibernate.Test.VisualBasic")>
+<Assembly: AssemblyCopyright("Licensed under LGPL.")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyDelaySign(False)>
+
+<Assembly: LevelOfParallelism(0)>
+
