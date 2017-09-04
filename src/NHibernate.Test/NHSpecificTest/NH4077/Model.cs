2bbb6d89a0c022770c4f94bf5241dd61622685eb
BhaaL <bhaal@0x1337.org>
2017-09-04 13:52:07 +0200

    NH-4077 - create test cases
    both fixtures use an evil PostXxxListener to cause/force an auto-flush of
    the session that is currently being committed inside a transaction.
    
    for PostInsertFixture it leads to to duplicate inserts.
    
    for PostUpdateFixture it causes an ArgumentOutOfRange exception by clearing
    a list that is currently being iterated.

---
+++ src/NHibernate.Test/NHSpecificTest/NH4077/Model.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH4077
+{
+	public class Entity
+	{
+		public virtual Guid Id { get; set; }
+		public virtual string Code { get; set; }
+	}
+}
