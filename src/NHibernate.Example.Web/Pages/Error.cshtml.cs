c8f1ce8945fa61ee3b5b84810a2b3e5ec79b0bb0
Nathan Brown <nathan@nkbrown.us>
2017-09-19 18:34:22 -0700

    NH-4073 - Fix problem in NHibernate.Example.Web
    `dotnet publish` now works.

--- src/NHibernate.Example.Web/Pages/Error.cshtml.cs
+++ src/NHibernate.Example.Web/Pages/Error.cshtml.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AspNetCoreFxWebApplication.Pages
+namespace NHibernate.Example.Web.Pages
 {
 	public class ErrorModel : PageModel
 	{
