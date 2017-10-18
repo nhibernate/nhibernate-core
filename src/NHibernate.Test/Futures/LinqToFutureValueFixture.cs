e84ffdff38430da9d8d09f73674a21a951be64b9
Nico Kreipke <git@nkreipke.de>
2017-10-18 14:40:54 +0200

    Added additional test cases for Sum

--- src/NHibernate.Test/Futures/LinqToFutureValueFixture.cs
+++ src/NHibernate.Test/Futures/LinqToFutureValueFixture.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using NHibernate.Linq;
 using NUnit.Framework;
@@ -48,13 +49,58 @@ namespace NHibernate.Test.Futures
 			}
 		}
 
+		[Test]
+		public void ToFutureValueWithSumOnEmptySetThrows()
+		{
+			using (var s = OpenSession())
+			{
+				var personsSum = s.Query<Person>()
+					.Where(x => false) // make this an empty set
+					.Select(x => x.Id)
+					.ToFutureValue(x => x.Sum());
+
+				int value;
+				Assert.Throws<ArgumentNullException>(() => value = personsSum.Value);
+			}
+		}
+
+		[Test]
+		public void ToFutureValueWithNullableSumReturnsResult()
+		{
+			using (var s = OpenSession())
+			{
+				var ageSum = s.Query<Person>()
+					.Select(x => x.Age)
+					.ToFutureValue(x => x.Sum());
+
+				Assert.IsNotNull(ageSum);
+				Assert.IsNotNull(ageSum.Value);
+				Assert.NotZero(ageSum.Value.Value);
+			}
+		}
+
+		[Test]
+		public void ToFutureValueWithNullableSumOnEmptySetReturnsNull()
+		{
+			using (var s = OpenSession())
+			{
+				var ageSum = s.Query<Person>()
+					.Where(x => false) // make this an empty set
+					.Select(x => x.Age)
+					.ToFutureValue(x => x.Sum());
+
+				Assert.IsNotNull(ageSum);
+				Assert.IsNull(ageSum.Value);
+			}
+		}
+
 		protected override void OnSetUp()
 		{
 			using (var session = OpenSession())
 			using (var transaction = session.BeginTransaction())
 			{
-				session.Save(new Person {Name = "Test1"});
-				session.Save(new Person {Name = "Test2"});
+				session.Save(new Person {Name = "Test1", Age = 20});
+				session.Save(new Person {Name = "Test2", Age = 30});
 				session.Save(new Person {Name = "Test3"});
 				session.Save(new Person {Name = "Test4"});
 				transaction.Commit();
