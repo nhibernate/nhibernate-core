0ae15ca4629ed24f8aec5a49f80b1b4bc5b4aefe
maca88 <bostjan.markezic@siol.net>
2017-10-23 18:22:04 +0200

    Updated tests to be more robust and added documentation @fredericDelaporte

--- src/NHibernate.Test/NHSpecificTest/GH1391/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/GH1391/Fixture.cs
@@ -12,43 +12,78 @@ namespace NHibernate.Test.NHSpecificTest.GH1391
 {
 	public class Fixture
 	{
+		private readonly Random _random = new Random();
+		
 		[Test]
-		public void MultipleThreadsNested()
+		public void Concurrent()
 		{
+			// Simulating two session factories, where one has tracking enabled and the other disabled
 			Parallel.For(0, 100, i =>
 			{
-				var guid = Guid.NewGuid();
-				using (new SessionIdLoggingContext(guid))
+				if (_random.Next(2) == 0)
 				{
-					Assert.That(SessionIdLoggingContext.SessionId, Is.EqualTo(guid));
-					var guid2 = Guid.NewGuid();
-					using (new SessionIdLoggingContext(guid2))
-					{
-						Assert.That(SessionIdLoggingContext.SessionId, Is.EqualTo(guid2));
-					}
-					Assert.That(SessionIdLoggingContext.SessionId, Is.EqualTo(guid));
+					Enabled();
 				}
-				Assert.That(SessionIdLoggingContext.SessionId, Is.Null);
+				else
+				{
+					Disabled();
+				}
 			});
+		}
+
+		[Test]
+		public void ConcurrentAsync()
+		{
+			async Task RunAsync(bool enabled)
+			{
+				for (var i = 0; i < 50; i++)
+				{
+					if (enabled)
+					{
+						await EnabledAsync().ConfigureAwait(false);
+					}
+					else
+					{
+						await DisabledAsync().ConfigureAwait(false);
+					}
+				}
+			}
+			// Simulating two session factories, where one has tracking enabled and the other disabled
+			Task.WaitAll(RunAsync(true), RunAsync(false));
 		}
 		
 		[Test]
-		public async Task AsyncContextNested()
+		public void Enabled()
 		{
 			var guid = Guid.NewGuid();
 			using (new SessionIdLoggingContext(guid))
 			{
-				var id = Thread.CurrentThread.ManagedThreadId;
-				await Task.Delay(10).ConfigureAwait(false);
-				
 				Assert.That(SessionIdLoggingContext.SessionId, Is.EqualTo(guid));
-				Assert.That(Thread.CurrentThread.ManagedThreadId, Is.Not.EqualTo(id));
+				var guid2 = Guid.NewGuid();
+				using (new SessionIdLoggingContext(guid2))
+				{
+					Assert.That(SessionIdLoggingContext.SessionId, Is.EqualTo(guid2));
+				}
+				Assert.That(SessionIdLoggingContext.SessionId, Is.EqualTo(guid));
+			}
+			Assert.That(SessionIdLoggingContext.SessionId, Is.Null);
+		}
+		
+		[Test]
+		public async Task EnabledAsync()
+		{
+			var guid = Guid.NewGuid();
+			using (new SessionIdLoggingContext(guid))
+			{
+				Assert.That(SessionIdLoggingContext.SessionId, Is.EqualTo(guid));
+				await Task.Delay(1).ConfigureAwait(false);
+				Assert.That(SessionIdLoggingContext.SessionId, Is.EqualTo(guid));
 
 				var guid2 = Guid.NewGuid();
 				using (new SessionIdLoggingContext(guid2))
 				{
 					Assert.That(SessionIdLoggingContext.SessionId, Is.EqualTo(guid2));
-					await Task.Delay(10).ConfigureAwait(false);
+					await Task.Delay(1).ConfigureAwait(false);
 					Assert.That(SessionIdLoggingContext.SessionId, Is.EqualTo(guid2));
 				}
 				Assert.That(SessionIdLoggingContext.SessionId, Is.EqualTo(guid));
@@ -63,11 +98,32 @@ namespace NHibernate.Test.NHSpecificTest.GH1391
 			using (new SessionIdLoggingContext(guid))
 			{
 				Assert.That(SessionIdLoggingContext.SessionId, Is.Null);
+				using (new SessionIdLoggingContext(guid))
+				{
+					Assert.That(SessionIdLoggingContext.SessionId, Is.Null);
+				}
+				Assert.That(SessionIdLoggingContext.SessionId, Is.Null);
 			}
-			guid = Guid.NewGuid();
+			Assert.That(SessionIdLoggingContext.SessionId, Is.Null);
+		}
+		
+		[Test]
+		public async Task DisabledAsync()
+		{
+			var guid = Guid.Empty;
 			using (new SessionIdLoggingContext(guid))
 			{
-				Assert.That(SessionIdLoggingContext.SessionId, Is.EqualTo(guid));
+				Assert.That(SessionIdLoggingContext.SessionId, Is.Null);
+				await Task.Delay(1).ConfigureAwait(false);
+				Assert.That(SessionIdLoggingContext.SessionId, Is.Null);
+				
+				using (new SessionIdLoggingContext(guid))
+				{
+					Assert.That(SessionIdLoggingContext.SessionId, Is.Null);
+					await Task.Delay(1).ConfigureAwait(false);
+					Assert.That(SessionIdLoggingContext.SessionId, Is.Null);
+				}
+				Assert.That(SessionIdLoggingContext.SessionId, Is.Null);
 			}
 			Assert.That(SessionIdLoggingContext.SessionId, Is.Null);
 		}
