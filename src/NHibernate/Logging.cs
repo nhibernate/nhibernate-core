9664350d893fcaf654c4eb46f53df358038cdf09
Nathan Brown <nathan@nkbrown.us>
2017-12-04 18:28:06 -0700

    Add some documentation to InternalLogValues

--- src/NHibernate/Logging.cs
+++ src/NHibernate/Logging.cs
@@ -962,15 +962,37 @@ namespace NHibernate
 		private readonly string _format;
 		private readonly object[] _args;
 
+		/// <summary>
+		/// Instantiates a new instance of the <see cref="InternalLogValues"/> structure.
+		/// </summary>
+		/// <param name="format">a composite format string</param>
+		/// <param name="args">An object array that contains zero or more objects to format.  Can be <c>null</c> if there are no values to format.</param>
 		public InternalLogValues(string format, object[] args)
 		{
 			_format = format ?? "[Null]";
 			_args = args;
 		}
 
+		/// <summary>
+		/// Returns the composite format string.
+		/// </summary>
+		/// <remarks>
+		/// A composite format string consists of zero or more runs of fixed text intermixed with
+		/// one or more format items, which are indicated by an index number delimited with brackets
+		/// (for example, {0}). The index of each format item corresponds to an argument in an object
+		/// list that follows the composite format string.
+		/// </remarks>
 		public string Format => _format;
+
+		/// <summary>
+		/// An object array that contains zero or more objects to format.  Can be <c>null</c> if there are no values to format.
+		/// </summary>
 		public object[] Args => _args;
 
+		/// <summary>
+		/// Returns the string that results from formatting the composite format string along with
+		/// its arguments by using the formatting conventions of the current culture.
+		/// </summary>
 		public override string ToString()
 		{
 			return _args?.Length > 0 ? string.Format(_format, _args) : Format;
