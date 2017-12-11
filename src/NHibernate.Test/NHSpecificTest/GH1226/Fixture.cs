f6db50019fb87813edc6c298c0930b542c420820
Frédéric Delaporte <fredericdelaporte@free.fr>
2017-12-11 13:00:16 +0100

    Reformated test case, to be squashed.

--- src/NHibernate.Test/NHSpecificTest/GH1226/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/GH1226/Fixture.cs
@@ -1,79 +1,92 @@
-﻿namespace NHibernate.Test.NHSpecificTest.GH1226
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH1226
 {
-   using System;
-   using System.Collections.Generic;
-   using System.Linq;
-   using NUnit.Framework;
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			base.OnSetUp();
 
-   [TestFixture]
-   public class Fixture : BugTestCase {
-      protected override void OnSetUp() {
-         base.OnSetUp();
+			using (var session = OpenSession())
+			{
+				using (var tx = session.BeginTransaction())
+				{
+					var bank = new Bank
+					{
+						Id = Guid.NewGuid(),
+						Code = "01234"
+					};
+					session.Save(bank);
 
-         using (var session = OpenSession()) {
-            using (var tx = session.BeginTransaction()) {
-               Bank bank = new Bank();
-					bank.Id = Guid.NewGuid();
-			   bank.Code = "01234";
-               session.Save(bank);
+					var account = new Account
+					{
+						Id = Guid.NewGuid(),
+						Bank = bank
+					};
+					session.Save(account);
 
-				Account account = new Account();
-					account.Id = Guid.NewGuid();
-					account.Bank = bank;
-               session.Save(account);
+					var account2 = new Account { Bank = bank };
+					session.Save(account2);
 
-               Account account2 = new Account();
-               account2.Bank = bank;
-               session.Save(account2);
+					tx.Commit();
+				}
+			}
+		}
 
-               tx.Commit();
-            }
-         }
-      }
+		[Test]
+		public void BankShouldBeJoinFetched()
+		{
+			using (var session = OpenSession())
+			{
+				var wasStatisticsEnabled = session.SessionFactory.Statistics.IsStatisticsEnabled;
+				session.SessionFactory.Statistics.IsStatisticsEnabled = true;
 
-      [Test]
-      public void BankShouldBeJoinFetched() {
-         using (var session = OpenSession()) {
-            bool wasStatisticsEnabled = session.SessionFactory.Statistics.IsStatisticsEnabled;
-            session.SessionFactory.Statistics.IsStatisticsEnabled = true;
+				long statementCount;
 
-            long statementCount;
+				using (var tx = session.BeginTransaction())
+				{
+					// Bug only occurs if the Banks are already in the session cache.
+					var preloadedBanks = session.CreateQuery("from Bank").List<Bank>();
 
-            using (var tx = session.BeginTransaction()) {
-               // Bug only occurs if the Banks are already in the session cache.
-				IList<Bank> preloadedBanks = session.CreateQuery("from Bank").List<Bank>();
+					var countBeforeQuery = session.SessionFactory.Statistics.PrepareStatementCount;
 
-               long countBeforeQuery = session.SessionFactory.Statistics.PrepareStatementCount;
+					Console.WriteLine("Query: -------------------------------------------------------");
 
-               Console.WriteLine("Query: -------------------------------------------------------");
+					var accounts = session.CreateQuery("from Account a left join fetch a.Bank").List<Account>();
+					IList<Bank> associatedBanks = accounts.Select(x => x.Bank).ToList();
 
-               IList<Account> accounts = session.CreateQuery("from Account a left join fetch a.Bank").List<Account>();
-               IList<Bank> associatedBanks = accounts.Select(x => x.Bank).ToList();
+					var countAfterQuery = session.SessionFactory.Statistics.PrepareStatementCount;
+					statementCount = countAfterQuery - countBeforeQuery;
 
-               long countAfterQuery = session.SessionFactory.Statistics.PrepareStatementCount;
-               statementCount = countAfterQuery - countBeforeQuery;
+					Console.WriteLine("End ----------------------------------------------------------");
 
-               Console.WriteLine("End ----------------------------------------------------------");
+					tx.Commit();
+				}
 
-               tx.Commit();
-            }
+				session.SessionFactory.Statistics.IsStatisticsEnabled = wasStatisticsEnabled;
 
-            session.SessionFactory.Statistics.IsStatisticsEnabled = wasStatisticsEnabled;
+				Assert.That(statementCount, Is.EqualTo(1));
+			}
+		}
 
-            Assert.AreEqual(1, statementCount);
-         }
-      }
+		protected override void OnTearDown()
+		{
+			base.OnTearDown();
 
-      protected override void OnTearDown() {
-         base.OnTearDown();
-
-         using (var session = OpenSession()) {
-            using (var tx = session.BeginTransaction()) {
-               session.Delete("from Account");
-               session.Delete("from Bank");
-               tx.Commit();
-            }
-         }
-      }
-   }
+			using (var session = OpenSession())
+			{
+				using (var tx = session.BeginTransaction())
+				{
+					session.Delete("from Account");
+					session.Delete("from Bank");
+					tx.Commit();
+				}
+			}
+		}
+	}
 }
