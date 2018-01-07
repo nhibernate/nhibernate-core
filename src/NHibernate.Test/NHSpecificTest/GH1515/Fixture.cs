1f0627a3b61ae762f3f67f9aab4ab30791baf11f
Dominic Ullmann <dominic.ullmann@elca.ch>
2018-01-07 11:51:07 +0100

    Make NHibernateUtil.Initialize / IsInitialized better reusable for sub-projects like Envers: - added unit tests

---
+++ src/NHibernate.Test/NHSpecificTest/GH1515/Fixture.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NHibernate.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH1515
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+
+		[Test]
+		public void LazyCollectionCanBeInitialized()
+		{
+			var collection = new LayzInitializationTestCollection(false);
+			Assert.That(!NHibernateUtil.IsInitialized(collection));
+			NHibernateUtil.Initialize(collection);
+			Assert.That(NHibernateUtil.IsInitialized(collection));
+		}
+
+		[Test]
+		public void PersistentCollectionCanBeInitialized()
+		{
+			var collection = new PersistentLayzInitializationTestCollection(false);
+			Assert.That(!NHibernateUtil.IsInitialized(collection));
+			NHibernateUtil.Initialize(collection);
+			Assert.That(NHibernateUtil.IsInitialized(collection));
+		}
+
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/GH1515/LayzInitializationTestCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NHibernate.Collection;
+
+namespace NHibernate.Test.NHSpecificTest.GH1515
+{
+	class LayzInitializationTestCollection : ILazyInitializedCollection
+	{
+
+		private bool isInitialized;
+
+		public LayzInitializationTestCollection(bool isInitialized)
+		{
+			this.isInitialized = isInitialized;
+		}
+
+		public Task ForceInitializationAsync(CancellationToken cancellationToken)
+		{
+			return Task.Run(() => ForceInitialization(), cancellationToken);
+		}
+
+		public bool WasInitialized { get { return isInitialized;  } }
+		public void ForceInitialization()
+		{
+			isInitialized = true;
+		}
+	}
+}
