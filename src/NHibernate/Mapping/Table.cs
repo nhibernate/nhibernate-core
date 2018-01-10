2108e49267e40a088c13fb5ed18efad3b4089a97
Roman Artiukhin <bahusdrive@gmail.com>
2018-01-10 19:57:03 +0530

    Removed tableCounter with tests

--- src/NHibernate/Mapping/Table.cs
+++ src/NHibernate/Mapping/Table.cs
@@ -26,9 +26,6 @@ namespace NHibernate.Mapping
 	[Serializable]
 	public class Table : IRelationalModel
 	{
-		[ThreadStatic]
-		private static int tableCounter;
-
 		private readonly List<string> checkConstraints = new List<string>();
 		private readonly LinkedHashMap<string, Column> columns = new LinkedHashMap<string, Column>();
 		private readonly Dictionary<ForeignKeyKey, ForeignKey> foreignKeys = new Dictionary<ForeignKeyKey, ForeignKey>();
@@ -52,7 +49,6 @@ namespace NHibernate.Mapping
 		/// </summary>
 		public Table()
 		{
-			uniqueInteger = tableCounter++;
 		}
 
 		public Table(string name) : this()
