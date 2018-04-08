6e8f1907cba792f75971a30a45e8e5fe09b8ce0d
Alexander Zaytsev <hazzik@gmail.com>
2018-04-08 12:22:54 +1200

    Fix TypeLoadException in StaticProxyFactory when class has not visible interfaces
    Fixes #1643

---
+++ src/NHibernate.Test/StaticProxyTest/StaticProxyFactoryFixture.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using NHibernate.Proxy;
+using NUnit.Framework;
+
+namespace NHibernate.Test.StaticProxyTest
+{
+	public class StaticProxyFactoryFixture
+	{
+		internal interface ISomething
+		{
+			int Id { get; }
+		}
+
+		public class TestClass : ISomething
+		{
+			public virtual int Id { get; set; }
+		}
+
+		[Test]
+		public void CanCreateProxyForClassWithInternalInterface()
+		{
+			var factory = new StaticProxyFactory();
+			factory.PostInstantiate(typeof(TestClass).FullName, typeof(TestClass), new HashSet<System.Type> {typeof(INHibernateProxy)}, null, null, null);
+			var proxy = factory.GetProxy(1, null);
+			Assert.That(proxy, Is.Not.Null);
+		}
+	}
+}
