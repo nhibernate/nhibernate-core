d190bc1d246698db609ca726e5cc45653d96da3a
Jeroen Weelink <github@jeroenweelink.com>
2018-04-25 22:06:34 +0200

    Set the output dir to ../build so that the build scripts can find them

--- doc/make.bat
+++ doc/make.bat
@@ -7,7 +7,7 @@ REM Command file for Sphinx documentation
 set PATH=%PATH%;..\Tools\python;..\Tools\python\Scripts
 set SPHINXBUILD=python -m sphinx
 set SOURCEDIR=reference
-set BUILDDIR=build
+set BUILDDIR=..\build
 set SPHINXPROJ=NHibernate
 
 if "%1" == "" goto help
