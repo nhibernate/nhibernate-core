6eb56387a170e5d63d87964fa71bda81fe417e93
Roman Artiukhin <bahusdrive@gmail.com>
2018-05-04 23:28:46 +0300

    Fixed CriteriaImpl.Clone for Entity Join

--- src/NHibernate/Impl/CriteriaImpl.cs
+++ src/NHibernate/Impl/CriteriaImpl.cs
@@ -594,7 +594,7 @@ namespace NHibernate.Impl
 						"Could not find parent for subcriteria in the previous subcriteria. If you see this error, it is a bug");
 				}
 				Subcriteria clonedSubCriteria =
-					new Subcriteria(clone, currentParent, subcriteria.Path, subcriteria.Alias, subcriteria.JoinType, subcriteria.WithClause);
+					new Subcriteria(clone, currentParent, subcriteria.Path, subcriteria.Alias, subcriteria.JoinType, subcriteria.WithClause, subcriteria.JoinEntityName);
 				clonedSubCriteria.SetLockMode(subcriteria.LockMode);
 				newParents[subcriteria] = clonedSubCriteria;
 			}
