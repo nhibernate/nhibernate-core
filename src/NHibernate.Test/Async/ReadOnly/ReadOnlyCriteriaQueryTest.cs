5c3033a1e5d4a39d2d343d79e641bc571ef28ab3
Roman Artiukhin <bahusdrive@gmail.com>
2018-05-16 00:31:29 +0300

    Fix CriteriaImpl.Clone for readonly query (#1696)

--- src/NHibernate.Test/Async/ReadOnly/ReadOnlyCriteriaQueryTest.cs
+++ src/NHibernate.Test/Async/ReadOnly/ReadOnlyCriteriaQueryTest.cs
@@ -187,6 +187,55 @@ namespace NHibernate.Test.ReadOnly
 		}
 		
 		[Test]
+		public async Task ModifiableSessionReadOnlyClonedCriteriaAsync()
+		{
+			await (DefaultTestSetupAsync());
+
+			using (ISession s = OpenSession())
+			using (ITransaction t = s.BeginTransaction())
+			{
+				ICriteria criteria = (ICriteria) s.CreateCriteria<Student>().SetReadOnly(true).Clone();
+				Assert.That(s.DefaultReadOnly, Is.False);
+				Assert.That(criteria.IsReadOnlyInitialized, Is.True, "Cloned criteria must have IsReadOnlyInitialized == true");
+				Assert.That(criteria.IsReadOnly, Is.True, "Cloned criteria must be readonly");
+				
+				Student gavin = await (criteria.UniqueResultAsync<Student>());
+				Assert.That(s.DefaultReadOnly, Is.False);
+				Assert.That(criteria.IsReadOnlyInitialized, Is.True);
+				Assert.That(criteria.IsReadOnly, Is.True);
+				Assert.That(s.IsReadOnly(gavin), Is.True);
+				Assert.That(NHibernateUtil.IsInitialized(gavin.PreferredCourse), Is.False);
+				await (CheckProxyReadOnlyAsync(s, gavin.PreferredCourse, true));
+				Assert.That(NHibernateUtil.IsInitialized(gavin.PreferredCourse), Is.False);
+
+				await (NHibernateUtil.InitializeAsync(gavin.PreferredCourse));
+				Assert.That(NHibernateUtil.IsInitialized(gavin.PreferredCourse), Is.True);
+				await (CheckProxyReadOnlyAsync(s, gavin.PreferredCourse, true));
+				Assert.That(NHibernateUtil.IsInitialized(gavin.Enrolments), Is.False);
+
+				await (NHibernateUtil.InitializeAsync(gavin.Enrolments));
+				Assert.That(NHibernateUtil.IsInitialized(gavin.Enrolments), Is.True);
+				Assert.That(gavin.Enrolments.Count, Is.EqualTo(1));
+				IEnumerator<Enrolment> enrolments = gavin.Enrolments.GetEnumerator();
+				enrolments.MoveNext();
+				Enrolment enrolment = enrolments.Current;
+				Assert.That(s.IsReadOnly(enrolment), Is.False);
+				Assert.That(NHibernateUtil.IsInitialized(enrolment.Course), Is.False);
+				await (CheckProxyReadOnlyAsync(s, enrolment.Course, false));
+
+				await (NHibernateUtil.InitializeAsync(enrolment.Course));
+				await (CheckProxyReadOnlyAsync(s, enrolment.Course, false));
+
+				await (s.DeleteAsync(gavin.PreferredCourse));
+				await (s.DeleteAsync(gavin));
+				await (s.DeleteAsync(enrolment.Course));
+				await (s.DeleteAsync(enrolment));
+
+				await (t.CommitAsync());
+			}
+		}
+		
+		[Test]
 		public async Task ModifiableSessionModifiableCriteriaAsync()
 		{
 			await (DefaultTestSetupAsync());
