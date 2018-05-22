ebc57f3b230b4fadc5239a6fc64510a2566dae24
Antonis Mygiakis <ador75@gmail.com>
2018-05-22 13:54:59 +0300

    Add unit test

---
+++ src/NHibernate.Test/NHSpecificTest/GH1704/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.GH1704
+{
+	class Entity
+	{
+		public virtual Guid Id { get; set; }
+		public virtual string Country { get; set; }
+		public virtual string City { get; set; }
+		public virtual decimal Budget { get; set; }
+	}
+
+	class GroupByEntity
+	{
+        public GroupByEntity(string country)
+        {
+            Country = country;
+        }
+        public virtual string Country { get; set; }
+		public virtual string City { get; set; }
+	}
+}
