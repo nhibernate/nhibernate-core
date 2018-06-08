nhibernate/nhibernate-core

commit dc71c7df5c22d82991bc1b009ecdfe2ec9902b45

NH-3670 - Dynamic component should allow generic dictionary (#305)
Fixes #755

Co-authored-by: Alexander Zaytsev <hazzik@gmail.com>

src/NHibernate/Mapping/ByCode/IPlainPropertyContainerMapper.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -40,6 +41,7 @@ namespace NHibernate.Mapping.ByCode
 		void Component<TComponent>(Expression<Func<TContainer, TComponent>> property, Action<IComponentMapper<TComponent>> mapping);
 		void Component<TComponent>(Expression<Func<TContainer, TComponent>> property);
 		void Component<TComponent>(Expression<Func<TContainer, IDictionary>> property, TComponent dynamicComponentTemplate, Action<IDynamicComponentMapper<TComponent>> mapping);
+		void Component<TComponent>(Expression<Func<TContainer, IDictionary<string, object>>> property, TComponent dynamicComponentTemplate, Action<IDynamicComponentMapper<TComponent>> mapping) where TComponent : class;
 
 		void Component<TComponent>(string notVisiblePropertyOrFieldName, Action<IComponentMapper<TComponent>> mapping);
 		void Component<TComponent>(string notVisiblePropertyOrFieldName);

doc/reference/modules/basic_mapping.xml
@@ -2081,7 +2081,8 @@
 
             <para>
                 The <literal>&lt;dynamic-component&gt;</literal> element allows an <literal>IDictionary</literal>
-                to be mapped as a component, where the property names refer to keys of the dictionary.
+                or <literal>IDictionary<string, object></literal>to be mapped as a component, where the property 
+                names refer to keys of the dictionary.
             </para>
             
         </sect2>

src/NHibernate.Test/Async/NHSpecificTest/NH1039Generic/Fixture.cs
@@ -1,0 +1,68 @@
+﻿//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by AsyncGenerator.
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+
+namespace NHibernate.Test.NHSpecificTest.NH1039Generic
+{
+	using System.Threading.Tasks;
+	[TestFixture]
+	public class FixtureAsync : BugTestCase
+	{
+		public override string BugNumber
+		{
+			get { return "NH1039Generic";  }
+		}
+
+		protected override void OnTearDown()
+		{
+			base.OnTearDown();
+			using (ISession s = OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				s.Delete("from Person");
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public async Task testAsync()
+		{
+			using (ISession s = OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				Person person = new Person("1");
+				person.Name = "John Doe";
+				var set = new HashSet<object>();
+				set.Add("555-1234");
+				set.Add("555-4321");
+				person.Properties.Add("Phones", set);
+
+				await (s.SaveAsync(person));
+				await (tx.CommitAsync());
+			}
+			using (ISession s = OpenSession())
+			using (ITransaction tx = s.BeginTransaction())
+			{
+				Person person = (Person)await (s.CreateCriteria(typeof(Person)).UniqueResultAsync());
+
+				Assert.AreEqual("1", person.ID);
+				Assert.AreEqual("John Doe", person.Name);
+				Assert.AreEqual(1, person.Properties.Count);
+				Assert.That(person.Properties["Phones"], Is.InstanceOf<ISet<object>>());
+				Assert.IsTrue(((ISet<object>) person.Properties["Phones"]).Contains("555-1234"));
+				Assert.IsTrue(((ISet<object>) person.Properties["Phones"]).Contains("555-4321"));
+			}
+		}
+	}
+}

src/NHibernate.Test/Async/NHSpecificTest/NH1796Generic/Fixture.cs
@@ -1,0 +1,76 @@
+﻿//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by AsyncGenerator.
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
+
+
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1796Generic
+{
+	using System.Threading.Tasks;
+	[TestFixture]
+	public class FixtureAsync: BugTestCase
+	{
+		[Test]
+		public async Task MergeAsync()
+		{
+			var entity = new Entity { Name = "Vinnie Luther" };
+			using (ISession s = OpenSession())
+			using (ITransaction t = s.BeginTransaction())
+			{
+				await (s.SaveAsync(entity));
+				await (t.CommitAsync());
+			}
+
+			entity.DynProps = new Dictionary<string, object>();
+			entity.DynProps["StrProp"] = "Modified";
+			using (ISession s = OpenSession())
+			using (ITransaction t = s.BeginTransaction())
+			{
+				await (s.MergeAsync(entity));
+				await (t.CommitAsync());
+			}
+
+			using (ISession s = OpenSession())
+			using (ITransaction t = s.BeginTransaction())
+			{
+				await (s.CreateQuery("delete from Entity").ExecuteUpdateAsync());
+				await (t.CommitAsync());
+			}
+		}
+
+		[Test]
+		public async Task SaveOrUpdateAsync()
+		{
+			var entity = new Entity { Name = "Vinnie Luther" };
+			using (ISession s = OpenSession())
+			using (ITransaction t = s.BeginTransaction())
+			{
+				await (s.SaveOrUpdateAsync(entity));
+				await (t.CommitAsync());
+			}
+
+			entity.DynProps = new Dictionary<string, object>();
+			entity.DynProps["StrProp"] = "Modified";
+			using (ISession s = OpenSession())
+			using (ITransaction t = s.BeginTransaction())
+			{
+				await (s.SaveOrUpdateAsync(entity));
+				await (t.CommitAsync());
+			}
+
+			using (ISession s = OpenSession())
+			using (ITransaction t = s.BeginTransaction())
+			{
+				await (s.CreateQuery("delete from Entity").ExecuteUpdateAsync());
+				await (t.CommitAsync());
+			}
+		}
+	}
+}

src/NHibernate.Test/Async/NHSpecificTest/NH2664Generic/Fixture.cs
@@ -1,0 +1,124 @@
+﻿//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by AsyncGenerator.
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
+
+
+using System.Collections;
+using System.Linq;
+using NHibernate.Linq;
+using NUnit.Framework;
+using System.Linq.Expressions;
+using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH2664Generic
+{
+	using System.Threading.Tasks;
+	[TestFixture]
+	public class FixtureAsync : TestCase
+	{
+		protected override string MappingsAssembly => "NHibernate.Test";
+
+		protected override IList Mappings => new[] {"NHSpecificTest.NH2664Generic.Mappings.hbm.xml"};
+
+		/// <summary>
+		/// push some data into the database
+		/// Really functions as a save test also 
+		/// </summary>
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var tran = session.BeginTransaction())
+			{
+				session.Save(
+					new Product
+					{
+						ProductId = "1",
+						Properties =
+						{
+							["Name"] = "First Product",
+							["Description"] = "First Description"
+						}
+					});
+
+				session.Save(
+					new Product
+					{
+						ProductId = "2",
+						Properties =
+						{
+							["Name"] = "Second Product",
+							["Description"] = "Second Description"
+						}
+					});
+
+				session.Save(
+					new Product
+					{
+						ProductId = "3",
+						Properties =
+						{
+							["Name"] = "val",
+							["Description"] = "val"
+						}
+					});
+
+				tran.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var tran = session.BeginTransaction())
+			{
+				session.CreateQuery("delete from Product").ExecuteUpdate();
+				tran.Commit();
+			}
+		}
+
+		[Test]
+		public async Task Query_DynamicComponentAsync()
+		{
+			using (var session = OpenSession())
+			{
+				var product = await ((
+					from p in session.Query<Product>()
+					where (string) p.Properties["Name"] == "First Product"
+					select p
+				).SingleAsync());
+
+				Assert.That(product, Is.Not.Null);
+				Assert.That(product.Properties["Name"], Is.EqualTo("First Product"));
+			}
+		}
+
+		[Test]
+		public async Task Multiple_Query_Does_Not_CacheAsync()
+		{
+			using (var session = OpenSession())
+			{
+				// Query by name
+				var product1 = await ((
+					from p in session.Query<Product>()
+					where (string) p.Properties["Name"] == "First Product"
+					select p
+				).SingleAsync());
+				Assert.That(product1.ProductId, Is.EqualTo("1"));
+
+				// Query by description (this test is to verify that the dictionary
+				// index isn't cached from the query above.
+				var product2 = await ((
+					from p in session.Query<Product>()
+					where (string) p.Properties["Description"] == "Second Description"
+					select p
+				).SingleAsync());
+				Assert.That(product2.ProductId, Is.EqualTo("2"));
+			}
+		}
+	}
+}

src/NHibernate.Test/Async/NHSpecificTest/NH3571Generic/Fixture.cs
@@ -1,0 +1,109 @@
+﻿//------------------------------------------------------------------------------
+// <auto-generated>
+//     This code was generated by AsyncGenerator.
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
+
+
+using System.Collections;
+using System.Linq;
+using NHibernate.Linq;
+using NUnit.Framework;
+using System.Linq.Expressions;
+using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH3571Generic
+{
+	using System.Threading.Tasks;
+	[TestFixture]
+	public class FixtureAsync : TestCase
+	{
+		protected override string MappingsAssembly => "NHibernate.Test";
+
+		protected override IList Mappings => new[] {"NHSpecificTest.NH3571Generic.Mappings.hbm.xml"};
+
+		/// <summary>
+		/// push some data into the database
+		/// Really functions as a save test also 
+		/// </summary>
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var tran = session.BeginTransaction())
+			{
+				var product = new Product {ProductId = "1"};
+				product.Details.Properties["Name"] = "First Product";
+				product.Details.Properties["Description"] = "First Description";
+
+				session.Save(product);
+
+				product = new Product {ProductId = "2"};
+				product.Details.Properties["Name"] = "Second Product";
+				product.Details.Properties["Description"] = "Second Description";
+
+				session.Save(product);
+
+				product = new Product {ProductId = "3"};
+				product.Details.Properties["Name"] = "val";
+				product.Details.Properties["Description"] = "val";
+
+				session.Save(product);
+
+				tran.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var tran = session.BeginTransaction())
+			{
+				session.CreateQuery("delete from Product").ExecuteUpdate();
+				tran.Commit();
+			}
+		}
+
+		[Test]
+		public async Task CanQueryDynamicComponentInComponentAsync()
+		{
+			using (var session = OpenSession())
+			{
+				var product = await ((
+					from p in session.Query<Product>()
+					where (string) p.Details.Properties["Name"] == "First Product"
+					select p
+				).SingleAsync());
+
+				Assert.That(product, Is.Not.Null);
+				Assert.That(product.Details.Properties["Name"], Is.EqualTo("First Product"));
+			}
+		}
+
+		[Test]
+		public async Task MultipleQueriesShouldNotCacheAsync()
+		{
+			using (var session = OpenSession())
+			{
+				// Query by name
+				var product1 = await ((
+					from p in session.Query<Product>()
+					where (string) p.Details.Properties["Name"] == "First Product"
+					select p
+				).SingleAsync());
+				Assert.That(product1.ProductId, Is.EqualTo("1"));
+
+				// Query by description (this test is to verify that the dictionary
+				// index isn't cached from the query above.
+				var product2 = await ((
+					from p in session.Query<Product>()
+					where (string) p.Details.Properties["Description"] == "Second Description"
+					select p
+				).SingleAsync());
+				Assert.That(product2.ProductId, Is.EqualTo("2"));
+			}
+		}
+	}
+}

src/NHibernate.Test/MappingByCode/ExplicitMappingTests/DynamicComponentMappingTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using NHibernate.Cfg.MappingSchema;
 using NHibernate.Mapping.ByCode;
@@ -15,6 +16,17 @@ namespace NHibernate.Test.MappingByCode.ExpliticMappingTests
 			public int Id { get; set; }
 			private IDictionary info;
 			public IDictionary Info
+			{
+				get { return info; }
+				set { info = value; }
+			}
+		}
+
+		private class PersonWithGenericInfo
+		{
+			public int Id { get; set; }
+			private IDictionary<string, object> info;
+			public IDictionary<string, object> Info
 			{
 				get { return info; }
 				set { info = value; }
@@ -39,6 +51,23 @@ namespace NHibernate.Test.MappingByCode.ExpliticMappingTests
 		}
 
 		[Test]
+		public void WhenMapDynCompoThenMapItAndItsPropertiesGeneric()
+		{
+			var mapper = new ModelMapper();
+			mapper.Class<PersonWithGenericInfo>(map =>
+			{
+				map.Id(x => x.Id, idmap => { });
+				map.Component(x => x.Info, new { MyInt = 5, MyDate = DateTime.Now }, z => { });
+			});
+
+			var hbmMapping = mapper.CompileMappingFor(new[] { typeof(PersonWithGenericInfo) });
+			var hbmClass = hbmMapping.RootClasses[0];
+			var hbmDynamicComponent = hbmClass.Properties.OfType<HbmDynamicComponent>().SingleOrDefault();
+			Assert.That(hbmDynamicComponent, Is.Not.Null);
+			Assert.That(hbmDynamicComponent.Properties.Select(x=> x.Name), Is.EquivalentTo(new [] {"MyInt", "MyDate"}));
+		}
+
+		[Test]
 		public void WhenMapDynCompoPropertiesThenShouldAssignPropertyType()
 		{
 			var mapper = new ModelMapper();
@@ -55,23 +84,69 @@ namespace NHibernate.Test.MappingByCode.ExpliticMappingTests
 		}
 
 		[Test]
+		public void WhenMapDynCompoPropertiesThenShouldAssignPropertyTypeGeneric()
+		{
+			var mapper = new ModelMapper();
+			mapper.Class<PersonWithGenericInfo>(map =>
+			{
+				map.Id(x => x.Id, idmap => { });
+				map.Component(x => x.Info, new { MyInt = 5, MyDate = DateTime.Now }, z => { });
+			});
+
+			var hbmMapping = mapper.CompileMappingFor(new[] { typeof(PersonWithGenericInfo) });
+			var hbmClass = hbmMapping.RootClasses[0];
+			var hbmDynamicComponent = hbmClass.Properties.OfType<HbmDynamicComponent>().Single();
+			Assert.That(hbmDynamicComponent.Properties.OfType<HbmProperty>().All(x => !string.IsNullOrEmpty(x.type1)), Is.True);
+		}
+
+		[Test]
 		public void WhenMapDynCompoAttributesThenMapAttributes()
 		{
 			var mapper = new ModelMapper();
 			mapper.Class<Person>(map =>
 			{
 				map.Id(x => x.Id, idmap => { });
-				map.Component(x => x.Info, new { MyInt = 5}, z =>
-															 {
-																											 z.Access(Accessor.Field);
-																											 z.Insert(false);
-																											 z.Update(false);
-																											 z.Unique(true);
-																											 z.OptimisticLock(false);
-															 });
+				map.Component(
+					x => x.Info,
+					new {MyInt = 5},
+					z =>
+					{
+						z.Access(Accessor.Field);
+						z.Insert(false);
+						z.Update(false);
+						z.Unique(true);
+						z.OptimisticLock(false);
+					});
 			});
 
 			var hbmMapping = mapper.CompileMappingFor(new[] { typeof(Person) });
+			var hbmClass = hbmMapping.RootClasses[0];
+			var hbmDynamicComponent = hbmClass.Properties.OfType<HbmDynamicComponent>().SingleOrDefault();
+			Assert.That(hbmDynamicComponent.access, Does.Contain("field"));
+			Assert.That(hbmDynamicComponent.insert, Is.False);
+			Assert.That(hbmDynamicComponent.update, Is.False);
+			Assert.That(hbmDynamicComponent.optimisticlock, Is.False);
+			Assert.That(hbmDynamicComponent.unique, Is.True);
+		}
+
+		[Test]
+		public void WhenMapDynCompoAttributesThenMapAttributesGeneric()
+		{
+			var mapper = new ModelMapper();
+			mapper.Class<PersonWithGenericInfo>(map =>
+			{
+				map.Id(x => x.Id, idmap => { });
+				map.Component(x => x.Info, new { MyInt = 5 }, z =>
+				{
+					z.Access(Accessor.Field);
+					z.Insert(false);
+					z.Update(false);
+					z.Unique(true);
+					z.OptimisticLock(false);
+				});
+			});
+
+			var hbmMapping = mapper.CompileMappingFor(new[] { typeof(PersonWithGenericInfo) });
 			var hbmClass = hbmMapping.RootClasses[0];
 			var hbmDynamicComponent = hbmClass.Properties.OfType<HbmDynamicComponent>().SingleOrDefault();
 			Assert.That(hbmDynamicComponent.access, Does.Contain("field"));

src/NHibernate.Test/MappingByCode/ExplicitMappingTests/JoinGenericDynamicComponentTests.cs
@@ -1,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using NUnit.Framework;
+
+namespace NHibernate.Test.MappingByCode.ExplicitMappingTests
+{
+	[TestFixture]
+	public class JoinGenericDynamicComponentTests
+	{
+		public class MyClass
+		{
+			public virtual int Code { get; set; }
+			public virtual string JoinedName { get; set; }
+			public virtual MyOther Relation { get; set; }
+			public virtual MyOther JoinedRelation { get; set; }
+			public virtual IDictionary<string, object> Attributes { get; set; }
+			public virtual IDictionary<string, object> JoinedAttributes { get; set; }
+		}
+
+		public class MyOther
+		{
+			public int Id { get; set; }
+		}
+
+		public class MyClassMap : ClassMapping<MyClass>
+		{
+			public MyClassMap()
+			{
+				// basic table related properties
+				ManyToOne(x => x.Relation);
+				Component(p => p.Attributes,
+				          new
+				          {
+					          IsVisible = false,
+					          Hash = default(Guid),
+					          Reference = default(MyOther)
+				          },
+				          m =>
+				          {
+					          m.Property(p => p.IsVisible);
+					          m.Property(p => p.Hash);
+					          m.ManyToOne(p => p.Reference);
+				          });
+				Property(x => x.Code);
+
+				// joined table stuff
+				Join("JoinedAttributes", x =>
+				{
+					x.Property(p => p.JoinedName);
+					x.Component(p => p.JoinedAttributes,
+					            new
+					            {
+						            OtherReference = default(MyOther),
+						            Description = string.Empty,
+						            Age = 0,
+					            },
+					            m =>
+					            {
+						            m.ManyToOne(p => p.OtherReference);
+						            m.Property(p => p.Description);
+						            m.Property(p => p.Age);
+					            });
+					x.ManyToOne(p => p.JoinedRelation);
+				});
+			}
+		}
+
+		private static HbmClass CompileClassMapping()
+		{
+			var mapper = new ModelMapper();
+			mapper.AddMapping(typeof(MyClassMap));
+
+			var hbmMapping = mapper.CompileMappingFor(new[] { typeof(MyClass) });
+			var hbmClass = hbmMapping.RootClasses[0];
+
+			return hbmClass;
+		}
+
+		[Test]
+		public void WhenPropertyIsMappedOnRootThenItBelongsToRootTable()
+		{
+			// <class name="MyClass"">
+			var hbmClass = CompileClassMapping();
+			Assert.That(hbmClass, Is.Not.Null);
+
+			var rootProperties = hbmClass.Properties;
+			// <property name="Code" 
+			var hbmPropCode = rootProperties
+				.FirstOrDefault(p => p.Name == "Code");
+
+			Assert.That(hbmPropCode, Is.Not.Null);
+			Assert.That(hbmPropCode, Is.TypeOf<HbmProperty>());
+		}
+
+		[Test]
+		public void WhenDynamicComponentIsMappedOnRootThenItBelongsToRootTable()
+		{
+			// <class name="MyClass"">
+			var hbmClass = CompileClassMapping();
+			Assert.That(hbmClass, Is.Not.Null);
+
+			var rootProperties = hbmClass.Properties;
+			// <dynamic-component name="Attributes"
+			var hbmPropAttributes = rootProperties
+					.FirstOrDefault(p => p.Name == "Attributes")
+				;
+
+			Assert.That(hbmPropAttributes, Is.Not.Null);
+			Assert.That(hbmPropAttributes, Is.TypeOf<HbmDynamicComponent>());
+		}
+
+		[Test]
+		public void WhenRelationIsMappedOnRootThenItBelongsToRootTable()
+		{
+			// <class name="MyClass"">
+			var hbmClass = CompileClassMapping();
+			Assert.That(hbmClass, Is.Not.Null);
+
+			var rootProperties = hbmClass.Properties;
+			//  <many-to-one name="Relation"
+			var hbmPropRelation = rootProperties
+				.FirstOrDefault(p => p.Name == "Relation");
+
+			Assert.That(hbmPropRelation, Is.Not.Null);
+			Assert.That(hbmPropRelation, Is.TypeOf<HbmManyToOne>());
+		}
+
+		[Test]
+		public void WhenJoinedPropertyIsMappedOnJoinThenItBelongsToJoinTable()
+		{
+			// <class name="MyClass"">
+			var hbmClass = CompileClassMapping();
+			Assert.That(hbmClass, Is.Not.Null);
+
+			// <join table="JoinedAttributes">
+			var hbmJoined = hbmClass.Joins.FirstOrDefault();
+			Assert.That(hbmJoined, Is.Not.Null);
+
+			var rootProperties = hbmJoined.Properties;
+			// <join table="JoinedAttributes">
+			//   <dynamic-component name="Attributes"
+			var hbmPropJoinedName = rootProperties
+				.FirstOrDefault(p => p.Name == "JoinedName");
+
+			Assert.That(hbmPropJoinedName, Is.Not.Null);
+			Assert.That(hbmPropJoinedName, Is.TypeOf<HbmProperty>());
+		}
+
+		[Test]
+		public void WhenJoinedRelationIsMappedOnJoinThenItBelongsToJoinTable()
+		{
+			// <class name="MyClass"">
+			var hbmClass = CompileClassMapping();
+			Assert.That(hbmClass, Is.Not.Null);
+
+			// <join table="JoinedAttributes">
+			var hbmJoined = hbmClass.Joins.FirstOrDefault();
+			Assert.That(hbmJoined, Is.Not.Null);
+
+			var rootProperties = hbmJoined.Properties;
+			// <join table="JoinedAttributes">
+			//   <many-to-one name="JoinedRelation"
+			var hbmPropJoinedRelation = rootProperties
+				.FirstOrDefault(p => p.Name == "JoinedRelation");
+
+			Assert.That(hbmPropJoinedRelation, Is.Not.Null);
+			Assert.That(hbmPropJoinedRelation, Is.TypeOf<HbmManyToOne>());
+		}
+
+		[Test]
+		public void WhenJoinedDynamicComponentIsMappedOnJoinThenItBelongsToJoinTable()
+		{
+			// <class name="MyClass"">
+			var hbmClass = CompileClassMapping();
+			Assert.That(hbmClass, Is.Not.Null);
+
+			// <join table="JoinedAttributes">
+			var hbmJoined = hbmClass.Joins.FirstOrDefault();
+			Assert.That(hbmJoined, Is.Not.Null);
+
+			var rootProperties = hbmJoined.Properties;
+			// <join table="JoinedAttributes">
+			//   <dynamic-component name="JoinedAttributes">
+			var hbmPropJoinedAttributes = rootProperties
+				.FirstOrDefault(p => p.Name == "JoinedAttributes");
+
+			Assert.That(hbmPropJoinedAttributes, Is.Not.Null);
+			Assert.That(hbmPropJoinedAttributes, Is.TypeOf<HbmDynamicComponent>());
+		}
+	}
+}

commit 5e81ece0192bea0e2d27d528fe91d5fab1681e41

Remove a binary breaking change introduced in #305

src/NHibernate/Mapping/ByCode/IPlainPropertyContainerMapper.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
+using NHibernate.Mapping.ByCode.Impl.CustomizersImpl;
+using NHibernate.Util;
 
 namespace NHibernate.Mapping.ByCode
 {
@@ -41,7 +43,6 @@ namespace NHibernate.Mapping.ByCode
 		void Component<TComponent>(Expression<Func<TContainer, TComponent>> property, Action<IComponentMapper<TComponent>> mapping);
 		void Component<TComponent>(Expression<Func<TContainer, TComponent>> property);
 		void Component<TComponent>(Expression<Func<TContainer, IDictionary>> property, TComponent dynamicComponentTemplate, Action<IDynamicComponentMapper<TComponent>> mapping);
-		void Component<TComponent>(Expression<Func<TContainer, IDictionary<string, object>>> property, TComponent dynamicComponentTemplate, Action<IDynamicComponentMapper<TComponent>> mapping) where TComponent : class;
 
 		void Component<TComponent>(string notVisiblePropertyOrFieldName, Action<IComponentMapper<TComponent>> mapping);
 		void Component<TComponent>(string notVisiblePropertyOrFieldName);
@@ -55,5 +56,20 @@ namespace NHibernate.Mapping.ByCode
 	{
 		void OneToOne<TProperty>(Expression<Func<TContainer, TProperty>> property, Action<IOneToOneMapper<TProperty>> mapping) where TProperty : class;
 		void OneToOne<TProperty>(string notVisiblePropertyOrFieldName, Action<IOneToOneMapper<TProperty>> mapping) where TProperty : class;
+	}
+	
+	public static class BasePlainPropertyContainerMapperExtensions
+	{
+		//6.0 TODO: Merge into IBasePlainPropertyContainerMapper<> interface
+		public static void Component<TContainer, TComponent>(
+			this IBasePlainPropertyContainerMapper<TContainer> mapper,
+			Expression<Func<TContainer, IDictionary<string, object>>> property,
+			TComponent dynamicComponentTemplate,
+			Action<IDynamicComponentMapper<TComponent>> mapping) where TComponent : class
+		{
+			var customizer = ReflectHelper.CastOrThrow<PropertyContainerCustomizer<TContainer>>(
+				mapper, "mapping a generic <string, object> dictionary as a dynamic component");
+			customizer.Component(property, dynamicComponentTemplate, mapping);
+		}
 	}
 }