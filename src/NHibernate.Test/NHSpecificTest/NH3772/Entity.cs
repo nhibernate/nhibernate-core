0b667429315ec7979a5e3290280ea20c210df678
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2018-06-15 03:40:59 +0200

    Add check to ensure that IUserCollectionType.Instantiate returns uninitialized collection

---
+++ src/NHibernate.Test/NHSpecificTest/NH3772/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH3772
+{
+	public class Entity
+	{
+		private ICollection<SubEntity> _subEntities;
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+
+		public virtual ICollection<SubEntity> SubEntities
+		{
+			get => _subEntities ?? (_subEntities = new HashSet<SubEntity>());
+			set => _subEntities = value;
+		}
+	}
+
+	public class SubEntity
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+	}
+}
