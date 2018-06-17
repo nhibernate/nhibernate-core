d8a7c8615427e36890a8f36a7848d55d83aaa776
Frédéric Delaporte <12201973+fredericDelaporte@users.noreply.github.com>
2018-06-17 18:43:54 +0200

    Add test case for deletion of component with null property
    Demonstrate #1170 - Incorrect query when items removed from a collection
    of components contain null values

---
+++ src/NHibernate.Test/NHSpecificTest/GH1170/Fixture.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH1170
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		[Test, KnownBug("GH1170")]
+		public void DeleteComponentWithNull()
+		{
+			using (var session = OpenSession())
+			using (var tx = session.BeginTransaction())
+			{
+				var parent = session.Query<Parent>().Single();
+				Assert.That(
+					parent.ChildComponents,
+					Has.Count.EqualTo(2).And.One.Property(nameof(ChildComponent.SomeString)).Null);
+				parent.ChildComponents.Remove(parent.ChildComponents.Single(c => c.SomeString == null));
+				tx.Commit();
+			}
+
+			using (var session = OpenSession())
+			using (var tx = session.BeginTransaction())
+			{
+				var parent = session.Query<Parent>().Single();
+				Assert.That(
+					parent.ChildComponents,
+					Has.Count.EqualTo(1).And.None.Property(nameof(ChildComponent.SomeString)).Null);
+				tx.Commit();
+			}
+		}
+
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var tx = session.BeginTransaction())
+			{
+				var parent = new Parent();
+				parent.ChildComponents.Add(new ChildComponent { SomeBool = true, SomeString = "something" });
+				parent.ChildComponents.Add(new ChildComponent { SomeBool = false, SomeString = null });
+				session.Save(parent);
+
+				tx.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var tx = session.BeginTransaction())
+			{
+				session.Delete("from Parent");
+				tx.Commit();
+			}
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/GH1170/Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.GH1170
+{
+	public class Parent
+	{
+		public Parent()
+		{
+			ChildComponents = new List<ChildComponent>();
+		}
+
+		public virtual ICollection<ChildComponent> ChildComponents { get; set; }
+		public virtual Guid Id { get; set; }
+	}
+
+	public class ChildComponent
+	{
+		public virtual bool SomeBool { get; set; }
+		public virtual string SomeString { get; set; }
+	}
+}
