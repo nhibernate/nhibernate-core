70c2bab0a0e84e8c1f92186289d52b39c06bfcc2
Lillo <rafa_lillo@outlook.com>
2018-07-19 23:05:32 -0300

    Fix SchemaValidator fails for PostgreSql sequences
    Fixes #1163

---
+++ src/NHibernate.Test/NHSpecificTest/GH1163/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.GH1163
+{
+	public class Account
+	{
+		public virtual long? Id { get; set; }
+		public virtual string Login { get; set; }
+		public virtual string PasswordHash { get; set; }
+		public virtual DateTime ValidFrom { get; set; }
+		public virtual DateTime? ValidUntil { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/GH1163/Fixture.cs
@@ -0,0 +1,43 @@
+﻿using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH1163
+{
+	[TestFixture]
+	public class Fixture : TestCaseMappingByCode
+	{
+		protected override bool AppliesTo(Dialect.Dialect dialect)
+		{
+			return dialect.SupportsSequences;
+		}
+
+		[Test]
+		public void ValidateSequences()
+		{
+			var validator = new SchemaValidator(cfg);
+			validator.Validate();
+		}
+
+		protected override HbmMapping GetMappings()
+		{
+			var mapper = new ModelMapper();
+			mapper.Class<Account>(m=>
+			{
+				m.Table("account");
+				m.Id(x => x.Id, c =>
+				{
+					c.Column("account_id");
+					c.Generator(Generators.Sequence, g => g.Params(new { sequence = "account_id_gen" }));
+				});
+				m.Property(x => x.Login, c => { c.Column("login_name"); c.NotNullable(true); });
+				m.Property(x => x.PasswordHash, c => { c.Column("password_hash"); c.NotNullable(true); });
+				m.Property(x => x.ValidFrom, c => { c.Column("valid_from"); c.NotNullable(true); });
+				m.Property(x => x.ValidUntil, c => { c.Column("valid_until"); });
+			});
+
+			return mapper.CompileMappingForAllExplicitlyAddedEntities();
+		}
+	}
+}
