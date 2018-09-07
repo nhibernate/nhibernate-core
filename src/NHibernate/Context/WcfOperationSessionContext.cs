209c87d09b25197b025e1830bb8f812e445b0abd
Frédéric Delaporte <12201973+fredericDelaporte@users.noreply.github.com>
2018-09-07 11:40:04 +0200

    Remove WcfOperationSessionContext from .Net Core and .Net Standard

--- src/NHibernate/Context/WcfOperationSessionContext.cs
+++ src/NHibernate/Context/WcfOperationSessionContext.cs
@@ -1,4 +1,10 @@
-using System;
+// There is no support of WCF Server under .Net Core, so it makes little sense to provide
+// a WCF OperationContext for it. Since it adds additional heavy dependencies, it has been
+// considered not desirable to provide it for .Net Standard. (It could be useful in case some
+// WCF server becames available in another frameworks or if a .Net Framework application
+// consumes the .Net standard distribution of NHibernate instead of the .Net Framework one)
+// See https://github.com/dotnet/wcf/issues/1200 and #1842
+#if NETFX
 using System.Collections;
 using System.ServiceModel;
 
@@ -50,3 +56,36 @@ namespace NHibernate.Context
 		public void Detach(OperationContext owner) { }
 	}
 }
+#else
+// 6.0 TODO: remove the whole #else
+using System;
+using System.Collections;
+using NHibernate.Engine;
+
+namespace NHibernate.Context
+{
+	/// <summary>
+	/// Obsolete class not usable in the .Net Core and .Net Standard distributions of NHibernate. Use the
+	/// .Net Framework distribution of NHibernate if you need it. See
+	/// https://github.com/nhibernate/nhibernate-core/issues/1842
+	/// </summary>
+	[Obsolete("Not supported in the .Net Core and .Net Standard distributions of NHibernate")]
+	public class WcfOperationSessionContext : MapBasedSessionContext
+	{
+		public WcfOperationSessionContext(ISessionFactoryImplementor factory) : base(factory)
+		{
+			throw new NotSupportedException(
+				"WcfOperationSessionContext is currently supported only by the .Net Framework distribution of NHibernate");
+		}
+
+		protected override IDictionary GetMap()
+		{
+			return null;
+		}
+
+		protected override void SetMap(IDictionary value)
+		{
+		}
+	}
+}
+#endif
