b23a9be66ab708c85bc47258d7b55c3c186e4695
Frédéric Delaporte <12201973+fredericDelaporte@users.noreply.github.com>
2018-09-14 13:33:06 +0200

    Add in ByCode support of all type mappings on Id

--- src/NHibernate/Mapping/ByCode/IIdMapper.cs
+++ src/NHibernate/Mapping/ByCode/IIdMapper.cs
@@ -1,19 +1,44 @@
 using System;
+using NHibernate.Mapping.ByCode.Impl;
 using NHibernate.Type;
+using NHibernate.Util;
 
 namespace NHibernate.Mapping.ByCode
 {
+	// 6.0 TODO: move into IIdMapper,
+	// and probably add an ITypeMapper for mutualizing the four Type methods with IElementMapper and IPropertyMapper
+	// (Note that there is no IKeyPropertyMapper to be concerned with, the KeyPropertyMapper use IPropertyMapper
+	// directly instead.)
+	public static class IdMapperExtensions
+	{
+		public static void Type<TPersistentType>(this IIdMapper idMapper) where TPersistentType : IIdentifierType
+		{
+			ReflectHelper
+				.CastOrThrow<IdMapper>(idMapper, "generic Type method")
+				.Type<TPersistentType>();
+		}
+
+		public static void Type<TPersistentType>(this IIdMapper idMapper, object parameters) where TPersistentType : IIdentifierType
+		{
+			ReflectHelper
+				.CastOrThrow<IdMapper>(idMapper, "generic parameterized Type method")
+				.Type<TPersistentType>(parameters);
+		}
+
+		public static void Type(this IIdMapper idMapper, System.Type persistentType, object parameters)
+		{
+			ReflectHelper
+				.CastOrThrow<IdMapper>(idMapper, "parameterized Type method")
+				.Type(persistentType, parameters);
+		}
+	}
+	
 	public interface IIdMapper : IAccessorPropertyMapper, IColumnsMapper
 	{
 		void Generator(IGeneratorDef generator);
 		void Generator(IGeneratorDef generator, Action<IGeneratorMapper> generatorMapping);
 
 		void Type(IIdentifierType persistentType);
-		//void Type<TPersistentType>() where TPersistentType : IIdentifierType;
-		//void Type<TPersistentType>(object parameters) where TPersistentType : IIdentifierType;
-		//void Type(System.System.Type persistentType, object parameters);
-		//void Column(Action<IColumnMapper> columnMapper);
-		//void Columns(params Action<IColumnMapper>[] columnMapper);
 		void UnsavedValue(object value);
 		void Length(int length);
 	}
