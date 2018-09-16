14b4930f27af2a088b61f1b83e8452348db6a401
Frédéric Delaporte <12201973+fredericDelaporte@users.noreply.github.com>
2018-09-16 15:25:31 +0200

    Switch IParameterAdjuster to public and add some doc on it

--- src/NHibernate/AdoNet/IParameterAdjuster.cs
+++ src/NHibernate/AdoNet/IParameterAdjuster.cs
@@ -1,11 +1,22 @@
 using System.Data.Common;
+using NHibernate.Driver;
 using NHibernate.SqlTypes;
-using NHibernate.Type;
 
 namespace NHibernate.AdoNet
 {
-	internal interface IParameterAdjuster
+	/// <summary>
+	/// Supports adjusting a <see cref="DbParameter"/> according to a <see cref="SqlType"/> and
+	/// the parameter's value. An <see cref="IDriver"/> may implement this interface.
+	/// </summary>
+	public interface IParameterAdjuster
 	{
+		/// <summary>
+		/// Adjust the provided parameter according to its <paramref name="sqlType"/> and
+		/// <paramref name="value"/>.
+		/// </summary>
+		/// <param name="parameter">The parameter to adjust.</param>
+		/// <param name="sqlType">The parameter's <see cref="SqlType"/>.</param>
+		/// <param name="value">The parameter's value.</param>
 		void AdjustParameterForValue(DbParameter parameter, SqlType sqlType, object value);
 	}
 }
