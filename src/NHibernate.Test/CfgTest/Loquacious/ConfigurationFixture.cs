3dc082fc4588ff32b898e5b67deeda671ab4cfe4
InlineAsm <ivar.tiller@gmail.com>
2018-09-18 09:55:29 +0200

    Loquatious QueryCache constraint should be an IQueryCacheFactory constraint (#1849)
    The configuration setting is used to create an instance of a factory, not of the query cache itself.

--- src/NHibernate.Test/CfgTest/Loquacious/ConfigurationFixture.cs
+++ src/NHibernate.Test/CfgTest/Loquacious/ConfigurationFixture.cs
@@ -29,7 +29,7 @@ namespace NHibernate.Test.CfgTest.Loquacious
 					.Through<HashtableCacheProvider>()
 					.PrefixingRegionsWith("xyz")
 					.Queries
-						.Through<StandardQueryCache>()
+						.Through<StandardQueryCacheFactory>()
 					.UsingMinimalPuts()
 					.WithDefaultExpiration(15)
 				.GeneratingCollections
@@ -69,7 +69,7 @@ namespace NHibernate.Test.CfgTest.Loquacious
 			Assert.That(cfg.Properties[Environment.SessionFactoryName], Is.EqualTo("SomeName"));
 			Assert.That(cfg.Properties[Environment.CacheProvider], Is.EqualTo(typeof(HashtableCacheProvider).AssemblyQualifiedName));
 			Assert.That(cfg.Properties[Environment.CacheRegionPrefix], Is.EqualTo("xyz"));
-			Assert.That(cfg.Properties[Environment.QueryCacheFactory], Is.EqualTo(typeof(StandardQueryCache).AssemblyQualifiedName));
+			Assert.That(cfg.Properties[Environment.QueryCacheFactory], Is.EqualTo(typeof(StandardQueryCacheFactory).AssemblyQualifiedName));
 			Assert.That(cfg.Properties[Environment.UseMinimalPuts], Is.EqualTo("true"));
 			Assert.That(cfg.Properties[Environment.CacheDefaultExpiration], Is.EqualTo("15"));
 			Assert.That(cfg.Properties[Environment.CollectionTypeFactoryClass], Is.EqualTo(typeof(DefaultCollectionTypeFactory).AssemblyQualifiedName));
@@ -97,6 +97,12 @@ namespace NHibernate.Test.CfgTest.Loquacious
 			Assert.That(cfg.Properties[Environment.MaxFetchDepth], Is.EqualTo("11"));
 			Assert.That(cfg.Properties[Environment.QuerySubstitutions], Is.EqualTo("true 1, false 0, yes 'Y', no 'N'"));
 			Assert.That(cfg.Properties[Environment.Hbm2ddlAuto], Is.EqualTo("validate"));
+
+			// Keywords import and auto-validation require a valid connection string, disable them before checking
+			// the session factory can be built.
+			cfg.SetProperty(Environment.Hbm2ddlKeyWords, "none");
+			cfg.SetProperty(Environment.Hbm2ddlAuto, null);
+			Assert.That(() => cfg.BuildSessionFactory().Dispose(), Throws.Nothing);
 		}
 
 		[Test]
