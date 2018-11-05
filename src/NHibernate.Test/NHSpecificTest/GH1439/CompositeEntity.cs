23ccd12b10dbf25cf2b547bf2e95fd59e02bb519
Alexander Zaytsev <hazzik@gmail.com>
2018-11-06 10:54:45 +1300

    fixup! Test case for Issue #1439

---
+++ src/NHibernate.Test/NHSpecificTest/GH1439/CompositeEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.GH1439
+{
+	public class CompositeEntity : IEquatable<CompositeEntity>
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual string LazyProperty { get; set; }
+
+		public bool Equals(CompositeEntity other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return Id == other.Id && string.Equals(Name, other.Name);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as CompositeEntity);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return (Id * 397) ^ (Name != null ? Name.GetHashCode() : 0);
+			}
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/GH1439/Fixture.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH1439
+{
+	public class Fixture : TestCaseMappingByCode
+	{
+		protected override HbmMapping GetMappings()
+		{
+			var mapper = new ModelMapper();
+			mapper.Class<CompositeEntity>(rc =>
+			{
+				rc.ComposedId(
+					c =>
+					{
+						c.Property(t => t.Id);
+						c.Property(t => t.Name);
+					});
+
+				rc.Property(x => x.LazyProperty, x => x.Lazy(true));
+			});
+
+			return mapper.CompileMappingForAllExplicitlyAddedEntities();
+		}
+
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				var e1 = new CompositeEntity { Id = 1, Name = "Bob", LazyProperty = "LazyProperty"};
+				session.Save(e1);
+
+				session.Flush();
+				transaction.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				session.CreateQuery("delete from System.Object").ExecuteUpdate();
+
+				session.Flush();
+				transaction.Commit();
+			}
+		}
+
+		[Test]
+		public void LazyPropertyShouldNotBeNullified()
+		{
+			using (var session = OpenSession())
+			using (var tran = session.BeginTransaction())
+			{
+				var result = (
+					from e in session.Query<CompositeEntity>()
+					where e.Name == "Bob"
+					select e
+				).ToList();
+				session.Flush();
+				tran.Commit();
+				Assert.That(result, Has.Count.EqualTo(1));
+			}
+		}
+	}
+}
