fa978cf48838d463eeb3df428786fb54cd637317
Alexander Zaytsev <hazzik@gmail.com>
2018-11-27 09:29:43 +1300

    Improve support of Npgsql 4
    - Integer types (Int32Type, Int64Type, etc) now convert argument to an expected type
    before setting the parameter value.
    - Since Npgsql 3.2.5, system transactions support has regressed. Disable some
    of the tests that started failing with.

--- src/NHibernate.Test/TestDialects/PostgreSQL83TestDialect.cs
+++ src/NHibernate.Test/TestDialects/PostgreSQL83TestDialect.cs
@@ -2,19 +2,19 @@
 {
 	public class PostgreSQL83TestDialect : TestDialect
 	{
-        public PostgreSQL83TestDialect(Dialect.Dialect dialect)
-            : base(dialect)
-        {
-        }
+		public PostgreSQL83TestDialect(Dialect.Dialect dialect)
+			: base(dialect)
+		{
+		}
 
-        public override bool SupportsSelectForUpdateOnOuterJoin
-        {
-            get { return false; }
-        }
+		public override bool SupportsSelectForUpdateOnOuterJoin => false;
 
-        public override bool SupportsNullCharactersInUtfStrings
-        {
-            get { return false; }
-        }
+		public override bool SupportsNullCharactersInUtfStrings => false;
+
+		/// <summary>
+		/// Npgsql since its 3.2.5 version fails some tests requiring this feature. The trouble was not occuring with
+		/// Npgsql 3.2.4.1.
+		/// </summary>
+		public override bool SupportsUsingConnectionOnSystemTransactionPrepare => false;
 	}
 }
