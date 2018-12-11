36acea8d06eb81cbddcd03611d90a6407f7123f0
Frédéric Delaporte <12201973+fredericdelaporte@users.noreply.github.com>
2018-12-11 13:18:32 +0100

    Clean up some usings in files having changed them

--- src/NHibernate/Util/FilterHelper.cs
+++ src/NHibernate/Util/FilterHelper.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using NHibernate.Dialect.Function;
 using NHibernate.Engine;
 using NHibernate.Hql;
