162b0790a74d97bf90bbc4f4093a0b359a91557b
Frédéric Delaporte <12201973+fredericdelaporte@users.noreply.github.com>
2018-12-13 16:39:09 +0100

    Clean-up the test case

--- src/NHibernate.Test/NHSpecificTest/NH3079/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH3079/Fixture.cs
@@ -1,300 +1,217 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using NHibernate.Dialect;
+﻿using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
-using NHibernate.Test.NHSpecificTest;
-using Iesi.Collections.Generic;
-using NHibernate;
-using System.Data;
-using NHibernate.Criterion;
 
 namespace NHibernate.Test.NHSpecificTest.NH3079
 {
-	/// <summary>
-	/// <para>
-	/// </para>
-	/// </remarks>
 	[TestFixture]
 	public class Fixture : BugTestCase
 	{
-		protected override void Configure(NHibernate.Cfg.Configuration configuration)
-		{
-			#region removing possible second-level-cache configs
-			configuration.Properties.Remove(NHibernate.Cfg.Environment.CacheProvider);
-			configuration.Properties.Remove(NHibernate.Cfg.Environment.UseQueryCache);
-			configuration.Properties.Add(NHibernate.Cfg.Environment.UseQueryCache, "true");
-			configuration.Properties.Remove(NHibernate.Cfg.Environment.UseSecondLevelCache); 
-			#endregion
-
-			base.Configure(configuration);
-		}
+		// Disable second level cache
+		protected override string CacheConcurrencyStrategy => null;
 
 		protected override void OnTearDown()
 		{
-			base.OnTearDown();
-			using (ISession s = OpenSession())
+			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
 			{
-				int countUpdate = 0;
+				s.CreateQuery("delete from Employment").ExecuteUpdate();
 
-				countUpdate =
-					s
-						.CreateSQLQuery("DELETE FROM T_EMPLOYMENT")
-						.ExecuteUpdate();
-								Assert.AreEqual(4, countUpdate);
+				s.CreateQuery("delete from System.Object").ExecuteUpdate();
 
-				countUpdate =
-					s
-						.CreateSQLQuery("DELETE FROM T_PERSON")
-						.ExecuteUpdate();
-				Assert.AreEqual(3, countUpdate);
-
-				countUpdate =
-					s
-						.CreateSQLQuery("DELETE FROM T_EMPLOYER")
-						.ExecuteUpdate();
-				Assert.AreEqual(3, countUpdate);
-
-				countUpdate =
-					s
-						.CreateSQLQuery("DELETE FROM T_PERSON_NO_COMP")
-						.ExecuteUpdate();
-				Assert.AreEqual(3, countUpdate);
-
-				s.Flush();
+				t.Commit();
 			}
 		}
 
 		protected override void OnSetUp()
 		{
-			base.OnSetUp();
-
-			using (ISession session = this.OpenSession())
-			using (ITransaction tx = session.BeginTransaction())
+			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
 			{
-				List<Person> personList = new List<Person>();
-				List<Employer> employerList = new List<Employer>();
-				
-				//Global id to avoid false positive assertion with positional parameter
-				int gId = 1;
+				var personList = new List<Person>();
+				var employerList = new List<Employer>();
 
-				//BEGIN: Person count: 3
-				for (int i = 0; i < 3; i++)
+				// Global id to avoid false positive assertion with positional parameter
+				var gId = 1;
+
+				for (var i = 0; i < 3; i++)
 				{
-					PersonCpId personCpId = new PersonCpId() { IdA = gId++, IdB = gId++ };
-					Person personObj = new Person() { CpId = personCpId, Name = "PERSON_" + personCpId.IdA + "_" + personCpId.IdB };
-					session.Save(personObj);
-					session.Flush();
+					var personCpId = new PersonCpId { IdA = gId++, IdB = gId++ };
+					var personObj = new Person
+						{ CpId = personCpId, Name = "PERSON_" + personCpId.IdA + "_" + personCpId.IdB };
+					s.Save(personObj);
 					personList.Add(personObj);
 				}
-				//END: Person count: 3
 
-				//BEGIN: Employer count: 3
-				for (int i = 0; i < 3; i++)
+				for (var i = 0; i < 3; i++)
 				{
-					EmployerCpId employerCpId = new EmployerCpId() { IdA = gId++, IdB = gId++ };
-					Employer employerObj = new Employer() { CpId = employerCpId, Name = "EMPLOYER_" + employerCpId.IdA + "_" + employerCpId.IdB };
-					session.Save(employerObj);
-					session.Flush();
+					var employerCpId = new EmployerCpId { IdA = gId++, IdB = gId++ };
+					var employerObj = new Employer
+						{ CpId = employerCpId, Name = "EMPLOYER_" + employerCpId.IdA + "_" + employerCpId.IdB };
+					s.Save(employerObj);
 					employerList.Add(employerObj);
 				}
-				//END: Employer count: 3
-				//BEGIN: employment count: 4
-				int[] employmentIdAraay = new int[] { 
-					gId++,                    
+
+				var employmentIds = new[]
+				{
+					gId++,
 					gId++,
 					gId++,
 					gId++,
 				};
-				string[] employmentNameArray = new string[]
+				var employmentNames = new[]
 				{
 					//P1 + E1
-					"EMPLOYMENT_" + employmentIdAraay[0] + "_" + 
-						personList[0].CpId.IdA + "_" + personList[0].CpId.IdA + "_" + 
-						employerList[0].CpId.IdA + "_" + employerList[0].CpId.IdB,
+					"EMPLOYMENT_" + employmentIds[0] + "_" +
+					personList[0].CpId.IdA + "_" + personList[0].CpId.IdA + "_" +
+					employerList[0].CpId.IdA + "_" + employerList[0].CpId.IdB,
 					//P1 + E2
-					"EMPLOYMENT_" + employmentIdAraay[1] + "_" + 
-						personList[0].CpId.IdA + "_" + personList[0].CpId.IdA + "_" + 
-						employerList[1].CpId.IdA + "_" + employerList[1].CpId.IdB,
+					"EMPLOYMENT_" + employmentIds[1] + "_" +
+					personList[0].CpId.IdA + "_" + personList[0].CpId.IdA + "_" +
+					employerList[1].CpId.IdA + "_" + employerList[1].CpId.IdB,
 					//P2 + E2
-					"EMPLOYMENT_" + employmentIdAraay[2] + "_" + 
-						personList[1].CpId.IdA + "_" + personList[1].CpId.IdA + "_" + 
-						employerList[1].CpId.IdA + "_" + employerList[1].CpId.IdB,
+					"EMPLOYMENT_" + employmentIds[2] + "_" +
+					personList[1].CpId.IdA + "_" + personList[1].CpId.IdA + "_" +
+					employerList[1].CpId.IdA + "_" + employerList[1].CpId.IdB,
 					//P2 + E3
-					"EMPLOYMENT_" + employmentIdAraay[2] + "_" + 
-						personList[1].CpId.IdA + "_" + personList[1].CpId.IdA + "_" + 
-						employerList[2].CpId.IdA + "_" + employerList[2].CpId.IdB
+					"EMPLOYMENT_" + employmentIds[2] + "_" +
+					personList[1].CpId.IdA + "_" + personList[1].CpId.IdA + "_" +
+					employerList[2].CpId.IdA + "_" + employerList[2].CpId.IdB
 				};
-				Person[] employemenPersonArray = new Person[] { 
+				var employmentPersons = new[]
+				{
 					personList[0],
 					personList[0],
 					personList[1],
 					personList[1]
 				};
-				Employer[] employemenEmployerArray = new Employer[] { 
+				var employmentEmployers = new[]
+				{
 					employerList[0],
 					employerList[1],
 					employerList[1],
 					employerList[2]
 				};
-				//persinting Employment's
-				for (int k = 0; k < employmentIdAraay.Length; k++)
+
+				for (var k = 0; k < employmentIds.Length; k++)
 				{
-					EmploymentCpId employmentCpId = new EmploymentCpId() { 
-						Id = employmentIdAraay [k],
-						PersonObj = employemenPersonArray[k],
-						EmployerObj = employemenEmployerArray[k]
+					var employmentCpId = new EmploymentCpId
+					{
+						Id = employmentIds[k],
+						PersonObj = employmentPersons[k],
+						EmployerObj = employmentEmployers[k]
 					};
-					Employment employmentObj = new Employment() { CpId = employmentCpId, Name = employmentNameArray[k] };
-					session.Save(employmentObj);
-					session.Flush();
+					var employmentObj = new Employment { CpId = employmentCpId, Name = employmentNames[k] };
+					s.Save(employmentObj);
 				}
-				//END: employment count: 4
 
-				//BEGIN: PersonNoComponent count: 3
-				for (int i = 0; i < 3; i++)
+				for (var i = 0; i < 3; i++)
 				{
-					PersonNoComponent personNoComponentObj = new PersonNoComponent() { IdA = gId++, IdB = gId++};
-					personNoComponentObj.Name = "PERSON_NO_COMPONENT_" + personNoComponentObj.IdA + "_" + personNoComponentObj.IdB;
-					session.Save(personNoComponentObj);
-					session.Flush();
+					var personNoComponentObj = new PersonNoComponent { IdA = gId++, IdB = gId++ };
+					personNoComponentObj.Name = "PERSON_NO_COMPONENT_" + personNoComponentObj.IdA + "_" +
+						personNoComponentObj.IdB;
+					s.Save(personNoComponentObj);
 				}
-				//END: PersonNoComponent count: 3
 
-				tx.Commit();
+				t.Commit();
 			}
 		}
 
-		protected override bool AppliesTo(global::NHibernate.Dialect.Dialect dialect)
-		{
-			//return dialect as MsSql2005Dialect != null;
-			return base.AppliesTo(dialect);
-		}
-
-		/// <summary>
-		/// Test that reproduces the problem.
-		/// </summary>
+		// Test reproducing the problem.
 		[Test]
 		public void GetPersonTest()
 		{
-			using (ISession session = this.OpenSession())
+			using (var session = OpenSession())
 			{
-				Person person1_2 = session.Get<Person>(new PersonCpId() { IdA = 1, IdB = 2 });
-				Assert.AreEqual("PERSON_1_2", person1_2.Name);
-				String[] expetedArray = new String[] { "EMPLOYMENT_13_1_1_7_8", "EMPLOYMENT_14_1_1_9_10" };
-				for (int i = 0; i < person1_2.EmploymentList.Count; i++)
-				{
-					Assert.AreEqual(expetedArray[i], new List<Employment>(person1_2.EmploymentList)[i].Name);
-				}
+				var person1_2 = session.Get<Person>(new PersonCpId { IdA = 1, IdB = 2 });
+				Assert.That(person1_2.Name, Is.EqualTo("PERSON_1_2"));
+				Assert.That(
+					person1_2.EmploymentList.Select(e => e.Name),
+					Is.EquivalentTo(new[] { "EMPLOYMENT_13_1_1_7_8", "EMPLOYMENT_14_1_1_9_10" }));
 			}
 		}
 
-		/// <summary>
-		/// Test that reproduces the problem.
-		/// </summary>
+		// Test reproducing the problem.
 		[Test]
 		public void GetEmployerTest()
 		{
-			using (ISession session = this.OpenSession())
+			using (var session = OpenSession())
 			{
-				Employer employer7_8 = session.Get<Employer>(new EmployerCpId() { IdA = 7, IdB = 8 });
-
-				Assert.AreEqual("EMPLOYER_7_8", employer7_8.Name);
-				String[] expetedArray = new String[] { "EMPLOYMENT_13_1_1_7_8", "EMPLOYMENT_13_1_1_7_8" };
-				for (int i = 0; i < employer7_8.EmploymentList.Count; i++)
-				{
-					Assert.AreEqual(expetedArray[i], new List<Employment>(employer7_8.EmploymentList)[i].Name);
-				}
-			}
-
-			using (ISession session = this.OpenSession())
-			{
-				Employer employer7_8 = session.Get<Employer>(new EmployerCpId() { IdA = 7, IdB = 8 });
-
-				Assert.AreEqual("EMPLOYER_7_8", employer7_8.Name);
-				String[] expetedArray = new String[] { "EMPLOYMENT_13_1_1_7_8", "EMPLOYMENT_13_1_1_7_8" };
-				for (int i = 0; i < employer7_8.EmploymentList.Count; i++)
-				{
-					Assert.AreEqual(expetedArray[i], new List<Employment>(employer7_8.EmploymentList)[i].Name);
-				}
+				var employer7_8 = session.Get<Employer>(new EmployerCpId { IdA = 7, IdB = 8 });
+				Assert.That(employer7_8.Name, Is.EqualTo("EMPLOYER_7_8"));
+				Assert.That(
+					employer7_8.EmploymentList.Select(e => e.Name),
+					Is.EquivalentTo(new[] { "EMPLOYMENT_13_1_1_7_8" }));
 			}
 		}
 
 		[Test]
 		public void GetEmploymentTest()
 		{
-			using (ISession session = this.OpenSession())
+			using (var session = OpenSession())
 			{
-				Employment employment_13_1_2_7_8 =
+				var employment_13_1_2_7_8 =
 					session.Get<Employment>(
-						new EmploymentCpId()
+						new EmploymentCpId
 						{
 							Id = 13,
 							PersonObj =
-								new Person()
+								new Person
 								{
-									CpId = new PersonCpId() { IdA = 1, IdB = 2 }
+									CpId = new PersonCpId { IdA = 1, IdB = 2 }
 								},
 							EmployerObj =
-								new Employer()
+								new Employer
 								{
-									CpId = new EmployerCpId() { IdA = 7, IdB = 8 }
+									CpId = new EmployerCpId { IdA = 7, IdB = 8 }
 								}
 						});
-				Assert.AreEqual("EMPLOYMENT_13_1_1_7_8", employment_13_1_2_7_8.Name);
+				Assert.That(employment_13_1_2_7_8.Name, Is.EqualTo("EMPLOYMENT_13_1_1_7_8"));
 			}
 		}
 
-		/// <summary>
-		/// Test that reproduces the problem.
-		/// </summary>
 		[Test]
-		public void HQLpersonPositional()
+		public void HqlPersonPositional()
 		{
-			using (ISession session = this.OpenSession())
+			using (var session = OpenSession())
 			{
-				IList<Person> personList =
+				var personList =
 					session
 						.GetNamedQuery("personPositional")
-						.SetParameter<PersonCpId>(0, new PersonCpId() { IdA = 1, IdB = 2 })
-						.SetParameter<PersonCpId>(1, new PersonCpId() { IdA = 3, IdB = 4 })
+						.SetParameter(0, new PersonCpId { IdA = 1, IdB = 2 })
+						.SetParameter(1, new PersonCpId { IdA = 3, IdB = 4 })
 						.List<Person>();
-				String[] expetedArray = new String[] { "PERSON_1_2", "PERSON_3_4" };
-				for (int i = 0; i < personList.Count; i++)
-				{
-					Assert.AreEqual(expetedArray[i], personList[i].Name);
-				}
+				Assert.That(
+					personList.Select(e => e.Name),
+					Is.EquivalentTo(new[] { "PERSON_1_2", "PERSON_3_4" }));
 			}
 		}
 
 		[Test]
-		public void HQLpersonNamed()
+		public void HqlPersonNamed()
 		{
-			using (ISession session = this.OpenSession())
+			using (var session = OpenSession())
 			{
-				IList<Person> personList =
+				var personList =
 					session
 						.GetNamedQuery("personNamed")
-						.SetParameter<PersonCpId>("id1", new PersonCpId() { IdA = 1, IdB = 2 })
-						.SetParameter<PersonCpId>("id2", new PersonCpId() { IdA = 3, IdB = 4 })
+						.SetParameter("id1", new PersonCpId { IdA = 1, IdB = 2 })
+						.SetParameter("id2", new PersonCpId { IdA = 3, IdB = 4 })
 						.List<Person>();
-				String[] expetedArray = new String[] { "PERSON_1_2", "PERSON_3_4" };
-				for (int i = 0; i < personList.Count; i++)
-				{
-					Assert.AreEqual(expetedArray[i], personList[i].Name);
-				}
+				Assert.That(
+					personList.Select(e => e.Name),
+					Is.EquivalentTo(new[] { "PERSON_1_2", "PERSON_3_4" }));
 			}
 		}
-
 
 		[Test]
 		public void GetPersonNoComponent()
 		{
-			using (ISession session = this.OpenSession())
+			using (var session = OpenSession())
 			{
-				PersonNoComponent person17_18 = session.Get<PersonNoComponent>(new PersonNoComponent() { IdA = 17, IdB = 18 });
-				Assert.AreEqual("PERSON_NO_COMPONENT_17_18", person17_18.Name);
+				var person17_18 =
+					session.Get<PersonNoComponent>(new PersonNoComponent { IdA = 17, IdB = 18 });
+				Assert.That(person17_18.Name, Is.EqualTo("PERSON_NO_COMPONENT_17_18"));
 			}
 		}
 	}
