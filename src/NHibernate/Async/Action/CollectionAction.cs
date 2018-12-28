df84e6285c0d23681c21fc7e0ee838a68039f2b7
Alexander Zaytsev <hazzik@gmail.com>
2018-12-29 12:43:43 +1300

    Delay entity insert on Persist until session is flushed
    Co-authored-by: Frédéric Delaporte <12201973+fredericdelaporte@users.noreply.github.com>

--- src/NHibernate/Async/Action/CollectionAction.cs
+++ src/NHibernate/Async/Action/CollectionAction.cs
@@ -25,6 +25,25 @@ namespace NHibernate.Action
 	public abstract partial class CollectionAction : IAsyncExecutable, IComparable<CollectionAction>, IDeserializationCallback, IAfterTransactionCompletionProcess
 	{
 
+		protected async Task<object> GetKeyAsync(CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (key is DelayedPostInsertIdentifier)
+			{
+				// need to look it up
+				var finalKey = await (persister.CollectionType.GetKeyOfOwnerAsync(collection.Owner, session, cancellationToken)).ConfigureAwait(false);
+				if (finalKey == key)
+				{
+					// we may be screwed here since the collection action is about to execute
+					// and we do not know the final owner key value
+				}
+
+				return finalKey;
+			}
+
+			return key;
+		}
+
 		#region IExecutable Members
 
 		/// <summary> Called before executing any actions</summary>
