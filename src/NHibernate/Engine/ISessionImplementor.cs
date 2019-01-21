8d9d34b640d3b4180cfca41e2e9917c41c80c452
Roman Artiukhin <bahusdrive@gmail.com>
2019-01-21 10:33:57 +0200

    Make BeginProcess/BeginContext optimizations work

--- src/NHibernate/Engine/ISessionImplementor.cs
+++ src/NHibernate/Engine/ISessionImplementor.cs
@@ -25,17 +25,25 @@ namespace NHibernate.Engine
 		{
 			if (session == null)
 				return null;
-			return (session as AbstractSessionImpl)?.BeginContext() ?? SessionIdLoggingContext.CreateOrNull(session.SessionId);
+			if (session is AbstractSessionImpl sessionImpl)
+			{
+				return sessionImpl.BeginContext();
+			}
+			return SessionIdLoggingContext.CreateOrNull(session.SessionId);
 		}
 
 		internal static IDisposable BeginProcess(this ISessionImplementor session)
 		{
 			if (session == null)
 				return null;
-			return (session as AbstractSessionImpl)?.BeginProcess() ??
-				// This method has only replaced bare call to setting the id, so this fallback is enough for avoiding a
-				// breaking change in case a custom session implementation is used.
-				SessionIdLoggingContext.CreateOrNull(session.SessionId);
+			if(session is AbstractSessionImpl sessionImpl)
+			{
+				return sessionImpl.BeginProcess();
+			}
+
+			// This method has only replaced bare call to setting the id, so this fallback is enough for avoiding a
+			// breaking change in case a custom session implementation is used.
+			return SessionIdLoggingContext.CreateOrNull(session.SessionId);
 		}
 
 		//6.0 TODO: Expose as ISessionImplementor.FutureBatch and replace method usages with property
