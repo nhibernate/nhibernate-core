44a5e028437950a431ae2f3481e70929c7b66da0
Roman Artiukhin <bahusdrive@gmail.com>
2019-02-21 15:42:12 +0200

    Restore AliasToBeanReaultTransfomer

---
+++ src/NHibernate.Test/TransformTests/AliasToBeanResultTransformerFixture.cs
@@ -0,0 +1,14 @@
+﻿using NHibernate.Transform;
+using NUnit.Framework;
+
+namespace NHibernate.Test.TransformTests
+{
+	[TestFixture]
+	public class AliasToBeanResultTransformerFixture : AliasToBeanFixtureBase
+	{
+		protected override IResultTransformer GetTransformer<T>()
+		{
+			return Transformers.AliasToBean<T>();
+		}
+	}
+}
