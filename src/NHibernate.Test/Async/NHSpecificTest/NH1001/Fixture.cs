15d8f8afd2751cf6fe344d8b8ed2419800d8161a
Alexander Zaytsev <hazzik@gmail.com>
2019-03-07 01:27:06 +1300

    Revert unrelated changes

--- src/NHibernate.Test/Async/NHSpecificTest/NH1001/Fixture.cs
+++ src/NHibernate.Test/Async/NHSpecificTest/NH1001/Fixture.cs
@@ -8,9 +8,10 @@
 //------------------------------------------------------------------------------
 
 
+using System.Linq;
 using NHibernate.Cfg;
+using NHibernate.Linq;
 using NUnit.Framework;
-using NHibernate.Stat;
 
 namespace NHibernate.Test.NHSpecificTest.NH1001
 {
@@ -23,53 +24,297 @@ namespace NHibernate.Test.NHSpecificTest.NH1001
 			cfg.SetProperty(Environment.GenerateStatistics, "true");
 		}
 
+		private int employeeId;
 
-		[Test]
-		[Ignore("To be fixed")]
-		public async Task TestAsync()
+		protected override void OnSetUp()
 		{
-			int employeeId;
-			using (ISession sess = OpenSession())
-			using (ITransaction tx = sess.BeginTransaction())
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
 			{
-				Department dept = new Department();
-				dept.Id = 11;
-				dept.Name = "Animal Testing";
+				var dept = new Department
+				{
+					Id = 11,
+					Name = "Animal Testing"
+				};
 
-				await (sess.SaveAsync(dept));
+				session.Save(dept);
 
-				Employee emp = new Employee();
-				emp.Id = 1;
-				emp.FirstName = "John";
-				emp.LastName = "Doe";
-				emp.Department1 = dept;
+				var dept2 = new Department
+				{
+					Id = 12,
+					Name = "HR"
+				};
 
-				await (sess.SaveAsync(emp));
+				session.Save(dept2);
 
-				await (tx.CommitAsync());
+				var dept3 = new Department
+				{
+					Id = 13,
+					Name = "Develop"
+				};
+
+				session.Save(dept3);
+
+				var address = new Address
+				{
+					Id = 15,
+					Line = "Test"
+				};
+
+				session.Save(address);
+
+				var phone1 = new Phone
+				{
+					Id = 20, 
+					Number = "123456"
+				};
+
+				session.Save(phone1);
+				
+				var phone2 = new Phone
+				{
+					Id = 21, 
+					Number = "7891011"
+				};
+
+				session.Save(phone2);
+
+				var emp = new Employee
+				{
+					Id = 1,
+					FirstName = "John",
+					LastName = "Doe",
+					Department1 = dept,
+					Department2 = dept2,
+					Department3 = dept3,
+					Address = address
+				};
+
+				emp.Phones.Add(phone1);
+				emp.Phones.Add(phone2);
+
+				session.Save(emp);
+
+				transaction.Commit();
 
 				employeeId = emp.Id;
 			}
+		}
 
-			ExecuteStatement(string.Format("UPDATE EMPLOYEES SET DEPARTMENT_ID = 99999 WHERE EMPLOYEE_ID = {0}", employeeId));
+		protected override void OnTearDown()
+		{
+			ExecuteStatement($"UPDATE EMPLOYEES SET DEPARTMENT_ID_1 = 11, DEPARTMENT_ID_2 = 12, DEPARTMENT_ID_3 = 13, ADDRESS_ID = 15 WHERE EMPLOYEE_ID = {employeeId}");
 
-			IStatistics stat = Sfi.Statistics;
-			stat.Clear();
-			using (ISession sess = OpenSession())
-			using (ITransaction tx = sess.BeginTransaction())
+			using (var session = OpenSession())
+			using (var tx = session.BeginTransaction())
 			{
-				await (sess.GetAsync<Employee>(employeeId));
-
-				Assert.AreEqual(1, stat.PrepareStatementCount);
-				await (tx.CommitAsync());
+				session.Delete("from Department");
+				session.Delete("from AddressEmployee");
+				session.Delete("from Phone");
+				session.Delete("from Employee");
+				tx.Commit();
 			}
+		}
 
-			using (ISession sess = OpenSession())
-			using (ITransaction tx = sess.BeginTransaction())
+		[Test]
+		public async Task DepartmentIsNotNullAsync()
+		{
+			ExecuteStatement($"UPDATE EMPLOYEES SET DEPARTMENT_ID_1 = 11, DEPARTMENT_ID_2 = 12, DEPARTMENT_ID_3 = 13, ADDRESS_ID = 15 WHERE EMPLOYEE_ID = {employeeId}");
+
+			var statistics = Sfi.Statistics;
+			statistics.Clear();
+
+			using (var session = OpenSession())
 			{
-				await (sess.DeleteAsync("from Employee"));
-				await (sess.DeleteAsync("from Department"));
-				await (tx.CommitAsync());
+				var employee = await (session.GetAsync<Employee>(employeeId));
+				Assert.That(employee.Department1, Is.Not.Null);
+				Assert.That(employee.Department2, Is.Not.Null);
+				Assert.That(employee.Department3, Is.Not.Null);
+				Assert.That(employee.Address, Is.Not.Null);
+				Assert.That(statistics.PrepareStatementCount, Is.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public async Task Department1IsNullAsync()
+		{
+			ExecuteStatement($"UPDATE EMPLOYEES SET DEPARTMENT_ID_1 = 99999, DEPARTMENT_ID_2 = 12, DEPARTMENT_ID_3 = 13, ADDRESS_ID = 15 WHERE EMPLOYEE_ID = {employeeId}");
+
+			var statistics = Sfi.Statistics;
+			statistics.Clear();
+
+			using (var session = OpenSession())
+			{
+				var employee = await (session.GetAsync<Employee>(employeeId));
+				Assert.That(employee.Department1, Is.Null);
+				Assert.That(employee.Department2, Is.Not.Null);
+				Assert.That(employee.Department3, Is.Not.Null);
+				Assert.That(employee.Address, Is.Not.Null);
+				Assert.That(employee.Phones, Is.Not.Null);
+				Assert.That(statistics.PrepareStatementCount, Is.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public async Task Department2IsNullAsync()
+		{
+			ExecuteStatement($"UPDATE EMPLOYEES SET DEPARTMENT_ID_1 = 11, DEPARTMENT_ID_2 = 99999, DEPARTMENT_ID_3 = 13, ADDRESS_ID = 15 WHERE EMPLOYEE_ID = {employeeId}");
+
+			var statistics = Sfi.Statistics;
+			statistics.Clear();
+
+			using (var session = OpenSession())
+			{
+				var employee = await (session.GetAsync<Employee>(employeeId));
+				Assert.That(employee.Department1, Is.Not.Null);
+				Assert.That(employee.Department2, Is.Null);
+				Assert.That(employee.Department3, Is.Not.Null);
+				Assert.That(employee.Address, Is.Not.Null);
+				Assert.That(statistics.PrepareStatementCount, Is.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public async Task Department3IsNullAsync()
+		{
+			ExecuteStatement($"UPDATE EMPLOYEES SET DEPARTMENT_ID_1 = 11, DEPARTMENT_ID_2 = 12, DEPARTMENT_ID_3 = NULL, ADDRESS_ID = 15 WHERE EMPLOYEE_ID = {employeeId}");
+
+			var statistics = Sfi.Statistics;
+			statistics.Clear();
+
+			using (var session = OpenSession())
+			{
+				var employee = await (session.GetAsync<Employee>(employeeId));
+				Assert.That(employee.Department1, Is.Not.Null);
+				Assert.That(employee.Department2, Is.Not.Null);
+				Assert.That(employee.Department3, Is.Null);
+				Assert.That(employee.Address, Is.Not.Null);
+				Assert.That(statistics.PrepareStatementCount, Is.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public void Department3IsNotFoundAsync()
+		{
+			ExecuteStatement($"UPDATE EMPLOYEES SET DEPARTMENT_ID_1 = 11, DEPARTMENT_ID_2 = 12, DEPARTMENT_ID_3 = 99999, ADDRESS_ID = 15 WHERE EMPLOYEE_ID = {employeeId}");
+
+			var statistics = Sfi.Statistics;
+			statistics.Clear();
+
+			using (var session = OpenSession())
+			{
+				Assert.That(() => session.GetAsync<Employee>(employeeId), Throws.InstanceOf<ObjectNotFoundException>());
+			}
+		}
+
+		[Test]
+		public async Task AddressNullAsync()
+		{
+			ExecuteStatement($"UPDATE EMPLOYEES SET DEPARTMENT_ID_1 = 11, DEPARTMENT_ID_2 = 12, DEPARTMENT_ID_3 = 13, ADDRESS_ID = 99999 WHERE EMPLOYEE_ID = {employeeId}");
+
+			var statistics = Sfi.Statistics;
+			statistics.Clear();
+
+			using (var session = OpenSession())
+			{
+				var employee = await (session.GetAsync<Employee>(employeeId));
+				Assert.That(employee.Department1, Is.Not.Null);
+				Assert.That(employee.Department2, Is.Not.Null);
+				Assert.That(employee.Department3, Is.Not.Null);
+				Assert.That(employee.Address, Is.Null);
+				Assert.That(statistics.PrepareStatementCount, Is.EqualTo(1));
+			}
+		}
+
+		[Test]
+		public async Task PhoneIsNullAsync()
+		{
+			ExecuteStatement($"UPDATE EMPLOYEES SET DEPARTMENT_ID_1 = 11, DEPARTMENT_ID_2 = 12, DEPARTMENT_ID_3 = 13, ADDRESS_ID = 15 WHERE EMPLOYEE_ID = {employeeId}");
+			ExecuteStatement($"UPDATE PHONES SET EMPLOYEE_ID = NULL");
+
+			var statistics = Sfi.Statistics;
+			statistics.Clear();
+
+			using (var session = OpenSession())
+			{
+				var employee = await (session.GetAsync<Employee>(employeeId));
+				Assert.That(employee.Department1, Is.Not.Null);
+				Assert.That(employee.Department2, Is.Not.Null);
+				Assert.That(employee.Department3, Is.Not.Null);
+				Assert.That(employee.Address, Is.Not.Null);
+				Assert.That(employee.Phones, Is.Not.Null);
+				Assert.That(employee.Phones.Count, Is.Zero);
+				Assert.That(statistics.PrepareStatementCount, Is.EqualTo(1));
+			}
+		}
+		
+		[Test]
+		public async Task Department2IsNull_QueryOverAsync()
+		{
+			ExecuteStatement($"UPDATE EMPLOYEES SET DEPARTMENT_ID_1 = 11, DEPARTMENT_ID_2 = 99999, DEPARTMENT_ID_3 = 13, ADDRESS_ID = 15 WHERE EMPLOYEE_ID = {employeeId}");
+
+			var statistics = Sfi.Statistics;
+			statistics.Clear();
+
+			using (var session = OpenSession())
+			{
+				var employee = await (session.QueryOver<Employee>()
+					.Fetch(SelectMode.Fetch, e => e.Department2)
+					.Fetch(SelectMode.Skip, e => e.Department1, e => e.Department3, e => e.Address)
+					.Where(e => e.Id == employeeId)
+					.SingleOrDefaultAsync());
+				Assert.That(employee.Department2, Is.Null);
+				Assert.That(statistics.PrepareStatementCount, Is.EqualTo(4), "Employee, Department1, Department3, and Address");
+
+				Assert.That(employee.Department1, Is.Not.Null);
+				Assert.That(employee.Department3, Is.Not.Null);
+				Assert.That(employee.Address, Is.Not.Null);
+			}
+		}
+
+		[Test]
+		public async Task Department2IsNull_LinqAsync()
+		{
+			ExecuteStatement($"UPDATE EMPLOYEES SET DEPARTMENT_ID_1 = 11, DEPARTMENT_ID_2 = 99999, DEPARTMENT_ID_3 = 13, ADDRESS_ID = 15 WHERE EMPLOYEE_ID = {employeeId}");
+
+			var statistics = Sfi.Statistics;
+			statistics.Clear();
+
+			using (var session = OpenSession())
+			{
+				//NOTE: HQL and Linq ignore fetch="join" in mapping.
+				var employee = await (session.Query<Employee>().Fetch(x => x.Department2).SingleAsync());
+				Assert.That(statistics.PrepareStatementCount, Is.EqualTo(5), "Employee, Department1, Department3, Address, and Phones");
+				Assert.That(employee.Department2, Is.Null);
+				Assert.That(statistics.PrepareStatementCount, Is.EqualTo(5), "Employee, Department1, Department3, Address, and Phones");
+
+				Assert.That(employee.Department1, Is.Not.Null);
+				Assert.That(employee.Department3, Is.Not.Null);
+				Assert.That(employee.Address, Is.Not.Null);
+			}
+		}
+
+		[Test]
+		public async Task Department2IsNullSkipPhones_QueryOverAsync()
+		{
+			ExecuteStatement($"UPDATE EMPLOYEES SET DEPARTMENT_ID_1 = 11, DEPARTMENT_ID_2 = 99999, DEPARTMENT_ID_3 = 13, ADDRESS_ID = 15 WHERE EMPLOYEE_ID = {employeeId}");
+
+			var statistics = Sfi.Statistics;
+			statistics.Clear();
+
+			using (var session = OpenSession())
+			{
+				var employee = await (session.QueryOver<Employee>()
+					.Fetch(SelectMode.Fetch, e => e.Department2)
+					.Fetch(SelectMode.Skip, e => e.Department1, e => e.Department3, e => e.Address, e => e.Phones)
+					.Where(e => e.Id == employeeId)
+					.SingleOrDefaultAsync());
+				Assert.That(employee.Department2, Is.Null);
+				Assert.That(statistics.PrepareStatementCount, Is.EqualTo(5), "Employee, Department1, Department3, Address, and Phones");
+
+				Assert.That(employee.Department1, Is.Not.Null);
+				Assert.That(employee.Department3, Is.Not.Null);
+				Assert.That(employee.Address, Is.Not.Null);
 			}
 		}
 	}
