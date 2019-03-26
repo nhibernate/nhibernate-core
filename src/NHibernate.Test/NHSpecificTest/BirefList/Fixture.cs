edf090001b78e5e0093e4d4378e6af8fb23239e4
Roger Kratz <roger.kratz@gmail.com>
2019-03-26 13:58:21 +0100

    Failing test

---
+++ src/NHibernate.Test/NHSpecificTest/BirefList/Fixture.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.BirefList
+{
+	public class Fixture : BugTestCase
+	{
+		private Parent _parent;
+
+		protected override void OnSetUp()
+		{
+			using (var s = OpenSession())
+			{
+				using (var tx = s.BeginTransaction())
+				{
+					_parent = new Parent();
+					_parent.AddChild(new Child());
+					s.Save(_parent);
+					tx.Commit();
+				}
+			}
+		}
+		
+		[Test]
+		public virtual void CanAddChild()
+		{
+			var newChild = new Child();
+			_parent.AddChild(newChild);
+			using (var s = OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+				s.Merge(newChild);
+				tx.Commit();
+			}
+
+			using (var s = OpenSession())
+			{
+				Assert.That(s.Get<Parent>(_parent.Id).Children.Count, Is.EqualTo(2));
+			}
+		}
+
+
+		protected override void OnTearDown()
+		{
+			using (var s = OpenSession())
+			{
+				using (var tx = s.BeginTransaction())
+				{
+					s.Delete(s.Load<Parent>(_parent.Id));
+					tx.Commit();
+				}
+			}
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/BirefList/Model.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.BirefList
+{
+	public class Parent
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual IList<Child> Children { get; protected set; } = new List<Child>();
+
+		public virtual void AddChild(Child child)
+		{
+			Children.Add(child);
+			child.Parent = this;
+		}
+		
+		public override bool Equals(object obj)
+		{
+			return obj is Parent other && Id == other.Id;
+		}
+
+		public override int GetHashCode()
+		{
+			return 0;
+		}
+	}
+
+	public class Child
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual Parent Parent { get; set; }
+		
+		public virtual int OrderIndex
+		{
+			get
+			{
+				if (Parent == null)
+					return -2;
+				return Parent.Children.IndexOf(this);
+			}
+		}
+		
+		public override bool Equals(object obj)
+		{
+			return obj is Child other && Id == other.Id;
+		}
+
+		public override int GetHashCode()
+		{
+			return 0;
+		}
+	}
+}
