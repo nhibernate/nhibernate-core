9f2b4a55380b8b0ef0f0846aa6c0868386bd41f0
Roman Artiukhin <bahusdrive@gmail.com>
2019-04-10 20:55:48 +0300

    Restore logger configuration

--- src/NHibernate.Test/TestsContext.cs
+++ src/NHibernate.Test/TestsContext.cs
@@ -3,6 +3,7 @@
 #if NETCOREAPP2_0
 using System.Configuration;
 using System.IO;
+using log4net.Repository.Hierarchy;
 using NHibernate.Cfg;
 using NHibernate.Cfg.ConfigurationSchema;
 #endif
@@ -27,6 +28,26 @@ namespace NHibernate.Test
 			{
 				Settings.ConfigurationManager = new NetCoreConfigurationManager();
 			}
+
+			ConfigureLog4Net();
+		}
+
+		private static void ConfigureLog4Net()
+		{
+			var hierarchy = (Hierarchy)log4net.LogManager.GetRepository(typeof(TestsContext).Assembly);
+
+			var consoleAppender = new log4net.Appender.ConsoleAppender
+			{
+				Layout = new log4net.Layout.PatternLayout("%d{ABSOLUTE} %-5p %c{1}:%L - %m%n"),
+			};
+
+			((Logger)hierarchy.GetLogger("NHibernate.Hql.Ast.ANTLR")).Level = log4net.Core.Level.Off;
+			((Logger)hierarchy.GetLogger("NHibernate.SQL")).Level = log4net.Core.Level.Off;
+			((Logger)hierarchy.GetLogger("NHibernate.AdoNet.AbstractBatcher")).Level = log4net.Core.Level.Off;
+			((Logger)hierarchy.GetLogger("NHibernate.Tool.hbm2ddl.SchemaExport")).Level = log4net.Core.Level.Error;
+			hierarchy.Root.Level = log4net.Core.Level.Warn;
+			hierarchy.Root.AddAppender(consoleAppender);
+			hierarchy.Configured = true;
 		}
 
 		class NetCoreConfigurationManager : IConfigurationManager
