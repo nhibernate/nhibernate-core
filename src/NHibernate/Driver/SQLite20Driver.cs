c8c55120f0c99a1b4bfdc522f5b2b99474bc7d5e
Alexander Zaytsev <hazzik@gmail.com>
2019-04-15 20:30:14 +1200

    fixup! NH-3724 Added support for notification handlers (InfoMessage event in most ADO.NET drivers)
    Fix whitespaces

--- src/NHibernate/Driver/SQLite20Driver.cs
+++ src/NHibernate/Driver/SQLite20Driver.cs
@@ -3,37 +3,37 @@ using System.Data.Common;
 
 namespace NHibernate.Driver
 {
-    /// <summary>
-    /// NHibernate driver for the System.Data.SQLite data provider for .NET.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// In order to use this driver you must have the System.Data.SQLite.dll assembly available
-    /// for NHibernate to load. This assembly includes the SQLite.dll or SQLite3.dll libraries.
-    /// </para>
-    /// <para>
-    /// You can get the System.Data.SQLite.dll assembly from
+	/// <summary>
+	/// NHibernate driver for the System.Data.SQLite data provider for .NET.
+	/// </summary>
+	/// <remarks>
+	/// <para>
+	/// In order to use this driver you must have the System.Data.SQLite.dll assembly available
+	/// for NHibernate to load. This assembly includes the SQLite.dll or SQLite3.dll libraries.
+	/// </para>
+	/// <para>
+	/// You can get the System.Data.SQLite.dll assembly from
 	/// <a href="https://system.data.sqlite.org/">https://system.data.sqlite.org/</a>
-    /// </para>
-    /// <para>
-    /// Please check <a href="https://www.sqlite.org/">https://www.sqlite.org/</a> for more information regarding SQLite.
-    /// </para>
-    /// </remarks>
-    public class SQLite20Driver : ReflectionBasedDriver
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="SQLite20Driver"/>.
-        /// </summary>
-        /// <exception cref="HibernateException">
-        /// Thrown when the <c>SQLite.NET</c> assembly can not be loaded.
-        /// </exception>
-        public SQLite20Driver() : base(
-            "System.Data.SQLite",
-            "System.Data.SQLite",
-            "System.Data.SQLite.SQLiteConnection",
-            "System.Data.SQLite.SQLiteCommand")
-        {
-        }
+	/// </para>
+	/// <para>
+	/// Please check <a href="https://www.sqlite.org/">https://www.sqlite.org/</a> for more information regarding SQLite.
+	/// </para>
+	/// </remarks>
+	public class SQLite20Driver : ReflectionBasedDriver
+	{
+		/// <summary>
+		/// Initializes a new instance of <see cref="SQLite20Driver"/>.
+		/// </summary>
+		/// <exception cref="HibernateException">
+		/// Thrown when the <c>SQLite.NET</c> assembly can not be loaded.
+		/// </exception>
+		public SQLite20Driver() : base(
+			"System.Data.SQLite",
+			"System.Data.SQLite",
+			"System.Data.SQLite.SQLiteConnection",
+			"System.Data.SQLite.SQLiteCommand")
+		{
+		}
 
 		public override DbConnection CreateConnection()
 		{
@@ -57,24 +57,22 @@ namespace NHibernate.Driver
 			}
 		}
 
-        
+		public override IResultSetsCommand GetResultSetsCommand(Engine.ISessionImplementor session)
+		{
+			return new BasicResultSetsCommand(session);
+		}
 
-        public override IResultSetsCommand GetResultSetsCommand(Engine.ISessionImplementor session)
-        {
-            return new BasicResultSetsCommand(session);
-        }
-
-        public override bool UseNamedPrefixInSql => true;
+		public override bool UseNamedPrefixInSql => true;
 
 		public override bool UseNamedPrefixInParameter => true;
 
 		public override string NamedPrefix => "@";
 
 		public override bool SupportsMultipleOpenReaders => false;
-        
-            public override bool SupportsMultipleQueries => true;
-        
-    public override bool SupportsNullEnlistment => false;
+
+		public override bool SupportsMultipleQueries => true;
+
+		public override bool SupportsNullEnlistment => false;
 
 		public override bool HasDelayedDistributedTransactionCompletion => true;
 	}
