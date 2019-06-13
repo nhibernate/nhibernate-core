f026d161d9f81b7bfcf8f4efc88c4ab4263cbac9
Roman Artiukhin <bahusdrive@gmail.com>
2019-06-13 07:38:22 +0300

    Adjust comment

--- src/NHibernate/Hql/Ast/ANTLR/Tree/EntityJoinJoinSequenceImpl.cs
+++ src/NHibernate/Hql/Ast/ANTLR/Tree/EntityJoinJoinSequenceImpl.cs
@@ -31,12 +31,14 @@ namespace NHibernate.Hql.Ast.ANTLR.Tree
 			var joinFragment = new ANSIJoinFragment();
 
 			var on = withClauseFragment ?? SqlString.Empty;
-			//Note: filters logic commented as GetOnCondition always returns empty string for entity join (as IsReferenceToPrimaryKey is always true)
-			//TODO: If filters for entity join really make sense we need to inline GetOnCondition part that retrieves filters
+			//Note: filters logic commented due to following issues
+			//1) Original code is non functional as SqlString is immutable and so all Append results are lost. Correct code would look like: on = on.Append(filters);
+			//2) Also it seems GetOnCondition always returns empty string for entity join (as IsReferenceToPrimaryKey is always true).
+			//   So if filters for entity join really make sense we need to inline GetOnCondition part that retrieves filters
 //			var filters = _entityType.GetOnCondition(_tableAlias, Factory, enabledFilters);
 //			if (!string.IsNullOrEmpty(filters))
 //			{
-//				on = on.Append(filters);
+//				on.Append(" and ").Append(filters);
 //			}
 			joinFragment.AddJoin(_tableName, _tableAlias, Array.Empty<string>(), Array.Empty<string>(), _joinType, on);
 			return joinFragment;
