90ea46768ce5a3748005a64dc6b97f5f3c89f643
Alexander Zaytsev <hazzik@gmail.com>
2019-06-17 17:52:27 +1200

    Make the test green again.

--- src/NHibernate.Test/NHSpecificTest/NH2919/Child.cs
+++ src/NHibernate.Test/NHSpecificTest/NH2919/Child.cs
@@ -9,7 +9,7 @@ namespace NHibernate.Test.NHSpecificTest.NH2919
 		public Child()
 		{
 			if ( CREATE_WITH_TOY )
-				_dynamicToy = new Toy();
+				DynamicToy = new Toy();
 		}
 
 		public virtual Guid ID { get; set; }
