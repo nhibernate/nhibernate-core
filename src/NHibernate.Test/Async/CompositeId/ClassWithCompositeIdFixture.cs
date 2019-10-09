e4d3c9610a520ebc0004500a47543a790d1e103a
Roman Artiukhin <bahusdrive@gmail.com>
2019-10-09 05:45:51 +0300

    Fix Order By for composite property projection in Criteria (#2160)
    Fixes #1103

--- src/NHibernate.Test/Async/CompositeId/ClassWithCompositeIdFixture.cs
+++ src/NHibernate.Test/Async/CompositeId/ClassWithCompositeIdFixture.cs
@@ -367,5 +367,35 @@ namespace NHibernate.Test.CompositeId
 				Assert.That(results.Count, Is.EqualTo(2));
 			}
 		}
+
+		//NH-2926 (GH-1103)
+		[Test]
+		public async Task QueryOverOrderByAndWhereWithIdProjectionDoesntThrowAsync()
+		{
+			// insert the new objects
+			using (ISession s = OpenSession())
+			using (ITransaction t = s.BeginTransaction())
+			{
+				ClassWithCompositeId theClass = new ClassWithCompositeId(id);
+				theClass.OneProperty = 5;
+
+				ClassWithCompositeId theSecondClass = new ClassWithCompositeId(secondId);
+				theSecondClass.OneProperty = 10;
+
+				await (s.SaveAsync(theClass));
+				await (s.SaveAsync(theSecondClass));
+
+				await (t.CommitAsync());
+			}
+
+			using (ISession s = OpenSession())
+			{
+				var results = await (s.QueryOver<ClassWithCompositeId>()
+								.Select(Projections.Id())
+								.Where(Restrictions.Eq(Projections.Id(), id))
+								.OrderBy(Projections.Id()).Desc.ListAsync<Id>());
+				Assert.That(results.Count, Is.EqualTo(1));
+			}
+		}
 	}
 }
