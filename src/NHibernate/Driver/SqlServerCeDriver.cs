2f99152ca6bbdd6a5e9219750ad53759e825d062
Alexander Zaytsev <hazzik@gmail.com>
2019-10-15 21:46:20 +1300

    Improve handling of SqlCeParameter.SqlDbType

--- src/NHibernate/Driver/SqlServerCeDriver.cs
+++ src/NHibernate/Driver/SqlServerCeDriver.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Reflection;
 using NHibernate.SqlTypes;
+using NHibernate.Util;
 
 namespace NHibernate.Driver
 {
@@ -12,6 +11,11 @@ namespace NHibernate.Driver
 	/// </summary>
 	public class SqlServerCeDriver : ReflectionBasedDriver
 	{
+		private static readonly Action<object, SqlDbType> SetSqlDbType =
+			DelegateHelper.BuildPropertySetter<SqlDbType>(
+				System.Type.GetType("System.Data.SqlServerCe.SqlCeParameter, System.Data.SqlServerCe", true),
+				"SqlDbType");
+
 		/// <summary>
 		/// Initializes a new instance of the <see cref="SqlServerCeDriver"/> class.
 		/// </summary>
@@ -21,19 +25,6 @@ namespace NHibernate.Driver
 				"System.Data.SqlServerCe.SqlCeConnection",
 				"System.Data.SqlServerCe.SqlCeCommand")
 		{
-		}
-
-		private PropertyInfo dbParamSqlDbTypeProperty;
-
-		public override void Configure(IDictionary<string, string> settings)
-		{
-			base.Configure(settings);
-
-			using (var cmd = CreateCommand())
-			{
-				var dbParam = cmd.CreateParameter();
-				dbParamSqlDbTypeProperty = dbParam.GetType().GetProperty("SqlDbType");
-			}
 		}
 
 		/// <summary>
@@ -109,11 +100,11 @@ namespace NHibernate.Driver
 		{
 			if (sqlType is BinaryBlobSqlType)
 			{
-				dbParamSqlDbTypeProperty.SetValue(dbParam, SqlDbType.Image, null);
+				SetSqlDbType(dbParam, SqlDbType.Image);
 			}
 			else if (sqlType is StringClobSqlType)
 			{
-				dbParamSqlDbTypeProperty.SetValue(dbParam, SqlDbType.NText, null);
+				SetSqlDbType(dbParam, SqlDbType.NText);
 			}
 		}
 
