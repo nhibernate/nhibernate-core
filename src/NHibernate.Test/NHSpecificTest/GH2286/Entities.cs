ded355859cfe05880eb7740cce886339314e034a
Roman Artiukhin <bahusdrive@gmail.com>
2019-12-17 13:49:52 +0200

    Fix subclass filter on key columns

---
+++ src/NHibernate.Test/NHSpecificTest/GH2286/Entities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.GH2286
+{
+	class Entity
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+	}
+
+	class Customer : Entity
+	{
+	}
+
+	class IndividualCustomer : Entity
+	{
+	}
+}
