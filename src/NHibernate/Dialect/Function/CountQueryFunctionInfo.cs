52110f76fe2849a202fbc03d0f1a96bf50ea5051
maca88 <bostjan.markezic@siol.net>
2020-04-14 20:24:35 +0200

    Fix build

--- src/NHibernate/Dialect/Function/CountQueryFunctionInfo.cs
+++ src/NHibernate/Dialect/Function/CountQueryFunctionInfo.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using NHibernate.Engine;
 using NHibernate.Type;
 
--- src/NHibernate/Dialect/SQLiteDialect.cs
+++ src/NHibernate/Dialect/SQLiteDialect.cs
@@ -121,7 +121,7 @@ namespace NHibernate.Dialect
 			RegisterFunction("bxor", new SQLFunctionTemplate(null, "((?1 | ?2) - (?1 & ?2))"));
 
 			// NH-3787: SQLite requires the cast in SQL too for not defaulting to string.
-			RegisterFunction("transparentcast", new SQLiteCastFunction());
+			RegisterFunction("transparentcast", new CastFunction());
 
 			if (_binaryGuid)
 				RegisterFunction("strguid", new SQLFunctionTemplate(NHibernateUtil.String, "substr(hex(?1), 7, 2) || substr(hex(?1), 5, 2) || substr(hex(?1), 3, 2) || substr(hex(?1), 1, 2) || '-' || substr(hex(?1), 11, 2) || substr(hex(?1), 9, 2) || '-' || substr(hex(?1), 15, 2) || substr(hex(?1), 13, 2) || '-' || substr(hex(?1), 17, 4) || '-' || substr(hex(?1), 21) "));
