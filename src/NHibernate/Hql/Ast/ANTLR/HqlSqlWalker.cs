7d4d72d260155e65bf066dd4d87088c20c70a0fe
Roman Artiukhin <bahusdrive@gmail.com>
2020-08-16 21:34:41 +0300

    Code review changes
    Co-authored-by: Frédéric Delaporte <12201973+fredericDelaporte@users.noreply.github.com>

--- src/NHibernate/Hql/Ast/ANTLR/HqlSqlWalker.cs
+++ src/NHibernate/Hql/Ast/ANTLR/HqlSqlWalker.cs
@@ -794,7 +794,7 @@ namespace NHibernate.Hql.Ast.ANTLR
 			if (path.Type == IDENT)
 			{
 				var pathIdentNode = (IdentNode) path;
-				//Until IDENT node is not expected for implicit join path we can throw on not found persister
+				// Since IDENT node is not expected for implicit join path, we can throw on not found persister
 				return (IQueryable) SessionFactoryHelper.RequireClassPersister(pathIdentNode.Path);
 			}
 			else if (path.Type == DOT)
