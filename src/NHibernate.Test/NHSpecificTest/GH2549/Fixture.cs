f257331fc439132b07b8172f5b39fd2712cd1bca
Roman Artiukhin <bahusdrive@gmail.com>
2020-09-17 17:33:44 +0300

    Apply filters for entity joins

---
+++ src/NHibernate.Test/NHSpecificTest/GH2549/Fixture.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH2549
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
+			{
+				s.Save(new Person {Id = 1, Name = "Name"});
+				s.Save(new Customer {Deleted = false, Name = "Name", Id = 1});
+				s.Save(new Customer {Deleted = true, Name = "Name", Id = 2});
+
+				t.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
+			{
+				s.CreateQuery("delete from System.Object").ExecuteUpdate();
+				t.Commit();
+			}
+		}
+
+		[Test]
+		public void EntityJoinFilter()
+		{
+			using (var s = OpenSession())
+			{
+				var list = (from p in s.Query<Person>()
+							join c in s.Query<Customer>() on p.Name equals c.Name
+							select p).ToList();
+
+				s.EnableFilter("DeletedCustomer").SetParameter("deleted", false);
+
+				var filteredList = (from p in s.Query<Person>()
+									join c in s.Query<Customer>() on p.Name equals c.Name
+									select p).ToList();
+
+				Assert.That(list, Has.Count.EqualTo(2));
+				Assert.That(filteredList, Has.Count.EqualTo(1));
+			}
+		}
+	}
+}
