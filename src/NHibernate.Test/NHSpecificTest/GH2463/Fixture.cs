9eaaa58229699bf28526d99a61458ef9513baef7
Roman Artiukhin <bahusdrive@gmail.com>
2020-11-09 13:12:20 +0200

    Fix missing subclass discriminator in ON clause

--- src/NHibernate.Test/NHSpecificTest/GH2463/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/GH2463/Fixture.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using NHibernate.Criterion;
 using NHibernate.DomainModel;
 using NUnit.Framework;
 
@@ -12,17 +13,57 @@ namespace NHibernate.Test.NHSpecificTest.GH2463
 			get { return new[] {"ABC.hbm.xml"}; }
 		}
 
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				var a = new A {Name = "A", AnotherName = "X"};
+				session.Save(a);
+
+				var b = new B {Name = "B", AnotherName = "X"};
+				session.Save(b);
+
+				transaction.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				session.CreateQuery("delete from System.Object").ExecuteUpdate();
+
+				transaction.Commit();
+			}
+		}
+
+		//Also see GH-2599
 		[Test]
-		public void CanJoinOnEntityWithDiscriminator()
+		public void CanJoinOnEntityWithDiscriminatorLinq()
 		{
 			using (var s = OpenSession())
 			{
-				s.Query<A>().Join(
-					s.Query<A>(),
-					a => a.Id,
-					b => b.Id,
+				var list = s.Query<A>().Join(
+					s.Query<B>(),
+					a => a.AnotherName,
+					b => b.AnotherName,
 					(a, b) =>
 						new {a, b}).ToList();
+			}
+		}
+
+		[Test]
+		public void CanJoinOnEntityWithDiscriminatorQueryOver()
+		{
+			using (var s = OpenSession())
+			{
+				A a = null;
+				B b = null;
+				var list = s.QueryOver<A>(() => a)
+							.JoinEntityAlias(() => b, () => a.AnotherName == b.AnotherName)
+							.Select((x) => a.AsEntity(), (x) => b.AsEntity()).List<object[]>();
 			}
 		}
 	}
