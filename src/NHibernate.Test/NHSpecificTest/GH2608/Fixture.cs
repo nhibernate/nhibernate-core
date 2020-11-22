fc00120714a7d7b7785c7498e2df6ba6d128ea63
Frédéric Delaporte <12201973+fredericdelaporte@users.noreply.github.com>
2020-11-22 20:17:17 +0100

    Add test for #2608

---
+++ src/NHibernate.Test/NHSpecificTest/GH2608/Fixture.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH2608
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				session.CreateQuery("delete from PersonalDetails").ExecuteUpdate();
+				session.CreateQuery("delete from System.Object").ExecuteUpdate();
+
+				transaction.Commit();
+			}
+		}
+
+		[Test]
+		public void MergeBidiPrimaryKeyOneToOne()
+		{
+			using (var s = OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+				var p = new Person { Name = "steve" };
+				p.Details = new PersonalDetails { SomePersonalDetail = "I have big feet", Person = p };
+				s.Merge(p);
+				tx.Commit();
+			}
+		}
+	}
+}
