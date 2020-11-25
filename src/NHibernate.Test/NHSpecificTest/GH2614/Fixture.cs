9b48a2e318cd9914475c1db115efc844b93c2e7c
csharper2010 <w.vogl@pharmatechnik.de>
2020-11-25 08:00:42 +0100

    Unit Test for #2614
    Polymorphic query with limit

---
+++ src/NHibernate.Test/NHSpecificTest/GH2614/Fixture.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH2614
+{
+	public class Fixture : BugTestCase
+	{
+		[Test]
+		public async Task TestOk()
+		{
+			using (ISession s = OpenSession())
+			{
+				using (ITransaction t = s.BeginTransaction())
+				{
+					try
+					{
+						{
+							s.Save(new ConcreteClass1 {Name = "C1"});
+							s.Save(new ConcreteClass2 {Name = "C2"});
+						}
+						{
+							var query = s.CreateQuery(@"
+							SELECT Name FROM NHibernate.Test.NHSpecificTest.GH2614.BaseClass ROOT");
+							query.SetMaxResults(5);
+							IList listAsync = await query.ListAsync();
+							Assert.AreEqual(2, listAsync.Count);
+							Assert.AreEqual(2, query.List().Count);
+						}
+					}
+					finally
+					{
+						s.Delete("from ConcreteClass1");
+						s.Delete("from ConcreteClass2");
+						t.Commit();
+					}
+				}
+			}
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/GH2614/Model.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.GH2614
+{
+	public abstract class BaseClass
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+	}
+
+	public class ConcreteClass1 : BaseClass
+	{
+	}
+
+	public class ConcreteClass2 : BaseClass
+	{
+	}
+}
