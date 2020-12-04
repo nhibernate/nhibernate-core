dcbf576b287fcdc583cbb8e82001cf571a34fdf8
csharper2010 <w.vogl@pharmatechnik.de>
2020-12-04 15:14:31 +0100

    One class per file
    to resolve codefactor issue

--- src/NHibernate.Test/NHSpecificTest/GH2614/BaseClass.cs
+++ src/NHibernate.Test/NHSpecificTest/GH2614/BaseClass.cs
@@ -1,18 +1,8 @@
-﻿using System.Collections.Generic;
-
 namespace NHibernate.Test.NHSpecificTest.GH2614
 {
 	public abstract class BaseClass
 	{
 		public virtual int Id { get; set; }
 		public virtual string Name { get; set; }
-	}
-
-	public class ConcreteClass1 : BaseClass
-	{
-	}
-
-	public class ConcreteClass2 : BaseClass
-	{
 	}
 }
