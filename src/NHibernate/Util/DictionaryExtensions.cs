48afee552bb53ca8915da627aa6c60a00dd8156a
maca88 <bostjan.markezic@siol.net>
2021-02-04 23:00:55 +0100

    Fix build

---
+++ src/NHibernate/Util/DictionaryExtensions.cs
@@ -0,0 +1,20 @@
+﻿#if NETSTANDARD2_0 || NETFX
+using System.Collections.Generic;
+
+namespace NHibernate.Util
+{
+	internal static class DictionaryExtensions
+	{
+		public static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+		{
+			if (dictionary.ContainsKey(key))
+			{
+				return false;
+			}
+
+			dictionary.Add(key, value);
+			return true;
+		}
+	}
+}
+#endif
