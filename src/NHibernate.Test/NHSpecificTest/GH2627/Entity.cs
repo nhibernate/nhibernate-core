a2d3122a43f310a0a040b4ac322f8841c754f0ed
Frédéric Delaporte <12201973+fredericDelaporte@users.noreply.github.com>
2021-02-07 18:40:09 +0100

    Fix NRE with merge and ignore not-found (#2657)
    Co-authored-by: Roman Artiukhin <bahusdrive@gmail.com>

---
+++ src/NHibernate.Test/NHSpecificTest/GH2627/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.GH2627
+{
+	public class Entity
+	{
+		public virtual Guid Id { get; set; }
+		public virtual string Name { get; set; }
+	}
+}
