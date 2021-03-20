47992e73ed6114093f31ed99829dd8c4c7663829
deAtog <deAtog@gmail.com>
2021-03-20 09:48:23 -0400

    Fix caching of OneToOneType from second level cache. (#2576)
    Co-authored-by: David Ellingsworth <David.Ellingsworth@davey.com>
    Co-authored-by: Roman Artiukhin <bahusdrive@gmail.com>

---
+++ src/NHibernate.Test/OneToOneType/Fixture.cs
@@ -0,0 +1,103 @@
+﻿using NHibernate.Test.NHSpecificTest;
+using NUnit.Framework;
+
+namespace NHibernate.Test.OneToOneType
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnTearDown()
+		{
+			using (var s = Sfi.OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+				s.CreateQuery("delete from Details").ExecuteUpdate();
+				s.CreateQuery("delete from Owner").ExecuteUpdate();
+
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void OneToOnePersistedOnOwnerUpdate()
+		{
+			object ownerId;
+
+			using (var s = Sfi.OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+				var owner = new Owner()
+				{
+					Name = "Owner",
+				};
+
+				ownerId = s.Save(owner);
+
+				tx.Commit();
+			}
+
+			using (var s = Sfi.OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+				Owner owner = s.Load<Owner>(ownerId);
+
+				owner.Details = new Details()
+				{
+					Data = "Owner Details"
+				};
+
+				tx.Commit();
+			}
+
+			using (var s = Sfi.OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+				Owner owner = s.Get<Owner>(ownerId);
+
+				Assert.NotNull(owner.Details);
+			}
+		}
+
+		[Test]
+		public void OneToOnePersistedOnOwnerUpdateForSessionUpdate()
+		{
+			Owner owner;
+
+			using (var s = Sfi.OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+				owner = new Owner()
+				{
+					Name = "Owner",
+				};
+
+				s.Save(owner);
+				tx.Commit();
+			}
+
+			using (var s = Sfi.OpenSession())
+			{
+				owner = s.Get<Owner>(owner.Id);
+			}
+
+			using (var s = Sfi.OpenSession())
+			using (var tx = s.BeginTransaction())
+			{
+				s.SaveOrUpdate(owner);
+				owner.Details = new Details()
+				{
+					Data = "Owner Details"
+				};
+
+				tx.Commit();
+			}
+
+			using (var s = Sfi.OpenSession())
+			{
+				owner = s.Get<Owner>(owner.Id);
+
+				Assert.IsNotNull(owner.Details);
+			}
+		}
+	}
+}
