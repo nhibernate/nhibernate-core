b1fe614ce42e3caf9464a0d86fb9545bd5f4e3c4
Roman Artiukhin <bahusdrive@gmail.com>
2021-11-25 12:32:59 +0200

    Better exception

--- src/NHibernate/Cache/SyncCacheLock.cs
+++ src/NHibernate/Cache/SyncCacheLock.cs
@@ -38,12 +38,17 @@ namespace NHibernate.Cache
 
 		public Task<IDisposable> ReadLockAsync()
 		{
-			throw new InvalidOperationException("This locker supports only sync operations.");
+			throw AsyncNotSupporteException();
 		}
 
 		public Task<IDisposable> WriteLockAsync()
 		{
-			throw new InvalidOperationException("This locker supports only sync operations.");
+			throw AsyncNotSupporteException();
+		}
+
+		private static InvalidOperationException AsyncNotSupporteException()
+		{
+			return new InvalidOperationException("This locker supports only sync operations.  Change 'cache.read_write_lock_factory' setting to `async` to support async operations.");
 		}
 	}
 }
