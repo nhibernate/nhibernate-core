5c8ad1612c3efe1164c32243c7858cdc75d7ffc5
Alex Zaytsev <hazzik@gmail.com>
2021-12-08 16:31:41 +1300

    Add runtime shim for LambdaExpression.Compile(bool)

--- src/NHibernate/Impl/LambdaExpressionExtensions.cs
+++ src/NHibernate/Impl/LambdaExpressionExtensions.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace NHibernate.Impl
 {
 #if NET461
 	internal static class LambdaExpressionExtensions
 	{
-		public static Delegate Compile(this LambdaExpression expression, bool preferInterpretation) =>
-		  expression.Compile(); //Concurrent Compile() call causes "Garbage Collector" suspend all threads too often
+		//NET4.6.1 does not have this method exposed,
+		//however it might be available in runtime
+		private static readonly MethodInfo CompileWithPreference = typeof(LambdaExpression)
+			.GetMethod("Compile", new[] { typeof(bool) });
+
+		public static Delegate Compile(this LambdaExpression expression, bool preferInterpretation)
+		{
+			if (CompileWithPreference != null)
+			{
+				return (Delegate) CompileWithPreference.Invoke(expression, new object[] { preferInterpretation });
+			}
+
+			return expression.Compile(); //Concurrent Compile() call causes "Garbage Collector" suspend all threads too often
+		}
 	}
 #endif
 }
