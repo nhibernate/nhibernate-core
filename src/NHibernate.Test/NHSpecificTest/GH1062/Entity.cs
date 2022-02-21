55f01c67266b63f30e7ded48008449ac7f443225
Roman Artiukhin <bahusdrive@gmail.com>
2022-02-21 11:30:46 +0200

    Fix dynamic insert for trigger-identity id generator (#3013)
    Fixes #1062

---
+++ src/NHibernate.Test/NHSpecificTest/GH1062/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.GH1062
+{
+	public class MyEntity
+	{
+		public virtual int Id { get; protected set; }
+
+		public virtual string Name { get; set; }
+	}
+}
