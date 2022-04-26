9f991664ad078c5af4dfe13f5b37655df66c2330
Frédéric Delaporte <12201973+fredericdelaporte@users.noreply.github.com>
2022-04-26 21:47:09 +0200

    Reuse original fixture class

--- src/NHibernate.Test/Async/NHSpecificTest/GH3046/Fixture.cs
+++ src/NHibernate.Test/Async/NHSpecificTest/GH3046/Fixture.cs
@@ -8,76 +8,14 @@
 //------------------------------------------------------------------------------
 
 
-using System.Linq;
 using NUnit.Framework;
-using NHibernate.Linq;
 
 namespace NHibernate.Test.NHSpecificTest.GH3046
 {
 	using System.Threading.Tasks;
-	// Duplicated from GH2549 with minor changes to trigger the trouble.
+	// Only the mapping needs a change for this test case. It is fetched by the fixture namespace, so it will use the mapping defined here.
 	[TestFixture]
-	public class FixtureAsync : BugTestCase
+	public class FixtureAsync : GH2549.FixtureAsync
 	{
-		protected override void OnSetUp()
-		{
-			using (var s = OpenSession())
-			using (var t = s.BeginTransaction())
-			{
-				s.Save(new Person {Id = 1, Name = "Name"});
-				s.Save(new Customer {Deleted = false, Name = "Name", Id = 1});
-				s.Save(new Customer {Deleted = true, Name = "Name", Id = 2});
-
-				t.Commit();
-			}
-		}
-
-		protected override void OnTearDown()
-		{
-			using (var s = OpenSession())
-			using (var t = s.BeginTransaction())
-			{
-				s.CreateQuery("delete from System.Object").ExecuteUpdate();
-				t.Commit();
-			}
-		}
-
-		[Test]
-		public async Task EntityJoinFilterLinqAsync()
-		{
-			using (var s = OpenSession())
-			{
-				var list = await ((from p in s.Query<Person>()
-							join c in s.Query<Customer>() on p.Name equals c.Name
-							select p).ToListAsync());
-
-				s.EnableFilter("DeletedCustomer").SetParameter("deleted", false);
-
-				var filteredList = await ((from p in s.Query<Person>()
-									join c in s.Query<Customer>() on p.Name equals c.Name
-									select p).ToListAsync());
-
-				Assert.That(list, Has.Count.EqualTo(2));
-				Assert.That(filteredList, Has.Count.EqualTo(1));
-			}
-		}
-
-		[Test]
-		public async Task EntityJoinFilterQueryOverAsync()
-		{
-			using (var s = OpenSession())
-			{
-				Customer c = null;
-				Person p = null;
-				var list = await (s.QueryOver(() => p).JoinEntityAlias(() => c, () => c.Name == p.Name).ListAsync());
-
-				s.EnableFilter("DeletedCustomer").SetParameter("deleted", false);
-
-				var filteredList = await (s.QueryOver(() => p).JoinEntityAlias(() => c, () => c.Name == p.Name).ListAsync());
-
-				Assert.That(list, Has.Count.EqualTo(2));
-				Assert.That(filteredList, Has.Count.EqualTo(1));
-			}
-		}
 	}
 }
--- src/NHibernate.Test/NHSpecificTest/GH3046/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/GH3046/Fixture.cs
@@ -1,71 +1,10 @@
-﻿using System.Linq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace NHibernate.Test.NHSpecificTest.GH3046
 {
-	// Duplicated from GH2549 with minor changes to trigger the trouble.
+	// Only the mapping needs a change for this test case. It is fetched by the fixture namespace, so it will use the mapping defined here.
 	[TestFixture]
-	public class Fixture : BugTestCase
+	public class Fixture : GH2549.Fixture
 	{
-		protected override void OnSetUp()
-		{
-			using (var s = OpenSession())
-			using (var t = s.BeginTransaction())
-			{
-				s.Save(new Person {Id = 1, Name = "Name"});
-				s.Save(new Customer {Deleted = false, Name = "Name", Id = 1});
-				s.Save(new Customer {Deleted = true, Name = "Name", Id = 2});
-
-				t.Commit();
-			}
-		}
-
-		protected override void OnTearDown()
-		{
-			using (var s = OpenSession())
-			using (var t = s.BeginTransaction())
-			{
-				s.CreateQuery("delete from System.Object").ExecuteUpdate();
-				t.Commit();
-			}
-		}
-
-		[Test]
-		public void EntityJoinFilterLinq()
-		{
-			using (var s = OpenSession())
-			{
-				var list = (from p in s.Query<Person>()
-							join c in s.Query<Customer>() on p.Name equals c.Name
-							select p).ToList();
-
-				s.EnableFilter("DeletedCustomer").SetParameter("deleted", false);
-
-				var filteredList = (from p in s.Query<Person>()
-									join c in s.Query<Customer>() on p.Name equals c.Name
-									select p).ToList();
-
-				Assert.That(list, Has.Count.EqualTo(2));
-				Assert.That(filteredList, Has.Count.EqualTo(1));
-			}
-		}
-
-		[Test]
-		public void EntityJoinFilterQueryOver()
-		{
-			using (var s = OpenSession())
-			{
-				Customer c = null;
-				Person p = null;
-				var list = s.QueryOver(() => p).JoinEntityAlias(() => c, () => c.Name == p.Name).List();
-
-				s.EnableFilter("DeletedCustomer").SetParameter("deleted", false);
-
-				var filteredList = s.QueryOver(() => p).JoinEntityAlias(() => c, () => c.Name == p.Name).List();
-
-				Assert.That(list, Has.Count.EqualTo(2));
-				Assert.That(filteredList, Has.Count.EqualTo(1));
-			}
-		}
 	}
 }
