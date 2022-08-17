67dcb7e15df7fad386a6292de0785366206e9b8e
Roman Artiukhin <bahusdrive@gmail.com>
2022-08-17 23:31:24 +0300

    Fix inner join issue when referenced in Where clause in LINQ (#3110)

--- src/NHibernate.Test/Async/Linq/ByMethod/JoinTests.cs
+++ src/NHibernate.Test/Async/Linq/ByMethod/JoinTests.cs
@@ -111,6 +111,23 @@ namespace NHibernate.Test.Linq.ByMethod
 			}
 		}
 
+		[Test(Description = "GH-3104")]
+		public async Task LeftJoinExtensionMethodWithInnerJoinAfterAsync()
+		{
+			var animals = await (db.Animals
+						   .LeftJoin(db.Mammals, o => o.Id, i => i.Id, (o, i) => new { animal = o, mammalLeft1 = i })
+						   .LeftJoin(db.Mammals, x => x.mammalLeft1.Id, y => y.Id, (o, i) => new { o.animal, o.mammalLeft1, mammalLeft2 = i })
+						   .Join(db.Mammals, o => o.mammalLeft2.Id, y => y.Id, (o, i) => new { o.animal, o.mammalLeft1, o.mammalLeft2, mammalInner = i })
+						   .Where(x => x.mammalLeft1.SerialNumber.StartsWith("9"))
+						   .Where(x => x.mammalLeft2.SerialNumber.StartsWith("9"))
+						   .Where(x => x.animal.SerialNumber.StartsWith("9"))
+						   .Where(x => x.mammalInner.SerialNumber.StartsWith("9"))
+						   .Select(x => new { SerialNumber = x.animal.SerialNumber })
+						   .ToListAsync());
+
+			Assert.That(animals.Count, Is.EqualTo(1));
+		}
+
 		[Test]
 		public async Task LeftJoinExtensionMethodWithOuterReferenceInWhereClauseOnlyAsync()
 		{
