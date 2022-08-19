8e27ffd7e8fabd2e590fde437c8b6644393a2709
Roman Artiukhin <bahusdrive@gmail.com>
2022-08-19 16:22:57 +0300

    Simplify SqlGenerator.FromFragmentSeparator (#3116)

--- src/NHibernate/Hql/Ast/ANTLR/SqlGenerator.cs
+++ src/NHibernate/Hql/Ast/ANTLR/SqlGenerator.cs
@@ -183,8 +183,7 @@ namespace NHibernate.Hql.Ast.ANTLR
 				return;
 			}
 
-			var left = (FromElement)a;
-			var right = (FromElement)next;
+			var right = (FromElement) next;
 
 			///////////////////////////////////////////////////////////////////////
 			// HACK ALERT !!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -210,34 +209,14 @@ namespace NHibernate.Hql.Ast.ANTLR
 			{
 				return;
 			}
-			if (right.Type == ENTITY_JOIN)
+
+			if (right.JoinSequence?.IsThetaStyle == false && right.JoinSequence.JoinCount != 0)
 			{
 				Out(" ");
 			}
-			else if (right.RealOrigin == left || (right.RealOrigin != null && right.RealOrigin == left.RealOrigin))
-			{
-				// right represents a joins originating from left; or
-				// both right and left reprersent joins originating from the same FromElement
-				if (right.JoinSequence != null && right.JoinSequence.IsThetaStyle)
-				{
-					Out(", ");
-				}
-				else
-				{
-					Out(" ");
-				}
-			}
 			else
 			{
-				if (right.JoinSequence?.IsThetaStyle == false && right.JoinSequence.JoinCount != 0)
-				{
-					Out(" ");
-				}
-				else
-				{
-					// these are just two unrelated table references
-					Out(", ");
-				}
+				Out(", ");
 			}
 		}
 
