c46ecea4d32d1fb226aa3fd4a6c37b9930321262
Roman Artiukhin <bahusdrive@gmail.com>
2022-08-22 17:07:14 +0300

    Test case

---
+++ src/NHibernate.Test/NHSpecificTest/GH3127/FixtureByCode.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Type;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH3127
+{
+	[TestFixture]
+	public class StringTypeAsGenericParamByCodeFixture : TestCaseMappingByCode
+	{
+		protected override HbmMapping GetMappings()
+		{
+			var mapper = new ModelMapper();
+			mapper.Class<Entity>(rc =>
+			{
+				rc.Id(x => x.Id, m => m.Generator(Generators.GuidComb));
+				rc.Property(x => x.Name, x => x.Type<StringType>());
+			});
+
+			return mapper.CompileMappingForAllExplicitlyAddedEntities();
+		}
+
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				var e1 = new Entity { Name = "Bob" };
+				session.Save(e1);
+
+				transaction.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				session.CreateQuery("delete from System.Object").ExecuteUpdate();
+
+				transaction.Commit();
+			}
+		}
+
+		[Test]
+		public void QueryWorks()
+		{
+			using (var session = OpenSession())
+			{
+				var result = from e in session.Query<Entity>()
+							 where e.Name == "Bob"
+							 select e;
+
+				Assert.That(result.ToList(), Has.Count.EqualTo(1));
+			}
+		}
+	}
+}
