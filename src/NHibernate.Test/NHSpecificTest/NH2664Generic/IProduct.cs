c12a9771432d26d5d2cdf2dac7c231ce18cf97fb
Roman Artiukhin <bahusdrive@gmail.com>
2022-09-08 19:05:23 +0300

    Test case

---
+++ src/NHibernate.Test/NHSpecificTest/NH2664Generic/IProduct.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH2664Generic
+{
+	public interface IProduct
+	{
+		string ProductId { get; set; }
+		IDictionary<string, object> Properties { get; set; }
+	}
+}
