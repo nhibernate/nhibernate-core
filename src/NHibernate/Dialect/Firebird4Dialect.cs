a8312a50b692a34120ded07f51cd966f4cdd31f1
Alex Zaytsev <hazzik@gmail.com>
2022-09-28 11:57:44 +1300

    Override current_timestamp in Firebird4

--- src/NHibernate/Dialect/Firebird4Dialect.cs
+++ src/NHibernate/Dialect/Firebird4Dialect.cs
@@ -1,6 +1,13 @@
+using NHibernate.Dialect.Function;
+
 namespace NHibernate.Dialect
 {
 	public class Firebird4Dialect: FirebirdDialect
 	{
+		protected override void RegisterFunctions()
+		{
+			base.RegisterFunctions();
+			RegisterFunction("current_timestamp", new NoArgSQLFunction("localtimestamp", NHibernateUtil.LocalDateTime, false));
+		}
 	}
 }
