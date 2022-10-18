ff6e5134ccfd395ec600d0e62603a7d2b04b5430
Rafael Lillo <rafa_lillo@outlook.com>
2022-10-18 12:58:12 +0100

    Test case for subclass column uses wrong table in generated SQL (#2242)
    See NH-3972 (#1189)

---
+++ src/NHibernate.Test/NHSpecificTest/NH3972/DataProblemState.cs
@@ -0,0 +1,14 @@
+﻿namespace NHibernate.Test.NHSpecificTest.NH3972
+{
+	public enum ProblemInternalState { New, Closed }
+
+	public class DataProblemState : DataState
+	{
+		public DataProblemState()
+		{
+			Type = DataRecordType.Problem;
+		}
+
+		public virtual ProblemInternalState State { get; set; }
+	}
+}
