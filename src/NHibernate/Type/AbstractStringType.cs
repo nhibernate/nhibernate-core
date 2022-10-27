3392254cfabe33b3fa99f2c197e69027e4fc579d
Alex Zaytsev <hazzik@gmail.com>
2022-10-27 05:02:26 +0000

    Do not call obsolete method

--- src/NHibernate/Type/AbstractStringType.cs
+++ src/NHibernate/Type/AbstractStringType.cs
@@ -58,8 +58,8 @@ namespace NHibernate.Type
 		{
 			var parameter = cmd.Parameters[index];
 
-			if (value is Enum e)
-				value = e.ToString(CultureInfo.InvariantCulture);
+			if (value is Enum)
+				value = value.ToString();
 
 			//Allow the driver to adjust the parameter for the value
 			session.Factory.ConnectionProvider.Driver.AdjustParameterForValue(parameter, SqlType, value);
