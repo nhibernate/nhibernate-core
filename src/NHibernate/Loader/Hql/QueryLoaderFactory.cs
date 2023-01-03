6ec4a4568742460062f6fc5868ea9d3b1c80a0fd
Michael Kaufmann <47595100+micmerchant@users.noreply.github.com>
2023-01-03 10:19:28 +0100

    Update src/NHibernate/Loader/Hql/QueryLoaderFactory.cs
    Co-authored-by: Frédéric Delaporte <12201973+fredericDelaporte@users.noreply.github.com>

--- src/NHibernate/Loader/Hql/QueryLoaderFactory.cs
+++ src/NHibernate/Loader/Hql/QueryLoaderFactory.cs
@@ -1,13 +1,4 @@
-﻿// Copyright (c) 2022 MOCCA Software GmbH. All rights reserved.
-// 
-// All rights are reserved. Reproduction or transmission in whole or in part,
-// any form or by any means, electronic, mechanical or otherwise, is prohibited
-// without the prior written consent of the copyright owner.
-// 
-// File Name: QueryLoaderFactory.cs
-// Created: 12/12/2022
-// Author:  Michael Kaufmann (mika)
-
+```
 using NHibernate.Engine;
 using NHibernate.Hql.Ast.ANTLR;
 using NHibernate.Hql.Ast.ANTLR.Tree;
