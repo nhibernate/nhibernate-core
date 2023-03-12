02c8e19889e6d6d37c61a41755e704e3d588c7e3
Roman Artiukhin <bahusdrive@gmail.com>
2023-03-12 12:38:10 +0200

    Fix fetching many-to-many with subclasses Test case

--- src/NHibernate.Test/NHSpecificTest/NH2174/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH2174/Fixture.cs
@@ -14,8 +14,10 @@ namespace NHibernate.Test.NHSpecificTest.NH2174
 			{
 				var doc = new Document {Id_Base = 1, Id_Doc = 2};
 				session.Save(doc);
-				session.Save(new DocumentDetailDocument {Id_Base = 1, Id_Doc = 2, Id_Item = 1, ReferencedDocument = doc});
+				var detail = new DocumentDetailDocument {Id_Base = 1, Id_Doc = 2, Id_Item = 1, ReferencedDocument = doc};
+				session.Save(detail);
 
+				doc.RefferedDetailsManyToMany.Add(detail);
 				transaction.Commit();
 			}
 		}
@@ -42,6 +44,18 @@ namespace NHibernate.Test.NHSpecificTest.NH2174
 			}
 		}
 
+		[Test(Description = "GH-3239")]
+		public void LinqFetchManyToMany()
+		{
+			using (var session = OpenSession())
+			{
+				var result = (from e in session.Query<Document>().Fetch(x => x.RefferedDetailsManyToMany)
+							select e).FirstOrDefault();
+
+				Assert.That(result.RefferedDetailsManyToMany, Has.Count.EqualTo(1));
+			}
+		}
+
 		[Test]
 		public void QueryOverFetch()
 		{
@@ -49,6 +63,16 @@ namespace NHibernate.Test.NHSpecificTest.NH2174
 			{
 				var result = session.QueryOver<Document>().Fetch(SelectMode.Fetch, x => x.RefferedDetails).SingleOrDefault();
 				Assert.That(result.RefferedDetails, Has.Count.EqualTo(1));
+			}
+		}
+
+		[Test(Description = "GH-3239")]
+		public void QueryOverFetchManyToMany()
+		{
+			using (var session = OpenSession())
+			{
+				var result = session.QueryOver<Document>().Fetch(SelectMode.Fetch, x => x.RefferedDetailsManyToMany).SingleOrDefault();
+				Assert.That(result.RefferedDetailsManyToMany, Has.Count.EqualTo(1));
 			}
 		}
 
