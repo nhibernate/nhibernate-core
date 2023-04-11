ddbfdce00dea2129cfe6788418159745561ab1c6
gliljas <gunnar.liljas@gmail.com>
2023-04-11 17:22:49 +0200

    Split file

--- src/NHibernate/Id/TableHiLoGenerator.cs
+++ src/NHibernate/Id/TableHiLoGenerator.cs
@@ -115,47 +115,4 @@ namespace NHibernate.Id
 
 		#endregion
 	}
-
-	internal class TenantStateStore<TState> where TState : new()
-	{
-		private TState _noTenantState;
-		private Action<TState> _initializer;
-		private readonly Lazy<ConcurrentDictionary<string, TState>> _tenantSpecificState = new Lazy<ConcurrentDictionary<string, TState>>(() => new ConcurrentDictionary<string, TState>());
-
-		public TenantStateStore(Action<TState> initializer)
-		{
-			_initializer = initializer;
-		}
-
-		public TenantStateStore()
-		{
-		}
-
-		internal TState LocateGenerationState(string tenantIdentifier)
-		{
-			if (tenantIdentifier == null)
-			{
-				if (_noTenantState == null)
-				{
-					_noTenantState = CreateNewState();
-				}
-				return _noTenantState;
-			}
-			else
-			{
-				return _tenantSpecificState.Value.GetOrAdd(tenantIdentifier, _ => CreateNewState());
-			}
-		}
-
-		internal TState NoTenantGenerationState => _noTenantState ??
-			throw new HibernateException("Could not locate previous generation state for no-tenant");
-
-
-		private TState CreateNewState()
-		{
-			var state = new TState();
-			_initializer?.Invoke(state);
-			return state;
-		}
-	}
 }
