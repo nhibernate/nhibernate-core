30250b972a840841519d56223941eb0dd3704f14
Alex Zaytsev <hazzik@gmail.com>
2023-05-19 22:26:56 +1000

    Add new driver for Net5.IBM.Data.Db2/Net.IBM.Data.Db2 DataProvider

---
+++ src/NHibernate/Driver/DB2NetDriver.cs
@@ -0,0 +1,12 @@
+﻿namespace NHibernate.Driver
+{
+	/// <summary>
+	/// A NHibernate Driver for using the Net5.IBM.Data.Db2/Net.IBM.Data.Db2 DataProvider.
+	/// </summary>
+	public class DB2NetDriver : DB2DriverBase
+	{
+		public DB2NetDriver() : base("IBM.Data.Db2")
+		{
+		}
+	}
+}
