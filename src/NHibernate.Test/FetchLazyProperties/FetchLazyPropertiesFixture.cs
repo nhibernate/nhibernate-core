cb700a02b4d00870957752479b9b8cb911591e85
Alexander Shutov <AlexanderShutov@yandex.ru>
2023-06-20 11:28:01 +0400

    Update FetchLazyPropertiesFixture.cs
    Add test TestLinqFetchAllProperties_WhenLazyPropertyChanged

--- src/NHibernate.Test/FetchLazyProperties/FetchLazyPropertiesFixture.cs
+++ src/NHibernate.Test/FetchLazyProperties/FetchLazyPropertiesFixture.cs
@@ -265,6 +265,25 @@ namespace NHibernate.Test.FetchLazyProperties
 			AssertFetchAllProperties(person);
 		}
 
+		[Test]
+		public void TestLinqFetchAllProperties_WhenLazyPropertyChanged()
+		{
+			Person person;
+			using (var s = OpenSession())
+			{
+				person = s.Get<Person>(1);
+				CollectionAssert.AreEqual(new byte[] { 0 }, person.Image);
+
+				person.Image = new byte[] { 1, 2, 3 };
+
+				var allPersons = s.Query<Person>().FetchLazyProperties().ToList();
+				// After execute FetchLazyProperties(), I expected to see that the person.Image would be { 1, 2, 3 }.
+				// Because I changed this person.Image manually, I didn't want to lose those changes.
+				// But test failed. Оld value returned { 0 }.
+				CollectionAssert.AreEqual(new byte[] { 1, 2, 3 }, person.Image); // test failed
+			}
+		}
+
 		private static void AssertFetchAllProperties(Person person)
 		{
 			Assert.That(person, Is.Not.Null);
