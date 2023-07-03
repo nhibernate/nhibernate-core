9fa4fb8dab05012e7039143f9b56be14dc2d7d4c
DavidIvan <davidivanmircea@gmail.com>
2023-07-03 13:23:40 +0300

    BuildCache method returns now a CacheBase type as per TODO

--- src/NHibernate/Cache/ICacheProvider.cs
+++ src/NHibernate/Cache/ICacheProvider.cs
@@ -14,10 +14,7 @@ namespace NHibernate.Cache
 		/// <param name="regionName">The name of the cache region.</param>
 		/// <param name="properties">Configuration settings.</param>
 		/// <returns>A cache.</returns>
-		// 6.0 TODO: return a CacheBase instead
-#pragma warning disable 618
-		ICache BuildCache(string regionName, IDictionary<string, string> properties);
-#pragma warning restore 618
+		CacheBase BuildCache(string regionName, IDictionary<string, string> properties);
 
 		/// <summary>
 		/// generate a timestamp
