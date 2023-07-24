5ad00aa5e23c22f163f4816ae4016d7551794395
Alex Zaytsev <hazzik@gmail.com>
2023-07-24 23:19:53 +1000

    Remove Serilog

--- src/NHibernate.Example.Web/Program.cs
+++ src/NHibernate.Example.Web/Program.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Serilog;
-using Serilog.Events;
 
 namespace NHibernate.Example.Web
 {
@@ -16,20 +7,12 @@ namespace NHibernate.Example.Web
 	{
 		public static void Main(string[] args)
 		{
-			Log.Logger = new LoggerConfiguration()
-				.MinimumLevel.Debug()
-				.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-				.Enrich.FromLogContext()
-				.WriteTo.Console()
-				.CreateLogger();
-
 			BuildWebHost(args).Run();
 		}
 
 		public static IWebHost BuildWebHost(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
 				.UseStartup<Startup>()
-				.UseSerilog()
 				.Build();
 	}
 }
