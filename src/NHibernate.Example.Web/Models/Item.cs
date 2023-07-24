7a1bf08fa52c789f4eb6b1b63ae4c1c069b0737e
Alex Zaytsev <hazzik@gmail.com>
2023-07-24 22:21:14 +0000

    Remove NHibernate.Example.Web project (#3379)

--- src/NHibernate.Example.Web/Models/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NHibernate.Example.Web.Models
-{
-	public class Item
-	{
-		public virtual int Id { get; set; }
-		public virtual decimal Price { get; set; }
-		public virtual string Description { get; set; }
-	}
-}
