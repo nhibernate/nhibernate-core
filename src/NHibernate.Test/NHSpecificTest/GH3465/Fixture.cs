7cd90cc4a273eb2a69ed6a7c3acea17c6c316e61
Frédéric Delaporte <12201973+fredericdelaporte@users.noreply.github.com>
2024-02-01 15:32:26 +0100

    Add a test case for #3465

---
+++ src/NHibernate.Test/NHSpecificTest/GH3465/Fixture.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH3465
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		[Test]
+		public void ThetaJoinSubQuery()
+		{
+			using (var session = OpenSession())
+			using (session.BeginTransaction())
+			{
+				var query = session.CreateQuery("select e.Id from EntityA e where exists (from e.Children b, EntityC c)");
+				Assert.DoesNotThrow(() => query.List());
+			}
+		}
+	}
+}
