2bc48bcf866de593e84e6146ceca9c8c47515c70
Frédéric Delaporte <12201973+fredericdelaporte@users.noreply.github.com>
2024-02-06 11:22:09 +0100

    Modernize syntax and mark test as explicit

--- src/NHibernate.Test/Async/NHSpecificTest/GH1313/Fixture.cs
+++ src/NHibernate.Test/Async/NHSpecificTest/GH1313/Fixture.cs
@@ -21,50 +21,41 @@ namespace NHibernate.Test.NHSpecificTest.GH1313
 	{
 		protected override void OnSetUp()
 		{
-			using (var session = OpenSession())
-			using (var transaction = session.BeginTransaction())
+			using var session = OpenSession();
+			using var transaction = session.BeginTransaction();
+
+			var account = new Account { Id = 1, Name = "Account_1", OldAccountNumber = 1 };
+			var order = new HoldClose
 			{
-				var account = new Account { Id = 1, Name = "Account_1", OldAccountNumber = 1 };
+				Account = account,
+				CloseDate = new DateTime(2023, 1, 1)
+			};
 
-				var order = new HoldClose
-				{
-					Account = account,
-					CloseDate = new DateTime(2023, 1, 1)
-				};
-
-				session.Save(account);
-				session.Save(order);
-				transaction.Commit();
-			}
+			session.Save(account);
+			session.Save(order);
+			transaction.Commit();
 		}
 
 		protected override void OnTearDown()
 		{
-			using (var session = OpenSession())
-			using (var transaction = session.BeginTransaction())
-			{
-				// The HQL delete does all the job inside the database without loading the entities, but it does
-				// not handle delete order for avoiding violating constraints if any. Use
-				// session.Delete("from System.Object");
-				// instead if in need of having NHibernate ordering the deletes, but this will cause
-				// loading the entities in the session.
-				session.CreateQuery("delete from System.Object").ExecuteUpdate();
+			using var session = OpenSession();
+			using var transaction = session.BeginTransaction();
 
-				transaction.Commit();
-			}
+			session.CreateQuery("delete from System.Object").ExecuteUpdate();
+			transaction.Commit();
 		}
 
 		[Test]
+		[Explicit("Not fixed yet")]
 		public async Task ManyToOneTargettingAFormulaAsync()
 		{
-			using (var session = OpenSession())
-			using (session.BeginTransaction())
-			{
-				var result = from e in session.Query<HoldClose>()
-							 select e;
+			using var session = OpenSession();
+			using var transaction = session.BeginTransaction();
 
-				Assert.That(await (result.ToListAsync()), Has.Count.EqualTo(1));
-			}
+			var result = session.Query<HoldClose>();
+
+			Assert.That(await (result.ToListAsync()), Has.Count.EqualTo(1));
+			await (transaction.CommitAsync());
 		}
 	}
 }
