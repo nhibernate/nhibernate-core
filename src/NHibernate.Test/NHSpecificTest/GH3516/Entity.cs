d4b9daf4cf21f9d007ca404fff0ee2def3947099
Frédéric Delaporte <12201973+fredericdelaporte@users.noreply.github.com>
2024-05-14 21:29:14 +0200

    Add injection test cases

---
+++ src/NHibernate.Test/NHSpecificTest/GH3516/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.GH3516
+{
+	public class Entity
+	{
+		public virtual Guid Id { get; set; }
+		public virtual string Name { get; set; }
+
+		public const string NameWithSingleQuote = "'; drop table Entity; --";
+		public const string NameWithEscapedSingleQuote = @"\'; drop table Entity; --";
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/GH3516/FixtureByCode.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH3516
+{
+	[TestFixture]
+	public class FixtureByCode : TestCaseMappingByCode
+	{
+		protected override HbmMapping GetMappings()
+		{
+			var mapper = new ModelMapper();
+			mapper.Class<Entity>(rc =>
+			{
+				rc.Id(x => x.Id, m => m.Generator(Generators.GuidComb));
+				rc.Property(x => x.Name);
+			});
+			return mapper.CompileMappingForAllExplicitlyAddedEntities();
+		}
+
+		protected override void OnSetUp()
+		{
+			using var session = OpenSession();
+			using var transaction = session.BeginTransaction();
+			var e = new Entity { Name = Entity.NameWithSingleQuote };
+			session.Save(e);
+			e = new Entity { Name = Entity.NameWithEscapedSingleQuote };
+			session.Save(e);
+
+			transaction.Commit();
+		}
+
+		protected override void OnTearDown()
+		{
+			using var session = OpenSession();
+			using var transaction = session.BeginTransaction();
+			session.CreateQuery("delete from System.Object").ExecuteUpdate();
+
+			transaction.Commit();
+		}
+
+		private static readonly string[] _stringInjectionsProperties =
+			new[]
+			{
+				nameof(Entity.NameWithSingleQuote),
+				nameof(Entity.NameWithEscapedSingleQuote)
+			};
+
+		[TestCaseSource(nameof(_stringInjectionsProperties))]
+		public void SqlInjectionInStrings(string propertyName)
+		{
+			using var session = OpenSession();
+
+			var query = session.CreateQuery($"from Entity e where e.Name = Entity.{propertyName}");
+			IList<Entity> list = null;
+			Assert.That(() => list = query.List<Entity>(), Throws.Nothing);
+			Assert.That(list, Has.Count.EqualTo(1), $"Unable to find entity with name {propertyName}");
+		}
+	}
+}
