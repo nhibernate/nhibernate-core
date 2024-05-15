aa7cd998132091dd106733ff937a77620fdfaa25
David Ellingsworth <David.Ellingsworth@davey.com>
2024-05-15 12:50:31 -0400

    GH-3530: Allow all properties of the LocaleEntity to be nullable.

--- src/NHibernate.Test/NHSpecificTest/GH3530/Entities.cs
+++ src/NHibernate.Test/NHSpecificTest/GH3530/Entities.cs
@@ -9,9 +9,9 @@ namespace NHibernate.Test.NHSpecificTest.GH3530
 	public class LocaleEntity
 	{
 		public virtual Guid Id { get; set; }
-		public virtual int IntegerValue { get; set; }
-		public virtual DateTime DateTimeValue { get; set; }
-		public virtual double DoubleValue { get; set; }
-		public virtual decimal DecimalValue { get; set; }
+		public virtual int? IntegerValue { get; set; }
+		public virtual DateTime? DateTimeValue { get; set; }
+		public virtual double? DoubleValue { get; set; }
+		public virtual decimal? DecimalValue { get; set; }
 	}
 }
