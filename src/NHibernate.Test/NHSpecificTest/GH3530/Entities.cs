2be793d5537a2c509dc83a3e1d35952defa254f6
David Ellingsworth <David.Ellingsworth@davey.com>
2024-05-16 14:32:36 -0400

    GH-3530: Switch to file scoped namespace per DeepSource.

--- src/NHibernate.Test/NHSpecificTest/GH3530/Entities.cs
+++ src/NHibernate.Test/NHSpecificTest/GH3530/Entities.cs
@@ -1,26 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NHibernate.SqlCommand;
 
-namespace NHibernate.Test.NHSpecificTest.GH3530
+namespace NHibernate.Test.NHSpecificTest.GH3530;
+
+public abstract class Entity
 {
-	public abstract class Entity
-	{
-		public virtual Guid Id { get; set; }
-	}
-
-	public abstract class DataEntity<T>:Entity where T : struct
-	{
-		public virtual T DataValue { get; set; }
-	}
-
-	public class IntegerEntity : DataEntity<int> { }
-	public class DateTimeEntity : DataEntity<DateTime> { }
-
-	public class DoubleEntity : DataEntity<double> { }
-	public class DecimalEntity : DataEntity<decimal> { }
-	public class  NHDateTimeEntity : DataEntity<DateTime> { }
+	public virtual Guid Id { get; set; }
 }
+
+public abstract class DataEntity<T>:Entity where T : struct
+{
+	public virtual T DataValue { get; set; }
+}
+
+public class IntegerEntity : DataEntity<int> { }
+public class DateTimeEntity : DataEntity<DateTime> { }
+
+public class DoubleEntity : DataEntity<double> { }
+public class DecimalEntity : DataEntity<decimal> { }
+public class  NHDateTimeEntity : DataEntity<DateTime> { }
