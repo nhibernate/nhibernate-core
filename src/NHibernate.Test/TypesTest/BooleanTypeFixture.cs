290d28018f45eaa1b2dfb4f94700862c9a4dfc1f
Alex Zaytsev <hazzik@gmail.com>
2024-05-21 18:13:41 +1000

    Fix test

--- src/NHibernate.Test/TypesTest/BooleanTypeFixture.cs
+++ src/NHibernate.Test/TypesTest/BooleanTypeFixture.cs
@@ -75,17 +75,19 @@ namespace NHibernate.Test.TypesTest
 		{
 			const string name0 = "name0";
 			const string name1 = "name1";
-			BooleanType type = NHibernateUtil.Boolean;
+			var type = NHibernateUtil.Boolean;
 			var session = Substitute.For<ISessionImplementor>();
 			var reader = Substitute.For<DbDataReader>();
-			reader[name0].Returns(expected);
-			reader[name1].Returns(expected);
+			reader.GetOrdinal(name0).Returns(0);
+			reader.GetOrdinal(name1).Returns(1);
+			reader[0].Returns(expected);
+			reader[1].Returns(expected);
 
 			var result0 = type.Get(reader, name0, session);
 			var result1 = type.Get(reader, name1, session);
 
-			Assert.AreEqual(expected, (bool) result0);
-			Assert.AreSame(result0, result1);
+			Assert.That((bool) result0, Is.EqualTo(expected));
+			Assert.That(result1, Is.SameAs(result0));
 		}
 
 		[Test]
