1bfb06455ee0e35531f44dfdf9db618e1b23454c
Alex Zaytsev <hazzik@gmail.com>
2024-06-04 15:38:53 +1000

    DB2NetDriver should be same as DB2CoreDriver

--- src/NHibernate/Driver/DB2NetDriver.cs
+++ src/NHibernate/Driver/DB2NetDriver.cs
@@ -1,4 +1,7 @@
-﻿namespace NHibernate.Driver
+﻿using System.Data.Common;
+using NHibernate.SqlTypes;
+
+namespace NHibernate.Driver
 {
 	/// <summary>
 	/// A NHibernate Driver for using the Net5.IBM.Data.Db2/Net.IBM.Data.Db2 DataProvider.
@@ -7,6 +10,16 @@
 	{
 		public DB2NetDriver() : base("IBM.Data.Db2")
 		{
+		}
+
+		public override bool UseNamedPrefixInSql => true;
+		public override bool UseNamedPrefixInParameter => true;
+		public override string NamedPrefix => "@";
+
+		protected override void InitializeParameter(DbParameter dbParam, string name, SqlType sqlType)
+		{
+			dbParam.ParameterName = FormatNameForParameter(name);
+			base.InitializeParameter(dbParam, name, sqlType);
 		}
 	}
 }
