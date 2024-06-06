3a651d0b9109309f3512f1a12353ae0b64c70ac9
Alex Zaytsev <hazzik@gmail.com>
2024-06-07 09:40:30 +1000

    Fix support for nullable struct components (#3554)

--- src/NHibernate.Test/NHSpecificTest/NH1284/Fixture.cs
+++ src/NHibernate.Test/NHSpecificTest/NH1284/Fixture.cs
@@ -2,35 +2,37 @@ using NUnit.Framework;
 
 namespace NHibernate.Test.NHSpecificTest.NH1284
 {
-	[TestFixture, Ignore("Not supported yet.")]
+	[TestFixture]
 	public class Fixture : BugTestCase
 	{
+		protected override void OnTearDown()
+		{
+			using var s = OpenSession();
+			using var tx = s.BeginTransaction();
+			s.Delete("from Person");
+			tx.Commit();
+		}
+
 		[Test]
 		public void EmptyValueTypeComponent()
 		{
 			Person jimmy;
-			using (ISession s = OpenSession())
-			using (ITransaction tx = s.BeginTransaction())
+			using (var s = OpenSession())
+			using (var tx = s.BeginTransaction())
 			{
-				Person p = new Person("Jimmy Hendrix");
+				var p = new Person("Jimmy Hendrix");
 				s.Save(p);
 				tx.Commit();
 			}
 
-			using (ISession s = OpenSession())
-			using (ITransaction tx = s.BeginTransaction())
+			using (var s = OpenSession())
+			using (var tx = s.BeginTransaction())
 			{
-				jimmy = (Person)s.Get(typeof(Person), "Jimmy Hendrix");
+				jimmy = s.Get<Person>("Jimmy Hendrix");
 				tx.Commit();
 			}
-			Assert.IsFalse(jimmy.Address.HasValue);
 
-			using (ISession s = OpenSession())
-			using (ITransaction tx = s.BeginTransaction())
-			{
-				s.Delete("from Person");
-				tx.Commit();
-			}
+			Assert.That(jimmy.Address, Is.Null);
 		}
 	}
 }
