d9072d4e9d691d66056e98b72169abec5d1490c9
Alex Zaytsev <hazzik@gmail.com>
2024-06-07 09:18:17 +1000

    code review changes
    Co-authored-by: Frédéric Delaporte <12201973+fredericDelaporte@users.noreply.github.com>

--- src/NHibernate/Driver/DB2CoreDriver.cs
+++ src/NHibernate/Driver/DB2CoreDriver.cs
@@ -9,8 +9,11 @@ namespace NHibernate.Driver
 	[Obsolete("Please use DB2NetDriver")]
 	public class DB2CoreDriver : DB2NetDriver
 	{
+		private static readonly INHibernateLogger Log = NHibernateLogger.For(typeof(DB2CoreDriver));
+		
 		public DB2CoreDriver() : base("IBM.Data.DB2.Core")
 		{
+		    Log.Warn("DB2CoreDriver is obsolete, please use DB2NetDriver instead.");
 		}
 	}
 }
