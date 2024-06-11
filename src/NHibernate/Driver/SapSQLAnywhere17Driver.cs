6610fe25adecbf1d43c8cdb17889d190e2f51511
David Ellingsworth <David.Ellingsworth@davey.com>
2024-06-11 09:03:35 -0400

    GH-3530: The SAP SQL Anywhere driver does not support the DbDataReader.GetChar method, wrap it in the NoCharDbDataReader.

--- src/NHibernate/Driver/SapSQLAnywhere17Driver.cs
+++ src/NHibernate/Driver/SapSQLAnywhere17Driver.cs
@@ -1,6 +1,9 @@
-﻿namespace NHibernate.Driver
+using System.Data.Common;
+using NHibernate.AdoNet;
+
+namespace NHibernate.Driver
 {
-	public class SapSQLAnywhere17Driver : ReflectionBasedDriver
+	public partial class SapSQLAnywhere17Driver : ReflectionBasedDriver
 	{
 		public SapSQLAnywhere17Driver()
 			: base("Sap.Data.SQLAnywhere", "Sap.Data.SQLAnywhere.v4.5", "Sap.Data.SQLAnywhere.SAConnection", "Sap.Data.SQLAnywhere.SACommand")
@@ -14,5 +17,12 @@
 		public override string NamedPrefix => ":";
 
 		public override bool RequiresTimeSpanForTime => true;
+
+		public override DbDataReader ExecuteReader(DbCommand command)
+		{
+			var reader = command.ExecuteReader();
+
+			return new NoCharDbDataReader(reader);
+		}
 	}
 }
