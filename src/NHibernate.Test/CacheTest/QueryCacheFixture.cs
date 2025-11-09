56e4ebece5d8d215d9e49ff08f0135741de89e58
Frédéric Delaporte <12201973+fredericDelaporte@users.noreply.github.com>
2025-11-09 20:19:53 +0100

    Fix test

--- src/NHibernate.Test/CacheTest/QueryCacheFixture.cs
+++ src/NHibernate.Test/CacheTest/QueryCacheFixture.cs
@@ -97,7 +97,7 @@ namespace NHibernate.Test.CacheTest
 		{
 			using (var s = OpenSession())
 			{
-				const string query = "select s.id_ as Id from Simple as s";
+				const string query = "select s.id_ as Id from Simple as s;";
 				var result1 = s
 					.CreateSQLQuery(query)
 					.SetCacheable(true)
