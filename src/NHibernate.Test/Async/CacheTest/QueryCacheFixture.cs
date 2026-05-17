858b0f50cbdeddf49d646be2b5ae78270d9d002f
delta-emil <9694906+delta-emil@users.noreply.github.com>
2026-05-17 18:07:12 +0300

    Fixes #3713: query cache result transformer aliases issue (#3714)
    Co-authored-by: Frédéric Delaporte <12201973+fredericDelaporte@users.noreply.github.com>

--- src/NHibernate.Test/Async/CacheTest/QueryCacheFixture.cs
+++ src/NHibernate.Test/Async/CacheTest/QueryCacheFixture.cs
@@ -11,6 +11,7 @@
 using System.Collections;
 using NHibernate.Cfg;
 using NHibernate.DomainModel;
+using NHibernate.Transform;
 using NUnit.Framework;
 using Environment = NHibernate.Cfg.Environment;
 
@@ -25,51 +26,65 @@ namespace NHibernate.Test.CacheTest
 		protected override void Configure(Configuration configuration)
 		{
 			configuration.SetProperty(Environment.UseQueryCache, "true");
+			configuration.SetProperty(Environment.GenerateStatistics, "true");
 		}
+
+		private const long SimpleId = 1;
 
 		protected override void OnSetUp()
 		{
-			using (var s = OpenSession())
-			{
-				s.Save(new Simple(), 1L);
-				s.Flush();
-			}
+			using var s = OpenSession();
+			using var t = s.BeginTransaction();
+			s.Save(new Simple(), SimpleId);
+			t.Commit();
 		}
 
 		protected override void OnTearDown()
 		{
-			using (var s = OpenSession())
-			{
-				s.Delete("from Simple");
-				s.Flush();
-			}
+			using var s = OpenSession();
+			using var t = s.BeginTransaction();
+			s.Delete("from Simple");
+			t.Commit();
 		}
 
 		[Test]
 		public async Task QueryCacheWithNullParametersAsync()
 		{
+			const string query = "from Simple s where s = :s or s.Name = :name or s.Address = :address";
+			Sfi.Statistics.Clear();
+			object simple;
 			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
 			{
-				const string query = "from Simple s where s = :s or s.Name = :name or s.Address = :address";
+				simple = await (s.LoadAsync(typeof(Simple), SimpleId));
 				await (s
 					.CreateQuery(query)
-					.SetEntity("s", await (s.LoadAsync(typeof(Simple), 1L)))
+					.SetEntity("s", simple)
 					.SetString("name", null)
 					.SetString("address", null)
 					.SetCacheable(true)
 					.UniqueResultAsync());
 
-				// Run a second time, just to test the query cache
+				Assert.That(Sfi.Statistics.QueryCacheHitCount, Is.EqualTo(0));
+				await (t.CommitAsync());
+			}
+
+			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
+			{
+				// Run a second time, just to test the query cache.
 				var result = await (s
 					.CreateQuery(query)
-					.SetEntity("s", await (s.LoadAsync(typeof(Simple), 1L)))
+					.SetEntity("s", simple)
 					.SetString("name", null)
 					.SetString("address", null)
 					.SetCacheable(true)
 					.UniqueResultAsync());
 
 				Assert.That(result, Is.Not.Null);
-				Assert.That(s.GetIdentifier(result), Is.EqualTo(1));
+				Assert.That(s.GetIdentifier(result), Is.EqualTo(SimpleId));
+				Assert.That(Sfi.Statistics.QueryCacheHitCount, Is.EqualTo(1));
+				await (t.CommitAsync());
 			}
 		}
 
@@ -84,22 +99,82 @@ namespace NHibernate.Test.CacheTest
 			if (TestDialect.HasBrokenDecimalType)
 				Assert.Ignore("Database does not support properly decimals.");
 
+			const string query = "select cast(200012 as decimal) from Simple where id_ = 1";
+			Sfi.Statistics.Clear();
 			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
 			{
 				var result = await (s
-					.CreateSQLQuery("select cast(200012 as decimal) from Simple where id_ = 1")
+					.CreateSQLQuery(query)
 					.SetCacheable(true)
 					.UniqueResultAsync<decimal>());
 
+				Assert.That(Sfi.Statistics.QueryCacheHitCount, Is.EqualTo(0));
 				Assert.That(result, Is.EqualTo(200012), "Unexpected non-cached result");
+				await (t.CommitAsync());
+			}
 
-				result = await (s
-					.CreateSQLQuery("select cast(200012 as decimal) from Simple where id_ = 1")
+			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
+			{
+
+				var result = await (s
+					.CreateSQLQuery(query)
 					.SetCacheable(true)
 					.UniqueResultAsync<decimal>());
 
+				Assert.That(Sfi.Statistics.QueryCacheHitCount, Is.EqualTo(1));
 				Assert.That(result, Is.EqualTo(200012), "Unexpected cached result");
 			}
+		}
+
+		[Test]
+		public async Task QueryCacheWithAliasToBeanTransformerAsync()
+		{
+			if (!TestDialect.HasActualIntegerTypes)
+				Assert.Ignore("Database does not support properly integers.");
+
+			const string query = "select s.id_ as Id from Simple s;";
+			Sfi.Statistics.Clear();
+
+			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
+			{
+				var result1 = await (s
+					.CreateSQLQuery(query)
+					.SetCacheable(true)
+					.SetResultTransformer(Transformers.AliasToBean<SimpleDTO>())
+					.UniqueResultAsync());
+
+				Assert.That(result1, Is.InstanceOf<SimpleDTO>());
+				var dto1 = (SimpleDTO) result1;
+				Assert.That(dto1.Id, Is.EqualTo(SimpleId), "Unexpected entity Id");
+				Assert.That(Sfi.Statistics.QueryCacheHitCount, Is.EqualTo(0), "Unexpected query cache hit count");
+				await (t.CommitAsync());
+			}
+
+			// Run a second time, just to test the query cache.
+			using (var s = OpenSession())
+			using (var t = s.BeginTransaction())
+			{
+				var result2 = await (s
+					.CreateSQLQuery(query)
+					.SetCacheable(true)
+					.SetResultTransformer(Transformers.AliasToBean<SimpleDTO>())
+					.UniqueResultAsync());
+
+				Assert.That(result2, Is.InstanceOf<SimpleDTO>());
+				var dto2 = (SimpleDTO) result2;
+				Assert.That(dto2.Id, Is.EqualTo(SimpleId), "Unexpected entity Id");
+				Assert.That(Sfi.Statistics.QueryCacheHitCount, Is.EqualTo(1), "Unexpected query cache hit count");
+				await (t.CommitAsync());
+			}
+		}
+
+		private class SimpleDTO
+		{
+			public long Id { get; set; }
+			public string Address { get; set; }
 		}
 	}
 }
