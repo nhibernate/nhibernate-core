8ea6e833c71a9a974eb9b392305c0ef0a9bafcf5
Alex Zaytsev <hazzik@gmail.com>
2026-08-28 10:46:26 +1000

    Use LOCALTIMESTAMP for the Firebird 4 current timestamp function

--- src/NHibernate/Dialect/Firebird4Dialect.cs
+++ src/NHibernate/Dialect/Firebird4Dialect.cs
@@ -2,14 +2,25 @@ using NHibernate.Dialect.Function;
 
 namespace NHibernate.Dialect
 {
-	public class Firebird4Dialect: Firebird3Dialect
+	/// <summary>
+	/// A dialect for Firebird 4 and above.
+	/// </summary>
+	public class Firebird4Dialect : Firebird3Dialect
 	{
+		/// <inheritdoc />
+		/// <remarks>
+		/// <c>CURRENT_TIMESTAMP</c> is time zone aware since Firebird 4, <c>LOCALTIMESTAMP</c> is not.
+		/// </remarks>
 		public override string CurrentTimestampSelectString => "select LOCALTIMESTAMP from RDB$DATABASE";
+
+		/// <inheritdoc />
+		public override string CurrentTimestampSQLFunctionName => "localtimestamp";
 
 		protected override void RegisterFunctions()
 		{
 			base.RegisterFunctions();
 			RegisterFunction("current_timestamp", new NoArgSQLFunction("localtimestamp", NHibernateUtil.LocalDateTime, false));
+			RegisterFunction("localtimestamp", new NoArgSQLFunction("localtimestamp", NHibernateUtil.LocalDateTime, false));
 		}
 	}
 }
