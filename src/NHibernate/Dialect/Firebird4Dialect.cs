bf82c6b21901662011d5bd81cdf6fd3126d5447d
Alex Zaytsev <hazzik@gmail.com>
2026-08-28 10:47:02 +1000

    Register the Firebird 4 built-in functions

--- src/NHibernate/Dialect/Firebird4Dialect.cs
+++ src/NHibernate/Dialect/Firebird4Dialect.cs
@@ -10,6 +10,12 @@ namespace NHibernate.Dialect
 		{
 			base.RegisterFunctions();
 			RegisterFunction("current_timestamp", new NoArgSQLFunction("localtimestamp", NHibernateUtil.LocalDateTime, false));
+			RegisterFunction("base64_encode", new StandardSQLFunction("base64_encode", NHibernateUtil.String));
+			RegisterFunction("base64_decode", new StandardSQLFunction("base64_decode", NHibernateUtil.Binary));
+			RegisterFunction("hex_encode", new StandardSQLFunction("hex_encode", NHibernateUtil.String));
+			RegisterFunction("hex_decode", new StandardSQLFunction("hex_decode", NHibernateUtil.Binary));
+			RegisterFunction("first_day", new SQLFunctionTemplate(NHibernateUtil.Date, "first_day(of month from ?1)"));
+			RegisterFunction("last_day", new SQLFunctionTemplate(NHibernateUtil.Date, "last_day(of month from ?1)"));
 		}
 	}
 }
